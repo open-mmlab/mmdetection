56b9749c128aeae02afa19fb602b913e8922cbc2
Kai Chen <chenkaidev@gmail.com>
2018-09-24 15:34:18 +0800

    modify distributed training api and use coalesced all_reduce

--- tools/configs/r50_fpn_frcnn_1x.py
+++ tools/configs/r50_fpn_frcnn_1x.py
@@ -121,8 +121,7 @@ log_config = dict(
 # yapf:enable
 # runtime settings
 total_epochs = 12
-device_ids = range(8)
-dist_params = dict(backend='nccl', port='29500', master_ip='127.0.0.1')
+dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/fpn_faster_rcnn_r50_1x'
 load_from = None
