d92be9dcaa819023a6179570a512a2e3856499dc
Kai Chen <chenkaidev@gmail.com>
2018-10-07 19:37:13 +0800

    some renaming

--- mmdet/core/utils/__init__.py
+++ mmdet/core/utils/__init__.py
@@ -1,9 +1,8 @@
 from .dist_utils import (init_dist, reduce_grads, DistOptimizerHook,
                          DistSamplerSeedHook)
-from .hooks import EmptyCacheHook
 from .misc import tensor2imgs, unmap, multi_apply
 
 __all__ = [
     'init_dist', 'reduce_grads', 'DistOptimizerHook', 'DistSamplerSeedHook',
-    'EmptyCacheHook', 'tensor2imgs', 'unmap', 'multi_apply'
+    'tensor2imgs', 'unmap', 'multi_apply'
 ]
