3d2b79bdc3fbdbabfccbe77b8c265ffc7a021d90
Kai Chen <chenkaidev@gmail.com>
2018-10-08 14:14:04 +0800

    fix path issues

--- configs/r50_fpn_rpn_1x.py
+++ configs/r50_fpn_rpn_1x.py
@@ -50,7 +50,7 @@ test_cfg = dict(
         min_bbox_size=0))
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '../data/coco/'
+data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
