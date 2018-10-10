b07edd6b1d29530c50855e193b45f21d40187348
pangjm <pjmzju@gmail.com>
2018-10-10 14:06:27 +0800

    fix test tools bugs

--- tools/test.py
+++ tools/test.py
@@ -3,11 +3,11 @@ import argparse
 import torch
 import mmcv
 from mmcv.runner import load_checkpoint, parallel_test, obj_from_dict
-from mmcv.parallel import scatter, MMDataParallel
+from mmcv.parallel import scatter, collate, MMDataParallel
 
 from mmdet import datasets
 from mmdet.core import results2json, coco_eval
-from mmdet.datasets import collate, build_dataloader
+from mmdet.datasets import build_dataloader
 from mmdet.models import build_detector, detectors
 
 
