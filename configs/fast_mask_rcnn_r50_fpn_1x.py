15e538f9080ab8aa283debf94c9d2ff708c31a17
pangjm <pjmzju@gmail.com>
2018-10-11 16:31:48 +0800

    minor revision

--- configs/fast_mask_rcnn_r50_fpn_1x.py
+++ configs/fast_mask_rcnn_r50_fpn_1x.py
@@ -116,7 +116,7 @@ lr_config = dict(
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=20,
+    interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='TensorboardLoggerHook')
