c76a95eca1d1da9b7532c6dc38dcf14414575654
Kai Chen <chenkaidev@gmail.com>
2018-10-12 02:45:09 +0800

    reorganize the training api

--- tools/train.py
+++ tools/train.py
@@ -4,8 +4,9 @@ import argparse
 from mmcv import Config
 from mmcv.runner import obj_from_dict
 
-from mmdet import datasets
-from mmdet.api import train_detector
+from mmdet import datasets, __version__
+from mmdet.api import (train_detector, init_dist, get_root_logger,
+                       set_random_seed)
 from mmdet.models import build_detector
 
 
@@ -16,10 +17,14 @@ def parse_args():
     parser.add_argument(
         '--validate',
         action='store_true',
-        help='whether to add a validate phase')
+        help='whether to evaluate the checkpoint during training')
     parser.add_argument(
-        '--gpus', type=int, default=1, help='number of gpus to use')
-    parser.add_argument('--seed', type=int, help='random seed')
+        '--gpus',
+        type=int,
+        default=1,
+        help='number of gpus to use '
+        '(only applicable to non-distributed training)')
+    parser.add_argument('--seed', type=int, default=None, help='random seed')
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
@@ -33,19 +38,43 @@ def parse_args():
 
 def main():
     args = parse_args()
+
     cfg = Config.fromfile(args.config)
+    # update configs according to CLI args
     if args.work_dir is not None:
         cfg.work_dir = args.work_dir
-    cfg.validate = args.validate
     cfg.gpus = args.gpus
-    cfg.seed = args.seed
-    cfg.launcher = args.launcher
-    cfg.local_rank = args.local_rank
-    # build model
+    if cfg.checkpoint_config is not None:
+        # save mmdet version in checkpoints as meta data
+        cfg.checkpoint_config.meta = dict(
+            mmdet_version=__version__, config=cfg.text)
+
+    # init distributed env first, since logger depends on the dist info.
+    if args.launcher == 'none':
+        distributed = False
+    else:
+        distributed = True
+        init_dist(args.launcher, **cfg.dist_params)
+
+    # init logger before other steps
+    logger = get_root_logger(cfg.log_level)
+    logger.info('Distributed training: {}'.format(distributed))
+
+    # set random seeds
+    if args.seed is not None:
+        logger.info('Set random seed to {}'.format(args.seed))
+        set_random_seed(args.seed)
+
     model = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
     train_dataset = obj_from_dict(cfg.data.train, datasets)
-    train_detector(model, train_dataset, cfg)
+    train_detector(
+        model,
+        train_dataset,
+        cfg,
+        distributed=distributed,
+        validate=args.validate,
+        logger=logger)
 
 
 if __name__ == '__main__':
