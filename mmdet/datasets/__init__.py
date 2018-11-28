7906bd2064d8511063d917d0d066d346d1edcaf2
wangg12 <guwang12@gmail.com>
2018-11-28 11:32:47 +0800

    support training on dataset with multiple ann_files

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -2,8 +2,9 @@ from .custom import CustomDataset
 from .coco import CocoDataset
 from .loader import GroupSampler, DistributedGroupSampler, build_dataloader
 from .utils import to_tensor, random_scale, show_ann
+from .concat_dataset import ConcatDataset
 
 __all__ = [
-    'CustomDataset', 'CocoDataset', 'GroupSampler', 'DistributedGroupSampler',
+    'CustomDataset', 'CocoDataset', 'GroupSampler', 'DistributedGroupSampler', 'ConcatDataset',
     'build_dataloader', 'to_tensor', 'random_scale', 'show_ann'
 ]
