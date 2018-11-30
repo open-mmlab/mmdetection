e6cd9492f3d54829f59bdc1a0936f072f5c6409e
myownskyW7 <727032989@qq.com>
2018-11-30 15:54:10 +0800

    config update

--- configs/faster_rcnn_r50_c4_1x.py
+++ configs/faster_rcnn_r50_c4_1x.py
@@ -88,7 +88,8 @@ test_cfg = dict(
         max_num=1000,
         nms_thr=0.7,
         min_bbox_size=0),
-    rcnn=dict(score_thr=0.05, max_per_img=100, nms_thr=0.5))
+    rcnn=dict(
+        score_thr=0.05, nms=dict(type='nms', iou_thr=0.5), max_per_img=100))
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
