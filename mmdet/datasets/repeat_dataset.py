56108abbcd6a0d5accbb40db9f7dc4a17f3eeaac
yhcao6 <yhcao6@gmail.com>
2018-12-06 22:16:51 +0800

    support recursion

--- mmdet/datasets/repeat_dataset.py
+++ mmdet/datasets/repeat_dataset.py
@@ -3,16 +3,15 @@ import numpy as np
 
 class RepeatDataset(object):
 
-    def __init__(self, dataset, repeat_times):
+    def __init__(self, dataset, times):
         self.dataset = dataset
-        self.repeat_times = repeat_times
+        self.times = times
         if hasattr(self.dataset, 'flag'):
-            self.flag = np.tile(self.dataset.flag, repeat_times)
-        self.length = len(self.dataset) * self.repeat_times
+            self.flag = np.tile(self.dataset.flag, times)
+        self._original_length = len(self.dataset)
 
     def __getitem__(self, idx):
-        return self.dataset[idx % len(self.dataset)]
+        return self.dataset[idx % self._original_length]
 
     def __len__(self):
-        return self.length
-
+        return self.times * self._original_length
