24590d9efd1f2c85cbe4ea5aada355119f00ba49
ThangVu <thangvubk@gmail.com>
2018-12-10 01:34:07 +0000

    add group norm support

--- mmdet/models/utils/norm.py
+++ mmdet/models/utils/norm.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-norm_cfg = {'BN': nn.BatchNorm2d, 'SyncBN': None, 'GN': None}
+norm_cfg = {'BN': nn.BatchNorm2d, 'SyncBN': None, 'GN': nn.GroupNorm}
 
 
 def build_norm_layer(cfg, num_features):
@@ -9,9 +9,15 @@ def build_norm_layer(cfg, num_features):
     cfg_.setdefault('eps', 1e-5)
     layer_type = cfg_.pop('type')
 
+    # args name matching
+    if layer_type == 'GN':
+        cfg_.setdefault('num_channels', num_features)
+    else:
+        cfg_.setdefault('num_features', num_features)
+
     if layer_type not in norm_cfg:
         raise KeyError('Unrecognized norm type {}'.format(layer_type))
     elif norm_cfg[layer_type] is None:
         raise NotImplementedError
 
-    return norm_cfg[layer_type](num_features, **cfg_)
+    return norm_cfg[layer_type](**cfg_)
