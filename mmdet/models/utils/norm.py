984a30abd81e4a1c89d0a3dd840022381204bb2f
ThangVu <thangvubk@gmail.com>
2018-12-18 06:39:48 +0000

    add group norm on backbone and unit tests

--- mmdet/models/utils/norm.py
+++ mmdet/models/utils/norm.py
@@ -6,14 +6,16 @@ norm_cfg = {'BN': nn.BatchNorm2d, 'SyncBN': None, 'GN': nn.GroupNorm}
 def build_norm_layer(cfg, num_features):
     assert isinstance(cfg, dict) and 'type' in cfg
     cfg_ = cfg.copy()
-    cfg_.setdefault('eps', 1e-5)
     layer_type = cfg_.pop('type')
 
     # args name matching
     if layer_type == 'GN':
+        assert 'num_groups' in cfg
         cfg_.setdefault('num_channels', num_features)
-    else:
+    elif layer_type == 'BN':
+        cfg_ = dict()  # rewrite neccessary info for BN from here
         cfg_.setdefault('num_features', num_features)
+    cfg_.setdefault('eps', 1e-5)
 
     if layer_type not in norm_cfg:
         raise KeyError('Unrecognized norm type {}'.format(layer_type))
