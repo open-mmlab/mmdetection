1bc92a0daa78c58656a1956ee82b2d8bad22cc40
thangvu <thangvubk@gmail.com>
2018-12-30 23:11:38 +0900

    revise group norm

--- mmdet/models/utils/norm.py
+++ mmdet/models/utils/norm.py
@@ -1,19 +1,28 @@
 import torch.nn as nn
 
+
 norm_cfg = {'BN': nn.BatchNorm2d, 'SyncBN': None, 'GN': nn.GroupNorm}
 
 
 def build_norm_layer(cfg, num_features):
+    """
+    cfg should contain:
+        type (str): identify norm layer type.
+        layer args: args needed to instantiate a norm layer.
+        frozen (bool): [optional] whether stop gradient updates
+            of norm layer, it is helpful to set frozen mode
+            in backbone's norms.
+    """
     assert isinstance(cfg, dict) and 'type' in cfg
     cfg_ = cfg.copy()
     layer_type = cfg_.pop('type')
+    frozen = cfg_.pop('frozen') if 'frozen' in cfg_ else False
 
     # args name matching
     if layer_type == 'GN':
         assert 'num_groups' in cfg
         cfg_.setdefault('num_channels', num_features)
     elif layer_type == 'BN':
-        cfg_ = dict()  # rewrite neccessary info for BN from here
         cfg_.setdefault('num_features', num_features)
     cfg_.setdefault('eps', 1e-5)
 
@@ -22,4 +31,8 @@ def build_norm_layer(cfg, num_features):
     elif norm_cfg[layer_type] is None:
         raise NotImplementedError
 
-    return norm_cfg[layer_type](**cfg_)
+    norm = norm_cfg[layer_type](**cfg_)
+    if frozen:
+        for param in norm.parameters():
+            param.requires_grad = False
+    return norm
