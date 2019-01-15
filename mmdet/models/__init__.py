9cdce8c62391ea4903e1c77c4c29f5d9f9f4c5d1
Kai Chen <chenkaidev@gmail.com>
2019-01-15 21:56:27 +0800

    ignore flake8 warnings

--- mmdet/models/__init__.py
+++ mmdet/models/__init__.py
@@ -1,10 +1,10 @@
-from .backbones import *
-from .necks import *
-from .roi_extractors import *
-from .anchor_heads import *
-from .bbox_heads import *
-from .mask_heads import *
-from .detectors import *
+from .backbones import *  # noqa: F401,F403
+from .necks import *  # noqa: F401,F403
+from .roi_extractors import *  # noqa: F401,F403
+from .anchor_heads import *  # noqa: F401,F403
+from .bbox_heads import *  # noqa: F401,F403
+from .mask_heads import *  # noqa: F401,F403
+from .detectors import *  # noqa: F401,F403
 from .registry import BACKBONES, NECKS, ROI_EXTRACTORS, HEADS, DETECTORS
 from .builder import (build_backbone, build_neck, build_roi_extractor,
                       build_head, build_detector)
