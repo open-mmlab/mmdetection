16b16e0670ab52c9107e8ecbdfdb7034660c39fc
yhcao6 <yhcao6@gmail.com>
2019-01-17 20:59:56 +0800

    separate deform_conv and deform_pool, clean some code

---
+++ mmdet/ops/dcn/functions/deform_pool.py
@@ -0,0 +1,80 @@
+import torch
+from torch.autograd import Function
+
+from .. import modulated_dcn_cuda as _backend
+
+
+class DeformRoIPoolingFunction(Function):
+
+    @staticmethod
+    def forward(ctx,
+                data,
+                rois,
+                offset,
+                spatial_scale,
+                pooled_size,
+                output_dim,
+                no_trans,
+                group_size=1,
+                part_size=None,
+                sample_per_part=4,
+                trans_std=.0):
+        ctx.spatial_scale = spatial_scale
+        ctx.pooled_size = pooled_size
+        ctx.output_dim = output_dim
+        ctx.no_trans = no_trans
+        ctx.group_size = group_size
+        ctx.part_size = pooled_size if part_size is None else part_size
+        ctx.sample_per_part = sample_per_part
+        ctx.trans_std = trans_std
+
+        assert 0.0 <= ctx.trans_std <= 1.0
+        if not data.is_cuda:
+            raise NotImplementedError
+
+        output = data.new(
+            *DeformRoIPoolingFunction._infer_shape(ctx, data, rois))
+        output_count = data.new(
+            *DeformRoIPoolingFunction._infer_shape(ctx, data, rois))
+        _backend.deform_psroi_pooling_cuda_forward(
+            data, rois, offset, output, output_count, ctx.no_trans,
+            ctx.spatial_scale, ctx.output_dim, ctx.group_size, ctx.pooled_size,
+            ctx.part_size, ctx.sample_per_part, ctx.trans_std)
+
+        # if data.requires_grad or rois.requires_grad or offset.requires_grad:
+        #     ctx.save_for_backward(data, rois, offset, output_count)
+        ctx.data = data
+        ctx.rois = rois
+        ctx.offset = offset
+        ctx.output_count = output_count
+
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if not grad_output.is_cuda:
+            raise NotImplementedError
+
+        # data, rois, offset, output_count = ctx.saved_tensors
+        data = ctx.data
+        rois = ctx.rois
+        offset = ctx.offset
+        output_count = ctx.output_count
+        grad_input = torch.zeros_like(data)
+        grad_offset = torch.zeros_like(offset)
+
+        _backend.deform_psroi_pooling_cuda_backward(
+            grad_output, data, rois, offset, output_count, grad_input,
+            grad_offset, ctx.no_trans, ctx.spatial_scale, ctx.output_dim,
+            ctx.group_size, ctx.pooled_size, ctx.part_size,
+            ctx.sample_per_part, ctx.trans_std)
+        return (grad_input, torch.zeros_like(rois), grad_offset, None, None,
+                None, None, None, None, None, None)
+
+    @staticmethod
+    def _infer_shape(ctx, data, rois):
+        n = rois.shape[0]
+        return n, ctx.output_dim, ctx.pooled_size, ctx.pooled_size
+
+
+deform_roi_pooling = DeformRoIPoolingFunction.apply
--- mmdet/ops/dcn/modules/deform_conv.py
+++ mmdet/ops/dcn/modules/deform_conv.py
@@ -2,13 +2,12 @@ import math
 
 import torch
 import torch.nn as nn
-from torch.nn.modules.module import Module
 from torch.nn.modules.utils import _pair
 
-from ..functions.deform_conv import deform_conv
+from ..functions.deform_conv import deform_conv, modulated_deform_conv
 
 
-class DeformConv(Module):
+class DeformConv(nn.Module):
 
     def __init__(self,
                  in_channels,
@@ -43,3 +42,84 @@ class DeformConv(Module):
         return deform_conv(input, offset, self.weight, self.stride,
                            self.padding, self.dilation,
                            self.num_deformable_groups)
+
+
+class ModulatedDeformConv(nn.Module):
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride,
+                 padding,
+                 dilation=1,
+                 deformable_groups=1,
+                 no_bias=True):
+        super(ModulatedDeformConv, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = _pair(kernel_size)
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.deformable_groups = deformable_groups
+        self.no_bias = no_bias
+
+        self.weight = nn.Parameter(
+            torch.Tensor(out_channels, in_channels, *self.kernel_size))
+        self.bias = nn.Parameter(torch.zeros(out_channels))
+        self.reset_parameters()
+        if self.no_bias:
+            self.bias.requires_grad = False
+
+    def reset_parameters(self):
+        n = self.in_channels
+        for k in self.kernel_size:
+            n *= k
+        stdv = 1. / math.sqrt(n)
+        self.weight.data.uniform_(-stdv, stdv)
+        self.bias.data.zero_()
+
+    def forward(self, input, offset, mask):
+        return modulated_deform_conv(input, offset, mask, self.weight,
+                                     self.bias, self.stride, self.padding,
+                                     self.dilation, self.deformable_groups)
+
+
+class ModulatedDeformConvPack(ModulatedDeformConv):
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride,
+                 padding,
+                 dilation=1,
+                 deformable_groups=1,
+                 no_bias=False):
+        super(ModulatedDeformConvPack,
+              self).__init__(in_channels, out_channels, kernel_size, stride,
+                             padding, dilation, deformable_groups, no_bias)
+
+        self.conv_offset_mask = nn.Conv2d(
+            self.in_channels,
+            self.deformable_groups * 3 * self.kernel_size[0] *
+            self.kernel_size[1],
+            kernel_size=self.kernel_size,
+            stride=(self.stride, self.stride),
+            padding=(self.padding, self.padding),
+            bias=True)
+        self.init_offset()
+
+    def init_offset(self):
+        self.conv_offset_mask.weight.data.zero_()
+        self.conv_offset_mask.bias.data.zero_()
+
+    def forward(self, input):
+        out = self.conv_offset_mask(input)
+        o1, o2, mask = torch.chunk(out, 3, dim=1)
+        offset = torch.cat((o1, o2), dim=1)
+        mask = torch.sigmoid(mask)
+        return modulated_deform_conv(input, offset, mask, self.weight,
+                                     self.bias, self.stride, self.padding,
+                                     self.dilation, self.deformable_groups)
