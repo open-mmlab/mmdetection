c35a11b6b1a76d428c3431c02f2db77e6d46abc2
Kai Chen <chenkaidev@gmail.com>
2019-04-06 19:26:07 -0700

    update imports

--- mmdet/ops/__init__.py
+++ mmdet/ops/__init__.py
@@ -1,14 +1,14 @@
-from .dcn import (DeformConv, DeformRoIPooling, DeformRoIPoolingPack,
-                  ModulatedDeformRoIPoolingPack, ModulatedDeformConv,
-                  ModulatedDeformConvPack, deform_conv, modulated_deform_conv,
-                  deform_roi_pooling)
+from .dcn import (DeformConv, DeformConvPack, ModulatedDeformConv,
+                  ModulatedDeformConvPack, DeformRoIPooling,
+                  DeformRoIPoolingPack, ModulatedDeformRoIPoolingPack,
+                  deform_conv, modulated_deform_conv, deform_roi_pooling)
 from .nms import nms, soft_nms
 from .roi_align import RoIAlign, roi_align
 from .roi_pool import RoIPool, roi_pool
 
 __all__ = [
     'nms', 'soft_nms', 'RoIAlign', 'roi_align', 'RoIPool', 'roi_pool',
-    'DeformConv', 'DeformRoIPooling', 'DeformRoIPoolingPack',
+    'DeformConv', 'DeformConvPack', 'DeformRoIPooling', 'DeformRoIPoolingPack',
     'ModulatedDeformRoIPoolingPack', 'ModulatedDeformConv',
     'ModulatedDeformConvPack', 'deform_conv', 'modulated_deform_conv',
     'deform_roi_pooling'
