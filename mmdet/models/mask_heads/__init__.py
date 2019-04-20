d0f607b87eed5ed04ae979409057f6b8d596f92a
Kai Chen <chenkaidev@gmail.com>
2019-04-20 16:16:18 -0700

    Code for CVPR 2019 paper "Hybrid Task Cascade for Instance Segmentation" (#478)
    * add HybridTaskCascade
    
    * add configs for other backbones
    
    * add keep_ratio argument for segmap transform
    
    * add readme for HTC
    
    * fix linting errors
    
    * split assign and sampling as in Cascade R-CNN
    
    * remove unused imports
    
    * add a large model
    
    * update htc

--- mmdet/models/mask_heads/__init__.py
+++ mmdet/models/mask_heads/__init__.py
@@ -1,3 +1,5 @@
 from .fcn_mask_head import FCNMaskHead
+from .htc_mask_head import HTCMaskHead
+from .fused_semantic_head import FusedSemanticHead
 
-__all__ = ['FCNMaskHead']
+__all__ = ['FCNMaskHead', 'HTCMaskHead', 'FusedSemanticHead']
