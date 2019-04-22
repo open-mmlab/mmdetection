801339ae2f50763de3786b25d3c0b35bc8a0a046
Cao Yuhang <yhcao6@gmail.com>
2019-04-22 13:10:33 +0800

    fix bug for cascade mask, the seg mask is ndarray

--- mmdet/models/mask_heads/fcn_mask_head.py
+++ mmdet/models/mask_heads/fcn_mask_head.py
@@ -133,6 +133,7 @@ class FCNMaskHead(nn.Module):
         if isinstance(mask_pred, torch.Tensor):
             mask_pred = mask_pred.sigmoid().cpu().numpy()
         assert isinstance(mask_pred, np.ndarray)
+        mask_pred = mask_pred.astype(np.float32)
 
         cls_segms = [[] for _ in range(self.num_classes - 1)]
         bboxes = det_bboxes.cpu().numpy()[:, :4]
