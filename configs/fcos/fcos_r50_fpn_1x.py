f53031561819393f16931196b09d71a78837dc13
Cao Yuhang <yhcao6@gmail.com>
2019-04-29 17:42:43 +0800

    fix log interval

--- configs/fcos/fcos_r50_fpn_1x.py
+++ configs/fcos/fcos_r50_fpn_1x.py
@@ -99,7 +99,7 @@ lr_config = dict(
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=1,
+    interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='TensorboardLoggerHook')
