d4a4a9f5a563a06cd6cd2d9b8dbe034eae53414a
liuzili <754664334@qq.com>
2019-05-04 09:37:58 +0800

    remove stride and dilation in conv2

--- mmdet/models/backbones/resnet.py
+++ mmdet/models/backbones/resnet.py
@@ -46,9 +46,7 @@ class BasicBlock(nn.Module):
             planes,
             planes,
             3,
-            stride=1,
             padding=1,
-            dilation=1,
             bias=False)
         self.add_module(self.norm2_name, norm2)
 
