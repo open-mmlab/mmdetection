8c8dc810f366bd4ea068d44fa39cbd795fe3fd2f
wondervictor <victorchanchina@gmail.com>
2019-05-08 10:39:37 +0800

    remove zip files

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -7,12 +7,10 @@ from .utils import to_tensor, random_scale, show_ann, get_dataset
 from .concat_dataset import ConcatDataset
 from .repeat_dataset import RepeatDataset
 from .extra_aug import ExtraAugmentation
-from .zip_dataset import ZipDataset
-from .coco_zip import CocoZipDataset
+
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'GroupSampler',
     'DistributedGroupSampler', 'build_dataloader', 'to_tensor', 'random_scale',
     'show_ann', 'get_dataset', 'ConcatDataset', 'RepeatDataset',
-    'ExtraAugmentation', 'CocoZipDataset', 'ZipDataset'
-]
+    'ExtraAugmentation']
