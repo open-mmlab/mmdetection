b998d86a73804bd5906d26f2d02ac12ad4cd2b8b
Jiarui XU <xvjiarui0826@gmail.com>
2019-05-09 01:07:22 +0800

    fixed line too long

--- mmdet/models/utils/__init__.py
+++ mmdet/models/utils/__init__.py
@@ -7,5 +7,6 @@ from .weight_init import (xavier_init, normal_init, uniform_init, kaiming_init,
 __all__ = [
     'conv_ws_2d', 'ConvWS2d', 'build_conv_layer', 'ConvModule',
     'build_norm_layer', 'convert_batchnorm',
-    'xavier_init', 'normal_init', 'uniform_init', 'kaiming_init', 'bias_init_with_prob'
+    'xavier_init', 'normal_init', 'uniform_init', 'kaiming_init',
+    'bias_init_with_prob'
 ]
