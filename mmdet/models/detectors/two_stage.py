e307266f447a52a4cbb85c05bb8f7c16d8c91e99
Kai Chen <chenkaidev@gmail.com>
2019-05-09 01:15:19 -0700

    Use different configs for proposal train/test (#587)
    * use different configs for proposal train/test
    
    * update configs for dcn
    
    * update inf speed
    
    * update inf speed in dcn, gn, htc
    
    * update comparision
    
    * keep backward compatibility

--- mmdet/models/detectors/two_stage.py
+++ mmdet/models/detectors/two_stage.py
@@ -108,7 +108,9 @@ class TwoStageDetector(BaseDetector, RPNTestMixin, BBoxTestMixin,
                 *rpn_loss_inputs, gt_bboxes_ignore=gt_bboxes_ignore)
             losses.update(rpn_losses)
 
-            proposal_inputs = rpn_outs + (img_meta, self.test_cfg.rpn)
+            proposal_cfg = self.train_cfg.get('rpn_proposal',
+                                              self.test_cfg.rpn)
+            proposal_inputs = rpn_outs + (img_meta, proposal_cfg)
             proposal_list = self.rpn_head.get_bboxes(*proposal_inputs)
         else:
             proposal_list = proposals
