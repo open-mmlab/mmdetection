dc45702f4b9573d2cfbbb2f8b8201a0b7db24343
Kai Chen <chenkaidev@gmail.com>
2019-05-10 22:59:57 -0700

    Rewrite multigpu testing with mmddp (#622)
    * rewrite multigpu testing with mmddp
    
    * fix an indent
    
    * update slurm testing script
    
    * update readme and scripts

--- mmdet/datasets/loader/build_loader.py
+++ mmdet/datasets/loader/build_loader.py
@@ -4,7 +4,7 @@ from mmcv.runner import get_dist_info
 from mmcv.parallel import collate
 from torch.utils.data import DataLoader
 
-from .sampler import GroupSampler, DistributedGroupSampler
+from .sampler import GroupSampler, DistributedGroupSampler, DistributedSampler
 
 # https://github.com/pytorch/pytorch/issues/973
 import resource
@@ -18,27 +18,31 @@ def build_dataloader(dataset,
                      num_gpus=1,
                      dist=True,
                      **kwargs):
+    shuffle = kwargs.get('shuffle', True)
     if dist:
         rank, world_size = get_dist_info()
-        sampler = DistributedGroupSampler(dataset, imgs_per_gpu, world_size,
-                                          rank)
+        if shuffle:
+            sampler = DistributedGroupSampler(dataset, imgs_per_gpu,
+                                              world_size, rank)
+        else:
+            sampler = DistributedSampler(dataset,
+                                         world_size,
+                                         rank,
+                                         shuffle=False)
         batch_size = imgs_per_gpu
         num_workers = workers_per_gpu
     else:
-        if not kwargs.get('shuffle', True):
-            sampler = None
-        else:
-            sampler = GroupSampler(dataset, imgs_per_gpu)
+        sampler = GroupSampler(dataset, imgs_per_gpu) if shuffle else None
         batch_size = num_gpus * imgs_per_gpu
         num_workers = num_gpus * workers_per_gpu
 
-    data_loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        sampler=sampler,
-        num_workers=num_workers,
-        collate_fn=partial(collate, samples_per_gpu=imgs_per_gpu),
-        pin_memory=False,
-        **kwargs)
+    data_loader = DataLoader(dataset,
+                             batch_size=batch_size,
+                             sampler=sampler,
+                             num_workers=num_workers,
+                             collate_fn=partial(collate,
+                                                samples_per_gpu=imgs_per_gpu),
+                             pin_memory=False,
+                             **kwargs)
 
     return data_loader
