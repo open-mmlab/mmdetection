8750012e46ad2bd3d4f43b704a53204c947b58f0
Cao Yuhang <yhcao6@gmail.com>
2019-05-11 17:56:53 +0800

    ms use value mode, add x101-64x4d cfg

--- configs/fcos/fcos_x101_64x4d_fpn_2x.py
+++ configs/fcos/fcos_x101_64x4d_fpn_2x.py
@@ -1,15 +1,16 @@
 # model settings
 model = dict(
     type='SingleStageDetector',
-    pretrained='open-mmlab://resnet101_caffe',
+    pretrained='open-mmlab://resnext101_64x4d',
     backbone=dict(
-        type='ResNet',
+        type='ResNeXt',
         depth=101,
+        groups=64,
+        base_width=4,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        normalize=dict(type='BN', frozen=True),
-        style='caffe'),
+        style='pytorch'),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -51,10 +52,10 @@ test_cfg = dict(
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
 img_norm_cfg = dict(
-    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
-    imgs_per_gpu=4,
-    workers_per_gpu=4,
+    imgs_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
@@ -112,10 +113,10 @@ log_config = dict(
 # yapf:enable
 # runtime settings
 total_epochs = 24
-device_ids = range(4)
+device_ids = range(8)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/fcos_r101_fpn_2x'
+work_dir = './work_dirs/fcos_x101_64x4d_fpn_2x'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
