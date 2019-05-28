351b080551c71c8d1ef5db028c7cdf4a56f5ccf2
Kai Chen <chenkaidev@gmail.com>
2019-05-28 21:28:28 +0800

    format losses

--- mmdet/core/loss/losses.py
+++ mmdet/core/loss/losses.py
@@ -261,19 +261,19 @@ def iou_loss(pred_bboxes, target_bboxes, reduction='mean'):
         return loss.sum()
 
 
-def select_iou_loss(pred,
-                    target,
-                    weight,
-                    avg_factor=None):
+# TODO: refactoring of different iou losses
+def select_iou_loss(pred, target, weight, avg_factor=None):
     if avg_factor is None:
-        avg_factor = pred.size()[0]
-    assert pred.size()[0] == target.size()[0]
+        avg_factor = pred.size(0)
+    assert pred.size(0) == target.size(0)
     target = target.clamp(min=0.)
     area_pred = (pred[:, 0] + pred[:, 2]) * (pred[:, 1] + pred[:, 3])
     area_gt = (target[:, 0] + target[:, 2]) * (target[:, 1] + target[:, 3])
-    area_i = (torch.min(pred[:, 0], target[:, 0]) + torch.min(pred[:, 2], target[:, 2])) * \
-             (torch.min(pred[:, 1], target[:, 1]) + torch.min(pred[:, 3], target[:, 3]))
+    area_i = ((torch.min(pred[:, 0], target[:, 0]) +
+               torch.min(pred[:, 2], target[:, 2])) *
+              (torch.min(pred[:, 1], target[:, 1]) +
+               torch.min(pred[:, 3], target[:, 3])))
     area_u = area_pred + area_gt - area_i
     iou = area_i / area_u
-    loc_losses = - torch.log(iou.clamp(min=1e-7))
+    loc_losses = -torch.log(iou.clamp(min=1e-7))
     return torch.sum(weight * loc_losses) / avg_factor
