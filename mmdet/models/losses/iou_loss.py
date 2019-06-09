94134253496b0f50f30b5c9e295aed5ea3060b9c
Kai Chen <chenkaidev@gmail.com>
2019-06-09 22:48:13 +0800

    add eps to iou_loss and handle weight=None

--- mmdet/models/losses/iou_loss.py
+++ mmdet/models/losses/iou_loss.py
@@ -7,7 +7,7 @@ from ..registry import LOSSES
 
 
 @weighted_loss
-def iou_loss(pred, target):
+def iou_loss(pred, target, eps=1e-6):
     """IoU loss.
 
     Computing the IoU loss between a set of predicted bboxes and target bboxes.
@@ -17,12 +17,12 @@ def iou_loss(pred, target):
         pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
             shape (n, 4).
         target (Tensor): Corresponding gt bboxes, shape (n, 4).
-        reduction (str): Reduction option.
+        eps (float): Eps to avoid log(0).
 
     Return:
         Tensor: Loss tensor.
     """
-    ious = bbox_overlaps(pred, target, is_aligned=True)
+    ious = bbox_overlaps(pred, target, is_aligned=True).clamp(min=eps)
     loss = -ious.log()
     return loss
 
@@ -72,23 +72,20 @@ def bounded_iou_loss(pred, target, beta=0.2, eps=1e-3):
 @LOSSES.register_module
 class IoULoss(nn.Module):
 
-    def __init__(self, reduction='mean', loss_weight=1.0):
+    def __init__(self, eps=1e-6, reduction='mean', loss_weight=1.0):
         super(IoULoss, self).__init__()
+        self.eps = eps
         self.reduction = reduction
         self.loss_weight = loss_weight
 
     def forward(self, pred, target, weight=None, avg_factor=None, **kwargs):
-        inds = torch.nonzero(weight[:, 0] > 0)
-        if inds.numel() > 0:
-            inds = inds.squeeze(1)
-            pred = pred[inds]
-            target = target[inds]
-        else:
+        if weight is not None and not torch.any(weight > 0):
             return (pred * weight).sum()  # 0
         loss = self.loss_weight * iou_loss(
             pred,
             target,
             weight,
+            eps=self.eps,
             reduction=self.reduction,
             avg_factor=avg_factor,
             **kwargs)
@@ -106,12 +103,7 @@ class BoundedIoULoss(nn.Module):
         self.loss_weight = loss_weight
 
     def forward(self, pred, target, weight=None, avg_factor=None, **kwargs):
-        inds = torch.nonzero(weight[:, 0] > 0)
-        if inds.numel() > 0:
-            inds = inds.squeeze(1)
-            pred = pred[inds]
-            target = target[inds]
-        else:
+        if weight is not None and not torch.any(weight > 0):
             return (pred * weight).sum()  # 0
         loss = self.loss_weight * bounded_iou_loss(
             pred,
