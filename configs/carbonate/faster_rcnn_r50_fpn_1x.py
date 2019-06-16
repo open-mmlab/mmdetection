39e295a9c1597faafd4934a59b23bbf19540e09d
Hellcatzm <18811474740@163.com>
2019-06-16 11:14:44 +0800

    测试用配置文件

--- configs/carbonate/faster_rcnn_r50_fpn_1x.py
+++ configs/carbonate/faster_rcnn_r50_fpn_1x.py
@@ -163,7 +163,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_r50_fpn_1x'
-load_from = './work_dirs/faster_rcnn_r50_fpn_1x/latest.pth'
+work_dir = './work_dirs/faster_rcnn_r50_fpn_1x_carb'
+load_from = './work_dirs/faster_rcnn_r50_fpn_1x_carb/latest.pth'
 resume_from = None
 workflow = [('train', 1)]
--- configs/carbonate/mask_rcnn_r101_fpn_1x.py
+++ configs/carbonate/mask_rcnn_r101_fpn_1x.py
@@ -175,7 +175,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/mask_rcnn_r101_fpn_1x'
+work_dir = './work_dirs/mask_rcnn_r101_fpn_1x_carb'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
