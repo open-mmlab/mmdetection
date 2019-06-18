e9279750b6997350ee3bb3f9758606572d95008a
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-06-18 12:17:01 +0300

    Prevent ignoring boxes in test mode

--- mmdet/datasets/xml_style.py
+++ mmdet/datasets/xml_style.py
@@ -53,6 +53,7 @@ class XMLDataset(CustomDataset):
             ]
             ignore = False
             if self.min_size:
+                assert not self.test_mode
                 w = bbox[2] - bbox[0]
                 h = bbox[3] - bbox[1]
                 if w < self.min_size or h < self.min_size:
