5bc07d3c4eb7ac6cfe64f00f0fe1a910de8e4247
Kai Chen <chenkaidev@gmail.com>
2019-06-18 17:20:35 +0800

    simplify the calculation of num_edges

--- mmdet/models/mask_heads/grid_head.py
+++ mmdet/models/mask_heads/grid_head.py
@@ -1,5 +1,3 @@
-import functools
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -77,8 +75,7 @@ class GridHead(nn.Module):
         self.neighbor_point = ((1, 3), (0, 2, 4), (1, 5), (0, 4, 6),
                                (1, 3, 5, 7), (2, 4, 8), (3, 7), (4, 6, 8), (5,
                                                                             7))
-        self.num_edges = functools.reduce(
-            lambda x, y: x + y, map(lambda x: len(x), self.neighbor_point))
+        self.num_edges = sum([len(p) for p in self.neighbor_point])
         self.first_order_convs = []
         self.second_order_convs = []
         for _point in self.neighbor_point:
