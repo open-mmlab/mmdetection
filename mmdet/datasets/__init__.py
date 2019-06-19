fc0172b42508d7214d369b87c152f68425ba12da
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-06-19 12:16:47 +0300

    Add support of the wider face dataset (#765)
    * Add support of the wider face dataset
    
    * Update readme
    
    * Prevent ignoring boxes in test mode
    
    * Rename wider to wider_face
    
    * Rename configs for wider face

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -2,6 +2,7 @@ from .custom import CustomDataset
 from .xml_style import XMLDataset
 from .coco import CocoDataset
 from .voc import VOCDataset
+from .wider_face import WIDERFaceDataset
 from .loader import GroupSampler, DistributedGroupSampler, build_dataloader
 from .utils import to_tensor, random_scale, show_ann, get_dataset
 from .concat_dataset import ConcatDataset
@@ -12,5 +13,5 @@ __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'GroupSampler',
     'DistributedGroupSampler', 'build_dataloader', 'to_tensor', 'random_scale',
     'show_ann', 'get_dataset', 'ConcatDataset', 'RepeatDataset',
-    'ExtraAugmentation'
+    'ExtraAugmentation', 'WIDERFaceDataset'
 ]
