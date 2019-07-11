7f22383a5f31db40ca6a80708d4c842c1c9bf11e
wswday <wswday@sina.com>
2019-07-11 18:36:49 +0800

    Fixing build-errors on Windows

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import Extension, find_packages, setup
 import numpy as np
 from Cython.Build import cythonize
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import platform
 
 
 def readme():
@@ -88,18 +89,30 @@ def make_cuda_ext(name, module, sources):
 
     return CUDAExtension(
         name='{}.{}'.format(module, name),
-        sources=[os.path.join(*module.split('.'), p) for p in sources])
+        sources=[os.path.join(*module.split('.'), p) for p in sources],
+        extra_compile_args={
+            "cxx": [],
+            "nvcc": [
+                "-D__CUDA_NO_HALF_OPERATORS__",
+                "-D__CUDA_NO_HALF_CONVERSIONS__",
+                "-D__CUDA_NO_HALF2_OPERATORS__",
+            ]
+        })
 
 
 def make_cython_ext(name, module, sources):
+    extra_compile_args = None
+    if not 'Windows' == platform.system():
+        extra_compile_args = {
+            'cxx': ['-Wno-unused-function', '-Wno-write-strings']
+        }
+
     extension = Extension(
         '{}.{}'.format(module, name),
         [os.path.join(*module.split('.'), p) for p in sources],
         include_dirs=[np.get_include()],
         language='c++',
-        extra_compile_args={
-            'cxx': ['-Wno-unused-function', '-Wno-write-strings']
-        })
+        extra_compile_args=extra_compile_args)
     extension, = cythonize(extension)
     return extension
 
