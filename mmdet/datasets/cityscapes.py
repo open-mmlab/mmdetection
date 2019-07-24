fd814f21972d3db19f41d1b1c150e892f7b9627f
Claudio <claudio.michaelis@uni-tuebingen.de>
2019-07-24 19:42:10 +0200

    Add CityscapesDataset to the registry

--- mmdet/datasets/cityscapes.py
+++ mmdet/datasets/cityscapes.py
@@ -1,6 +1,8 @@
 from .coco import CocoDataset
+from .registry import DATASETS
 
 
+@DATASETS.register_module
 class CityscapesDataset(CocoDataset):
 
     CLASSES = ('person', 'rider', 'car', 'truck', 'bus',
