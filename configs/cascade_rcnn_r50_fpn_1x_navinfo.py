f9cd0ee80580c86a3aa48c29caa8ce22e0398404
NAVINFO\liubao <liubao@navinfo.com>
2019-08-02 16:26:05 +0800

    add TensorboardLoggerHook

--- configs/cascade_rcnn_r50_fpn_1x_navinfo.py
+++ configs/cascade_rcnn_r50_fpn_1x_navinfo.py
@@ -229,7 +229,7 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
+        dict(type='TensorboardLoggerHook')
     ])
 # yapf:enable
 # runtime settings
