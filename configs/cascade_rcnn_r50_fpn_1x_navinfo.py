c8ed0d7f6363765f74fe3d6142dc5aaac03fa11d
NAVINFO\liubao <liubao@navinfo.com>
2019-08-02 17:08:48 +0800

    imgs_per_gpu=8

--- configs/cascade_rcnn_r50_fpn_1x_navinfo.py
+++ configs/cascade_rcnn_r50_fpn_1x_navinfo.py
@@ -178,7 +178,7 @@ data_root = '/workspace/datasets/coco_navinfo/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
-    imgs_per_gpu=2,
+    imgs_per_gpu=8,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
