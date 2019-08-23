9ff6faaeb85244bf0eda7141cb2b4edefc5b0b49
Kai Chen <chenkaidev@gmail.com>
2019-08-23 21:19:08 +0800

    add NASFPN

--- mmdet/models/anchor_heads/__init__.py
+++ mmdet/models/anchor_heads/__init__.py
@@ -4,10 +4,12 @@ from .ga_retina_head import GARetinaHead
 from .ga_rpn_head import GARPNHead
 from .guided_anchor_head import FeatureAdaption, GuidedAnchorHead
 from .retina_head import RetinaHead
+from .retina_sepbn_head import RetinaSepBNHead
 from .rpn_head import RPNHead
 from .ssd_head import SSDHead
 
 __all__ = [
     'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
-    'GARPNHead', 'RetinaHead', 'GARetinaHead', 'SSDHead', 'FCOSHead'
+    'GARPNHead', 'RetinaHead', 'RetinaSepBNHead', 'GARetinaHead', 'SSDHead',
+    'FCOSHead'
 ]
