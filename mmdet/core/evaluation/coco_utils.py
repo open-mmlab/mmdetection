ffe0a14e8c3a8e8f2cd4039e835f883799a2d614
RaymondKirk <ray.tunstill@live.co.uk>
2019-09-17 10:27:34 +0100

    Added seg length check to ensure two objects exist

--- mmdet/core/evaluation/coco_utils.py
+++ mmdet/core/evaluation/coco_utils.py
@@ -142,7 +142,7 @@ def segm2json(dataset, results):
 
             # segm results
             # some detectors use different score for det and segm
-            if label in seg[0]:
+            if len(seg) == 2 and label in seg[0]:
                 segms = seg[0][label]
                 mask_score = seg[1][label]
             else:
