ad2867f983bae39dd9c70ab91c64e714fa400990
valuefish <valuefish@gmail.com>
2019-09-18 15:46:34 +0800

    reformat code in test.py

--- tools/test.py
+++ tools/test.py
@@ -59,8 +59,8 @@ def multi_gpu_test(model, data_loader):
 def collect_results(result_part, size):
     rank, world_size = get_dist_info()
     # dump result part to tensor with pickle
-    part_tensor = torch.tensor(bytearray(pickle.dumps(result_part)),
-                               dtype=torch.uint8, device='cuda')
+    part_tensor = torch.tensor(
+        bytearray(pickle.dumps(result_part)), dtype=torch.uint8, device='cuda')
     # gather all result part tensor shape
     shape_tensor = torch.tensor(part_tensor.shape, device='cuda')
     shape_list = [shape_tensor.clone() for _ in range(world_size)]
@@ -69,14 +69,17 @@ def collect_results(result_part, size):
     shape_max = torch.tensor(shape_list).max()
     part_send = torch.zeros(shape_max, dtype=torch.uint8, device='cuda')
     part_send[:shape_tensor[0]] = part_tensor
-    part_recv_list = [part_tensor.new_zeros(shape_max)
-                      for _ in range(world_size)]
+    part_recv_list = [
+        part_tensor.new_zeros(shape_max) for _ in range(world_size)
+    ]
     # gather all result part
     dist.all_gather(part_recv_list, part_send)
 
     if rank == 0:
-        part_list = [pickle.loads(t[:shape_list[i][0]].cpu().numpy().tobytes())
-                     for i, t in enumerate(part_recv_list)]
+        part_list = [
+            pickle.loads(t[:shape_list[i][0]].cpu().numpy().tobytes())
+            for i, t in enumerate(part_recv_list)
+        ]
         # sort the results
         ordered_results = []
         for res in zip(*part_list):
