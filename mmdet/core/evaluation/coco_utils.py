cf8eb3eae25e3b4a527ba18cef1c115ef55e68f9
RaymondKirk <ray.tunstill@live.co.uk>
2019-09-24 09:01:05 +0100

    Added type check over len check

--- mmdet/core/evaluation/coco_utils.py
+++ mmdet/core/evaluation/coco_utils.py
@@ -142,7 +142,7 @@ def segm2json(dataset, results):
 
             # segm results
             # some detectors use different score for det and segm
-            if len(seg) == 2 and label in seg[0]:
+            if isinstance(seg, tuple) and label in seg[0]:
                 segms = seg[0][label]
                 mask_score = seg[1][label]
             else:
