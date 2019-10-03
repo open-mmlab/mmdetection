4d9a5f47c27c60fb9f3b9a1866507afba3fcba38
Jon Crall <erotemic@gmail.com>
2019-10-03 01:26:00 -0400

    Add two doctest examples for resnet and resnext (#1474)
    * Add doctest example for resnet and resnext
    
    * Explicit import in doctests
    
    * Fix missing torch import in doctest

--- mmdet/models/backbones/resnet.py
+++ mmdet/models/backbones/resnet.py
@@ -352,6 +352,20 @@ class ResNet(nn.Module):
             memory while slowing down the training speed.
         zero_init_residual (bool): whether to use zero init for last norm layer
             in resblocks to let them behave as identity.
+
+    Example:
+        >>> from mmdet.models import ResNet
+        >>> import torch
+        >>> self = ResNet(depth=18)
+        >>> self.eval()
+        >>> inputs = torch.rand(1, 3, 32, 32)
+        >>> level_outputs = self.forward(inputs)
+        >>> for level_out in level_outputs:
+        ...     print(tuple(level_out.shape))
+        (1, 64, 8, 8)
+        (1, 128, 4, 4)
+        (1, 256, 2, 2)
+        (1, 512, 1, 1)
     """
 
     arch_settings = {
