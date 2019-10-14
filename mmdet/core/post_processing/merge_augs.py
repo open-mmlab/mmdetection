6f08b31a8e2af2a2e4383c5d210a5abff795ea56
Cao Yuhang <yhcao6@gmail.com>
2019-10-15 00:00:17 +0800

    fix yapf format

--- mmdet/core/post_processing/merge_augs.py
+++ mmdet/core/post_processing/merge_augs.py
@@ -78,10 +78,7 @@ def merge_aug_scores(aug_scores):
         return np.mean(aug_scores, axis=0)
 
 
-def merge_aug_masks(aug_masks,
-                    img_metas,
-                    rcnn_test_cfg,
-                    weights=None):
+def merge_aug_masks(aug_masks, img_metas, rcnn_test_cfg, weights=None):
     """Merge augmented mask prediction.
 
     Args:
