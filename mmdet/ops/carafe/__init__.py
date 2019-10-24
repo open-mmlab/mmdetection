f28bc1712fb7bb6ea8b6de86d82ee5ddfb523eff
myownskyW7 <727032989@qq.com>
2019-10-24 08:13:06 -0700

    fix flake 8

--- mmdet/ops/carafe/__init__.py
+++ mmdet/ops/carafe/__init__.py
@@ -1,7 +1,7 @@
+from .carafe_pack import CARAFEPack
 from .functions.carafe import carafe
 from .functions.carafe_naive import carafe_naive
 from .modules.CARAFE import CARAFE
 from .modules.CARAFE_NAIVE import CARAFENAIVE
-from .carafe_pack import CARAFEPack
 
 __all__ = ['carafe', 'carafe_naive', 'CARAFE', 'CARAFENAIVE', 'CARAFEPack']
