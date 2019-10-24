e0f32083fc213345b46ce92d2093dc7daa8e37a2
myownskyW7 <727032989@qq.com>
2019-10-24 08:01:38 -0700

    fix flake 8 error

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -1,6 +1,6 @@
 from .bfp import BFP
 from .fpn import FPN
-from .hrfpn import HRFPN
 from .fpn_carafe import FPN_CARAFE
+from .hrfpn import HRFPN
 
 __all__ = ['FPN', 'BFP', 'HRFPN', 'FPN_CARAFE']
--- mmdet/ops/carafe/setup.py
+++ mmdet/ops/carafe/setup.py
@@ -1,7 +1,6 @@
 import os
 
 import torch
-
 from setuptools import find_packages, setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
