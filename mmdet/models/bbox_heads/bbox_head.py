0457ca771910ecfe5920fe83e5c7630a56e8d13c
joncrall <erotemic@gmail.com>
2019-10-29 15:32:32 -0400

    Fix logic error in BBoxHead.loss

--- mmdet/models/bbox_heads/bbox_head.py
+++ mmdet/models/bbox_heads/bbox_head.py
@@ -117,12 +117,13 @@ class BBoxHead(nn.Module):
                 losses['acc'] = accuracy(cls_score, labels)
         if bbox_pred is not None:
             pos_inds = labels > 0
-            if self.reg_class_agnostic:
-                pos_bbox_pred = bbox_pred.view(bbox_pred.size(0), 4)[pos_inds]
-            else:
-                pos_bbox_pred = bbox_pred.view(bbox_pred.size(0), -1,
-                                               4)[pos_inds, labels[pos_inds]]
-            if pos_inds.numel() > 0:
+            if pos_inds.any():
+                if self.reg_class_agnostic:
+                    pos_bbox_pred = bbox_pred.view(bbox_pred.size(0),
+                                                   4)[pos_inds]
+                else:
+                    pos_bbox_pred = bbox_pred.view(
+                        bbox_pred.size(0), -1, 4)[pos_inds, labels[pos_inds]]
                 losses['loss_bbox'] = self.loss_bbox(
                     pos_bbox_pred,
                     bbox_targets[pos_inds],
