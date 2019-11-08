b965a5c90341293e646837b561b91c8e94614dc5
Korabelnikov Aleks <nesoriti@yandex.ru>
2019-11-08 14:57:13 +0300

    Update test.py
    see #1644 #1646

--- tools/test.py
+++ tools/test.py
@@ -21,13 +21,15 @@ def single_gpu_test(model, data_loader, show=False):
     results = []
     dataset = data_loader.dataset
     prog_bar = mmcv.ProgressBar(len(dataset))
+    score_thr = model.module.test_cfg['score_thr']
+
     for i, data in enumerate(data_loader):
         with torch.no_grad():
-            result = model(return_loss=False, rescale=not show, **data)
+            result = model(return_loss=False, rescale=False, **data)
         results.append(result)
 
         if show:
-            model.module.show_result(data, result)
+            model.module.show_result(data, result, score_thr=score_thr)
 
         batch_size = data['img'][0].size(0)
         for _ in range(batch_size):
