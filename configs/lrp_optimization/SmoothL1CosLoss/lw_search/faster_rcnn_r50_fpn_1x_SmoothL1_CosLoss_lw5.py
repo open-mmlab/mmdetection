556cf34aa4e7e9a094a088b634de77dcc36250b5
cancam <bariscancam>
2019-11-27 02:05:01 +0300

    lw_experiments corrected

--- configs/lrp_optimization/SmoothL1CosLoss/lw_search/faster_rcnn_r50_fpn_1x_SmoothL1_CosLoss_lw5.py
+++ configs/lrp_optimization/SmoothL1CosLoss/lw_search/faster_rcnn_r50_fpn_1x_SmoothL1_CosLoss_lw5.py
@@ -42,9 +42,9 @@ model = dict(
         target_stds=[0.1, 0.1, 0.2, 0.2],
         reg_class_agnostic=False,
         loss_cls=dict(
-            type='LRPLoss', use_sigmoid=False, loss_weight=1.0, \
+            type='LRPLoss', use_sigmoid=False, loss_weight=5.0, \
                   use_modulator = False, gamma = 1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=5.0)))
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)))
 # model training and testing settings
 train_cfg = dict(
     rpn=dict(
