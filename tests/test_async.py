791e3aa8b65a155c81b7c78f16c05004bef98178
Rinat Shigapov <rinatshigapov@gmail.com>
2019-12-05 04:09:07 -0600

    async tests are run in Python 3.7

--- tests/test_async.py
+++ tests/test_async.py
@@ -2,13 +2,16 @@
 
 import asyncio
 import os
+import sys
 
 import asynctest
 import mmcv
 import torch
 
 from mmdet.apis import async_inference_detector, init_detector
-from mmdet.utils.contextmanagers import concurrent
+
+if sys.version_info >= (3, 7):
+    from mmdet.utils.contextmanagers import concurrent
 
 
 class AsyncTestCase(asynctest.TestCase):
@@ -44,21 +47,25 @@ class MaskRCNNDetector:
         for _ in range(self.streamqueue_size):
             self.streamqueue.put_nowait(torch.cuda.Stream(device=self.device))
 
-    async def apredict(self, img):
-        if isinstance(img, str):
-            img = mmcv.imread(img)
-        async with concurrent(self.streamqueue):
-            result = await async_inference_detector(self.model, img)
-        return result
+    if sys.version_info >= (3, 7):
+
+        async def apredict(self, img):
+            if isinstance(img, str):
+                img = mmcv.imread(img)
+            async with concurrent(self.streamqueue):
+                result = await async_inference_detector(self.model, img)
+            return result
 
 
 class AsyncInferenceTestCase(AsyncTestCase):
 
-    async def test_simple_inference(self):
-        root_dir = os.path.dirname(os.path.dirname(__name__))
-        model_config = os.path.join(root_dir,
-                                    "configs/mask_rcnn_r50_fpn_1x.py")
-        detector = MaskRCNNDetector(model_config)
-        await detector.init()
-        img_path = os.path.join(root_dir, "demo/demo.jpg")
-        await detector.apredict(img_path)
+    if sys.version_info >= (3, 7):
+
+        async def test_simple_inference(self):
+            root_dir = os.path.dirname(os.path.dirname(__name__))
+            model_config = os.path.join(root_dir,
+                                        "configs/mask_rcnn_r50_fpn_1x.py")
+            detector = MaskRCNNDetector(model_config)
+            await detector.init()
+            img_path = os.path.join(root_dir, "demo/demo.jpg")
+            await detector.apredict(img_path)
