9774b701a2c2bf42355f1afa9bc5d86ffc589b78
cancam <bariscancam>
2019-12-07 15:00:56 +0300

    config files updated

--- configs/lrp_optimization/initial_experiments/faster_rcnn_r50_fpn_1x_orig.py
+++ configs/lrp_optimization/initial_experiments/faster_rcnn_r50_fpn_1x_orig.py
@@ -165,7 +165,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 1
+total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/lrp_optimization/faster_rcnn_r50_fpn_1x_orig'
