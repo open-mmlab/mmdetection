385daed15cea5e8bf0b949cf13a9563bd07ff19d
cancam <bariscancam>
2019-12-07 23:13:43 +0300

    config files updated

--- configs/lrp_optimization/initial_experiments/faster_rcnn_r50_fpn_1x_orig.py
+++ configs/lrp_optimization/initial_experiments/faster_rcnn_r50_fpn_1x_orig.py
@@ -146,7 +146,7 @@ data = dict(
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
