0fdf652e3c2326113c035cccbff1ca3cd308ffbb
Yvan Satyawan <y_satyawan@hotmail.com>
2019-12-11 13:19:35 +0100

    WFCOS with Deep Scores dataset

---
+++ configs/wfcos/wfcos_resnet101_fpn_nosplit_3gpu_ds.py
@@ -0,0 +1,145 @@
+model = dict(
+    type='WFCOS',
+    pretrained='open-mmlab://resnet101_caffe',
+    backbone=dict(
+        type='ResNet',
+        depth=101,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        style='caffe'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs=True,
+        extra_convs_on_inputs=False,  # use P5
+        num_outs=5,
+        relu_before_extra_convs=True),
+    bbox_head=dict(
+        type='WFCOSHead',
+        num_classes=124,
+        in_channels=256,
+        max_energy=20,
+        stacked_convs=4,
+        feat_channels=256,
+        strides=[8, 16, 32, 64, 128],
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='IoULoss', loss_weight=1.0),
+        loss_energy=dict(
+            type='CrossEntropyLoss', loss_weight=1.0, use_sigmoid=False
+        ),
+        split_convs=False,
+        r=500.
+    ))
+# training and testing settings
+train_cfg = dict(
+    assigner=dict(
+        type='MaxIoUAssigner',
+        pos_iou_thr=0.5,
+        neg_iou_thr=0.4,
+        min_pos_iou=0,
+        ignore_iof_thr=-1),
+    allowed_border=-1,
+    pos_weight=-1,
+    debug=False)
+test_cfg = dict(
+    nms_pre=1000,
+    min_bbox_size=0,
+    score_thr=0.05,
+    nms=dict(type='nms', iou_thr=0.5),
+    max_per_img=100)
+# dataset settings
+dataset_type = 'DeepScoresDataset'
+data_root = 'data/deep_scores_dense/'
+img_norm_cfg = dict(
+    mean=[240.15232515949037, 240.15229097456378, 240.15232515949037],
+    std=[57.178083212078896, 57.178143244444556, 57.178083212078896],
+    to_rgb=False)
+img_scale_train = (1333, 640)
+img_scale_test = (3000, 3828)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomCrop', crop_size=(640, 800)),
+    dict(type='Resize', img_scale=img_scale_train, keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale_test,
+        flip=False,
+        transforms=[
+            # dict(type='RandomCrop', crop_size=(640, 800)),
+            dict(type='Resize', img_scale=img_scale_test, keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    imgs_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'deepscores_train.json',
+        img_prefix=data_root + 'images_png/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'deepscores_val.json',
+        img_prefix=data_root + 'images_png/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'deepscores_small.json',
+        img_prefix=data_root + 'images_png/',
+        pipeline=test_pipeline))
+# optimizer
+optimizer = dict(
+    type='SGD',
+    lr=0.01,
+    momentum=0.9,
+    weight_decay=0.0001,
+    paramwise_options=dict(bias_lr_mult=2., bias_decay_mult=0.))
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='constant',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 3,
+    step=[16, 22])
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+# runtime settings
+total_epochs = 36
+device_ids = range(3)
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = './work_dirs/wfcos_resnet101_fpn_nosplit_3gpu_ds'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
