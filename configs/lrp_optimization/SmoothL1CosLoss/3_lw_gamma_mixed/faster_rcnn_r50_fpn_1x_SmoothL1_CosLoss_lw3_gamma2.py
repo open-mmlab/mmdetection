a0505d731559cf99c9cb7e1de65301d096ae8ae1
cancam <bariscancam>
2019-12-15 12:53:46 +0300

    new configs.

--- configs/lrp_optimization/SmoothL1CosLoss/3_lw_gamma_mixed/faster_rcnn_r50_fpn_1x_SmoothL1_CosLoss_lw3_gamma2.py
+++ configs/lrp_optimization/SmoothL1CosLoss/3_lw_gamma_mixed/faster_rcnn_r50_fpn_1x_SmoothL1_CosLoss_lw3_gamma2.py
@@ -42,7 +42,7 @@ model = dict(
         target_stds=[0.1, 0.1, 0.2, 0.2],
         reg_class_agnostic=False,
         loss_cls=dict(
-           type='LRPLoss', use_sigmoid=False, loss_weight=2.5, \
+           type='LRPLoss', use_sigmoid=False, loss_weight=3.0, \
                   use_modulator = True, gamma = 2.0),
         loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)))
 # model training and testing settings
@@ -169,7 +169,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/lrp_optimization/faster_rcnn_r50_fpn_1x_SmoothL1_CosLoss_lw25_gamma2'
+work_dir = './work_dirs/lrp_optimization/faster_rcnn_r50_fpn_1x_SmoothL1_CosLoss_lw3_gamma2'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
