e6412882a6cb4c05acb2a4b74ee67e7937b93b74
GothicAi <35886047+GothicAi@users.noreply.github.com>
2019-12-16 19:09:03 +0800

    add instaboost in data pipeline

--- mmdet/datasets/pipelines/__init__.py
+++ mmdet/datasets/pipelines/__init__.py
@@ -6,11 +6,12 @@ from .test_aug import MultiScaleFlipAug
 from .transforms import (Albu, Expand, MinIoURandomCrop, Normalize, Pad,
                          PhotoMetricDistortion, RandomCrop, RandomFlip, Resize,
                          SegResizeFlipPadRescale)
+from .instaboost import InstaBoost
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
     'LoadProposals', 'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad',
     'RandomCrop', 'Normalize', 'SegResizeFlipPadRescale', 'MinIoURandomCrop',
-    'Expand', 'PhotoMetricDistortion', 'Albu'
+    'Expand', 'PhotoMetricDistortion', 'Albu', 'InstaBoost'
 ]
