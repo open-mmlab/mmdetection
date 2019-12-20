cd7c79a2e95b99824448c1443c312df1e2a337a4
Rinat Shigapov <rinatshigapov@gmail.com>
2019-12-20 02:54:38 -0600

    fix linting

--- tests/async_benchmark.py
+++ tests/async_benchmark.py
@@ -5,9 +5,9 @@ import os
 import shutil
 import urllib
 
+import mmcv
 import torch
 
-import mmcv
 from mmdet.apis import (async_inference_detector, inference_detector,
                         init_detector, show_result)
 from mmdet.utils.contextmanagers import concurrent
@@ -27,7 +27,8 @@ async def main():
     8074.52 ms	9660.94 ms
     7976.44 ms	9406.83 ms
 
-    Async variant takes about 0.83-0.85 of the time of the synchronous interface.
+    Async variant takes about 0.83-0.85 of the time of the synchronous
+    interface.
 
     """
     project_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -37,18 +38,19 @@ async def main():
         project_dir, 'checkpoints/mask_rcnn_r50_fpn_1x_20181010-069fa190.pth')
 
     if not os.path.exists(checkpoint_file):
-        url = ('https://s3.ap-northeast-2.amazonaws.com/open-mmlab/mmdetection/'
-               'models/mask_rcnn_r50_fpn_1x_20181010-069fa190.pth')
-        print(f'Downloading {url} ...')
+        url = ('https://s3.ap-northeast-2.amazonaws.com/open-mmlab/mmdetection'
+               '/models/mask_rcnn_r50_fpn_1x_20181010-069fa190.pth')
+        print('Downloading {} ...'.format(url))
         local_filename, _ = urllib.request.urlretrieve(url)
         os.makedirs(os.path.dirname(checkpoint_file), exist_ok=True)
         shutil.move(local_filename, checkpoint_file)
-        print(f'Saved as {checkpoint_file}')
+        print('Saved as {}'.format(checkpoint_file))
     else:
-        print(f'Using existing checkpoint {checkpoint_file}')
+        print('Using existing checkpoint {}'.format(checkpoint_file))
 
     device = 'cuda:0'
-    model = init_detector(config_file, checkpoint=checkpoint_file, device=device)
+    model = init_detector(
+        config_file, checkpoint=checkpoint_file, device=device)
 
     # queue is used for concurrent inference of multiple images
     streamqueue = asyncio.Queue()
@@ -70,17 +72,33 @@ async def main():
 
     num_of_images = 20
     with profile_time('benchmark', 'async'):
-        tasks = [asyncio.create_task(detect(img)) for _ in range(num_of_images)]
+        tasks = [
+            asyncio.create_task(detect(img)) for _ in range(num_of_images)
+        ]
         async_results = await asyncio.gather(*tasks)
 
     with torch.cuda.stream(torch.cuda.default_stream()):
         with profile_time('benchmark', 'sync'):
-            sync_results = [inference_detector(model, img) for _ in range(num_of_images)]
+            sync_results = [
+                inference_detector(model, img) for _ in range(num_of_images)
+            ]
 
     result_dir = os.path.join(project_dir, 'demo')
-    show_result(img, async_results[0], model.CLASSES, score_thr=0.5, show=False,
-                out_file=os.path.join(result_dir, 'result_async.jpg'))
-    show_result(img, sync_results[0], model.CLASSES, score_thr=0.5, show=False,
-                out_file=os.path.join(result_dir, 'result_sync.jpg'))
+    show_result(
+        img,
+        async_results[0],
+        model.CLASSES,
+        score_thr=0.5,
+        show=False,
+        out_file=os.path.join(result_dir, 'result_async.jpg'))
+    show_result(
+        img,
+        sync_results[0],
+        model.CLASSES,
+        score_thr=0.5,
+        show=False,
+        out_file=os.path.join(result_dir, 'result_sync.jpg'))
 
-asyncio.run(main())
+
+if __name__ == '__main__':
+    asyncio.run(main())
