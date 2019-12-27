d2fe7c2df6e22498bbcafaa82d66e7e880d65267
cancam <bariscancam>
2019-12-27 09:20:34 +0300

    configs fixed.

--- configs/lrp_optimization/IoUExpLoss/faster_rcnn_r50_fpn_1x_IoU_ExpLoss.py
+++ configs/lrp_optimization/IoUExpLoss/faster_rcnn_r50_fpn_1x_IoU_ExpLoss.py
@@ -94,7 +94,7 @@ test_cfg = dict(
         nms_thr=0.7,
         min_bbox_size=0),
     rcnn=dict(
-        score_thr=0.00, nms=dict(type='nms', iou_thr=0.5), max_per_img=100)
+        score_thr=0.05, nms=dict(type='nms', iou_thr=0.5), max_per_img=100)
     # soft-nms is also supported for rcnn testing
     # e.g., nms=dict(type='soft_nms', iou_thr=0.5, min_score=0.05)
 )
