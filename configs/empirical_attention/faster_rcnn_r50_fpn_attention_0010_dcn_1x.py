62fb5332ee3c227361951a3141c82f6fa8f6db48
wwzhang <wayne.zw@outlook.com>
2020-01-01 15:07:02 +0800

    change cfg to pass test

--- configs/empirical_attention/faster_rcnn_r50_fpn_attention_0010_dcn_1x.py
+++ configs/empirical_attention/faster_rcnn_r50_fpn_attention_0010_dcn_1x.py
@@ -13,7 +13,7 @@ model = dict(
             spatial_range=-1, num_heads=8, attention_type='0010', kv_stride=2),
         stage_with_gen_attention=[[], [], [0, 1, 2, 3, 4, 5], [0, 1, 2]],
         dcn=dict(
-            modulated=False, deformable_groups=1, fallback_on_stride=False),
+            type='DCN', deformable_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True),
     ),
     neck=dict(
