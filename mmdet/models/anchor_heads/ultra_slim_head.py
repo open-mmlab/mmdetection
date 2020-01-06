090f6ce787ef835593390931209507577262ea32
likun <likun10@baidu.com>
2020-01-06 12:22:58 +0800

    ultra_slim_head

---
+++ mmdet/models/anchor_heads/ultra_slim_head.py
@@ -0,0 +1,263 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from mmcv.cnn import xavier_init
+from mmdet.core import AnchorGenerator_Ultra, anchor_target, multi_apply,delta2bbox, multiclass_nms
+from .anchor_head import AnchorHead
+from ..losses import smooth_l1_loss
+from ..registry import HEADS
+from ..backbones import ultra_slim
+from torch.nn import Conv2d
+from mmdet.core import force_fp32
+
+
+@HEADS.register_module
+class UltraSlimHead(AnchorHead):
+	def __init__(self,
+		input_size=(320,240),
+		num_classes=2,
+		in_channels=(64, 128, 256, 256),
+		min_boxes = ([10, 16, 24], [32, 48], [64, 96], [128, 192, 256]),
+		target_means=(.0, .0, .0, .0),
+		target_stds=(1.0, 1.0, 1.0, 1.0)
+		):
+		super(AnchorHead, self).__init__()
+		self.input_size = input_size
+		self.num_classes = num_classes
+		self.in_channels = in_channels
+		self.min_boxes_list = [_min_boxes for _min_boxes in min_boxes]
+		self.cls_out_channels = num_classes
+
+		reg_convs = []
+		cls_convs = []
+		num_anchors = [3, 2, 2, 3]
+		for i in range(len(in_channels)-1):
+			reg_convs.append(
+				ultra_slim.SeperableConv2d(
+					in_channels[i],
+					num_anchors[i] * 4,
+					kernel_size=3,
+					padding=1))
+			cls_convs.append(
+				ultra_slim.SeperableConv2d(
+					in_channels[i],
+					num_anchors[i] * num_classes,
+					kernel_size=3,
+					padding=1))
+		reg_convs.append(Conv2d(in_channels[-1],num_anchors[3] * 4,kernel_size=3,padding=1))
+		cls_convs.append(Conv2d(in_channels[-1],num_anchors[3] * num_classes,kernel_size=3,padding=1))
+		self.reg_convs = nn.ModuleList(reg_convs)
+		self.cls_convs = nn.ModuleList(cls_convs)
+		self.anchor_generators = []
+		for min_boxes in self.min_boxes_list:
+			anchor_generator = AnchorGenerator_Ultra(input_size,
+				min_boxes)
+			self.anchor_generators.append(anchor_generator)
+		self.target_means = target_means
+		self.target_stds = target_stds
+		self.use_sigmoid_cls = False
+		self.cls_focal_loss = False
+		self.fp16_enabled = False
+
+
+
+	def init_weights(self):
+		self.reg_convs.apply(ultra_slim.xavier_init)
+		self.cls_convs.apply(ultra_slim.xavier_init)
+
+	def forward(self, feats):
+		cls_scores = []
+		bbox_preds = []
+		for feat, reg_conv, cls_conv in zip(feats, self.reg_convs,
+													self.cls_convs):
+			cls_scores.append(cls_conv(feat))
+			bbox_preds.append(reg_conv(feat))
+		return cls_scores, bbox_preds
+
+	def _get_anchors(self, featmap_sizes, img_metas):
+
+		num_imgs = len(img_metas)
+		num_levels = len(featmap_sizes)
+        # since feature map sizes of all images are the same, we only compute
+        # anchors for one time
+		multi_level_anchors = []
+		for i in range(num_levels):
+			anchors = self.anchor_generators[i].grid_anchors(
+				featmap_sizes[i])
+			multi_level_anchors.append(anchors)
+		anchor_list = [multi_level_anchors for _ in range(num_imgs)]
+        # for each image, we compute valid flags of multi level anchors
+		valid_flag_list = []
+		for img_id, img_meta in enumerate(img_metas):
+			multi_level_flags = []
+			for i in range(num_levels):
+				feat_h, feat_w = featmap_sizes[i]
+				anchor_stride_w = int(self.input_size[0] / feat_w)
+				anchor_stride_h = int(self.input_size[1] / feat_h)
+				w, h, _ = img_meta['pad_shape']
+				valid_feat_w = min(int(np.ceil(w / anchor_stride_w)), feat_w)
+				valid_feat_h = min(int(np.ceil(h / anchor_stride_h)), feat_h)				
+				flags = self.anchor_generators[i].valid_flags(
+					(feat_w, feat_h), (valid_feat_w, valid_feat_h))
+				multi_level_flags.append(flags)
+			valid_flag_list.append(multi_level_flags)
+		return anchor_list, valid_flag_list
+
+
+	@force_fp32(apply_to=('cls_scores', 'bbox_preds'))
+	def loss(self,
+             cls_scores,
+             bbox_preds,
+             gt_bboxes,
+             gt_labels,
+             img_metas,
+             cfg,
+             gt_bboxes_ignore=None):
+		featmap_sizes = [featmap.size()[-2:] for featmap in cls_scores]
+		assert len(featmap_sizes) == len(self.anchor_generators)
+
+		
+		anchor_list, valid_flag_list = self._get_anchors(
+            featmap_sizes, img_metas)
+		cls_reg_targets = anchor_target(
+            anchor_list,
+            valid_flag_list,
+            gt_bboxes,
+            img_metas,
+            self.target_means,
+            self.target_stds,
+            cfg,
+            gt_bboxes_ignore_list=gt_bboxes_ignore,
+            gt_labels_list=gt_labels,
+            label_channels=1,
+            sampling=False,
+            unmap_outputs=True)
+
+		if cls_reg_targets is None:
+			return None
+
+		(labels_list, label_weights_list, bbox_targets_list, bbox_weights_list,
+         num_total_pos, num_total_neg) = cls_reg_targets
+
+		num_images = len(img_metas)
+		all_cls_scores = torch.cat([
+            s.permute(0, 2, 3, 1).reshape(
+                num_images, -1, self.cls_out_channels) for s in cls_scores
+        ], 1)
+
+		all_labels = torch.cat(labels_list, -1).view(num_images, -1)
+		all_label_weights = torch.cat(label_weights_list,
+                                      -1).view(num_images, -1)
+		all_bbox_preds = torch.cat([
+            b.permute(0, 2, 3, 1).reshape(num_images, -1, 4)
+            for b in bbox_preds
+        ], -2)
+		all_bbox_targets = torch.cat(bbox_targets_list,
+                                     -2).view(num_images, -1, 4)
+
+		all_bbox_weights = torch.cat(bbox_weights_list,
+                                     -2).view(num_images, -1, 4)
+		num = all_bbox_preds.size(0)
+		pos = all_labels > 0
+		pos_idx = pos.unsqueeze(pos.dim()).expand_as(all_bbox_preds)
+		loc_p = all_bbox_preds[pos_idx].view(-1, 4)
+		loc_t = all_bbox_targets[pos_idx].view(-1, 4)
+		losses_bbox = F.smooth_l1_loss(loc_p, loc_t, reduction='sum')
+		batch_conf = all_cls_scores.view(-1, self.num_classes)
+
+		def log_sum_exp(x):
+			x_max = x.data.max()
+			return torch.log(torch.sum(torch.exp(x-x_max), 1, keepdim=True)) + x_max
+
+		loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, all_labels.view(-1, 1))
+		loss_c[all_labels.view(-1, 1)] = 0 # filter out pos boxes for now
+		loss_c = loss_c.view(num, -1)
+		_, loss_idx = loss_c.sort(1, descending=True)
+		_, idx_rank = loss_idx.sort(1)
+		num_pos = pos.long().sum(1, keepdim=True)
+		num_neg = torch.clamp(cfg.neg_pos_ratio*num_pos, max=pos.size(1)-1)
+		neg = idx_rank < num_neg.expand_as(idx_rank)
+		pos_idx = pos.unsqueeze(2).expand_as(all_cls_scores)
+		neg_idx = neg.unsqueeze(2).expand_as(all_cls_scores)
+		conf_p = all_cls_scores[(pos_idx+neg_idx).gt(0)].view(-1,self.num_classes)
+		targets_weighted = all_labels[(pos+neg).gt(0)]
+		losses_cls = F.cross_entropy(conf_p, targets_weighted, reduction='sum')
+		N = max(num_pos.data.sum().float(), 1)
+		losses_bbox /= N
+		losses_cls /= N
+		losses_bbox = cfg.reg_loss_weight*losses_bbox		
+		return dict(loss_cls=losses_cls, loss_bbox=losses_bbox)
+
+	@force_fp32(apply_to=('cls_scores', 'bbox_preds'))
+	def get_bboxes(self, cls_scores, bbox_preds, img_metas, cfg,
+                   rescale=False):
+		assert len(cls_scores) == len(bbox_preds)
+		num_levels = len(cls_scores)
+
+		mlvl_anchors = [
+			self.anchor_generators[i].grid_anchors(cls_scores[i].size()[-2:])
+			for i in range(num_levels)
+		]
+		result_list = []
+		for img_id in range(len(img_metas)):
+			cls_score_list = [
+                cls_scores[i][img_id].detach() for i in range(num_levels)
+            ]
+			bbox_pred_list = [
+                bbox_preds[i][img_id].detach() for i in range(num_levels)
+			]
+			img_shape = img_metas[img_id]['img_shape']
+			scale_factor = img_metas[img_id]['scale_factor']
+			proposals = self.get_bboxes_single(cls_score_list, bbox_pred_list,
+                                               mlvl_anchors, img_shape,
+                                               scale_factor, cfg, rescale)
+			result_list.append(proposals)
+		return result_list
+
+	def get_bboxes_single(self,
+                          cls_scores,
+                          bbox_preds,
+                          mlvl_anchors,
+                          img_shape,
+                          scale_factor,
+                          cfg,
+                          rescale=False):
+		assert len(cls_scores) == len(bbox_preds) == len(mlvl_anchors)
+		mlvl_bboxes = []
+		mlvl_scores = []
+		for cls_score, bbox_pred, anchors in zip(cls_scores, bbox_preds,
+                                                 mlvl_anchors):
+			assert cls_score.size()[-2:] == bbox_pred.size()[-2:]
+			cls_score = cls_score.permute(1, 2,
+                                          0).reshape(-1, self.cls_out_channels)
+			if self.use_sigmoid_cls:
+				scores = cls_score.sigmoid()
+			else:
+				scores = cls_score.softmax(-1)
+			bbox_pred = bbox_pred.permute(1, 2, 0).reshape(-1, 4)
+			nms_pre = cfg.get('nms_pre', -1)
+			if nms_pre > 0 and scores.shape[0] > nms_pre:
+				if self.use_sigmoid_cls:
+					max_scores, _ = scores.max(dim=1)
+				else:
+					max_scores, _ = scores[:, 1:].max(dim=1)
+				_, topk_inds = max_scores.topk(nms_pre)
+				anchors = anchors[topk_inds, :]
+				bbox_pred = bbox_pred[topk_inds, :]
+				scores = scores[topk_inds, :]
+			bboxes = delta2bbox(anchors, bbox_pred, self.target_means,
+                                self.target_stds, img_shape)
+			mlvl_bboxes.append(bboxes)
+			mlvl_scores.append(scores)
+		mlvl_bboxes = torch.cat(mlvl_bboxes)
+		if rescale:
+			mlvl_bboxes /= mlvl_bboxes.new_tensor(scale_factor)
+		mlvl_scores = torch.cat(mlvl_scores)
+		if self.use_sigmoid_cls:
+			padding = mlvl_scores.new_zeros(mlvl_scores.shape[0], 1)
+			mlvl_scores = torch.cat([padding, mlvl_scores], dim=1)
+		det_bboxes, det_labels = multiclass_nms(mlvl_bboxes, mlvl_scores,
+                                                cfg.score_thr, cfg.nms,
+                                                cfg.max_per_img)
+		return det_bboxes, det_labels
