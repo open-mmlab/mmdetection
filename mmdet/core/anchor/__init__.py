d967afdd18478ef4963b894cafdfa46ac4a12433
likun <likun10@baidu.com>
2020-01-06 12:15:25 +0800

    anchor_init

--- mmdet/core/anchor/__init__.py
+++ mmdet/core/anchor/__init__.py
@@ -3,8 +3,12 @@ from .anchor_target import anchor_inside_flags, anchor_target
 from .guided_anchor_target import ga_loc_target, ga_shape_target
 from .point_generator import PointGenerator
 from .point_target import point_target
+from .anchor_generator_fb import AnchorGenerator_FB
+from .anchor_generator_ultra import AnchorGenerator_Ultra
+
 
 __all__ = [
     'AnchorGenerator', 'anchor_target', 'anchor_inside_flags', 'ga_loc_target',
-    'ga_shape_target', 'PointGenerator', 'point_target'
+    'ga_shape_target', 'PointGenerator', 'point_target' ,'AnchorGenerator_FB' ,
+    'AnchorGenerator_Ultra'
 ]
