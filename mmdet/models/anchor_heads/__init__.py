921073e6c3073b9673e8ae095c3e7649704a6e0a
likun <likun10@baidu.com>
2020-01-06 12:15:59 +0800

    anchor_head_init

--- mmdet/models/anchor_heads/__init__.py
+++ mmdet/models/anchor_heads/__init__.py
@@ -7,12 +7,15 @@ from .ga_rpn_head import GARPNHead
 from .guided_anchor_head import FeatureAdaption, GuidedAnchorHead
 from .reppoints_head import RepPointsHead
 from .retina_head import RetinaHead
-from .retina_sepbn_head import RetinaSepBNHead
 from .rpn_head import RPNHead
 from .ssd_head import SSDHead
+from .faceboxes_head_densification import FaceboxesHead_DENS
+from .faceboxes_head_densification_v2 import FaceboxesHead_DENS_V2
+from .ultra_slim_head import UltraSlimHead
 
 __all__ = [
     'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
-    'GARPNHead', 'RetinaHead', 'RetinaSepBNHead', 'GARetinaHead', 'SSDHead',
-    'FCOSHead', 'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead'
+    'GARPNHead', 'RetinaHead', 'GARetinaHead', 'SSDHead', 'FCOSHead',
+    'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead','FaceboxesHead_DENS',
+    'FaceboxesHead_DENS_V2' , 'UltraSlimHead'
 ]
