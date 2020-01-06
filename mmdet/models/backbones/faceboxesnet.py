63caceff219a084ff04027de8c4f8d3a361f1a07
likun <likun10@baidu.com>
2020-01-06 12:24:50 +0800

    backbones_faceboxes

---
+++ mmdet/models/backbones/faceboxesnet.py
@@ -0,0 +1,123 @@
+import logging
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from mmcv.cnn import (xavier_init, constant_init, kaiming_init,
+                      normal_init)
+from mmcv.runner import load_checkpoint
+from ..registry import BACKBONES
+
+
+class BasicConv2d(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=1e-5)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.relu(x, inplace=True)
+
+
+class Inception(nn.Module):
+
+    def __init__(self):
+        super(Inception, self).__init__()
+        self.branch1x1 = BasicConv2d(128, 32, kernel_size=1, padding=0)
+        self.branch1x1_2 = BasicConv2d(128, 32, kernel_size=1, padding=0)
+        self.branch3x3_reduce = BasicConv2d(128, 24, kernel_size=1, padding=0)
+        self.branch3x3 = BasicConv2d(24, 32, kernel_size=3, padding=1)
+        self.branch3x3_reduce_2 = BasicConv2d(128, 24, kernel_size=1, padding=0)
+        self.branch3x3_2 = BasicConv2d(24, 32, kernel_size=3, padding=1)
+        self.branch3x3_3 = BasicConv2d(32, 32, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        branch1x1 = self.branch1x1(x)
+        branch1x1_pool = F.avg_pool2d(x, kernel_size=3, stride=1, padding=1)
+        branch1x1_2 = self.branch1x1_2(branch1x1_pool)
+        branch3x3_reduce = self.branch3x3_reduce(x)
+        branch3x3 = self.branch3x3(branch3x3_reduce)
+        branch3x3_reduce_2 = self.branch3x3_reduce_2(x)
+        branch3x3_2 = self.branch3x3_2(branch3x3_reduce_2)
+        branch3x3_3 = self.branch3x3_3(branch3x3_2)
+        outputs = [branch1x1, branch1x1_2, branch3x3, branch3x3_3]
+        return torch.cat(outputs, 1)
+
+
+class CRelu(nn.Module):
+
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CRelu, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=1e-5)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = torch.cat([x, -x], 1)
+        x = F.relu(x, inplace=True)
+        return x
+
+
+@BACKBONES.register_module
+class FaceBoxes(nn.Module):
+
+    def __init__(self, input_size):
+        super(FaceBoxes, self).__init__()        
+        assert input_size in (300, 512, 1024,1333)
+        self.input_size = input_size
+        self.conv1 = CRelu(3, 24, kernel_size=7, stride=4, padding=3)
+        self.conv2 = CRelu(48, 64, kernel_size=5, stride=2, padding=2)
+        self.inception1 = Inception()
+        self.inception2 = Inception()
+        self.inception3 = Inception()
+        self.conv3_1 = BasicConv2d(128, 128, kernel_size=1, stride=1, padding=0)
+        self.conv3_2 = BasicConv2d(128, 256, kernel_size=3, stride=2, padding=1)
+        self.conv4_1 = BasicConv2d(256, 128, kernel_size=1, stride=1, padding=0)
+        self.conv4_2 = BasicConv2d(128, 256, kernel_size=3, stride=2, padding=1)
+
+    def init_weights(self, pretrained=None):
+        if isinstance(pretrained, str):
+            logger = logging.getLogger()
+            load_checkpoint(self, pretrained, strict=False, logger=logger)
+        elif pretrained is None:
+            for m in self.modules():
+                if isinstance(m, nn.Conv2d):
+                    if m.bias is not None:
+                        nn.init.xavier_normal_(m.weight.data)
+                        m.bias.data.fill_(0.02)
+                    else:
+                        m.weight.data.normal_(0, 0.01)
+                elif isinstance(m, nn.BatchNorm2d):
+                    m.weight.data.fill_(1)
+                    m.bias.data.zero_()
+        else:
+            raise TypeError('pretrained must be a str or None')             
+
+
+    def forward(self, x):
+
+        output = list()
+        x = self.conv1(x)
+        #stride should be 2
+        #change....
+        x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        x = self.conv2(x)
+        #stride should be 2
+        x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        x = self.inception1(x)
+        x = self.inception2(x)
+        x = self.inception3(x)
+        output.append(x)    
+        x = self.conv3_1(x)
+        x = self.conv3_2(x)
+        output.append(x)
+        x = self.conv4_1(x)
+        x = self.conv4_2(x)
+        output.append(x)
+        return tuple(output)
+
+
