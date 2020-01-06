2e00543ba34297855882482cc0d8b7da55141725
likun <likun10@baidu.com>
2020-01-06 12:16:23 +0800

    backbones_init

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,5 +2,7 @@ from .hrnet import HRNet
 from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
+from .faceboxesnet import FaceBoxes
+from .ultra_slim import UltraSlim
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet','FaceBoxes', 'UltraSlim']
