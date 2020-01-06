6505ed75df3d6dad17a3fcd8bb4affcac6818a74
likun <likun10@baidu.com>
2020-01-06 12:18:23 +0800

    faceboxes_v2_config

---
+++ configs/wider_face/faceboxes_v2.py
@@ -0,0 +1,136 @@
+# model settings
+input_size = 1024
+model = dict(
+    type='SingleStageDetector',
+    pretrained=None,
+    backbone=dict(
+        type='FaceBoxes',
+        input_size=input_size,
+        ),
+    neck=dict(
+        type='FPN',
+        in_channels=[128, 256, 256],
+        out_channels=64,
+        num_outs=3),
+    bbox_head=dict(
+        type='FaceboxesHead_DENS_V2',
+        input_size=input_size,
+        in_channels=(64, 64, 64),
+        num_classes=2,
+        target_means=(.0, .0, .0, .0),
+        target_stds=(0.1, 0.1, 0.2, 0.2)))
+# model training and testing settings
+cudnn_benchmark = True
+train_cfg = dict(
+    assigner=dict(
+        type='MaxIoUAssigner',
+        pos_iou_thr=0.35,
+        neg_iou_thr=0.35,
+        min_pos_iou=0.2,
+        ignore_iof_thr=-1,
+        gt_max_assign_all=False),
+    smoothl1_beta=1,
+    #### reduction='sum',
+    reg_loss_weight=2.,
+    allowed_border=-1,
+    #-1 to 3
+    pos_weight=-1,
+    neg_pos_ratio=7, 
+    debug=False)
+test_cfg = dict(
+    nms_pre=1000,
+    nms=dict(type='nms', iou_thr=0.35),
+    min_bbox_size=0,
+    score_thr=0.05,
+    max_per_img=100)
+# dataset settings
+dataset_type = 'WIDERFaceDataset'
+data_root = 'data/WIDERFace/'
+img_norm_cfg = dict(mean=[104, 117, 123], std=[1,1,1], to_rgb=True) 
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=False),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    imgs_per_gpu=32,
+    workers_per_gpu=8,
+    train=dict(
+        type='RepeatDataset',
+        times=1,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=data_root + 'train.txt',
+            img_prefix=data_root + 'WIDER_train/',
+            min_size=10,
+            pipeline=train_pipeline)),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'val.txt',
+        img_prefix=data_root + 'WIDER_val/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'val.txt',
+        img_prefix=data_root + 'WIDER_val/',
+        pipeline=test_pipeline))
+# optimizer
+optimizer = dict(type='SGD', lr=1e-3, momentum=0.9, weight_decay=5e-4)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=1.0 / 3,
+    step=[200, 250])
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=1,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+# runtime settings
+total_epochs = 300
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = './work_dirs/faceboxes_v2'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
