4438693395b69b0cbee174628aa8708bf4d635bd
Yvan Satyawan <y_satyawan@hotmail.com>
2020-01-08 15:21:00 +0100

    Refactoring broke something apparently

--- mmdet/datasets/pipelines/loading.py
+++ mmdet/datasets/pipelines/loading.py
@@ -69,7 +69,7 @@ class LoadAnnotations(object):
         return results
 
     def _load_labels(self, results):
-        results['gt_labels'] = results['ann_info']['labels_list']
+        results['gt_labels'] = results['ann_info']['labels']
         return results
 
     def _poly2mask(self, mask_ann, img_h, img_w):
