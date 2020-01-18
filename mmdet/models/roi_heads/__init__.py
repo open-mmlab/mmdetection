9b7d31a3b3f65320f29ea1fde294df73190cf553
ZwwWayne <wayne.zw@outlook.com>
2020-01-18 19:38:44 +0800

    reformat (roi_head): reformat __init__.py for isort=4.3.21

--- mmdet/models/roi_heads/__init__.py
+++ mmdet/models/roi_heads/__init__.py
@@ -1,9 +1,9 @@
 from .base_roi_head import BaseRoIHead
 from .cascade_roi_head import CascadeRoIHead
 from .double_roi_head import DoubleHeadRoIHead
-from .mask_scoring_roi_head import MaskScoringRoIHead
-from .htc_roi_head import HybridTaskCascadeRoIHead
 from .grid_roi_head import GridRoIHead
+from .htc_roi_head import HybridTaskCascadeRoIHead
+from .mask_scoring_roi_head import MaskScoringRoIHead
 
 __all__ = [
     'BaseRoIHead', 'CascadeRoIHead', 'DoubleHeadRoIHead', 'MaskScoringRoIHead',
