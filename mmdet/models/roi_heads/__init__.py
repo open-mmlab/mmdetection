e497def341aa8c8355aadf93c78d66cabfdbc4f4
beansi <wayne.zw@outlook.com>
2020-01-18 14:31:20 +0800

    fix(htc_head): add htc_head in __init__.py

--- mmdet/models/roi_heads/__init__.py
+++ mmdet/models/roi_heads/__init__.py
@@ -2,7 +2,9 @@ from .base_roi_head import BaseRoIHead
 from .cascade_roi_head import CascadeRoIHead
 from .double_roi_head import DoubleHeadRoIHead
 from .mask_scoring_roi_head import MaskScoringRoIHead
+from .htc_roi_head import HybridTaskCascadeRoIHead
 
 __all__ = [
-    'BaseRoIHead', 'CascadeRoIHead', 'DoubleHeadRoIHead', 'MaskScoringRoIHead'
+    'BaseRoIHead', 'CascadeRoIHead', 'DoubleHeadRoIHead', 'MaskScoringRoIHead',
+    'HybridTaskCascadeRoIHead'
 ]
