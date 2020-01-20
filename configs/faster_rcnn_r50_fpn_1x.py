3a61c312f89151a7a448f5a6366f84e1026c5938
cancam <bariscancam@gmail.com>
2020-01-20 12:02:25 +0300

    precommit check passed.

--- configs/faster_rcnn_r50_fpn_1x.py
+++ configs/faster_rcnn_r50_fpn_1x.py
@@ -161,7 +161,7 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        #dict(type='TensorboardLoggerHook')
+        # dict(type='TensorboardLoggerHook')
     ])
 # yapf:enable
 # runtime settings
