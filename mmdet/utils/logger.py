f7eb2a16a03f7dbf855daecc1f0116c5301ccfbd
Kai Chen <chenkaidev@gmail.com>
2020-01-31 22:56:36 +0800

    minor fix for the format of StreamHandler

--- mmdet/utils/logger.py
+++ mmdet/utils/logger.py
@@ -26,16 +26,14 @@ def get_root_logger(log_file=None, log_level=logging.INFO):
     if logger.hasHandlers():
         return logger
 
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=log_level)
+    format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=format_str, level=log_level)
     rank, _ = get_dist_info()
     if rank != 0:
         logger.setLevel('ERROR')
     elif log_file is not None:
         file_handler = logging.FileHandler(log_file, 'w')
-        file_handler.setFormatter(
-            logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        file_handler.setFormatter(logging.Formatter(format_str))
         file_handler.setLevel(log_level)
         logger.addHandler(file_handler)
 
