ba10e90fcce997002157e74af294af06e292956c
TaoBowoa180011 <48510115+TaoBowoa180011@users.noreply.github.com>
2020-02-08 12:33:21 +0800

    Update __init__.py

--- mmdet/models/detectors/__init__.py
+++ mmdet/models/detectors/__init__.py
@@ -14,10 +14,11 @@ from .retinanet import RetinaNet
 from .rpn import RPN
 from .single_stage import SingleStageDetector
 from .two_stage import TwoStageDetector
+from .reasoning_rcnn import ReasoningRCNN
 
 __all__ = [
     'BaseDetector', 'SingleStageDetector', 'TwoStageDetector', 'RPN',
     'FastRCNN', 'FasterRCNN', 'MaskRCNN', 'CascadeRCNN', 'HybridTaskCascade',
     'DoubleHeadRCNN', 'RetinaNet', 'FCOS', 'GridRCNN', 'MaskScoringRCNN',
-    'RepPointsDetector', 'FOVEA'
+    'RepPointsDetector', 'FOVEA','ReasoningRCNN'
 ]
