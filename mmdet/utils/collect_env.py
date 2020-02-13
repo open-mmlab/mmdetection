94d7f05803dc95b895015830cfcc2d829bc85bf5
Cao Yuhang <yhcao6@gmail.com>
2020-02-13 23:26:19 +0800

    fix import error

--- mmdet/utils/collect_env.py
+++ mmdet/utils/collect_env.py
@@ -9,7 +9,6 @@ import torch
 import torchvision
 
 import mmdet
-from mmdet.ops.utils import get_compiler_version, get_compiling_cuda_version
 
 
 def collect_env():
@@ -53,6 +52,7 @@ def collect_env():
 
     env_info['MMCV'] = mmcv.__version__
     env_info['MMDetection'] = mmdet.__version__
+    from mmdet.ops import get_compiler_version, get_compiling_cuda_version
     env_info['MMDetection Compiler'] = get_compiler_version()
     env_info['MMDetection CUDA Compiler'] = get_compiling_cuda_version()
     return env_info
