da37c9141baf467338e5ab416c28d263514f6a7a
Cao Yuhang <yhcao6@gmail.com>
2020-02-15 15:32:37 +0800

    Use official DDP to implement MMDDP (#2090)
    * Use official DDP to implement MMDDP
    
    * update mmcv requirement
    
    * Update the version of MMCV
    
    Co-authored-by: Kai Chen <chenkaidev@gmail.com>

--- mmdet/apis/train.py
+++ mmdet/apis/train.py
@@ -209,7 +209,10 @@ def _dist_train(model,
             seed=cfg.seed) for ds in dataset
     ]
     # put model on gpus
-    model = MMDistributedDataParallel(model.cuda())
+    model = MMDistributedDataParallel(
+        model.cuda(),
+        device_ids=[torch.cuda.current_device()],
+        broadcast_buffers=False)
 
     # build runner
     optimizer = build_optimizer(model, cfg.optimizer)
