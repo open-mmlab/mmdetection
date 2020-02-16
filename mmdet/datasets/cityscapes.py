258a219e1ab7647139f1111486d83789dfc98253
Jiarui XU <xvjiarui0826@gmail.com>
2020-02-16 20:08:57 +0800

    add cityscapes convert script

--- mmdet/datasets/cityscapes.py
+++ mmdet/datasets/cityscapes.py
@@ -2,7 +2,6 @@
 # and https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/evaluation/evalInstanceLevelSemanticLabeling.py # noqa
 
 import glob
-import multiprocessing as mp
 import os
 import os.path as osp
 import tempfile
@@ -12,8 +11,6 @@ import cityscapesscripts.helpers.labels as CSLabels
 import mmcv
 import numpy as np
 import pycocotools.mask as maskUtils
-from mmcv.runner import get_dist_info
-from pycocotools.coco import COCO
 
 from mmdet.utils import print_log
 from .coco import CocoDataset
@@ -23,140 +20,12 @@ from .registry import DATASETS
 @DATASETS.register_module
 class CityscapesDataset(CocoDataset):
     """
-    The correct CLASSES order should be
+    The correct CLASSES order in official cityscapes should be
     CLASSES = ('person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
                 'bicycle')
     """
     CLASSES = ('person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
                'bicycle')
-
-    def pseudo_coco(self, gt_dir):
-        images = self._collect_files(gt_dir)
-
-        coco = COCO()
-        dataset = dict()
-        annotations = list()
-        categories = list()
-
-        img_id = 0
-        anno_id = 0
-        cat_id = 0
-
-        # cat_id is not the same as id in CSLabels.labels
-        name2cat_id = dict()
-        for label in CSLabels.labels:
-            if label.hasInstances and not label.ignoreInEval:
-                cat = dict(id=cat_id, name=label.name)
-                name2cat_id[cat['name']] = cat['id']
-                categories.append(cat)
-                cat_id += 1
-
-        for img_info in images:
-            img_info['id'] = img_id
-            anno_info = img_info.pop('anno_info')
-            for anno in anno_info:
-                anno['id'] = anno_id
-                anno['image_id'] = img_info['id']
-                category_name = anno.pop('category_name')
-                anno['category_id'] = name2cat_id[category_name]
-                annotations.append(anno)
-                anno_id += 1
-            img_id += 1
-
-        dataset['images'] = images
-        dataset['annotations'] = annotations
-        dataset['categories'] = categories
-        coco.dataset = dataset
-        coco.createIndex()
-
-        return coco
-
-    def _collect_files(self, gt_dir):
-        suffix = 'leftImg8bit.png'
-        files = []
-        for img_file in glob.glob(osp.join(self.img_prefix, '**/*.png')):
-            assert img_file.endswith(suffix), img_file
-            inst_file = gt_dir + img_file[
-                len(self.img_prefix):-len(suffix)] + 'gtFine_instanceIds.png'
-            # Note that labelIds are not converted to trainId for seg map
-            segm_file = gt_dir + img_file[
-                len(self.img_prefix):-len(suffix)] + 'gtFine_labelIds.png'
-            files.append((img_file, inst_file, segm_file))
-        assert len(files), 'No images found in {}'.format(self.img_prefix)
-
-        print_log('Loading annotation images')
-        with mmcv.Timer(print_tmpl='It took {}s to load annotation.'):
-            pool = mp.Pool(
-                processes=max(mp.cpu_count() // get_dist_info()[1] // 2, 4))
-            images = pool.map(self._load_img_info, files)
-        print_log('Loaded {} images from {}'.format(
-            len(images), self.img_prefix))
-
-        return images
-
-    def _load_img_info(self, files):
-        img_file, inst_file, segm_file = files
-        inst_img = mmcv.imread(inst_file, 'unchanged')
-        # ids < 24 are stuff labels (filtering them first is about 5% faster)
-        unique_inst_ids = np.unique(inst_img[inst_img >= 24])
-        anno_info = []
-        for inst_id in unique_inst_ids:
-            # For non-crowd annotations, inst_id // 1000 is the label_id
-            # Crowd annotations have <1000 instance ids
-            label_id = inst_id // 1000 if inst_id >= 1000 else inst_id
-            label = CSLabels.id2label[label_id]
-            if not label.hasInstances or label.ignoreInEval:
-                continue
-
-            iscrowd = inst_id < 1000
-            category_name = label.name
-            mask = np.asarray(inst_img == inst_id, dtype=np.uint8, order='F')
-            # encode mask to RLE to save memory
-            mask_rle = maskUtils.encode(mask[:, :, None])[0]
-            area = maskUtils.area(mask_rle)
-            inds = np.nonzero(mask)
-            ymin, ymax = inds[0].min(), inds[0].max()
-            xmin, xmax = inds[1].min(), inds[1].max()
-            # convert to COCO style XYWH format
-            bbox = [xmin, ymin, xmax + 1 - xmin, ymax + 1 - ymin]
-            if xmax <= xmin or ymax <= ymin:
-                continue
-
-            anno = dict(
-                iscrowd=iscrowd,
-                category_name=category_name,
-                bbox=bbox,
-                area=area,
-                segmentation=mask_rle)
-            anno_info.append(anno)
-        img_info = dict(
-            # remove img_prefix for filename
-            filename=img_file.replace(self.img_prefix, ''),
-            height=inst_img.shape[0],
-            width=inst_img.shape[1],
-            anno_info=anno_info,
-            segm_file=segm_file)
-        return img_info
-
-    def load_annotations(self, ann_file):
-        self.coco = self.pseudo_coco(ann_file)
-        self.cat_ids = self.coco.getCatIds()
-        self.cat2label = {
-            cat_id: i + 1
-            for i, cat_id in enumerate(self.cat_ids)
-        }
-        self.img_ids = self.coco.getImgIds()
-        img_infos = []
-        for i in self.img_ids:
-            info = self.coco.loadImgs([i])[0]
-            img_infos.append(info)
-        return img_infos
-
-    def get_ann_info(self, idx):
-        img_id = self.img_infos[idx]['id']
-        ann_ids = self.coco.getAnnIds(imgIds=[img_id])
-        ann_info = self.coco.loadAnns(ann_ids)
-        return self._parse_ann_info(self.img_infos[idx], ann_info)
 
     def _filter_imgs(self, min_size=32):
         """Filter images too small or without ground truths."""
@@ -203,6 +72,7 @@ class CityscapesDataset(CocoDataset):
             else:
                 gt_bboxes.append(bbox)
                 gt_labels.append(self.cat2label[ann['category_id']])
+                ann['segmentation']['counts'] = ann['segmentation']['counts']
                 gt_masks_ann.append(ann['segmentation'])
 
         if gt_bboxes:
