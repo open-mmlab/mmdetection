89d014f9753c9e4cc1891d749282e7038d5c5c04
Jiarui XU <xvjiarui0826@gmail.com>
2020-02-23 00:47:32 +0800

    add optimizer registry

---
+++ mmdet/optimizer/__init__.py
@@ -0,0 +1,4 @@
+from .mmsgd import MMSGD
+from .registry import OPTIMIZERS
+
+__all__ = ['OPTIMIZERS', 'MMSGD']
---
+++ mmdet/optimizer/mmsgd.py
@@ -0,0 +1,84 @@
+import torch
+from torch.optim.optimizer import Optimizer, required
+
+from .registry import OPTIMIZERS
+
+
+@OPTIMIZERS.register_module
+class MMSGD(Optimizer):
+    """A clone of torch.optim.SGD.
+
+    This optimizer serves as an example how to customize an optimizer.
+    Here we directly copy paste torch.optim.SGD for demonstration.
+    """
+
+    def __init__(self,
+                 params,
+                 lr=required,
+                 momentum=0,
+                 dampening=0,
+                 weight_decay=0,
+                 nesterov=False):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError(
+                "Invalid weight_decay value: {}".format(weight_decay))
+
+        defaults = dict(
+            lr=lr,
+            momentum=momentum,
+            dampening=dampening,
+            weight_decay=weight_decay,
+            nesterov=nesterov)
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError(
+                "Nesterov momentum requires a momentum and zero dampening")
+        super(MMSGD, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(MMSGD, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('nesterov', False)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            dampening = group['dampening']
+            nesterov = group['nesterov']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                if weight_decay != 0:
+                    d_p.add_(weight_decay, p.data)
+                if momentum != 0:
+                    param_state = self.state[p]
+                    if 'momentum_buffer' not in param_state:
+                        buf = param_state['momentum_buffer'] = torch.clone(
+                            d_p).detach()
+                    else:
+                        buf = param_state['momentum_buffer']
+                        buf.mul_(momentum).add_(1 - dampening, d_p)
+                    if nesterov:
+                        d_p = d_p.add(momentum, buf)
+                    else:
+                        d_p = buf
+
+                p.data.add_(-group['lr'], d_p)
+
+        return loss
