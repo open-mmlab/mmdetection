34e0ee41411d1f3131633bfac5f6aca8cc4fe1e3
rydenisbak <rydenisbak@mail.ru>
2020-02-25 20:30:08 +0300

    no weight decay in depthwise convolution (#2107)
    * no weight decay in depthwise convolution
    
    * fix name of var
    
    * fix train.py:196:80 (99 > 79 characters)
    
    * fix trailing whitespace
    
    * no_wd_in_dw is independ setting now
    
    * add Hellock's suggestions
    
    Co-authored-by: Kai Chen <chenkaidev@gmail.com>

--- mmdet/core/optimizer/builder.py
+++ mmdet/core/optimizer/builder.py
@@ -1,5 +1,7 @@
 import re
 
+import torch
+
 from mmdet.utils import build_from_cfg
 from .registry import OPTIMIZERS
 
@@ -16,13 +18,16 @@ def build_optimizer(model, optimizer_cfg):
             Optional fields are:
                 - any arguments of the corresponding optimizer type, e.g.,
                   weight_decay, momentum, etc.
-                - paramwise_options: a dict with 3 accepted fileds
-                  (bias_lr_mult, bias_decay_mult, norm_decay_mult).
+                - paramwise_options: a dict with 4 accepted fileds
+                  (bias_lr_mult, bias_decay_mult, norm_decay_mult,
+                  dwconv_decay_mult).
                   `bias_lr_mult` and `bias_decay_mult` will be multiplied to
                   the lr and weight decay respectively for all bias parameters
                   (except for the normalization layers), and
                   `norm_decay_mult` will be multiplied to the weight decay
                   for all weight and bias parameters of normalization layers.
+                  `dwconv_decay_mult` will be multiplied to the weight decay
+                  for all weight and bias parameters of depthwise conv layers.
 
     Returns:
         torch.optim.Optimizer: The initialized optimizer.
@@ -49,12 +54,15 @@ def build_optimizer(model, optimizer_cfg):
         base_wd = optimizer_cfg.get('weight_decay', None)
         # weight_decay must be explicitly specified if mult is specified
         if ('bias_decay_mult' in paramwise_options
-                or 'norm_decay_mult' in paramwise_options):
+                or 'norm_decay_mult' in paramwise_options
+                or 'dwconv_decay_mult' in paramwise_options):
             assert base_wd is not None
         # get param-wise options
         bias_lr_mult = paramwise_options.get('bias_lr_mult', 1.)
         bias_decay_mult = paramwise_options.get('bias_decay_mult', 1.)
         norm_decay_mult = paramwise_options.get('norm_decay_mult', 1.)
+        dwconv_decay_mult = paramwise_options.get('dwconv_decay_mult', 1.)
+        named_modules = dict(model.named_modules())
         # set param-wise lr and weight decay
         params = []
         for name, param in model.named_parameters():
@@ -76,6 +84,14 @@ def build_optimizer(model, optimizer_cfg):
                 param_group['lr'] = base_lr * bias_lr_mult
                 if base_wd is not None:
                     param_group['weight_decay'] = base_wd * bias_decay_mult
+
+            module_name = name.replace('.weight', '').replace('.bias', '')
+            if module_name in named_modules and base_wd is not None:
+                module = named_modules[module_name]
+                # if this Conv2d is depthwise Conv2d
+                if isinstance(module, torch.nn.Conv2d) and \
+                        module.in_channels == module.groups:
+                    param_group['weight_decay'] = base_wd * dwconv_decay_mult
             # otherwise use the global settings
 
             params.append(param_group)
