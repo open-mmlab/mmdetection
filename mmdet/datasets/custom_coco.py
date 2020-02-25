76795d4d1f1bb93ac8987e917bd0dd07c0e91acd
Ilya Krylov <ilya.krylov@intel.com>
2020-02-25 09:39:40 +0300

    added support of min_size in CustomCocoDataset

--- mmdet/datasets/custom_coco.py
+++ mmdet/datasets/custom_coco.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from .coco import CocoDataset
 from .registry import DATASETS
 
@@ -5,7 +7,66 @@ from .registry import DATASETS
 @DATASETS.register_module
 class CustomCocoDataset(CocoDataset):
 
-    def __init__(self, classes, *args, **kwargs):
+    def __init__(self, classes, min_size=None, *args, **kwargs):
         self.CLASSES = classes
         super().__init__(*args, **kwargs)
+
+        self.min_size = min_size
+
+    def _parse_ann_info(self, img_info, ann_info):
+        """Parse bbox and mask annotation.
+
+        Args:
+            ann_info (list[dict]): Annotation info of an image.
+            with_mask (bool): Whether to parse mask annotations.
+
+        Returns:
+            dict: A dict containing the following keys: bboxes, bboxes_ignore,
+                labels, masks, seg_map. "masks" are raw annotations and not
+                decoded into binary masks.
+        """
+        gt_bboxes = []
+        gt_labels = []
+        gt_bboxes_ignore = []
+        gt_masks_ann = []
+
+        for i, ann in enumerate(ann_info):
+            if ann.get('ignore', False):
+                continue
+            x1, y1, w, h = ann['bbox']
+            if ann['area'] <= 0 or w < 1 or h < 1:
+                continue
+            if self.min_size is not None:
+                if w < self.min_size or h < self.min_size:
+                    continue
+            bbox = [x1, y1, x1 + w - 1, y1 + h - 1]
+            if ann.get('iscrowd', False):
+                gt_bboxes_ignore.append(bbox)
+            else:
+                gt_bboxes.append(bbox)
+                gt_labels.append(self.cat2label[ann['category_id']])
+                gt_masks_ann.append(ann['segmentation'])
+
+        if gt_bboxes:
+            gt_bboxes = np.array(gt_bboxes, dtype=np.float32)
+            gt_labels = np.array(gt_labels, dtype=np.int64)
+        else:
+            gt_bboxes = np.zeros((0, 4), dtype=np.float32)
+            gt_labels = np.array([], dtype=np.int64)
+
+        if gt_bboxes_ignore:
+            gt_bboxes_ignore = np.array(gt_bboxes_ignore, dtype=np.float32)
+        else:
+            gt_bboxes_ignore = np.zeros((0, 4), dtype=np.float32)
+
+        seg_map = img_info['filename'].replace('jpg', 'png')
+
+        ann = dict(
+            bboxes=gt_bboxes,
+            labels=gt_labels,
+            bboxes_ignore=gt_bboxes_ignore,
+            masks=gt_masks_ann,
+            seg_map=seg_map)
+
+        return ann
 
