532b0fefc8f1d29def869287520c83503fa121ba
rydenisbak <rydenisbak@mail.ru>
2020-02-27 17:30:47 +0300

    fix trailing whitespace

--- mmdet/core/fp16/hooks.py
+++ mmdet/core/fp16/hooks.py
@@ -97,7 +97,7 @@ def wrap_fp16_model(model):
 def patch_norm_fp32(module):
     if isinstance(module, (nn.modules.batchnorm._BatchNorm, nn.GroupNorm)):
         module.float()
-        torch_version = [int(v) for v in torch.__version__.split('.')]        
+        torch_version = [int(v) for v in torch.__version__.split('.')]
         if isinstance(module, nn.GroupNorm) or torch_version < [1, 3]:
             module.forward = patch_forward_method(module.forward, torch.half,
                                                   torch.float)
