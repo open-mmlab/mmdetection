a183c2d3879ae99d6adef07aea5e01ce6e88f8e7
rydenisbak <rydenisbak@mail.ru>
2020-02-27 09:52:20 +0300

    speed up fp16 for torch >= 1.3.0

--- mmdet/core/fp16/hooks.py
+++ mmdet/core/fp16/hooks.py
@@ -97,10 +97,8 @@ def wrap_fp16_model(model):
 def patch_norm_fp32(module):
     if isinstance(module, (nn.modules.batchnorm._BatchNorm, nn.GroupNorm)):
         module.float()
-        path_forward_condition = isinstance(module, nn.GroupNorm)
-        major, minor = [int(v) for v in torch.__version__.split('.')][:2]
-        path_forward_condition |= major < 1 or major == 1 and minor < 3
-        if path_forward_condition:
+        torch_version = [int(v) for v in torch.__version__.split('.')]        
+        if isinstance(module, nn.GroupNorm) or torch_version < [1, 3]:
             module.forward = patch_forward_method(module.forward, torch.half,
                                                   torch.float)
     for child in module.children():
