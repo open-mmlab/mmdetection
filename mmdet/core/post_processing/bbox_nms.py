fdc52354fd0c21a790d1fc0a9e7e5ec0a629234d
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2020-03-05 14:13:46 +0300

    fix NMS export

--- mmdet/core/post_processing/bbox_nms.py
+++ mmdet/core/post_processing/bbox_nms.py
@@ -104,6 +104,11 @@ class GenericMulticlassNMS(Function):
         def cast(x, dtype):
             return g.op('Cast', x, to_i=sym_help.cast_pytorch_to_onnx[dtype])
 
+        def get_size(x, dim):
+            shape = g.op('Shape', x)
+            dim = _slice(g, shape, axes=[0], starts=[dim], ends=[dim + 1])
+            return cast(dim, 'Long')
+
         nms_op_type = nms_cfg.get('type', 'nms')
         assert nms_op_type == 'nms'
         assert 'iou_thr' in nms_cfg
@@ -111,14 +116,19 @@ class GenericMulticlassNMS(Function):
         assert 0 <= iou_threshold <= 1
 
         # Transpose and reshape input tensors to fit ONNX NonMaxSuppression.
-        multi_bboxes = g.op(
-            'Transpose',
-            reshape(g, multi_bboxes, [0, -1, 4]),
-            perm_i=[1, 0, 2])
-        multi_scores = g.op(
-            'Unsqueeze',
-            g.op('Transpose', multi_scores, perm_i=[1, 0]),
-            axes_i=[1])
+        multi_bboxes = reshape(g, multi_bboxes, [0, -1, 4])
+        multi_bboxes = g.op('Transpose', multi_bboxes, perm_i=[1, 0, 2])
+
+        batches_num = get_size(multi_bboxes, 0)
+        spatial_num = get_size(multi_bboxes, 1)
+
+        multi_scores = g.op('Transpose', multi_scores, perm_i=[1, 0])
+        scores_shape = g.op('Concat',
+                            batches_num,
+                            g.op('Constant', value_t=torch.LongTensor([-1])),
+                            spatial_num, axis_i=0)
+        multi_scores = reshape(g, multi_scores, scores_shape)
+        classes_num = get_size(multi_scores, 1)
 
         assert max_num > 0
 
@@ -130,30 +140,34 @@ class GenericMulticlassNMS(Function):
 
         # Flatten bboxes and scores.
         multi_bboxes_flat = reshape(g, multi_bboxes, [-1, 4])
-        multi_scores_flat = reshape(g, multi_scores, [
-            -1,
-        ])
+        multi_scores_flat = reshape(g, multi_scores, [-1, ])
 
         # Flatten indices.
-        class_indices = cast(
-            _slice(g, indices, axes=[1], starts=[0], ends=[1]), 'Long')
-        box_indices = cast(
-            _slice(g, indices, axes=[1], starts=[2], ends=[3]), 'Long')
+        batch_indices = cast(_slice(g, indices, axes=[1], starts=[0], ends=[1]), 'Long')
+        class_indices = cast(_slice(g, indices, axes=[1], starts=[1], ends=[2]), 'Long')
+        box_indices = cast(_slice(g, indices, axes=[1], starts=[2], ends=[3]), 'Long')
 
-        num_boxes = cast(
-            _slice(
-                g, g.op('Shape', multi_bboxes), axes=[0], starts=[1],
-                ends=[2]), 'Long')
-        flat_indices = cast(
-            g.op('Add', cast(g.op('Mul', class_indices, num_boxes), 'Long'),
-                 box_indices), 'Long')
+        def add(*args, dtype='Long'):
+            x = g.op('Add', args[0], args[1])
+            if dtype is not None:
+                x = cast(x, dtype)
+            return x
+
+        def mul(*args, dtype='Long'):
+            x = g.op('Mul', args[0], args[1])
+            if dtype is not None:
+                x = cast(x, dtype)
+            return x
+
+        flat_box_indices = add(mul(batch_indices, spatial_num), box_indices)
+        flat_score_indices = add(mul(add(mul(batch_indices, classes_num), class_indices), spatial_num), box_indices)
 
         # Select bboxes.
         out_bboxes = reshape(
-            g, g.op('Gather', multi_bboxes_flat, flat_indices, axis_i=0),
+            g, g.op('Gather', multi_bboxes_flat, flat_box_indices, axis_i=0),
             [-1, 4])
         out_scores = reshape(
-            g, g.op('Gather', multi_scores_flat, flat_indices, axis_i=0),
+            g, g.op('Gather', multi_scores_flat, flat_score_indices, axis_i=0),
             [-1, 1])
         class_indices = reshape(g, cast(class_indices, 'Float'), [-1, 1])
 
