cfa8e55cb24b433b4d253a131083c307cf3baf1a
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-03-11 14:52:30 +0800

    Config fix (#2233)
    * add norm_cfg in ResNet pytorch backbone
    
    * delete redundent fields

--- configs/reppoints/reppoints_moment_x101_dcn_fpn_2x.py
+++ configs/reppoints/reppoints_moment_x101_dcn_fpn_2x.py
@@ -12,6 +12,7 @@ model = dict(
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
         style='pytorch',
         dcn=dict(
             modulated=False,
@@ -146,5 +147,4 @@ log_level = 'INFO'
 work_dir = './work_dirs/reppoints_moment_x101_dcn_fpn_2x'
 load_from = None
 resume_from = None
-auto_resume = True
 workflow = [('train', 1)]
