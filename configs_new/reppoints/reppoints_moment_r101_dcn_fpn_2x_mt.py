de245e389d4c37bd863bcff0f22d6c5160b5baf5
Jiarui XU <xvjiarui0826@gmail.com>
2020-03-12 18:23:56 +0800

    merge act_cfg, dcn from master

--- configs_new/reppoints/reppoints_moment_r101_dcn_fpn_2x_mt.py
+++ configs_new/reppoints/reppoints_moment_r101_dcn_fpn_2x_mt.py
@@ -3,7 +3,6 @@ model = dict(
     pretrained='torchvision://resnet101',
     backbone=dict(
         depth=101,
-        dcn=dict(
-            modulated=False, deformable_groups=1, fallback_on_stride=False),
+        dcn=dict(type='DCN', deformable_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True)))
 work_dir = './work_dirs/reppoints_moment_r101_dcn_fpn_2x_mt'
