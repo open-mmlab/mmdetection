3f64de8d6ed0f5df21502e7030509dcce6cfb85c
Yossi Biton <yossi.biton@alibaba-inc.com>
2020-03-14 16:04:17 +0200

    followed @xvjiarui suggestions for the pull request

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -1,3 +1,3 @@
-from .version import __version__, short_version
+from .version import CPU_ONLY, __version__, short_version
 
-__all__ = ['__version__', 'short_version']
+__all__ = ['__version__', 'short_version', 'CPU_ONLY']
