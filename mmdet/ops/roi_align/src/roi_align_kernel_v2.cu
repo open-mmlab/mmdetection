6edd6eee2f0461a0f7f510dd10a1d8dfcfe00743
Ye Liu <goolhanrry@gmail.com>
2020-03-15 22:16:10 +0800

    Fix type errors in roi_align_v2 (#2263)

--- mmdet/ops/roi_align/src/roi_align_kernel_v2.cu
+++ mmdet/ops/roi_align/src/roi_align_kernel_v2.cu
@@ -287,7 +287,7 @@ at::Tensor ROIAlignForwardV2Laucher(const at::Tensor& input,
   auto output_size = num_rois * pooled_height * pooled_width * channels;
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  dim3 grid(std::min(at::cuda::ATenCeilDiv(output_size, 512L), 4096L));
+  dim3 grid(std::min(at::cuda::ATenCeilDiv(static_cast<int64_t>(output_size), static_cast<int64_t>(512)), static_cast<int64_t>(4096)));
   dim3 block(512);
 
   if (output.numel() == 0) {
@@ -327,7 +327,7 @@ at::Tensor ROIAlignBackwardV2Laucher(
 
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  dim3 grid(std::min(at::cuda::ATenCeilDiv(grad.numel(), 512L), 4096L));
+  dim3 grid(std::min(at::cuda::ATenCeilDiv(static_cast<int64_t>(grad.numel()), static_cast<int64_t>(512)), static_cast<int64_t>(4096)));
   dim3 block(512);
 
   // handle possibly empty gradients
