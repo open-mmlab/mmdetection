8a91d4d278d1329dfb351f20811247e0336074d4
Jiarui XU <xvjiarui0826@gmail.com>
2020-03-25 21:32:03 +0800

    add norm_eval

--- configs/htc/htc_x101_32x4d_fpn_16x1_20e_coco.py
+++ configs/htc/htc_x101_32x4d_fpn_16x1_20e_coco.py
@@ -10,6 +10,7 @@ model = dict(
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
         style='pytorch'))
 data = dict(imgs_per_gpu=1, workers_per_gpu=1)
 # learning policy
--- configs/htc/htc_x101_64x4d_fpn_dconv_c3-c5_mstrain_400_1400_16x1_20e_coco.py
+++ configs/htc/htc_x101_64x4d_fpn_dconv_c3-c5_mstrain_400_1400_16x1_20e_coco.py
@@ -10,6 +10,7 @@ model = dict(
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
         style='pytorch',
         dcn=dict(type='DCN', deformable_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True)))
