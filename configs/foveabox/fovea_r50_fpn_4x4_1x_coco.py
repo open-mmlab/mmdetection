62f35e6ec717409e796f6fdef96b296344c2ec9c
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-03-26 16:37:25 +0800

    Add norm eval in configs (#2319)
    * add norm_eval
    
    * upgrade mmcv

--- configs/foveabox/fovea_r50_fpn_4x4_1x_coco.py
+++ configs/foveabox/fovea_r50_fpn_4x4_1x_coco.py
@@ -13,6 +13,7 @@ model = dict(
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
         style='pytorch'),
     neck=dict(
         type='FPN',
