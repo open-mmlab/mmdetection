9877dab4713c2e5421c1acd178618a0f8b5f88d2
Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>
2020-03-29 18:27:08 +0800

    Fix (flip): not setting default values of flip during loading (#2344)
    * Fix (flip): not setting default values of flip during loading
    
    * Fix (loading): correct the format of default img_norm_cfg

--- mmdet/datasets/pipelines/loading.py
+++ mmdet/datasets/pipelines/loading.py
@@ -29,11 +29,12 @@ class LoadImageFromFile(object):
         results['ori_shape'] = img.shape
         # Set initial values for default meta_keys
         results['pad_shape'] = img.shape
-        results['flip'] = False
         results['scale_factor'] = 1.0
         num_channels = 1 if len(img.shape) < 3 else img.shape[2]
-        results['img_norm_cfg'] = [[0.0] * num_channels, [1.0] * num_channels,
-                                   False]
+        results['img_norm_cfg'] = dict(
+            mean=np.zeros(num_channels, dtype=np.float32),
+            std=np.ones(num_channels, dtype=np.float32),
+            to_rgb=False)
         return results
 
     def __repr__(self):
