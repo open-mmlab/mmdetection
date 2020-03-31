c62aba3460cfbf00db6f7c1a1151277a06c1abe9
marload <rladhkstn8@gmail.com>
2020-03-31 16:56:40 +0900

    Modify the code casting digit string as int to convert to float and then to int

--- mmdet/datasets/xml_style.py
+++ mmdet/datasets/xml_style.py
@@ -48,10 +48,10 @@ class XMLDataset(CustomDataset):
             difficult = int(obj.find('difficult').text)
             bnd_box = obj.find('bndbox')
             bbox = [
-                int(bnd_box.find('xmin').text),
-                int(bnd_box.find('ymin').text),
-                int(bnd_box.find('xmax').text),
-                int(bnd_box.find('ymax').text)
+                int(float(bnd_box.find('xmin').text)),
+                int(float(bnd_box.find('ymin').text)),
+                int(float(bnd_box.find('xmax').text)),
+                int(float(bnd_box.find('ymax').text))
             ]
             ignore = False
             if self.min_size:
