43939707172c82b0d0e1c3c2eb24317bdfe384e7
marload <rladhkstn8@gmail.com>
2020-04-01 17:06:07 +0900

    Fix: Indentation Error

--- mmdet/datasets/xml_style.py
+++ mmdet/datasets/xml_style.py
@@ -47,8 +47,7 @@ class XMLDataset(CustomDataset):
             label = self.cat2label[name]
             difficult = int(obj.find('difficult').text)
             bnd_box = obj.find('bndbox')
-	    
-	    # Coordinates may be float type
+            # Coordinates may be float type
             bbox = [
                 int(float(bnd_box.find('xmin').text)),
                 int(float(bnd_box.find('ymin').text)),
