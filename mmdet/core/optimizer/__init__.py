1f5b360af97a61b80ebbd008fee1808600b14c18
Kai Chen <chenkaidev@gmail.com>
2020-04-05 00:51:38 +0800

    add optimizer constructor

--- mmdet/core/optimizer/__init__.py
+++ mmdet/core/optimizer/__init__.py
@@ -1,5 +1,9 @@
-from .builder import build_optimizer
+from .builder import build_optimizer, build_optimizer_constructor
 from .copy_of_sgd import CopyOfSGD
-from .registry import OPTIMIZERS
+from .default_constructor import DefaultOptimizerConstructor
+from .registry import OPTIMIZER_BUILDERS, OPTIMIZERS
 
-__all__ = ['OPTIMIZERS', 'build_optimizer', 'CopyOfSGD']
+__all__ = [
+    'OPTIMIZER_BUILDERS', 'OPTIMIZERS', 'DefaultOptimizerConstructor',
+    'build_optimizer', 'build_optimizer_constructor', 'CopyOfSGD'
+]
