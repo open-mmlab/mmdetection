86b7222a479f97e19adf5afd1566ca4d68be3016
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2020-04-07 18:21:29 +0300

    add RIOFeatureExtractor stub

--- tools/export.py
+++ tools/export.py
@@ -5,12 +5,14 @@ from copy import copy
 import numpy as np
 import onnx
 import torch
+import torch.onnx.symbolic_helper as sym_help
 from mmcv.parallel import collate, scatter
 
 from mmdet.apis import init_detector
 from mmdet.apis.inference import LoadImage
 from mmdet.datasets.pipelines import Compose
 from mmdet.models.anchor_heads.anchor_head import AnchorHead
+from mmdet.models.roi_extractors import SingleRoIExtractor
 from mmdet.utils.deployment import register_extra_symbolics, TracerStub
 
 
@@ -117,6 +119,63 @@ def stub_anchor_generator(anchor_head):
             anchor_generators[i].grid_anchors.params['base_anchors'] = anchor_generators[
                 i].base_anchors.cpu().numpy()
 
+
+class ROIFeatureExtractorStub(TracerStub):
+
+    class Wrapper(torch.nn.Module):
+        def __init__(self, inner):
+            super().__init__()
+            self.inner = inner
+
+        def __call__(self, rois, *feats, roi_scale_factor=None):
+            return self.inner(feats, rois, roi_scale_factor=roi_scale_factor)
+
+        def __getattr__(self, item):
+            if item == 'inner':
+                return super().__getattr__(item)
+            # Forward calls to self.inner.
+            inner = self.inner
+            try:
+                v = inner.__getattribute__(item)
+            except AttributeError:
+                v = inner.__getattr__(item)
+            return v
+
+    def __init__(self, inner, namespace='mmdet_custom',
+                 name='roi_feature_extractor', **kwargs):
+        super().__init__(self.Wrapper(inner), namespace=namespace, name=name, **kwargs)
+
+    def forward(self, feats, rois, roi_scale_factor=None):
+        # Force `roi_scale_factor` to be passed in as kwarg.
+        if roi_scale_factor is None:
+            return super().forward(rois, *feats)
+        else:
+            return super().forward(rois, *feats, roi_scale_factor=roi_scale_factor)
+
+    def symbolic(self, g, rois, *feats):
+        # roi_scale_factor = float(self.params.get('roi_scale_factor', 1.0))
+        rois = sym_help._slice_helper(g, rois, axes=[1], starts=[1], ends=[5])
+        roi_feats, _ = g.op('ExperimentalDetectronROIFeatureExtractor',
+            rois,
+            *feats,
+            output_size_i=self.inner.roi_layers[0].out_size[0],
+            pyramid_scales_i=self.inner.featmap_strides,
+            sampling_ratio_i=self.inner.roi_layers[0].sample_num,
+            image_id_i=0,
+            distribute_rois_between_levels_i=1,
+            preserve_rois_order_i=1,
+            outputs=2
+            )
+        return roi_feats
+
+
+def stub_roi_feature_extractor(extractor):
+    if extractor is not None and isinstance(extractor, SingleRoIExtractor):
+        return ROIFeatureExtractorStub(extractor)
+    else:
+        return extractor
+
+
 def main(args):
     model = init_detector(args.config, args.checkpoint, device='cpu')
     model.eval().cuda()
@@ -125,6 +184,7 @@ def main(args):
     if not args.no_stubs:
         stub_anchor_generator(getattr(model, 'rpn_head', None))
         stub_anchor_generator(getattr(model, 'bbox_head', None))
+        model.bbox_roi_extractor = stub_roi_feature_extractor(getattr(model, 'bbox_roi_extractor', None))
     
     image = np.zeros((128, 128, 3), dtype=np.uint8)
     with torch.no_grad():
