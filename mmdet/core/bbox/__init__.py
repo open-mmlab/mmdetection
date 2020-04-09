38d616541767530a0511760193bc6a05c2689471
Jiarui XU <xvjiarui0826@gmail.com>
2020-04-09 20:36:56 +0800

    restore __init__

--- mmdet/core/bbox/__init__.py
+++ mmdet/core/bbox/__init__.py
@@ -1,7 +1,9 @@
-from .assigners import AssignResult
+from .assigners import AssignResult, BaseAssigner, MaxIoUAssigner
 from .bbox_target import bbox_target
 from .geometry import bbox_overlaps
-from .samplers import SamplingResult
+from .samplers import (BaseSampler, CombinedSampler,
+                       InstanceBalancedPosSampler, IoUBalancedNegSampler,
+                       PseudoSampler, RandomSampler, SamplingResult)
 from .transforms import (bbox2delta, bbox2result, bbox2roi, bbox_flip,
                          bbox_mapping, bbox_mapping_back, delta2bbox,
                          distance2bbox, roi2bbox)
@@ -10,8 +12,11 @@ from .builder import (  # isort:skip, avoid recursive imports
     assign_and_sample, build_assigner, build_sampler)
 
 __all__ = [
-    'bbox_overlaps', 'AssignResult', 'SamplingResult', 'build_assigner',
-    'build_sampler', 'assign_and_sample', 'bbox2delta', 'delta2bbox',
-    'bbox_flip', 'bbox_mapping', 'bbox_mapping_back', 'bbox2roi', 'roi2bbox',
-    'bbox2result', 'distance2bbox', 'bbox_target'
+    'bbox_overlaps', 'BaseAssigner', 'MaxIoUAssigner', 'AssignResult',
+    'BaseSampler', 'PseudoSampler', 'RandomSampler',
+    'InstanceBalancedPosSampler', 'IoUBalancedNegSampler', 'CombinedSampler',
+    'SamplingResult', 'build_assigner', 'build_sampler', 'assign_and_sample',
+    'bbox2delta', 'delta2bbox', 'bbox_flip', 'bbox_mapping',
+    'bbox_mapping_back', 'bbox2roi', 'roi2bbox', 'bbox2result',
+    'distance2bbox', 'bbox_target'
 ]
