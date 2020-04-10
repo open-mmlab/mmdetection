43c30f055c396bf3d0d2ff9660906801bf4fdfa9
Cao Yuhang <yhcao6@gmail.com>
2020-04-11 01:35:56 +0800

    fix doc of fpn

--- mmdet/models/necks/pafpn.py
+++ mmdet/models/necks/pafpn.py
@@ -18,10 +18,22 @@ class PAFPN(FPN):
         in_channels (List[int]): Number of input channels per scale.
         out_channels (int): Number of output channels (used at each scale)
         num_outs (int): Number of output scales.
-        start_level (int): Index of the first input scale to use as an output
-            scale.
-        end_level (int, default=-1): Index of the last input scale to use as
-            an output scale.
+        start_level (int): Index of the start input backbone level used to
+            build the feature pyramid. Default: 0.
+        end_level (int): Index of the end input backbone level (exclusive) to
+            build the feature pyramid. Default: -1, which means the last level.
+        add_extra_convs (bool): Whether to add conv layers on top of the
+            original feature maps. Default: False.
+        extra_convs_on_inputs (bool): Whether to apply extra conv on
+            the original feature from the backbone. Default: False.
+        relu_before_extra_convs (bool): Whether to apply relu before the extra
+            conv. Default: False.
+        no_norm_on_lateral (bool): Whether to apply norm on lateral.
+            Default: False.
+        conv_cfg (dict): Config dict for convolution layer. Default: None.
+        norm_cfg (dict): Config dict for normalization layer. Default: None.
+        act_cfg (str): Config dict for activation layer in ConvModule.
+            Default: None.
     """
 
     def __init__(self,
@@ -87,39 +99,38 @@ class PAFPN(FPN):
 
         # build outputs
         # part 1: from original levels
-        outs = [
+        inter_outs = [
             self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)
         ]
 
-        # part 2: add pafpn
+        # part 2: add bottom-up path
         for i in range(0, used_backbone_levels - 1):
-            outs[i + 1] += self.lateral_deconvs[i](outs[i])
+            inter_outs[i + 1] += self.lateral_deconvs[i](inter_outs[i])
 
-        paouts = []
-        paouts.append(outs[0])
-        paouts.extend([
-            self.pafpn_convs[i - 1](outs[i])
+        outs = []
+        outs.append(inter_outs[0])
+        outs.extend([
+            self.pafpn_convs[i - 1](inter_outs[i])
             for i in range(1, used_backbone_levels)
         ])
 
         # part 3: add extra levels
-        if self.num_outs > len(paouts):
+        if self.num_outs > len(outs):
             # use max pool to get more levels on top of outputs
             # (e.g., Faster R-CNN, Mask R-CNN)
             if not self.add_extra_convs:
                 for i in range(self.num_outs - used_backbone_levels):
-                    paouts.append(F.max_pool2d(paouts[-1], 1, stride=2))
+                    outs.append(F.max_pool2d(outs[-1], 1, stride=2))
             # add conv layers on top of original feature maps (RetinaNet)
             else:
                 if self.extra_convs_on_inputs:
                     orig = inputs[self.backbone_end_level - 1]
-                    paouts.append(self.fpn_convs[used_backbone_levels](orig))
+                    outs.append(self.fpn_convs[used_backbone_levels](orig))
                 else:
-                    paouts.append(self.fpn_convs[used_backbone_levels](
-                        paouts[-1]))
+                    outs.append(self.fpn_convs[used_backbone_levels](outs[-1]))
                 for i in range(used_backbone_levels + 1, self.num_outs):
                     if self.relu_before_extra_convs:
-                        paouts.append(self.fpn_convs[i](F.relu(paouts[-1])))
+                        outs.append(self.fpn_convs[i](F.relu(outs[-1])))
                     else:
-                        paouts.append(self.fpn_convs[i](paouts[-1]))
-        return tuple(paouts)
+                        outs.append(self.fpn_convs[i](outs[-1]))
+        return tuple(outs)
