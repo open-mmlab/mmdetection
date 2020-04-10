d2cfdec07f49fdf074a9b0fed4b1a2b0f5fd1cd5
Cao Yuhang <yhcao6@gmail.com>
2020-04-11 00:41:03 +0800

    simplify unit tests

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -25,12 +25,14 @@ def test_conv_2d():
     for in_h, in_w, in_cha, out_cha, k, s, p, d in product(
             *list(test_cases.values())):
         # wrapper op with 0-dim input
-        x = torch.empty(0, in_cha, in_h, in_w)
+        x = torch.randn(0, in_cha, in_h, in_w)
+        torch.manual_seed(0)
         wrapper = Conv2d(in_cha, out_cha, k, stride=s, padding=p, dilation=d)
         wrapper_out = wrapper(x)
 
         # torch op with 3-dim input as shape reference
-        x = torch.empty(3, in_cha, in_h, in_w)
+        x = torch.randn(3, in_cha, in_h, in_w)
+        torch.manual_seed(0)
         ref = nn.Conv2d(in_cha, out_cha, k, stride=s, padding=p, dilation=d)
         ref_out = ref(x)
 
@@ -41,17 +43,10 @@ def test_conv_2d():
         assert wrapper.weight.grad is not None
         assert wrapper.weight.grad.shape == wrapper.weight.shape
 
-        x = torch.randn(3, in_cha, in_h, in_w)
-        torch.manual_seed(0)
-        wrapper = Conv2d(in_cha, out_cha, k, stride=s, padding=p, dilation=d)
-        wrapper_out = wrapper(x)
-        torch.manual_seed(0)
-        ref = nn.Conv2d(in_cha, out_cha, k, stride=s, padding=p, dilation=d)
-        ref_out = ref(x)
-        assert torch.equal(wrapper_out, ref_out)
+        assert torch.equal(wrapper(x), ref(x))
 
     # eval mode
-    x = torch.empty(0, in_cha, in_h, in_w)
+    x = torch.randn(0, in_cha, in_h, in_w)
     wrapper = Conv2d(in_cha, out_cha, k, stride=s, padding=p, dilation=d)
     wrapper.eval()
     wrapper(x)
@@ -72,9 +67,10 @@ def test_conv_transposed_2d():
     for in_h, in_w, in_cha, out_cha, k, s, p, d in product(
             *list(test_cases.values())):
         # wrapper op with 0-dim input
-        x = torch.empty(0, in_cha, in_h, in_w)
+        x = torch.randn(0, in_cha, in_h, in_w)
         # out padding must be smaller than either stride or dilation
         op = min(s, d) - 1
+        torch.manual_seed(0)
         wrapper = ConvTranspose2d(
             in_cha,
             out_cha,
@@ -86,7 +82,8 @@ def test_conv_transposed_2d():
         wrapper_out = wrapper(x)
 
         # torch op with 3-dim input as shape reference
-        x = torch.empty(3, in_cha, in_h, in_w)
+        x = torch.randn(3, in_cha, in_h, in_w)
+        torch.manual_seed(0)
         ref = nn.ConvTranspose2d(
             in_cha,
             out_cha,
@@ -104,31 +101,10 @@ def test_conv_transposed_2d():
         assert wrapper.weight.grad is not None
         assert wrapper.weight.grad.shape == wrapper.weight.shape
 
-        x = torch.randn(3, in_cha, in_h, in_w)
-        torch.manual_seed(0)
-        wrapper = ConvTranspose2d(
-            in_cha,
-            out_cha,
-            k,
-            stride=s,
-            padding=p,
-            dilation=d,
-            output_padding=op)
-        wrapper_out = wrapper(x)
-        torch.manual_seed(0)
-        ref = nn.ConvTranspose2d(
-            in_cha,
-            out_cha,
-            k,
-            stride=s,
-            padding=p,
-            dilation=d,
-            output_padding=op)
-        ref_out = ref(x)
-        assert torch.equal(wrapper_out, ref_out)
+        assert torch.equal(wrapper(x), ref(x))
 
     # eval mode
-    x = torch.empty(0, in_cha, in_h, in_w)
+    x = torch.randn(0, in_cha, in_h, in_w)
     wrapper = ConvTranspose2d(
         in_cha, out_cha, k, stride=s, padding=p, dilation=d, output_padding=op)
     wrapper.eval()
@@ -150,26 +126,19 @@ def test_max_pool_2d():
     for in_h, in_w, in_cha, out_cha, k, s, p, d in product(
             *list(test_cases.values())):
         # wrapper op with 0-dim input
-        x = torch.empty(0, in_cha, in_h, in_w)
+        x = torch.randn(0, in_cha, in_h, in_w)
         wrapper = MaxPool2d(k, stride=s, padding=p, dilation=d)
         wrapper_out = wrapper(x)
 
         # torch op with 3-dim input as shape reference
-        x = torch.empty(3, in_cha, in_h, in_w)
+        x = torch.randn(3, in_cha, in_h, in_w)
         ref = nn.MaxPool2d(k, stride=s, padding=p, dilation=d)
         ref_out = ref(x)
 
         assert wrapper_out.shape[0] == 0
         assert wrapper_out.shape[1:] == ref_out.shape[1:]
 
-        x = torch.randn(3, in_cha, in_h, in_w)
-        torch.manual_seed(0)
-        wrapper = MaxPool2d(k, stride=s, padding=p, dilation=d)
-        wrapper_out = wrapper(x)
-        torch.manual_seed(0)
-        ref = nn.MaxPool2d(k, stride=s, padding=p, dilation=d)
-        ref_out = ref(x)
-        assert torch.equal(wrapper_out, ref_out)
+        assert torch.equal(wrapper(x), ref(x))
 
 
 def test_linear():
@@ -183,12 +152,14 @@ def test_linear():
     for in_h, in_w, in_feature, out_feature in product(
             *list(test_cases.values())):
         # wrapper op with 0-dim input
-        x = torch.empty(0, in_feature)
+        x = torch.randn(0, in_feature)
+        torch.manual_seed(0)
         wrapper = Linear(in_feature, out_feature)
         wrapper_out = wrapper(x)
 
         # torch op with 3-dim input as shape reference
-        x = torch.empty(3, in_feature)
+        x = torch.randn(3, in_feature)
+        torch.manual_seed(0)
         ref = nn.Linear(in_feature, out_feature)
         ref_out = ref(x)
 
@@ -199,17 +170,10 @@ def test_linear():
         assert wrapper.weight.grad is not None
         assert wrapper.weight.grad.shape == wrapper.weight.shape
 
-        x = torch.empty(3, in_feature)
-        torch.manual_seed(0)
-        wrapper = Linear(in_feature, out_feature)
-        wrapper_out = wrapper(x)
-        torch.manual_seed(0)
-        ref = nn.Linear(in_feature, out_feature)
-        ref_out = ref(x)
-        assert torch.equal(wrapper_out, ref_out)
+        assert torch.equal(wrapper(x), ref(x))
 
     # eval mode
-    x = torch.empty(0, in_feature)
+    x = torch.randn(0, in_feature)
     wrapper = Linear(in_feature, out_feature)
     wrapper.eval()
     wrapper(x)
@@ -220,27 +184,27 @@ def test_nn_op_forward_called():
 
     for m in ['Conv2d', 'ConvTranspose2d', 'MaxPool2d']:
         with patch('torch.nn.{}.forward'.format(m)) as nn_module_forward:
-            # empty input
-            x = torch.empty(0, 3, 10, 10)
+            # randn input
+            x = torch.randn(0, 3, 10, 10)
             wrapper = eval(m)(3, 2, 1)
             wrapper(x)
             nn_module_forward.assert_called_with(x)
 
-            # non-empty input
-            x = torch.empty(1, 3, 10, 10)
+            # non-randn input
+            x = torch.randn(1, 3, 10, 10)
             wrapper = eval(m)(3, 2, 1)
             wrapper(x)
             nn_module_forward.assert_called_with(x)
 
     with patch('torch.nn.Linear.forward') as nn_module_forward:
-        # empty input
-        x = torch.empty(0, 3)
+        # randn input
+        x = torch.randn(0, 3)
         wrapper = Linear(3, 3)
         wrapper(x)
         nn_module_forward.assert_not_called()
 
-        # non-empty input
-        x = torch.empty(1, 3)
+        # non-randn input
+        x = torch.randn(1, 3)
         wrapper = Linear(3, 3)
         wrapper(x)
         nn_module_forward.assert_called_with(x)
