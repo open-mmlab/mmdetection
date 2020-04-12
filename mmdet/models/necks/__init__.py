c845792667438617e99bf7acefdec66cb0a41947
jinw960 <jinw960@yonsei.ac.kr>
2020-04-12 19:50:54 +0900

    add import efficientnet, bifpn into __init__

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -3,5 +3,6 @@ from .fpn import FPN
 from .fpn_carafe import FPN_CARAFE
 from .hrfpn import HRFPN
 from .nas_fpn import NASFPN
+from .bifpn import BIFPN
 
 __all__ = ['FPN', 'BFP', 'HRFPN', 'NASFPN', 'FPN_CARAFE']
