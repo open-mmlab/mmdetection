1abaf90814e565e58d2108418c03da1b8e016323
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2020-04-13 21:00:01 +0800

    Update single_stage.py

--- mmdet/models/detectors/single_stage.py
+++ mmdet/models/detectors/single_stage.py
@@ -7,7 +7,7 @@ from .base import BaseDetector
 
 
 @DETECTORS.register_module
-class SingleStageDetector(BaseDetector):
+class SingleStageDetector(BaseDetector):#继承BASEDETECTOR，单阶段模型
     """Base class for single-stage detectors.
 
     Single-stage detectors directly and densely predict bounding boxes on the
@@ -22,26 +22,27 @@ class SingleStageDetector(BaseDetector):
                  test_cfg=None,
                  pretrained=None):
         super(SingleStageDetector, self).__init__()
-        self.backbone = builder.build_backbone(backbone)
-        if neck is not None:
-            self.neck = builder.build_neck(neck)
-        self.bbox_head = builder.build_head(bbox_head)
-        self.train_cfg = train_cfg
+        self.backbone = builder.build_backbone(backbone)#主干，所有基于builder的函数都是直接使用build_from_config这个函数进行建立，
+        if neck is not None:                             #返回的就是obj_cls(**args)
+            self.neck = builder.build_neck(neck)#neck部分
+        self.bbox_head = builder.build_head(bbox_head)#head部分
+        #分离出train,test两个部分的参数字典
+        self.train_cfg = train_cfg                               
         self.test_cfg = test_cfg
         self.init_weights(pretrained=pretrained)
 
     def init_weights(self, pretrained=None):
         super(SingleStageDetector, self).init_weights(pretrained)
-        self.backbone.init_weights(pretrained=pretrained)
+        self.backbone.init_weights(pretrained=pretrained)#主干进行预训练初始化
         if self.with_neck:
-            if isinstance(self.neck, nn.Sequential):
+            if isinstance(self.neck, nn.Sequential):#对neck的部分使用其他的初始化方式
                 for m in self.neck:
                     m.init_weights()
             else:
                 self.neck.init_weights()
         self.bbox_head.init_weights()
 
-    def extract_feat(self, img):
+    def extract_feat(self, img):#抽取图片特征
         """Directly extract features from the backbone+neck
         """
         x = self.backbone(img)
@@ -63,10 +64,10 @@ class SingleStageDetector(BaseDetector):
                       img_metas,
                       gt_bboxes,
                       gt_labels,
-                      gt_bboxes_ignore=None):
+                      gt_bboxes_ignore=None):#这个是用于训练的前向
         x = self.extract_feat(img)
         outs = self.bbox_head(x)
-        loss_inputs = outs + (gt_bboxes, gt_labels, img_metas, self.train_cfg)
+        loss_inputs = outs + (gt_bboxes, gt_labels, img_metas, self.train_cfg)#将结果和GT放在一起共同进入loss
         losses = self.bbox_head.loss(
             *loss_inputs, gt_bboxes_ignore=gt_bboxes_ignore)
         return losses
