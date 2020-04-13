16714d15c09521e86656bfff10c699ef566d75e0
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2020-04-13 17:23:03 +0300

    add mask support for openvino model wrapper

--- mmdet/utils/deployment/openvino_backend.py
+++ mmdet/utils/deployment/openvino_backend.py
@@ -12,21 +12,6 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-"""
- Copyright (c) 2020 Intel Corporation
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-"""
 
 import logging
 import os.path as osp
@@ -35,8 +20,9 @@ from collections import OrderedDict
 import numpy as np
 import torch
 from lxml import etree
+from openvino.inference_engine import IECore
 
-from mmdet.models import build_detector
+from ...models import build_detector
 
 
 class PerformanceCounters(object):
@@ -63,21 +49,6 @@ class PerformanceCounters(object):
                                           stats['status'], stats['real_time'] / stats['calls']))
 
 
-def to_numpy(x):
-    if isinstance(x, torch.Tensor):
-        x = x.detach().cpu().numpy()
-    return x
-
-
-class IECore(object):
-    def __new__(cls):
-        from openvino.inference_engine import IECore as IECore_
-
-        if not hasattr(cls, 'instance'):
-            cls.instance = IECore_()
-        return cls.instance
-
-
 class ModelOpenVINO(object):
 
     def __init__(self,
@@ -98,15 +69,18 @@ class ModelOpenVINO(object):
         logging.info('Reading network from IR...')
         if bin_file_path is None:
             bin_file_path = osp.splitext(xml_file_path)[0] + '.bin'
+        if mapping_file_path is None:
+            mapping_file_path = osp.splitext(xml_file_path)[0] + '.mapping'
+
         self.net = IENetwork(model=xml_file_path, weights=bin_file_path)
 
-        self.mapping = {'labels': 'labels', 'boxes': '23597/Split.0'}
-        self.mapping = {}
-        self.mapping_file_path = mapping_file_path
-        self.net_inputs_mapping = OrderedDict((i, i) for i in self.net.inputs.keys())
-        if required_inputs is not None:
-            assert set(required_inputs) == set(self.net.inputs.keys())
-        self.net_outputs_mapping = self.configure_outputs(self.net, required_outputs)
+        self.orig_ir_mapping = self.get_mapping(mapping_file_path)
+        self.ir_orig_mapping = {v: k for k, v in self.orig_ir_mapping.items()}
+
+        self.net_inputs_mapping = OrderedDict({})
+        self.net_outputs_mapping = OrderedDict({})
+        self.configure_inputs(required_inputs)
+        self.configure_outputs(required_outputs)
 
         if 'CPU' in device:
             self.check_cpu_support(ie, self.net)
@@ -141,38 +115,56 @@ class ModelOpenVINO(object):
             logging.warning('Please try to specify cpu extensions library path.')
             raise ValueError('Some of the layers are not supported.')
 
-    def get_mapping(self):
-        if len(self.mapping) == 0 and self.mapping_file_path is not None:
+    def get_mapping(self, mapping_file_path=None):
+        mapping = {}
+        if mapping_file_path is not None:
             logging.info('Loading mapping file...')
-            self.mapping = {}
-            root = etree.parse(self.mapping_file_path).getroot()
+            root = etree.parse(mapping_file_path).getroot()
             for m in root:
                 if m.tag != 'map':
                     continue
                 framework = m.find('framework')
                 ir = m.find('IR')
-                self.mapping[framework.get('name')] = ir.get('name')
-                if framework.get('name') != ir.get('name'):
-                    self.mapping[framework.get('name')] += '.0'
-        return self.mapping
+                framework_name = framework.get('name')
+                ir_name = ir.get('name')
+                mapping[framework_name] = ir_name
+                if framework_name != ir_name:
+                    # FIXME. This may not be correct for all operations.
+                    mapping[framework_name] += '.0'
+        return mapping
 
-    def configure_outputs(self, net, required_outputs):
-        net_outputs_mapping = OrderedDict()
-        if required_outputs is None:
-            for o in net.outputs.keys():
-                net_outputs_mapping[o] = o
-        else:
-            for required_output in required_outputs:
-                output = self.get_mapping()[required_output]
-                try:
-                    net.add_outputs(output)
-                except RuntimeError:
-                    pass
-                if output in net.outputs:
-                    net_outputs_mapping[output] = required_output
-                else:
-                    raise ValueError('Failed to identify output "{}"'.format(required_output))
-        return net_outputs_mapping
+    def try_add_extra_outputs(self, extra_outputs):
+        if extra_outputs is None:
+            return
+        for extra_output in extra_outputs:
+            if extra_output not in self.orig_ir_mapping:
+                continue
+            ir_name = self.orig_ir_mapping[extra_output]
+            try:
+                self.net.add_outputs(ir_name)
+            except RuntimeError:
+                pass
+
+    def configure_inputs(self, required):
+        self.net_inputs_mapping = OrderedDict((i, i) for i in self.net.inputs.keys())
+        self.check_required(self.net_inputs_mapping.keys(), required)
+
+    def configure_outputs(self, required):
+        self.try_add_extra_outputs(required)
+        self.net_outputs_mapping = OrderedDict((i, self.ir_orig_mapping[i]) for i in self.net.outputs.keys())
+        self.check_required(self.orig_ir_mapping.keys(), required)
+
+    def set_outputs(self, outputs):
+        self.check_required(self.orig_ir_mapping.keys(), outputs)
+        self.net_outputs_mapping = OrderedDict((self.orig_ir_mapping[i], i) for i in outputs)
+
+    @staticmethod
+    def check_required(available, required):
+        if required is None:
+            return
+        for x in required:
+            if x not in available:
+                raise ValueError(f'Failed to identify data blob with name "{x}"')
 
     def rename_outputs(self, outputs):
         return {self.net_outputs_mapping[k]: v for k, v in outputs.items() if k in self.net_outputs_mapping}
@@ -193,10 +185,6 @@ class ModelOpenVINO(object):
             self.perf_counters.update(perf_counters)
         return self.rename_outputs(outputs)
 
-    def __del__(self):
-        del self.net
-        del self.exec_net
-
     def print_performance_counters(self):
         if self.perf_counters:
             self.perf_counters.print()
@@ -209,33 +197,56 @@ class ModelOpenVINO(object):
 
 class DetectorOpenVINO(ModelOpenVINO):
     def __init__(self, *args, with_detection_output=False, **kwargs):
+        self.with_detection_output = False
+        self.with_mask = False
         super().__init__(*args,
                          required_inputs=('image', ),
-                         required_outputs=('detection_out',) if with_detection_output else ('boxes', 'labels'),
+                         required_outputs=None,
                          **kwargs)
-
-        self.with_detection_output = with_detection_output
-
         self.n, self.c, self.h, self.w = self.net.inputs['image'].shape
         assert self.n == 1, 'Only batch 1 is supported.'
 
+    def configure_outputs(self, required):
+        self.try_add_extra_outputs(['boxes', 'labels', 'masks', 'detection_out'])
+        outputs = []
+        
+        try:
+            self.check_required(self.orig_ir_mapping.keys(), ['detection_out'])
+            self.with_detection_output = True
+            outputs.append('detection_out')
+        except ValueError:
+            self.check_required(self.orig_ir_mapping.keys(), ['boxes', 'labels'])
+            self.with_detection_output = False
+            outputs.extend(['boxes', 'labels'])
+
+        try:
+            self.check_required(self.orig_ir_mapping.keys(), ['masks'])
+            self.with_mask = True
+            outputs.append('masks')
+        except ValueError:
+            self.with_mask = False
+
+        self.set_outputs(outputs)
+
     def __call__(self, inputs, **kwargs):
         inputs = self.unify_inputs(inputs)
+        data_h, data_w = inputs['image'].shape[-2:]
+        inputs['image'] = np.pad(inputs['image'],
+                                 ((0, 0), (0, 0), (0, self.h - data_h), (0, self.w - data_w)),
+                                 mode='constant')
+
         output = super().__call__(inputs)
         if self.with_detection_output:
             detection_out = output['detection_out']
-            output = {}
-            output['labels'] = detection_out[0, 0, :, 1]
-            valid_detections_mask = output['labels'] > 0
+            output['labels'] = detection_out[0, 0, :, 1].astype(np.int32) - 1
             output['boxes'] = detection_out[0, 0, :, 3:] * np.tile(inputs['image'].shape[2:][::-1], 2)
             output['boxes'] = np.concatenate((output['boxes'], detection_out[0, 0, :, 2:3]), axis=1)
+            del output['detection_out']
 
-            output['boxes'] = output['boxes'][valid_detections_mask]
-            output['labels'] = output['labels'][valid_detections_mask].astype(np.int32) - 1
-        else:
-            classes = output['labels']
-            valid_detections_mask = classes >= 0
-            output['labels'] = classes[valid_detections_mask]
-            output['boxes'] = output['boxes'][valid_detections_mask]
+        valid_detections_mask = output['labels'] >= 0
+        output['labels'] = output['labels'][valid_detections_mask]
+        output['boxes'] = output['boxes'][valid_detections_mask]
+        if 'masks' in output:
+            output['masks'] = output['masks'][valid_detections_mask]
 
         return output
