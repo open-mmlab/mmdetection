7fb825ba59ec043d13556bd180c4ebbe43a9cc51
Cao Yuhang <yhcao6@gmail.com>
2020-04-14 17:59:06 +0800

    fix some configs

--- configs/hrnet/fcos_hrnetv2p_w40_gn-head_mstrain_640-800_4x4_2x_coco.py
+++ configs/hrnet/fcos_hrnetv2p_w40_gn-head_mstrain_640-800_4x4_2x_coco.py
@@ -1,4 +1,4 @@
-_base_ = './fcos_hrnetv2p_w32_gn-head_4x4_1x_coco.py'
+_base_ = './fcos_hrnetv2p_w32_gn-head_mstrain_640-800_4x4_2x_coco.py'
 model = dict(
     pretrained='open-mmlab://msra/hrnetv2_w40',
     backbone=dict(
