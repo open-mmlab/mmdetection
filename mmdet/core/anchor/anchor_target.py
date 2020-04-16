6dea14fb4f7e88f1c69188a9d73d2a91d90c85c5
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2020-04-16 22:02:16 +0800

    Update anchor_target.py

--- mmdet/core/anchor/anchor_target.py
+++ mmdet/core/anchor/anchor_target.py
@@ -105,50 +105,50 @@ def anchor_target_single(flat_anchors,
                          unmap_outputs=True):
     inside_flags = anchor_inside_flags(flat_anchors, valid_flags,
                                        img_meta['img_shape'][:2],
-                                       cfg.allowed_border)
-    if not inside_flags.any():
+                                       cfg.allowed_border)#筛出图片中没有超出边界的anchors
+    if not inside_flags.any():#如果没有满足的，就返回空的，
         return (None, ) * 6
     # assign gt and sample anchors
-    anchors = flat_anchors[inside_flags, :]
+    anchors = flat_anchors[inside_flags, :]#取出在图片范围内的anchor
 
-    if sampling:
+    if sampling:#如果要对anchor进行采样，用于二阶段
         assign_result, sampling_result = assign_and_sample(
             anchors, gt_bboxes, gt_bboxes_ignore, None, cfg)
-    else:
-        bbox_assigner = build_assigner(cfg.assigner)
+    else:#如果不需要对anchors进行采样，用于一阶段
+        bbox_assigner = build_assigner(cfg.assigner)#就直接进行分配
         assign_result = bbox_assigner.assign(anchors, gt_bboxes,
                                              gt_bboxes_ignore, gt_labels)
         bbox_sampler = PseudoSampler()
         sampling_result = bbox_sampler.sample(assign_result, anchors,
                                               gt_bboxes)
-
-    num_valid_anchors = anchors.shape[0]
-    bbox_targets = torch.zeros_like(anchors)
-    bbox_weights = torch.zeros_like(anchors)
+    #设置好所有标签的大小，用于填充
+    num_valid_anchors = anchors.shape[0]#有效anchors的数量
+    bbox_targets = torch.zeros_like(anchors)#目标准备
+    bbox_weights = torch.zeros_like(anchors)#每个ancors的权值
     labels = anchors.new_zeros(num_valid_anchors, dtype=torch.long)
     label_weights = anchors.new_zeros(num_valid_anchors, dtype=torch.float)
 
-    pos_inds = sampling_result.pos_inds
-    neg_inds = sampling_result.neg_inds
-    if len(pos_inds) > 0:
+    pos_inds = sampling_result.pos_inds    #正样本索引
+    neg_inds = sampling_result.neg_inds     #负样本索引
+    if len(pos_inds) > 0:#如果有正样本
         pos_bbox_targets = bbox2delta(sampling_result.pos_bboxes,
                                       sampling_result.pos_gt_bboxes,
-                                      target_means, target_stds)
-        bbox_targets[pos_inds, :] = pos_bbox_targets
-        bbox_weights[pos_inds, :] = 1.0
-        if gt_labels is None:
-            labels[pos_inds] = 1
+                                      target_means, target_stds)#对正样本和GT进行delta编码
+        bbox_targets[pos_inds, :] = pos_bbox_targets #将其放入到正样本位置
+        bbox_weights[pos_inds, :] = 1.0   #正样本权重设为1
+        if gt_labels is None: #如果没有GT，那么
+            labels[pos_inds] = 1  
         else:
             labels[pos_inds] = gt_labels[sampling_result.pos_assigned_gt_inds]
         if cfg.pos_weight <= 0:
             label_weights[pos_inds] = 1.0
         else:
             label_weights[pos_inds] = cfg.pos_weight
-    if len(neg_inds) > 0:
+    if len(neg_inds) > 0:#如果有负样本，权重设为1，这个不是标签
         label_weights[neg_inds] = 1.0
 
     # map up to original set of anchors
-    if unmap_outputs:
+    if unmap_outputs:#这个是将
         num_total_anchors = flat_anchors.size(0)
         labels = unmap(labels, num_total_anchors, inside_flags)
         label_weights = unmap(label_weights, num_total_anchors, inside_flags)
@@ -162,7 +162,7 @@ def anchor_target_single(flat_anchors,
 def anchor_inside_flags(flat_anchors,
                         valid_flags,
                         img_shape,
-                        allowed_border=0):
+                        allowed_border=0):#对anchor进行规范化，超出边界的anchor索引要设为无效，
     img_h, img_w = img_shape[:2]
     if allowed_border >= 0:
         inside_flags = valid_flags & \
@@ -171,18 +171,18 @@ def anchor_inside_flags(flat_anchors,
             (flat_anchors[:, 2] < img_w + allowed_border).type(torch.uint8) & \
             (flat_anchors[:, 3] < img_h + allowed_border).type(torch.uint8)
     else:
-        inside_flags = valid_flags
+        inside_flags = valid_flags #返回在图片中的有效anchors索引
     return inside_flags
 
 
-def unmap(data, count, inds, fill=0):#将数据reshape,并将它放在指定的索引位置
+def unmap(data, count, inds, fill=0):#将数据reshape,并将它放在指定的索引位置,用数据进行填充
     """ Unmap a subset of item (data) back to the original set of items (of
     size count) """
     if data.dim() == 1:#如果数据是一维的
-        ret = data.new_full((count, ), fill)
-        ret[inds] = data#指定的inds也是data
+        ret = data.new_full((count, ), fill)#设置好返回数据的大小
+        ret[inds] = data#指定的inds也是data，将data放在相应的位置
     else:
-        new_size = (count, ) + data.size()[1:]
+        new_size = (count, ) + data.size()[1:]#多维数据放置也用相同的方法，类似于进行广播，但是只有一个位置有数据
         ret = data.new_full(new_size, fill)
         ret[inds, :] = data
     return ret
