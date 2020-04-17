c64bbc7c744e5ee958eb6533f128889eae5252eb
Jiarui XU <xvjiarui0826@gmail.com>
2020-04-17 11:27:00 +0800

    remove dupulicate unmap

--- mmdet/core/utils/__init__.py
+++ mmdet/core/utils/__init__.py
@@ -1,7 +1,6 @@
 from .dist_utils import DistOptimizerHook, allreduce_grads
-from .misc import multi_apply, tensor2imgs, unmap
+from .misc import multi_apply, tensor2imgs
 
 __all__ = [
-    'allreduce_grads', 'DistOptimizerHook', 'tensor2imgs', 'unmap',
-    'multi_apply'
+    'allreduce_grads', 'DistOptimizerHook', 'tensor2imgs', 'multi_apply'
 ]
