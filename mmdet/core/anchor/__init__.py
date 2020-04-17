c235c965bc8b266d19b0658a3991a40d18209912
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-04-17 13:06:22 +0800

    Remove duplicate unmap function (#2471)
    * remove dupulicate unmap
    
    * move unmap from anchor/utils to utils/misc

--- mmdet/core/anchor/__init__.py
+++ mmdet/core/anchor/__init__.py
@@ -1,8 +1,8 @@
 from .anchor_generator import AnchorGenerator
 from .point_generator import PointGenerator
-from .utils import anchor_inside_flags, calc_region, images_to_levels, unmap
+from .utils import anchor_inside_flags, calc_region, images_to_levels
 
 __all__ = [
     'AnchorGenerator', 'anchor_inside_flags', 'PointGenerator',
-    'images_to_levels', 'unmap', 'calc_region'
+    'images_to_levels', 'calc_region'
 ]
