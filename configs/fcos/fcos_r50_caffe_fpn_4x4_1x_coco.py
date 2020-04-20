f1dbf805078a879041117e536ee05fe8bb5cc551
Cao Yuhang <yhcao6@gmail.com>
2020-04-20 14:15:43 +0800

    add grad clip for fcos

--- configs/fcos/fcos_r50_caffe_fpn_4x4_1x_coco.py
+++ configs/fcos/fcos_r50_caffe_fpn_4x4_1x_coco.py
@@ -93,7 +93,8 @@ data = dict(
 # optimizer
 optimizer = dict(
     lr=0.01, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
-optimizer_config = dict(_delete_=True, grad_clip=None)
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(warmup='constant')
 total_epochs = 12
