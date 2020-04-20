58da4ffd78e91659a5e44ffc63f06ddbaa956c6a
Kai Chen <chenkaidev@gmail.com>
2020-04-20 22:22:09 +0800

    Remove datasets.loader and add datasets.samplers (#2482)
    * remove datasets.loader and add datasets.samplers
    
    * fix typo in the filename

---
+++ mmdet/datasets/samplers/__init__.py
@@ -0,0 +1,4 @@
+from .distributed_sampler import DistributedSampler
+from .group_sampler import DistributedGroupSampler, GroupSampler
+
+__all__ = ['DistributedSampler', 'DistributedGroupSampler', 'GroupSampler']
