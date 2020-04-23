e3e531dcaad6fe5e5a73a6b28821f92797348ba0
Jiarui XU <xvjiarui0826@gmail.com>
2020-04-23 18:59:02 +0800

    remove circulate import in bbox

--- mmdet/core/bbox/registry.py
+++ mmdet/core/bbox/registry.py
@@ -6,21 +6,12 @@ BBOX_CODERS = Registry('bbox_coder')
 
 
 def build_assigner(cfg, **default_args):
-    from .assigners import BaseAssigner
-    if isinstance(cfg, BaseAssigner):
-        return cfg
     return build_from_cfg(cfg, BBOX_ASSIGNERS, default_args)
 
 
 def build_sampler(cfg, **default_args):
-    from .samplers import BaseSampler
-    if isinstance(cfg, BaseSampler):
-        return cfg
     return build_from_cfg(cfg, BBOX_SAMPLERS, default_args)
 
 
 def build_bbox_coder(cfg, **default_args):
-    from .coder import BaseBBoxCoder
-    if isinstance(cfg, BaseBBoxCoder):
-        return cfg
     return build_from_cfg(cfg, BBOX_CODERS, default_args)
