668de34cb7a922a4d9392548d15ed7c781ac8816
Ilya Krylov <krylov.ib@gmail.com>
2020-04-24 14:53:57 +0300

    minor fix

--- tools/config_py_to_yaml_or_json.py
+++ tools/config_py_to_yaml_or_json.py
@@ -19,28 +19,29 @@ def main():
     args = parse_args()
     filename = osp.abspath(osp.expanduser(args.input))
     output = args.output
+
     assert output.endswith('.yaml') or output.endswith('.yml') or output.endswith('.json')
-
     assert osp.exists(filename)
-    if filename.endswith('.py'):
-        module_name = osp.basename(filename)[:-3]
-        if '.' in module_name:
-            raise ValueError('Dots are not allowed in config file path.')
-        config_dir = osp.dirname(filename)
-        sys.path.insert(0, config_dir)
-        mod = import_module(module_name)
-        sys.path.pop(0)
-        cfg_dict = {
-            name: value
-            for name, value in mod.__dict__.items()
-            if not name.startswith('__')
-        }
+    assert filename.endswith('.py')
 
-        with open(output, 'w') as f:
-            if output.endswith('.json'):
-                json.dump(cfg_dict, f, indent=4)
-            else:
-                yaml.dump(cfg_dict, f)
+    module_name = osp.basename(filename)[:-3]
+    if '.' in module_name:
+        raise ValueError('Dots are not allowed in config file path.')
+    config_dir = osp.dirname(filename)
+    sys.path.insert(0, config_dir)
+    mod = import_module(module_name)
+    sys.path.pop(0)
+    cfg_dict = {
+        name: value
+        for name, value in mod.__dict__.items()
+        if not name.startswith('__')
+    }
+
+    with open(output, 'w') as f:
+        if output.endswith('.json'):
+            json.dump(cfg_dict, f, indent=4)
+        else:
+            yaml.dump(cfg_dict, f)
 
 
 if __name__ == '__main__':
