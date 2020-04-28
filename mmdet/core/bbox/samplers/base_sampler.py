4f34d9329a597dd323eb2a8c96c7211251b4f901
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2020-04-28 15:06:25 +0800

    Update base_sampler.py

--- mmdet/core/bbox/samplers/base_sampler.py
+++ mmdet/core/bbox/samplers/base_sampler.py
@@ -6,7 +6,10 @@ from .sampling_result import SamplingResult
 
 
 class BaseSampler(metaclass=ABCMeta):
-
+    '''
+    基本采样类，正样本调用 _sample_pos，负样本调用 _sample_neg,需要重新写
+    主类调用 sample，它利用分配结果类assign_result，根据给定的正负样本比例来进行采样
+    '''
     def __init__(self,
                  num,
                  pos_fraction,
@@ -49,29 +52,31 @@ class BaseSampler(metaclass=ABCMeta):
             :obj:`SamplingResult`: Sampling result.
         """
         bboxes = bboxes[:, :4]
-
-        gt_flags = bboxes.new_zeros((bboxes.shape[0], ), dtype=torch.uint8)
+        
+        #将GT和BOX组合在一起，在这之前要有一个标识
+        gt_flags = bboxes.new_zeros((bboxes.shape[0], ), dtype=torch.uint8) 
         if self.add_gt_as_proposals and len(gt_bboxes) > 0:
             bboxes = torch.cat([gt_bboxes, bboxes], dim=0)
             assign_result.add_gt_(gt_labels)
             gt_ones = bboxes.new_ones(gt_bboxes.shape[0], dtype=torch.uint8)
             gt_flags = torch.cat([gt_ones, gt_flags])
-
+        
+        #想要多少个正样本
         num_expected_pos = int(self.num * self.pos_fraction)
         pos_inds = self.pos_sampler._sample_pos(
-            assign_result, num_expected_pos, bboxes=bboxes, **kwargs)
+            assign_result, num_expected_pos, bboxes=bboxes, **kwargs)#调用采样函数提取出正样本的索引
         # We found that sampled indices have duplicated items occasionally.
         # (may be a bug of PyTorch)
         pos_inds = pos_inds.unique()
-        num_sampled_pos = pos_inds.numel()
-        num_expected_neg = self.num - num_sampled_pos
+        num_sampled_pos = pos_inds.numel()#统计采样个数
+        num_expected_neg = self.num - num_sampled_pos #计算还需要多少负样本
         if self.neg_pos_ub >= 0:
             _pos = max(1, num_sampled_pos)
             neg_upper_bound = int(self.neg_pos_ub * _pos)
             if num_expected_neg > neg_upper_bound:
                 num_expected_neg = neg_upper_bound
         neg_inds = self.neg_sampler._sample_neg(
-            assign_result, num_expected_neg, bboxes=bboxes, **kwargs)
+            assign_result, num_expected_neg, bboxes=bboxes, **kwargs)#负样本收集
         neg_inds = neg_inds.unique()
 
         return SamplingResult(pos_inds, neg_inds, bboxes, gt_bboxes,
