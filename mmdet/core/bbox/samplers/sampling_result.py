2727966c25dce3882ca09e6771e842bd087f1b74
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2020-04-28 14:34:24 +0800

    Update sampling_result.py

--- mmdet/core/bbox/samplers/sampling_result.py
+++ mmdet/core/bbox/samplers/sampling_result.py
@@ -1,12 +1,15 @@
 import torch
 
 
-class SamplingResult(object):
+class SamplingResult(object):#当完成正负样本采样以后，用这个类将正负样本的对应索引全部保存
 
     def __init__(self, pos_inds, neg_inds, bboxes, gt_bboxes, assign_result,
                  gt_flags):
+        #正样本个数
         self.pos_inds = pos_inds
+        #负样本个数
         self.neg_inds = neg_inds
+        #正样本
         self.pos_bboxes = bboxes[pos_inds]
         self.neg_bboxes = bboxes[neg_inds]
         self.pos_is_gt = gt_flags[pos_inds]
@@ -20,5 +23,5 @@ class SamplingResult(object):
             self.pos_gt_labels = None
 
     @property
-    def bboxes(self):
+    def bboxes(self):#返回所有的正负样本
         return torch.cat([self.pos_bboxes, self.neg_bboxes])
