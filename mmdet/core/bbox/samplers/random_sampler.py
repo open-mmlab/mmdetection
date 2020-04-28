af5b5acbf585f9fd8b61567c356ec1730b7d508f
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2020-04-28 15:12:20 +0800

    Update random_sampler.py

--- mmdet/core/bbox/samplers/random_sampler.py
+++ mmdet/core/bbox/samplers/random_sampler.py
@@ -18,26 +18,26 @@ class RandomSampler(BaseSampler):
     @staticmethod
     def random_choice(gallery, num):
         """Random select some elements from the gallery.
-
+         随机选num个
         It seems that Pytorch's implementation is slower than numpy so we use
         numpy to randperm the indices.
         """
-        assert len(gallery) >= num
-        if isinstance(gallery, list):
+        assert len(gallery) >= num  #个数要足够
+        if isinstance(gallery, list): #如果是个列表，转化成numpy
             gallery = np.array(gallery)
-        cands = np.arange(len(gallery))
-        np.random.shuffle(cands)
-        rand_inds = cands[:num]
+        cands = np.arange(len(gallery))#所有的索引
+        np.random.shuffle(cands)#打乱顺序
+        rand_inds = cands[:num]#取出前num个
         if not isinstance(gallery, np.ndarray):
-            rand_inds = torch.from_numpy(rand_inds).long().to(gallery.device)
+            rand_inds = torch.from_numpy(rand_inds).long().to(gallery.device)#重新转为tensor再放到设备上
         return gallery[rand_inds]
 
     def _sample_pos(self, assign_result, num_expected, **kwargs):
         """Randomly sample some positive samples."""
-        pos_inds = torch.nonzero(assign_result.gt_inds > 0)
-        if pos_inds.numel() != 0:
-            pos_inds = pos_inds.squeeze(1)
-        if pos_inds.numel() <= num_expected:
+        pos_inds = torch.nonzero(assign_result.gt_inds > 0)#正样本就是>0的样本
+        if pos_inds.numel() != 0:#如果有
+            pos_inds = pos_inds.squeeze(1)#去掉一维
+        if pos_inds.numel() <= num_expected: #如果不够
             return pos_inds
         else:
             return self.random_choice(pos_inds, num_expected)
