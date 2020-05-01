672fd5de50a16cda1553e176f6038518fadf4c6e
Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>
2020-05-01 22:47:29 +0800

    Fix ATSS warning on dist.ReduceOp (#2576)

--- mmdet/models/anchor_heads/atss_head.py
+++ mmdet/models/anchor_heads/atss_head.py
@@ -15,7 +15,7 @@ def reduce_mean(tensor):
     if not (dist.is_available() and dist.is_initialized()):
         return tensor
     tensor = tensor.clone()
-    dist.all_reduce(tensor.div_(dist.get_world_size()), op=dist.reduce_op.SUM)
+    dist.all_reduce(tensor.div_(dist.get_world_size()), op=dist.ReduceOp.SUM)
     return tensor
 
 
