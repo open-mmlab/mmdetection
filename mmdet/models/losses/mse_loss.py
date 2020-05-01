34f29ab24b2b32a35a17f4557c3108e670278051
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2020-05-01 12:52:25 +0800

    Update mse_loss.py

--- mmdet/models/losses/mse_loss.py
+++ mmdet/models/losses/mse_loss.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from ..registry import LOSSES
 from .utils import weighted_loss
 
-mse_loss = weighted_loss(F.mse_loss)
+mse_loss = weighted_loss(F.mse_loss)#这里不用再次定义mse_loss了，所以就没有@weight_loss了，直接将函数放到里面就可以了
 
 
 @LOSSES.register_module
