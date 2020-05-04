da5855bb878d28398ef23a8c9583c0802d867e48
Cao Yuhang <yhcao6@gmail.com>
2020-05-04 18:07:27 +0800

    fix torch.nonzero usage in pytorch 1.5

--- mmdet/core/bbox/assigners/point_assigner.py
+++ mmdet/core/bbox/assigners/point_assigner.py
@@ -122,7 +122,8 @@ class PointAssigner(BaseAssigner):
 
         if gt_labels is not None:
             assigned_labels = assigned_gt_inds.new_full((num_points, ), -1)
-            pos_inds = torch.nonzero(assigned_gt_inds > 0).squeeze()
+            pos_inds = torch.nonzero(
+                assigned_gt_inds > 0, as_tuple=False).squeeze()
             if pos_inds.numel() > 0:
                 assigned_labels[pos_inds] = gt_labels[
                     assigned_gt_inds[pos_inds] - 1]
--- mmdet/core/bbox/samplers/pseudo_sampler.py
+++ mmdet/core/bbox/samplers/pseudo_sampler.py
@@ -19,9 +19,9 @@ class PseudoSampler(BaseSampler):
 
     def sample(self, assign_result, bboxes, gt_bboxes, **kwargs):
         pos_inds = torch.nonzero(
-            assign_result.gt_inds > 0).squeeze(-1).unique()
+            assign_result.gt_inds > 0, as_tuple=False).squeeze(-1).unique()
         neg_inds = torch.nonzero(
-            assign_result.gt_inds == 0).squeeze(-1).unique()
+            assign_result.gt_inds == 0, as_tuple=False).squeeze(-1).unique()
         gt_flags = bboxes.new_zeros(bboxes.shape[0], dtype=torch.uint8)
         sampling_result = SamplingResult(pos_inds, neg_inds, bboxes, gt_bboxes,
                                          assign_result, gt_flags)
