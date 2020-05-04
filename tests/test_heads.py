6983a668b7d2a57de05947e2c981403b88846a2e
wangxinjiang <wangxinjiang@sensetime.com>
2020-05-04 18:47:49 +0800

    Added FSAFHead test and supported zero gt in FSAFHead

--- tests/test_heads.py
+++ tests/test_heads.py
@@ -113,42 +113,43 @@ def test_fsaf_head_loss():
             allowed_border=-1,
             pos_weight=-1,
             debug=False))
-    self = FSAFHead(num_classes=4, in_channels=1, train_cfg=train_cfg, **cfg)
+    head = FSAFHead(num_classes=4, in_channels=1, train_cfg=train_cfg, **cfg)
+    if torch.cuda.is_available():
+        head.cuda()
+        # FSAF head expects a multiple levels of features per image
+        feat = [
+            torch.rand(1, 1, s // (2**(i + 2)), s // (2**(i + 2))).cuda()
+            for i in range(len(head.anchor_generator.strides))
+        ]
+        cls_scores, bbox_preds = head.forward(feat)
+        gt_bboxes_ignore = None
 
-    # FSAF head expects a multiple levels of features per image
-    feat = [
-        torch.rand(1, 1, s // (2**(i + 2)), s // (2**(i + 2)))
-        for i in range(len(self.anchor_generator.strides))
-    ]
-    cls_scores, bbox_preds = self.forward(feat)
-    gt_bboxes_ignore = None
+        # When truth is non-empty then both cls and box loss should be nonzero
+        #  for random inputs
+        gt_bboxes = [
+            torch.Tensor([[23.6667, 23.8757, 238.6326, 151.8874]]).cuda(),
+        ]
+        gt_labels = [torch.LongTensor([2]).cuda()]
+        one_gt_losses = head.loss(cls_scores, bbox_preds, gt_bboxes, gt_labels,
+                                  img_metas, gt_bboxes_ignore)
+        onegt_cls_loss = sum(one_gt_losses['loss_cls'])
+        onegt_box_loss = sum(one_gt_losses['loss_bbox'])
+        assert onegt_cls_loss.item() > 0, 'cls loss should be non-zero'
+        assert onegt_box_loss.item() > 0, 'box loss should be non-zero'
 
-    # When truth is non-empty then both cls and box loss should be nonzero for
-    # random inputs
-    gt_bboxes = [
-        torch.Tensor([[23.6667, 23.8757, 238.6326, 151.8874]]),
-    ]
-    gt_labels = [torch.LongTensor([2])]
-    one_gt_losses = self.loss(cls_scores, bbox_preds, gt_bboxes, gt_labels,
-                              img_metas, gt_bboxes_ignore)
-    onegt_cls_loss = sum(one_gt_losses['loss_cls'])
-    onegt_box_loss = sum(one_gt_losses['loss_bbox'])
-    assert onegt_cls_loss.item() > 0, 'cls loss should be non-zero'
-    assert onegt_box_loss.item() > 0, 'box loss should be non-zero'
+        # Test that empty ground truth encourages the network to predict bkg
+        gt_bboxes = [torch.empty((0, 4)).cuda()]
+        gt_labels = [torch.LongTensor([]).cuda()]
 
-    # Test that empty ground truth encourages the network to predict background
-    gt_bboxes = [torch.empty((0, 4))]
-    gt_labels = [torch.LongTensor([])]
-
-    empty_gt_losses = self.loss(cls_scores, bbox_preds, gt_bboxes, gt_labels,
-                                img_metas, gt_bboxes_ignore)
-    # When there is no truth, the cls loss should be nonzero but there should
-    # be no box loss.
-    empty_cls_loss = sum(empty_gt_losses['loss_cls'])
-    empty_box_loss = sum(empty_gt_losses['loss_bbox'])
-    assert empty_cls_loss.item() > 0, 'cls loss should be non-zero'
-    assert empty_box_loss.item() == 0, (
-        'there should be no box loss when there are no true boxes')
+        empty_gt_losses = head.loss(cls_scores, bbox_preds, gt_bboxes,
+                                    gt_labels, img_metas, gt_bboxes_ignore)
+        # When there is no truth, the cls loss should be nonzero but there
+        # should be no box loss.
+        empty_cls_loss = sum(empty_gt_losses['loss_cls'])
+        empty_box_loss = sum(empty_gt_losses['loss_bbox'])
+        assert empty_cls_loss.item() > 0, 'cls loss should be non-zero'
+        assert empty_box_loss.item() == 0, (
+            'there should be no box loss when there are no true boxes')
 
 
 def test_ga_anchor_head_loss():
