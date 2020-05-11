8b38737fb10cf31fce774503c323182fbf632674
Wang Xinjiang <swanxinjiang@gmail.com>
2020-05-11 21:37:39 +0800

    Make FSAF loss more robust to no gt (#2680)
    * Changed the default num_pos to make fsaf loss without pos samples more robust
    
    * moved avg_factor out of the for loop in fsaf_head
    
    Co-authored-by: wangxinjiang <wangxinjiang@sensetime.com>

--- mmdet/models/dense_heads/fsaf_head.py
+++ mmdet/models/dense_heads/fsaf_head.py
@@ -239,12 +239,16 @@ class FSAFHead(RetinaHead):
             labels_list,
             list(range(len(losses_cls))),
             min_levels=argmin)
-        # Clamp num_pos to 1e-3 to prevent 0/0
-        num_pos = torch.cat(pos_inds, 0).sum().float().clamp(min=1e-3)
+        num_pos = torch.cat(pos_inds, 0).sum().float()
         acc = self.calculate_accuracy(cls_scores, labels_list, pos_inds)
+
+        if num_pos == 0:  # No gt
+            avg_factor = num_pos + float(num_total_neg)
+        else:
+            avg_factor = num_pos
         for i in range(len(losses_cls)):
-            losses_cls[i] /= num_pos
-            losses_bbox[i] /= num_pos
+            losses_cls[i] /= avg_factor
+            losses_bbox[i] /= avg_factor
         return dict(
             loss_cls=losses_cls,
             loss_bbox=losses_bbox,
