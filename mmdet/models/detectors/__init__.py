32bca597859a447de62ddf0f2c2f482809730da9
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2020-05-11 14:33:50 +0800

    Revert "Implement NAS-FCOS"
    This reverts commit ebae11cbbdc5b4f3cc7226c763e9254c94a9c92b.

--- mmdet/models/detectors/__init__.py
+++ mmdet/models/detectors/__init__.py
@@ -13,7 +13,6 @@ from .mask_scoring_rcnn import MaskScoringRCNN
 from .reppoints_detector import RepPointsDetector
 from .retinanet import RetinaNet
 from .rpn import RPN
-from .nasfcos import NASFCOS
 from .single_stage import SingleStageDetector
 from .two_stage import TwoStageDetector
 
@@ -21,5 +20,5 @@ __all__ = [
     'ATSS', 'BaseDetector', 'SingleStageDetector', 'TwoStageDetector', 'RPN',
     'FastRCNN', 'FasterRCNN', 'MaskRCNN', 'CascadeRCNN', 'HybridTaskCascade',
     'RetinaNet', 'FCOS', 'GridRCNN', 'MaskScoringRCNN', 'RepPointsDetector',
-    'FOVEA', 'FSAF', 'NASFCOS'
+    'FOVEA', 'FSAF'
 ]
--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -4,7 +4,5 @@ from .fpn_carafe import FPN_CARAFE
 from .hrfpn import HRFPN
 from .nas_fpn import NASFPN
 from .pafpn import PAFPN
-from .nasfcos_fpn import NASFCOS_FPN
 
-__all__ = ['FPN', 'BFP', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
-           'NASFCOS_FPN']
+__all__ = ['FPN', 'BFP', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN']
