86f07e89a400d3cf73464374c7d1ef3d85649c8e
mmeendez8 <miguelmndez@gmail.com>
2020-05-21 11:55:39 +0200

    Fix with copy img

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -857,6 +857,8 @@ class Albu(object):
         # back to the original format
         results = self.mapper(results, self.keymap_back)
 
+        results['img'] = results['img'].copy()
+
         # update final shape
         if self.update_pad_shape:
             results['pad_shape'] = results['img'].shape
