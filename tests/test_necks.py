ac325e2d350086b14e6a8f0208d14313e056d39e
wangxinjiang <wangxinjiang@sensetime.com>
2020-05-22 21:49:32 +0800

    resolved issues

--- tests/test_necks.py
+++ tests/test_necks.py
@@ -7,9 +7,9 @@ from mmdet.models.necks import FPN
 
 def test_fpn():
     """Tests fpn """
-
+    s = 64
     in_channels = [8, 16, 32, 64]
-    feat_sizes = [64, 32, 16, 8]
+    feat_sizes = [s // 2**i for i in range(4)]  # [64, 32, 16, 8]
     out_channels = 8
     # `num_outs` is not equal to len(in_channels) - start_level
     with pytest.raises(AssertionError):
@@ -48,10 +48,9 @@ def test_fpn():
     ]
     outs = fpn_model(feats)
     assert len(outs) == fpn_model.num_outs
-    for i in range(len(in_channels)):
-        # feat size of input and output should be the same
-        if i >= fpn_model.start_level:
-            feats[i].shape[2:] == outs[i].shape[2:]
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
 
     # Tests for fpn with no extra convs (pooling is used instead)
     fpn_model = FPN(
@@ -60,7 +59,11 @@ def test_fpn():
         start_level=1,
         add_extra_convs=False,
         num_outs=5)
-    fpn_model(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
 
     # Tests for fpn with lateral bns
     fpn_model = FPN(
@@ -71,7 +74,11 @@ def test_fpn():
         no_norm_on_lateral=False,
         norm_cfg=dict(type='BN', requires_grad=True),
         num_outs=5)
-    fpn_model(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
     bn_exist = False
     for m in fpn_model.modules():
         if isinstance(m, _BatchNorm):
@@ -79,24 +86,33 @@ def test_fpn():
     assert bn_exist
 
     # Bilinear upsample
-    fpn_bilinear_upsample = FPN(
+    fpn_model = FPN(
         in_channels=in_channels,
         out_channels=out_channels,
         start_level=1,
         add_extra_convs=True,
         upsample_cfg=dict(mode='bilinear', align_corners=True),
         num_outs=5)
-    fpn_bilinear_upsample(feats)
+    fpn_model(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
 
     # Scale factor instead of fixed upsample size upsample
-    scale_factor_upsample = FPN(
+    fpn_model = FPN(
         in_channels=in_channels,
         out_channels=out_channels,
         start_level=1,
         add_extra_convs=True,
         upsample_cfg=dict(scale_factor=2),
         num_outs=5)
-    scale_factor_upsample(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
 
     # Extra convs source is 'inputs'
     fpn_model = FPN(
@@ -106,7 +122,11 @@ def test_fpn():
         num_outs=5,
         extra_convs_source='inputs')
     assert fpn_model.extra_convs_source == 'inputs'
-    fpn_model(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
 
     # Extra convs source is 'laterals'
     fpn_model = FPN(
@@ -116,7 +136,11 @@ def test_fpn():
         num_outs=5,
         extra_convs_source='laterals')
     assert fpn_model.extra_convs_source == 'laterals'
-    fpn_model(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
 
     # Extra convs source is 'outputs'
     fpn_model = FPN(
@@ -126,7 +150,11 @@ def test_fpn():
         num_outs=5,
         extra_convs_source='outputs')
     assert fpn_model.extra_convs_source == 'outputs'
-    fpn_model(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
 
     # extra_convs_on_inputs=False is equal to `extra convs source` is 'outputs'
     fpn_model = FPN(
@@ -137,4 +165,8 @@ def test_fpn():
         num_outs=5,
     )
     assert fpn_model.extra_convs_source == 'outputs'
-    fpn_model(feats)
+    outs = fpn_model(feats)
+    assert len(outs) == fpn_model.num_outs
+    for i in range(fpn_model.num_outs):
+        outs[i].shape[1] == out_channels
+        outs[i].shape[2] == outs[i].shape[3] == s // (2**i)
