546740296c178ab5535aa174f249d20b92aee5f5
Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>
2020-05-23 20:07:28 +0800

    Support RegNetX models (#2710)
    * Add RegNet, docs/refactor TBD
    
    * Support RegNet
    
    * Add RegNets
    
    * Modify docs and refactor code
    
    * fix pre-train bug
    
    * Performance ready
    
    * Use public model weights
    
    * Performance & settings done
    
    * Fix unittest bug
    
    * Try another way of test config
    
    * Add unittest and refactor
    
    * Reformat
    
    * Finish refactor regnet
    
    * merge tests
    
    * fix conflicts
    
    * Fix resnext CI bug
    
    * rename parameter
    
    * Add ref link & clean _delete_
    
    * tweaks of doc
    
    * try resnext doctest
    
    * add expansion in block
    
    * remove doctest of resnext

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -1,7 +1,10 @@
 from .hrnet import HRNet
+from .regnet import RegNet
 from .res2net import Res2Net
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 
-__all__ = ['ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net']
+__all__ = [
+    'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net'
+]
