754ec972e93a5aed80a438bd29b7962544ccf3c0
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2020-05-24 17:41:32 +0800

    Implement NASFCOS (#2682)
    * Implement NAS-FCOS
    
    1. Supports NASFCOS
    2. Added NASFCOS_FPN
    3. Added NASFCOS_Head
    4. Added r50 config
    
    * Revert "Implement NAS-FCOS"
    
    This reverts commit ebae11cbbdc5b4f3cc7226c763e9254c94a9c92b.
    
    * Revert "Revert "Implement NAS-FCOS""
    
    This reverts commit 32bca597859a447de62ddf0f2c2f482809730da9.
    
    * Implement NASFCOS
    
    1. Supports NASFCOS
    2. Added NASFCOS_FPN
    3. Added NASFCOS_Head
    4. Added r50 config
    
    * reformatted & use list to build convs in head
    
    1. code refornatted
    2. add docstring in nasfcos_head.py
    3. use a list of conv config to build convs, rather than names
    
    * cells merged
    
    1. merge cells in nasfpn ans nasfcos_fpn
    2. add nasfpn_cell_factory.py
    
    * reformatted
    
    * fixed
    
    * fixed
    
    * Update nasfpn_cell_factory.py
    
    * pre-commit
    
    * bug fixed
    
    * renamed&moved
    
    * not done
    
    * fixed
    
    * fixed
    
    * Delete remove_parawise_cfg_nasfcos_r50_nashead_4x4_coco.py
    
    * fixed
    
    * Create 1x_bias_lr.py
    
    * Revert "Create 1x_bias_lr.py"
    
    This reverts commit 54602ded283a8f646aebfa0f216350fd2ce82e6d.
    
    * fixed
    
    * fixed
    
    * fixed docstring
    
    * init modified
    
    * formatted
    
    * fixed
    
    * updated
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * pass test
    
    * fixed
    
    * fixed
    
    * fixed
    
    * rename
    
    * 123
    
    * nasfcos_readme
    
    * Update README.md
    
    * rename & leave empty
    
    * GN->GN-head in README
    
    * fixed

--- mmdet/models/dense_heads/__init__.py
+++ mmdet/models/dense_heads/__init__.py
@@ -7,6 +7,7 @@ from .fsaf_head import FSAFHead
 from .ga_retina_head import GARetinaHead
 from .ga_rpn_head import GARPNHead
 from .guided_anchor_head import FeatureAdaption, GuidedAnchorHead
+from .nasfcos_head import NASFCOSHead
 from .pisa_retinanet_head import PISARetinaHead
 from .pisa_ssd_head import PISASSDHead
 from .reppoints_head import RepPointsHead
@@ -19,5 +20,5 @@ __all__ = [
     'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
     'GARPNHead', 'RetinaHead', 'RetinaSepBNHead', 'GARetinaHead', 'SSDHead',
     'FCOSHead', 'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead',
-    'ATSSHead', 'FSAFHead', 'PISARetinaHead', 'PISASSDHead'
+    'ATSSHead', 'FSAFHead', 'NASFCOSHead', 'PISARetinaHead', 'PISASSDHead'
 ]
