1a63f5b55684b853c431dacd39270817e379341f
tianyuandu <tianyuandu@gmail.com>
2020-06-01 19:50:17 +0800

    fix unittest

--- tests/test_corner_pool.py
+++ tests/test_corner_pool.py
@@ -1,22 +1,22 @@
 """
 CommandLine:
-    pytest tests/test_center_pool.py
+    pytest tests/test_corner_pool.py
 """
 import pytest
 import torch
 
-from mmdet.ops import CenterPool
+from mmdet.ops import CornerPool
 
 
-def test_center_pool_device_and_dtypes_cpu():
+def test_corner_pool_device_and_dtypes_cpu():
     """
     CommandLine:
-        xdoctest -m tests/test_center_pool.py \
-            test_center_pool_device_and_dtypes_cpu
+        xdoctest -m tests/test_corner_pool.py \
+            test_corner_pool_device_and_dtypes_cpu
     """
     with pytest.raises(AssertionError):
         # pool mode must in ['bottom', 'left', 'right', 'top']
-        pool = CenterPool('center')
+        pool = CornerPool('corner')
 
     lr_tensor = torch.tensor([[[[0, 0, 0, 0, 0], [2, 1, 3, 0, 2],
                                 [5, 4, 1, 1, 6], [0, 0, 0, 0, 0],
@@ -28,7 +28,7 @@ def test_center_pool_device_and_dtypes_cpu():
     left_answer = torch.tensor([[[[0, 0, 0, 0, 0], [3, 3, 3, 2, 2],
                                   [6, 6, 6, 6, 6], [0, 0, 0, 0, 0],
                                   [0, 0, 0, 0, 0]]]])
-    pool = CenterPool('left')
+    pool = CornerPool('left')
     left_tensor = pool(lr_tensor)
     assert left_tensor.type() == lr_tensor.type()
     assert torch.equal(left_tensor, left_answer)
@@ -36,7 +36,7 @@ def test_center_pool_device_and_dtypes_cpu():
     right_answer = torch.tensor([[[[0, 0, 0, 0, 0], [2, 2, 3, 3, 3],
                                    [5, 5, 5, 5, 6], [0, 0, 0, 0, 0],
                                    [0, 0, 0, 0, 0]]]])
-    pool = CenterPool('right')
+    pool = CornerPool('right')
     right_tensor = pool(lr_tensor)
     assert right_tensor.type() == lr_tensor.type()
     assert torch.equal(right_tensor, right_answer)
@@ -44,7 +44,7 @@ def test_center_pool_device_and_dtypes_cpu():
     top_answer = torch.tensor([[[[0, 3, 4, 0, 0], [0, 3, 4, 0, 0],
                                  [0, 3, 4, 0, 0], [0, 2, 2, 0, 0],
                                  [0, 0, 2, 0, 0]]]])
-    pool = CenterPool('top')
+    pool = CornerPool('top')
     top_tensor = pool(tb_tensor)
     assert top_tensor.type() == tb_tensor.type()
     assert torch.equal(top_tensor, top_answer)
@@ -52,7 +52,7 @@ def test_center_pool_device_and_dtypes_cpu():
     bottom_answer = torch.tensor([[[[0, 3, 1, 0, 0], [0, 3, 1, 0, 0],
                                     [0, 3, 4, 0, 0], [0, 3, 4, 0, 0],
                                     [0, 3, 4, 0, 0]]]])
-    pool = CenterPool('bottom')
+    pool = CornerPool('bottom')
     bottom_tensor = pool(tb_tensor)
     assert bottom_tensor.type() == tb_tensor.type()
     assert torch.equal(bottom_tensor, bottom_answer)
