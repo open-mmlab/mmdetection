703084f62cdb8176688567b4a7190e618eb1e28d
wangxinjiang <wangxinjiang@sensetime.com>
2020-06-08 17:03:37 +0800

    Deprecate DistOptimizerHook

--- mmdet/core/utils/dist_utils.py
+++ mmdet/core/utils/dist_utils.py
@@ -42,15 +42,5 @@ def allreduce_grads(params, coalesce=True, bucket_size_mb=-1):
 
 
 class DistOptimizerHook(OptimizerHook):
-
-    def __init__(self, grad_clip=None, coalesce=True, bucket_size_mb=-1):
-        self.grad_clip = grad_clip
-        self.coalesce = coalesce
-        self.bucket_size_mb = bucket_size_mb
-
-    def after_train_iter(self, runner):
-        runner.optimizer.zero_grad()
-        runner.outputs['loss'].backward()
-        if self.grad_clip is not None:
-            self.clip_grads(runner.model.parameters())
-        runner.optimizer.step()
+    """Deprecated optimizer hook for distributed training"""
+    pass
