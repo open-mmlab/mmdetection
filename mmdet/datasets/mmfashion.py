1045c26d8bd94609d0fed10235f6aaf26d90aa29
Jiaqi Wang <wj017@ie.cuhk.edu.hk>
2020-06-09 23:09:19 +0800

    Update mmfashion.py

--- mmdet/datasets/mmfashion.py
+++ mmdet/datasets/mmfashion.py
@@ -4,6 +4,6 @@ from .coco import CocoDataset
 
 @DATASETS.register_module()
 class mmfashionDataset(CocoDataset):
-    CLASSES=('top', 'skirt', 'leggings', 'dress', 'outer',
-             'pants', 'bag', 'neckwear', 'headwear', 'eyeglass',
-             'belt', 'footwear', 'hair', 'skin', 'face')
+    CLASSES = ('top', 'skirt', 'leggings', 'dress', 'outer', 'pants', 'bag',
+               'neckwear', 'headwear', 'eyeglass', 'belt', 'footwear', 'hair',
+               'skin', 'face')
