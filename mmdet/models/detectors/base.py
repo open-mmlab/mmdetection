a47c48a422db31f11e63d1eac9fcd3875661cccf
Kai Chen <chenkaidev@gmail.com>
2020-06-10 23:35:24 +0800

    switch to EpochBasedRunner

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -1,8 +1,11 @@
 import warnings
 from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
 
 import mmcv
 import numpy as np
+import torch
+import torch.distributed as dist
 import torch.nn as nn
 from mmcv.utils import print_log
 
@@ -148,6 +151,48 @@ class BaseDetector(nn.Module, metaclass=ABCMeta):
             return self.forward_train(img, img_metas, **kwargs)
         else:
             return self.forward_test(img, img_metas, **kwargs)
+
+    def _parse_losses(self, losses):
+        log_vars = OrderedDict()
+        for loss_name, loss_value in losses.items():
+            if isinstance(loss_value, torch.Tensor):
+                log_vars[loss_name] = loss_value.mean()
+            elif isinstance(loss_value, list):
+                log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
+            else:
+                raise TypeError(
+                    f'{loss_name} is not a tensor or list of tensors')
+
+        loss = sum(_value for _key, _value in log_vars.items()
+                   if 'loss' in _key)
+
+        log_vars['loss'] = loss
+        for loss_name, loss_value in log_vars.items():
+            # reduce loss when distributed training
+            if dist.is_available() and dist.is_initialized():
+                loss_value = loss_value.data.clone()
+                dist.all_reduce(loss_value.div_(dist.get_world_size()))
+            log_vars[loss_name] = loss_value.item()
+
+        return loss, log_vars
+
+    def train_step(self, data, optimizer):
+        losses = self(**data)
+        loss, log_vars = self._parse_losses(losses)
+
+        outputs = dict(
+            loss=loss, log_vars=log_vars, num_samples=len(data['img'].data))
+
+        return outputs
+
+    def val_step(self, data, optimizer):
+        losses = self(**data)
+        loss, log_vars = self._parse_losses(losses)
+
+        outputs = dict(
+            loss=loss, log_vars=log_vars, num_samples=len(data['img'].data))
+
+        return outputs
 
     def show_result(self,
                     img,
