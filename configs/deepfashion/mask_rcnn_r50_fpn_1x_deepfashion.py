1c10d32bc51ac4586e619c49046e8fb7127e0ce3
veralauee <veralau9425@gmail.com>
2020-06-11 15:11:21 -0700

    refactor deepfashion config file

--- configs/deepfashion/mask_rcnn_r50_fpn_1x_deepfashion.py
+++ configs/deepfashion/mask_rcnn_r50_fpn_1x_deepfashion.py
@@ -1,17 +1,11 @@
 _base_ = [
     '../_base_/models/mask_rcnn_r50_fpn.py',
-    '../_base_/datasets/deepfashion.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    '../_base_/datasets/deepfashion.py', '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime.py'
 ]
 model = dict(
     roi_head=dict(
-        bbox_head=dict(num_classes=15),
-        mask_head=dict(num_classes=15)))
+        bbox_head=dict(num_classes=15), mask_head=dict(num_classes=15)))
 # runtime settings
 total_epochs = 12
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-work_dir = './work_dirs/mask_rcnn_r50_fpn_1x_deepfashion'
-load_from = None
-resume_from = None
 workflow = [('train', 5)]
