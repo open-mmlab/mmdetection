1f1ed2c8e82cc66b458bce6c2d9e66a0431c0027
Jiarui XU <xvjiarui0826@gmail.com>
2020-06-13 23:44:25 +0800

    add todo

--- mmdet/ops/dcn/deform_conv.py
+++ mmdet/ops/dcn/deform_conv.py
@@ -177,6 +177,7 @@ class ModulatedDeformConvFunction(Function):
         channels_out = weight.size(0)
         height, width = input.shape[2:4]
         kernel_h, kernel_w = weight.shape[2:4]
+        # TODO: support different padding/stride/dilation in height and width
         height_out = (height + 2 * ctx.padding -
                       (ctx.dilation * (kernel_h - 1) + 1)) // ctx.stride + 1
         width_out = (width + 2 * ctx.padding -
