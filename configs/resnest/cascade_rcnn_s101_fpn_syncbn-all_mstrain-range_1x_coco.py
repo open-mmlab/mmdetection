10c74a7dc4a6461b80368ca9b8aec651f6e65a87
Jiarui XU <xvjiarui0826@gmail.com>
2020-06-24 19:58:03 +0800

    add range ms

---
+++ configs/resnest/cascade_rcnn_s101_fpn_syncbn-all_mstrain-range_1x_coco.py
@@ -0,0 +1,4 @@
+_base_ = './cascade_rcnn_s50_fpn_syncbn-all_mstrain-range_1x_coco.py'
+model = dict(
+    pretrained='pretrain_model/resnest101-d6979a76.pth',
+    backbone=dict(stem_channels=128, depth=101))
