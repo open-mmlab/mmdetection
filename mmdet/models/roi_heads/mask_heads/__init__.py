88ef48f08fbe9a2c3562ca16447d9a80dd2bf65d
Jiarui XU <xvjiarui0826@gmail.com>
2020-06-26 23:25:10 +0800

    rename

--- mmdet/models/roi_heads/mask_heads/__init__.py
+++ mmdet/models/roi_heads/mask_heads/__init__.py
@@ -3,8 +3,8 @@ from .fcn_mask_head import FCNMaskHead
 from .fused_semantic_head import FusedSemanticHead
 from .grid_head import GridHead
 from .htc_mask_head import HTCMaskHead
+from .mask_point_head import MaskPointHead
 from .maskiou_head import MaskIoUHead
-from .point_head import MaskPointHead
 
 __all__ = [
     'FCNMaskHead', 'HTCMaskHead', 'FusedSemanticHead', 'GridHead',
