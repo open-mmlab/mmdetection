888a0e3b2623e7108c794d329e17f7f778e2ff7c
Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>
2020-06-28 23:49:42 +0800

    rename regnet configs (#3143)
    * rename regnet configs
    
    * Further fix bugs
    
    * Update 400MF
    
    * fix name bugs in configs
    
    * fix bn default

--- configs/regnet/mask_rcnn_regnetx-3.2GF_fpn_mstrain_3x_coco.py
+++ configs/regnet/mask_rcnn_regnetx-3.2GF_fpn_mstrain_3x_coco.py
@@ -20,15 +20,20 @@ model = dict(
         out_channels=256,
         num_outs=5))
 img_norm_cfg = dict(
-    # The mean and std is used in PyCls when training RegNets
+    # The mean and std are used in PyCls when training RegNets
     mean=[103.53, 116.28, 123.675],
     std=[57.375, 57.12, 58.395],
     to_rgb=False)
 train_pipeline = [
-    # RegNet convert images to float32 directly after loading in PyCls
+    # Images are converted to float32 directly after loading in PyCls
     dict(type='LoadImageFromFile', to_float32=True),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -55,3 +60,7 @@ data = dict(
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.00005)
+lr_config = dict(step=[28, 34])
+total_epochs = 36
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
