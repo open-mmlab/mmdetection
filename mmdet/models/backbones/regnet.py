0526c01bf1f3250b0045348866d33c257f57e2d2
ZwwWayne <wayne.zw@outlook.com>
2020-06-28 22:39:35 +0800

    Update 400MF

--- mmdet/models/backbones/regnet.py
+++ mmdet/models/backbones/regnet.py
@@ -62,6 +62,8 @@ class RegNet(ResNet):
         (1, 1008, 1, 1)
     """
     arch_settings = {
+        'regnetx_400mf':
+        dict(w0=24, wa=24.48, wm=2.54, group_w=16, depth=22, bot_mul=1.0),
         'regnetx_800mf':
         dict(w0=56, wa=35.73, wm=2.28, group_w=16, depth=16, bot_mul=1.0),
         'regnetx_1.6gf':
