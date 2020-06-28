5f0b194ba069d9600da29acbc513638634368b86
wwzhang <wayne.zw@outlook.com>
2020-06-28 21:21:49 +0800

    Fix num_classes in SSD

--- configs/wider_face/ssd300_wider_face.py
+++ configs/wider_face/ssd300_wider_face.py
@@ -2,7 +2,7 @@ _base_ = [
     '../_base_/models/ssd300.py', '../_base_/datasets/wider_face.py',
     '../_base_/default_runtime.py'
 ]
-model = dict(bbox_head=dict(num_classes=2))
+model = dict(bbox_head=dict(num_classes=1))
 # optimizer
 optimizer = dict(type='SGD', lr=0.012, momentum=0.9, weight_decay=5e-4)
 optimizer_config = dict()
