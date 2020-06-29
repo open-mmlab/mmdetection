9c52747ad9fa1bb7dda85e9b628d5af50a8f9adb
gt950513 <gt950513@mail.ustc.edu.cn>
2020-06-29 14:17:50 +0800

    use Compose in AutoAugment

--- mmdet/datasets/pipelines/auto_augment.py
+++ mmdet/datasets/pipelines/auto_augment.py
@@ -1,10 +1,9 @@
 import copy
 
 import numpy as np
-import pycocotools.mask as mask_util
 
-from mmdet.core import BitmapMasks, PolygonMasks
 from ..builder import PIPELINES
+from .compose import Compose
 
 
 @PIPELINES.register_module()
@@ -14,7 +13,7 @@ class AutoAugment(object):
     This data augmentation is proposed in
     `Learning Data Augmentation Strategies for Object Detection <https://arxiv.org/pdf/1906.11172>`_  # noqa: E501
     Required key is "img", and optional keys are "gt_bboxes", "gt_masks",
-    "gt_seg".
+    "gt_semantic_seg".
 
     Args:
         policies (list[list[dict]]): The policies of auto augmentation. Each
@@ -51,7 +50,7 @@ class AutoAugment(object):
         >>> results = augmentation(results)
     """
 
-    def __init__(self, policies=None):
+    def __init__(self, policies):
         assert isinstance(policies, list) and len(policies) > 0, \
             'The type and length of policies must be list and more than 0, ' \
             'respectively.'
@@ -67,59 +66,9 @@ class AutoAugment(object):
         self.policies = copy.deepcopy(policies)
 
     def __call__(self, results):
-        if self.policies is not None:
-            img = results['img']
-            gt_bboxes = results.get('gt_bboxes', None)
-            gt_masks = results.get('gt_masks', None)
-            gt_seg = results.get('gt_seg', None)
-
-            img, gt_bboxes, gt_masks, gt_seg = self.auto_augment_core(
-                img, gt_bboxes, gt_masks, gt_seg, self.policies)
-
-            results['img'] = img
-            if 'gt_bboxes' in results:
-                results['gt_bboxes'] = gt_bboxes
-            if 'gt_masks' in results:
-                results['gt_masks'] = gt_masks
-            if 'gt_seg' in results:
-                results['gt_seg'] = gt_seg
-
-        return results
-
-    def auto_augment_core(self, img, gt_bboxes, gt_masks, gt_seg, policies):
-        h, w, c = img.shape
-        wh_rescale = np.array([[w, h, w, h]])
-        # normalize bboxes
-        gt_bboxes /= wh_rescale
-        gt_masks = gt_masks.copy()
-        if gt_masks is not None:
-            gt_masks_numpy = gt_masks.to_ndarray()
-        gt_seg = gt_seg.copy()
-
-        # auto augment
-        select_policies = policies[np.random.randint(len(policies))]
-        for policy in select_policies:
-            policy = policy.copy()
-            p = eval(policy.pop('type'))(**policy)
-            img, gt_bboxes, gt_masks_numpy, gt_seg = p(img, gt_bboxes,
-                                                       gt_masks_numpy, gt_seg)
-
-        h, w, c = img.shape
-        wh_rescale = np.array([[w, h, w, h]])
-        gt_bboxes *= wh_rescale
-        if isinstance(gt_masks, BitmapMasks):
-            gt_masks = BitmapMasks(gt_masks_numpy, h, w)
-        elif isinstance(gt_masks, PolygonMasks):
-            encoded_gt_masks = []
-            for i in range(len(gt_masks_numpy)):
-                encoded_gt_masks.append(
-                    mask_util.encode(
-                        np.array(
-                            gt_masks_numpy[i, :, :, np.newaxis],
-                            order='F',
-                            dtype='uint8'))[0])  # encoded with RLE
-            gt_masks = PolygonMasks(encoded_gt_masks, h, w)
-        return img, gt_bboxes, gt_masks, gt_seg
+        select_policies = self.policies[np.random.randint(len(self.policies))]
+        augmentation = Compose(select_policies)
+        return augmentation(results)
 
     def __repr__(self):
         return f'{self.__class__.__name__}' \
