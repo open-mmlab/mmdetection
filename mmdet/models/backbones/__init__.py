4e40f6f0a1b9101f2d53fbc4b801bcf074ff849a
Siyuan Qiao <joe.siyuan.qiao@gmail.com>
2020-06-30 05:56:17 -0400

    Add support for DetectoRS. (#3064)
    * Add support for DetectoRS.
    
    * Fix Travis CI build failure and remove loading rfp_pretrained for test
    
    * Revise the code according to the PR reviews:
    
    * remove unnecessary `,`
    
    * add docstring to ASPP and ConvAWS2d
    
    * make args of ASPP configurable
    
    * simplify the code of ConvAWS2d
    
    * import nn and functional for SAConv2d
    
    * rewrite rfp_conv with build_conv_layer and constant_init
    
    * rename rfp_inp with rfp_inplanes
    
    * reformat code to pass Travis CI build
    
    * Revise codes according to the PR reviews:
    
    * Create new backbone DetectoRS_ResNe(X)t to incorporate RFP and SAC.
    
    * Initialize modules with mmcv.cnn.
    
    * Revise the codes of RFP.
    
    * Revise codes according to the PR comments:
    
    * Add docstrings to Bottleneck in mmdet/models/backbones/detectors_resnet.py.
    
    * Add docstrings to _load_from_state_dict in mmdet/ops/conv_ws.py.
    
    * Initialize the switch of SAC with mmcv.
    
    * minior refactor and docstring
    
    * update doc
    
    * update README
    
    * move init into init_weights()
    
    * update model zoo
    
    * add gfl
    
    Co-authored-by: Jiarui XU <xvjiarui0826@gmail.com>

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -1,3 +1,5 @@
+from .detectors_resnet import DetectoRS_ResNet
+from .detectors_resnext import DetectoRS_ResNeXt
 from .hourglass import HourglassNet
 from .hrnet import HRNet
 from .regnet import RegNet
@@ -8,5 +10,5 @@ from .ssd_vgg import SSDVGG
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
-    'HourglassNet'
+    'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt'
 ]
