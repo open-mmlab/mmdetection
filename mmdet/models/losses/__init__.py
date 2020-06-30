e7f433780e491a12f346351b155588a50f565eb7
Alexander Liao <peiyuanl@andrew.cmu.edu>
2020-06-29 22:46:29 -0400

    fix isort error

--- mmdet/models/losses/__init__.py
+++ mmdet/models/losses/__init__.py
@@ -6,8 +6,8 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
 from .focal_loss import FocalLoss, sigmoid_focal_loss
 from .gaussian_focal_loss import GaussianFocalLoss
 from .ghm_loss import GHMC, GHMR
-from .iou_loss import (BoundedIoULoss, GIoULoss, CIoULoss, DIoULoss,
-                       IoULoss, bounded_iou_loss, iou_loss)
+from .iou_loss import (BoundedIoULoss, CIoULoss, DIoULoss, GIoULoss, IoULoss,
+                       bounded_iou_loss, iou_loss)
 from .mse_loss import MSELoss, mse_loss
 from .pisa_loss import carl_loss, isr_p
 from .smooth_l1_loss import L1Loss, SmoothL1Loss, l1_loss, smooth_l1_loss
--- mmdet/models/losses/iou_loss.py
+++ mmdet/models/losses/iou_loss.py
@@ -1,6 +1,7 @@
+import math
+
 import torch
 import torch.nn as nn
-import math
 
 from mmdet.core import bbox_overlaps
 from ..builder import LOSSES
