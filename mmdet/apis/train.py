a04d3c62846eeee490d938b7071f26beb3347e27
wangxinjiang <wangxinjiang@sensetime.com>
2020-07-24 22:25:35 +0800

    fix a bug in custom_hooks

--- mmdet/apis/train.py
+++ mmdet/apis/train.py
@@ -124,7 +124,13 @@ def train_detector(model,
 
     # user-defined hooks
     if cfg.get('custom_hooks', None):
-        for hook_cfg in cfg.hooks:
+        custom_hooks = cfg.custom_hooks
+        assert isinstance(custom_hooks, list), \
+            f'custom_hooks expect list type, but got {type(custom_hooks)}'
+        for hook_cfg in cfg.custom_hooks:
+            assert isinstance(hook_cfg, dict), \
+                'Each item in custom_hooks expects dict type, but got ' \
+                f'{type(hook_cfg)}'
             hook_cfg = hook_cfg.copy()
             priority = hook_cfg.pop('priority', 'NORMAL')
             hook = build_from_cfg(hook_cfg, HOOKS)
