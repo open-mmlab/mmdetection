06c2e4fab4d0b4b1b25bb10e9f25f93502644854
Jiarui XU <xvjiarui0826@gmail.com>
2020-07-30 12:18:48 +0800

    update s101 pretrain

--- configs/resnest/mask_rcnn_s101_fpn_syncbn-all_mstrain_1x_coco.py
+++ configs/resnest/mask_rcnn_s101_fpn_syncbn-all_mstrain_1x_coco.py
@@ -1,4 +1,4 @@
 _base_ = './mask_rcnn_s50_fpn_syncbn-all_mstrain_1x_coco.py'
 model = dict(
-    pretrained='pretrain_model/resnest101_d2-9cb052ad.pth',
+    pretrained='pretrain_model/resnest101_d2-f3b931b2.pth',
     backbone=dict(stem_channels=128, depth=101))
