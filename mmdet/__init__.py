f324df79bdfa7e1d07dd05c4fc0ee27f0622c0ef
ZwwWayne <wayne.zw@outlook.com>
2020-08-02 14:17:10 +0800

    check mmcv version

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -1,3 +1,11 @@
+import mmcv
+
 from .version import __version__, short_version
+
+mmcv_version = '1.0.4'
+
+assert mmcv.__version__ == mmcv_version, \
+    f'MMCV=={mmcv.__version__} is used but incompatible.' \
+    f'Please install mmcv=={mmcv_version}'
 
 __all__ = ['__version__', 'short_version']
