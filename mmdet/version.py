41fcbc3bcc1b9643aafae91e47981c731f637ade
ZwwWayne <wayne.zw@outlook.com>
2020-08-03 23:40:34 +0800

    use function to parse version

--- mmdet/version.py
+++ mmdet/version.py
@@ -2,4 +2,18 @@
 
 __version__ = '2.3.0rc0'
 short_version = '2.3.0rc0'
-version_info = (2, 3, '0rc0')
+
+
+def parse_version_info():
+    version_info = []
+    for x in short_version.split('.'):
+        if x.isdigit():
+            version_info.append(int(x))
+        elif x.find('rc') != -1:
+            patch_version = x.split('rc')
+            version_info.append(int(patch_version[0]))
+            version_info.append(f'rc{patch_version[1]}')
+    return tuple(version_info)
+
+
+version_info = parse_version_info()
