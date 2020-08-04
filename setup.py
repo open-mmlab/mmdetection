9d3f9b03c4d10683fd4402ad08b7f70d7568b3da
Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>
2020-08-04 15:18:09 +0800

    Remove git hash in __version__ (#3466)
    * update setup.py
    
    * update collect_env accordingly
    
    * rm VERSION and hardcode version.py
    
    * Use get_git_hash in mmcv
    
    * fix bug of version_file
    
    * use function to parse version
    
    * use __version__
    
    * only print first 7 of git hash
    
    * resolve comments
    
    * Fix torch install issue in action

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 import os
-import subprocess
-import time
 from setuptools import find_packages, setup
 
 import torch
@@ -16,68 +14,6 @@ def readme():
 
 
 version_file = 'mmdet/version.py'
-
-
-def get_git_hash():
-
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        sha = out.strip().decode('ascii')
-    except OSError:
-        sha = 'unknown'
-
-    return sha
-
-
-def get_hash():
-    if os.path.exists('.git'):
-        sha = get_git_hash()[:7]
-    elif os.path.exists(version_file):
-        try:
-            from mmdet.version import __version__
-            sha = __version__.split('+')[-1]
-        except ImportError:
-            raise ImportError('Unable to get git version')
-    else:
-        sha = 'unknown'
-
-    return sha
-
-
-def write_version_py():
-    content = """# GENERATED VERSION FILE
-# TIME: {}
-
-__version__ = '{}'
-short_version = '{}'
-version_info = ({})
-"""
-    sha = get_hash()
-    with open('mmdet/VERSION', 'r') as f:
-        SHORT_VERSION = f.read().strip()
-    VERSION_INFO = ', '.join(
-        [x if x.isdigit() else f'"{x}"' for x in SHORT_VERSION.split('.')])
-    VERSION = SHORT_VERSION + '+' + sha
-
-    version_file_str = content.format(time.asctime(), VERSION, SHORT_VERSION,
-                                      VERSION_INFO)
-    with open(version_file, 'w') as f:
-        f.write(version_file_str)
 
 
 def get_version():
@@ -190,12 +126,12 @@ def parse_requirements(fname='requirements.txt', with_version=True):
 
 
 if __name__ == '__main__':
-    write_version_py()
     setup(
         name='mmdet',
         version=get_version(),
         description='OpenMMLab Detection Toolbox and Benchmark',
         long_description=readme(),
+        long_description_content_type='text/markdown',
         author='OpenMMLab',
         author_email='openmmlab@gmail.com',
         keywords='computer vision, object detection',
