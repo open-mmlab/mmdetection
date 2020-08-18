f411a6a6782907bf2cd2ca7c68a29e8c4a6cbab1
v-qjqs <qjliqiaofei@gmail.com>
2020-08-18 10:43:30 +0800

    add doc string and bug fix

--- mmdet/datasets/pipelines/auto_augment.py
+++ mmdet/datasets/pipelines/auto_augment.py
@@ -68,6 +68,15 @@ class AutoAugment(object):
         self.transforms = [Compose(policy) for policy in self.policies]
 
     def __call__(self, results):
+        """Call function to randomly select transforms and perform
+        transformation.
+
+        Args:
+            results (dict): Result dict from loading pipeline.
+
+        Returns:
+            dict: Transformed results.
+        """
         transform = np.random.choice(self.transforms)
         return transform(results)
 
@@ -80,20 +89,21 @@ class Translate(object):
     """Translate images along with x-axis or y-axis.
 
     Args:
-        # base_offset (float): base_offset * 25.0 is the actual offset of
-        #     translation.
-        level (int or float): The level for Translate and should be in
+        level (int | float): The level for Translate and should be in
             range (0,_MAX_LEVEL]. This value controls the offset used for
             translate the image/bboxes/masks/seg along with x-axis or y-axis.
         prob (float): The probability for perform translating and should be in
             range 0 to 1.
-        fill_val (float or tuple): The filled values for image border.
+        fill_val (int | float | tuple): The filled values for image border.
             If float, the same fill_val will be used for all the three channels
             of image. If tuple, the should be 3 elements (e.g. equals the
-            number of channels for image)
+            number of channels for image).
+        seg_ignore_label (int): The ``fill_val`` used for segmentation map.
+            Note this value must equals ``ignore_label`` in ``semantic_head``
+            of the corresponding config. Default 255.
         axis (str): Translate images along with x-axis or y-axis. The option
             of axis is 'x' or 'y'.
-        max_translate_offset (int or float): Pixel's maximum offset value for
+        max_translate_offset (int | float): Pixel's maximum offset value for
             Translate along with x-axis or y-axis.
     """
     _MAX_LEVEL = 10
@@ -101,7 +111,8 @@ class Translate(object):
     def __init__(self,
                  level,
                  prob=0.5,
-                 fill_val=128.,
+                 fill_val=128,
+                 seg_ignore_label=255,
                  axis='x',
                  max_translate_offset=250.):
         assert isinstance(level, (int, float)), \
@@ -132,6 +143,7 @@ class Translate(object):
         self.level = level
         self.prob = prob
         self.fill_val = fill_val
+        self.seg_ignore_label = seg_ignore_label
         self.axis = axis
         self.max_translate_offset = max_translate_offset
 
@@ -142,6 +154,21 @@ class Translate(object):
                    fill_val,
                    flags=cv2.INTER_NEAREST,
                    borderMode=cv2.BORDER_CONSTANT):
+        """Affine wrapper which transforms the source data using the given
+        trans_matrix.
+
+        Args:
+            data (np.ndarray): Source data.
+            trans_matrix (np.ndarray): Transformation matrix with shape (2, 3).
+            out_size (tuple): Size of the output data with format (w, h).
+            fill_val (int | float | tuple): Value used in case of a constant
+                border.
+            flags: Interpolation methods used in ``cv2.warpAffine``.
+            borderMode: pixel extrapolation method used in ``cv2.warpAffine``.
+
+        Returns:
+            np.ndarray: transformed data with the same shape as input data.
+        """
         return cv2.warpAffine(
             data,
             trans_matrix,
@@ -151,6 +178,19 @@ class Translate(object):
             borderValue=fill_val)
 
     def __call__(self, results, min_size=0.0, neg_offset_prob=0.5):
+        """Call function to translate images, bounding boxes, masks and
+        semantic segmentation maps.
+
+        Args:
+            results (dict): Result dict from loading pipeline.
+            min_size (int | float): Minimum pixel size for the
+             translated bboxes.
+            neg_offset_prob (float): The probability that turns the
+             offset negative.
+
+        Returns:
+            dict: Translated results.
+        """
         if np.random.rand() > self.prob:
             return results
 
@@ -166,21 +206,27 @@ class Translate(object):
         self._translate_bboxes(results, offset)
         # fill_val set to 0 for background of mask.
         self._translate_masks(results, trans_matrix, fill_val=0)
-        # fill_val set to 255 for the ignored value of segmentation map.
-        self._translate_seg(results, trans_matrix, fill_val=255)
+        # fill_val set to ``seg_ignore_label`` for the ignored value
+        # of segmentation map.
+        self._translate_seg(
+            results, trans_matrix, fill_val=self.seg_ignore_label)
         self._filter_invalid(results, min_size=min_size)
         return results
 
     def _translate_img(self, results, trans_matrix, fill_val):
+        """Translate images horizontally or vertically, according to
+        ``trans_matrix``."""
         for key in results.get('img_fields', ['img']):
             results[key] = self.warpAffine(results[key], trans_matrix,
                                            results[key].shape[:2][::-1],
                                            fill_val)
 
     def _translate_bboxes(self, results, offset):
+        """Shift bboxes horizontally or vertically, according to ``offset``."""
         h, w, c = results['img_shape']
-        for key in results.get('box_fields', []):
-            min_x, min_y, max_x, max_y = results[key]
+        for key in results.get('bbox_fields', []):
+            min_x, min_y, max_x, max_y = np.split(
+                results[key], results[key].shape[-1], axis=-1)
             if self.axis == 'x':
                 min_x = np.maximum(0, min_x + offset)
                 max_x = np.minimum(w, max_x + offset)
@@ -194,10 +240,13 @@ class Translate(object):
             max_x = np.clip(max_x, 0, w)
             max_y = np.clip(max_y, 0, h)
             # the boxs translated outside of image will be filtered along with
-            # the corresponding masks, by invoking func '_filter_invalid'.
-            results[key] = np.stack([min_x, min_y, max_x, max_y], -1)
+            # the corresponding masks, by invoking ``_filter_invalid``.
+            results[key] = np.concatenate([min_x, min_y, max_x, max_y],
+                                          axis=-1)
 
     def _translate_masks(self, results, trans_matrix, fill_val=0):
+        """Translate masks horizontally or vertically, according to
+        ``trans_matrix``."""
         h, w, c = results['img_shape']
         for key in results.get('mask_fields', []):
             translate_masks = []
@@ -216,13 +265,17 @@ class Translate(object):
                                 order='F',
                                 dtype='uint8'))[0])
 
-            if isinstance(results[key], BitmapMasks):
+            if isinstance(results[key],
+                          BitmapMasks) and len(translate_masks) > 0:
                 results[key] = BitmapMasks(
                     np.concatenate(translate_masks), h, w)
-            elif isinstance(results[key], PolygonMasks):
+            elif isinstance(results[key],
+                            PolygonMasks) and len(translate_masks) > 0:
                 results[key] = PolygonMasks(translate_masks, h, w)
 
     def _translate_seg(self, results, trans_matrix, fill_val=255):
+        """Translate segmentation maps horizontally or vertically, according to
+        ``trans_matrix``."""
         for key in results.get('seg_fields', []):
             results[key] = self.warpAffine(results[key], trans_matrix,
                                            results[key].shape[:2][::-1],
@@ -259,6 +312,7 @@ class Translate(object):
         repr_str += f'(level={self.level}, '
         repr_str += f'prob={self.prob}, '
         repr_str += f'fill_val={self.fill_val}, '
+        repr_str += f'seg_ignore_label={self.seg_ignore_label}, '
         repr_str += f'axis={self.axis}, '
         repr_str += f'max_translate_offset={self.max_translate_offset})'
         return repr_str
