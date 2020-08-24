e8ee996552290ce891664636ab7792df2fdca027
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-08-24 12:19:31 +0200

    Fix schedule

--- configs/efficientnet/retinanet_effb0_fpn_1x_coco.py
+++ configs/efficientnet/retinanet_effb0_fpn_1x_coco.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/models/retinanet_r50_fpn.py',
     '../_base_/datasets/coco_detection.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
 ]
 model = dict(
     pretrained='open-mmlab://efficientnet_b0',
@@ -60,7 +60,9 @@ data = dict(
 optimizer = dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=4e-5)
 
 lr_config = dict(
-    policy='cosine',
+    _delete_=True,
+    policy='CosineAnnealing',
+    min_lr=0,
     warmup='linear',
     warmup_iters=1,
     warmup_ratio=0.1,
