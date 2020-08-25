aca7323af9f3428421a024de109ca321b597c1fb
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2020-08-25 11:35:33 +0300

    update backends code

--- tools/test_exported.py
+++ tools/test_exported.py
@@ -67,7 +67,7 @@ def empty_result(num_classes=80, with_mask=False):
     return bbox_results
 
 
-class VideoDataset(object):
+class VideoDataset:
     def __init__(self, path, cfg, device='cpu'):
         self.path = path
         self.video = cv2.VideoCapture(self.path)
