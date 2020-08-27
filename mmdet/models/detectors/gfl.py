e4e007968f76cc0008712304fa3149671e914cc1
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-08-27 20:24:36 +0000

    Support TTA of GFL

--- mmdet/models/detectors/gfl.py
+++ mmdet/models/detectors/gfl.py
@@ -1,9 +1,10 @@
 from ..builder import DETECTORS
 from .single_stage import SingleStageDetector
+from .single_stage_test_mixins import BBoxTestMixin
 
 
 @DETECTORS.register_module()
-class GFL(SingleStageDetector):
+class GFL(SingleStageDetector, BBoxTestMixin):
 
     def __init__(self,
                  backbone,
@@ -14,3 +15,21 @@ class GFL(SingleStageDetector):
                  pretrained=None):
         super(GFL, self).__init__(backbone, neck, bbox_head, train_cfg,
                                   test_cfg, pretrained)
+
+    def aug_test(self, imgs, img_metas, rescale=False):
+        """Test function with test time augmentation.
+
+        Args:
+            imgs (list[Tensor]): the outer list indicates test-time
+                augmentations and inner Tensor should have a shape NxCxHxW,
+                which contains all images in the batch.
+            img_metas (list[list[dict]]): the outer list indicates test-time
+                augs (multiscale, flip, etc.) and the inner list indicates
+                images in a batch. each dict has image information.
+            rescale (bool, optional): Whether to rescale the results.
+                Defaults to False.
+
+        Returns:
+            list[ndarray]: bbox results of each class
+        """
+        return self.aug_test_bboxes(imgs, img_metas, rescale=rescale)
