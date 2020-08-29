63bf658b80b6158b374c993a8682e2e20c1e7e94
ZwwWayne <wayne.zw@outlook.com>
2020-08-29 15:22:19 +0800

    go back to use mmdet

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ def parse_requirements(fname='requirements.txt', with_version=True):
 
 if __name__ == '__main__':
     setup(
-        name='mmdetection',
+        name='mmdet',
         version=get_version(),
         description='OpenMMLab Detection Toolbox and Benchmark',
         long_description=readme(),
