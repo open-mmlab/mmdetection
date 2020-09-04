9c85a1cc847832584c95248065337af0166f38c1
wangxinjiang <wangxinjiang@sensetime.com>
2020-09-04 15:02:06 +0800

    Add replace_ImageToTensor for batch inference

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -1,4 +1,5 @@
 from .collect_env import collect_env
+from .config_hacks import replace_ImageToTensor
 from .logger import get_root_logger
 
-__all__ = ['get_root_logger', 'collect_env']
+__all__ = ['get_root_logger', 'collect_env', 'replace_ImageToTensor']
