6a963d8ee7225aa2993077b2dcffd001fd49df2a
Cao Yuhang <yhcao6@gmail.com>
2020-09-07 21:08:40 +0800

    sup show batch

--- mmdet/apis/test.py
+++ mmdet/apis/test.py
@@ -27,14 +27,15 @@ def single_gpu_test(model,
 
         batch_size = len(result)
         if show or out_dir:
-            assert batch_size == 1, 'show or out during test only support ' \
-                                    'batch size of 1'
-            img_tensor = data['img'][0]
+            if batch_size == 1 and isinstance(data['img'][0], torch.Tensor):
+                img_tensor = data['img'][0]
+            else:
+                img_tensor = data['img'][0].data[0]
             img_metas = data['img_metas'][0].data[0]
             imgs = tensor2imgs(img_tensor, **img_metas[0]['img_norm_cfg'])
             assert len(imgs) == len(img_metas)
 
-            for img, img_meta in zip(imgs, img_metas):
+            for i, (img, img_meta) in enumerate(zip(imgs, img_metas)):
                 h, w, _ = img_meta['img_shape']
                 img_show = img[:h, :w, :]
 
@@ -48,7 +49,7 @@ def single_gpu_test(model,
 
                 model.module.show_result(
                     img_show,
-                    result[0],
+                    result[i],
                     show=show,
                     out_file=out_file,
                     score_thr=show_score_thr)
