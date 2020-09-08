1e4d6d9e73e3208e15cb5ba8f7f7b9cf4e72ec48
yuzj <yuzongjie@gzrobot.com>
2020-09-08 19:10:04 +0800

    fix the isort error

--- mmdet/models/backbones/res2net.py
+++ mmdet/models/backbones/res2net.py
@@ -3,11 +3,13 @@ import math
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint as cp
-from mmcv.cnn import (build_conv_layer, build_norm_layer,
-                      constant_init, kaiming_init)
+from mmcv.cnn import (build_conv_layer, build_norm_layer, constant_init,
+                      kaiming_init)
 from mmcv.runner import load_checkpoint
 from torch.nn.modules.batchnorm import _BatchNorm
+
 from mmdet.utils import get_root_logger
+
 from ..builder import BACKBONES
 from .resnet import Bottleneck as _Bottleneck
 from .resnet import ResNet
