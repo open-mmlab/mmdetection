eb055eed89ab0da656b3f3d82c73c6036d892f40
yuzj <yuzongjie@gzrobot.com>
2020-09-08 18:02:29 +0800

    fix the format error

--- mmdet/models/backbones/res2net.py
+++ mmdet/models/backbones/res2net.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.utils.checkpoint as cp
 from mmcv.cnn import (build_conv_layer, build_norm_layer,
-                      constant_init, kaiming_init)
+                        constant_init, kaiming_init)
 from mmcv.runner import load_checkpoint
 from torch.nn.modules.batchnorm import _BatchNorm
 from mmdet.utils import get_root_logger
@@ -338,8 +338,8 @@ class Res2Net(ResNet):
                 for m in self.modules():
                     if isinstance(m, Bottle2neck):
                         for n in m.convs:
-                           if hasattr(n, 'conv_offset'): 
-                               constant_init(n.conv_offset, 0)
+                            if hasattr(n, 'conv_offset'): 
+                                constant_init(n.conv_offset, 0)
                                
             if self.zero_init_residual:
                 for m in self.modules():
