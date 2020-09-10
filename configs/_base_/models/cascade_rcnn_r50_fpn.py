40a6b6d1b515b40a45d003e1647c4d7904f70178
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2020-09-10 13:47:18 +0200

    reducing detection threshold for test 4 camera video

--- configs/_base_/models/cascade_rcnn_r50_fpn.py
+++ configs/_base_/models/cascade_rcnn_r50_fpn.py
@@ -178,6 +178,6 @@ test_cfg = dict(
         nms_thr=0.7,
         min_bbox_size=0),
     rcnn=dict(
-        score_thr=0.30,
-        nms=dict(type='nms', iou_threshold=0.60),
+        score_thr=0.10,
+        nms=dict(type='nms', iou_threshold=0.30),
         max_per_img=100))
