f5465aba33f6be117968a5717fd858911a326c95
RyanXLi <xlide@connect.ust.hk>
2020-09-10 10:58:18 +0800

    changed config

--- configs/yolo/yolov3_d53_608_spp.py
+++ configs/yolo/yolov3_d53_608_spp.py
@@ -139,9 +139,9 @@ lr_config = dict(
 total_epochs = 273
 evaluation = dict(interval=7, metric=['bbox'])
 
-# log_config = dict(  # config to register logger hook
-#     interval=50,  # Interval to print the log
-#     hooks=[
-#         dict(type='TensorboardLoggerHook'),
-#         dict(type='TextLoggerHook')
-#     ])  # The logger used to record the training process.
+log_config = dict(  # config to register logger hook
+    interval=50,  # Interval to print the log
+    hooks=[
+        dict(type='TensorboardLoggerHook'),
+        dict(type='TextLoggerHook')
+    ])  # The logger used to record the training process.
