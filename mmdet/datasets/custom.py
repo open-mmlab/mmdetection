1e345a89710fa001a92e2717473401cddc6cdef7
Jon <jonbakerfish@gmail.com>
2020-09-12 22:59:44 +0800

    only call `get_subset_by_classes` during training
    Only filter data when training but not in testing. Because test data is not labeled.

--- mmdet/datasets/custom.py
+++ mmdet/datasets/custom.py
@@ -81,7 +81,7 @@ class CustomDataset(Dataset):
         # load annotations (and proposals)
         self.data_infos = self.load_annotations(self.ann_file)
         # filter data infos if classes are customized
-        if self.custom_classes:
+        if self.custom_classes and not self.test_mode:
             self.data_infos = self.get_subset_by_classes()
 
         if self.proposal_file is not None:
