b0cffc6531286aa21d97c8ac7fd1fb19eb27f8ba
Ilya Krylov <ilya.krylov@intel.com>
2020-09-17 13:35:34 +0300

    added support of spn as rpn

--- mmdet/models/detectors/two_stage.py
+++ mmdet/models/detectors/two_stage.py
@@ -147,7 +147,8 @@ class TwoStageDetector(BaseDetector):
                 gt_bboxes,
                 gt_labels=None,
                 gt_bboxes_ignore=gt_bboxes_ignore,
-                proposal_cfg=proposal_cfg)
+                proposal_cfg=proposal_cfg,
+                gt_masks=gt_masks)
             losses.update(rpn_losses)
         else:
             proposal_list = proposals
