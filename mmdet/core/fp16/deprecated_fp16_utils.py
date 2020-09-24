b37fd15e6c13087cf73d8fefe33642ded5e3e132
RyanXLi <xlide@connect.ust.hk>
2020-09-24 14:06:56 +0800

    changed names from depr to deprecated

--- mmdet/core/fp16/deprecated_fp16_utils.py
+++ mmdet/core/fp16/deprecated_fp16_utils.py
@@ -4,7 +4,7 @@ from mmcv.runner import (Fp16OptimizerHook, auto_fp16, force_fp32,
                          wrap_fp16_model)
 
 
-class Depr_Fp16OptimizerHook(Fp16OptimizerHook):
+class DeprecatedFp16OptimizerHook(Fp16OptimizerHook):
     """A wrapper class for the FP16 optimizer hook. This class wraps
     :class:`Fp16OptimizerHook` in `mmcv.runner` and shows a warning that the
     :class:`Fp16OptimizerHook` from `mmdet.core` will be deprecated.
@@ -23,7 +23,7 @@ class Depr_Fp16OptimizerHook(Fp16OptimizerHook):
             'instead')
 
 
-def depr_auto_fp16(*args, **kwargs):
+def deprecated_auto_fp16(*args, **kwargs):
     warnings.warn(
         'Importing auto_fp16 from "mmdet.core" will be '
         'deprecated in the future. Please import them from "mmcv.runner" '
@@ -31,7 +31,7 @@ def depr_auto_fp16(*args, **kwargs):
     return auto_fp16(*args, **kwargs)
 
 
-def depr_force_fp32(*args, **kwargs):
+def deprecated_force_fp32(*args, **kwargs):
     warnings.warn(
         'Importing force_fp32 from "mmdet.core" will be '
         'deprecated in the future. Please import them from "mmcv.runner" '
@@ -39,7 +39,7 @@ def depr_force_fp32(*args, **kwargs):
     return force_fp32(*args, **kwargs)
 
 
-def depr_wrap_fp16_model(*args, **kwargs):
+def deprecated_wrap_fp16_model(*args, **kwargs):
     warnings.warn(
         'Importing wrap_fp16_model from "mmdet.core" will be '
         'deprecated in the future. Please import them from "mmcv.runner" '
