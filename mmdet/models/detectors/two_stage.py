53bd2bbe7ebbc2d593a87eb26577293232b1e782
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-09-25 22:06:30 +0900

    Clean outdated comments (#3833)

--- mmdet/models/detectors/two_stage.py
+++ mmdet/models/detectors/two_stage.py
@@ -204,7 +204,6 @@ class TwoStageDetector(BaseDetector):
         If rescale is False, then returned bboxes and masks will fit the scale
         of imgs[0].
         """
-        # recompute feats to save memory
         x = self.extract_feats(imgs)
         proposal_list = self.rpn_head.aug_test_rpn(x, img_metas)
         return self.roi_head.aug_test(
