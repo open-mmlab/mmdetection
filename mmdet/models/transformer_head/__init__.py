297c59d5ecb5efe63de53e22ae44bf1b21dd18ae
v-qjqs <qjliqiaofei@gmail.com>
2020-09-28 10:49:49 +0800

    supports for DETR transformer

---
+++ mmdet/models/transformer_head/__init__.py
@@ -0,0 +1,8 @@
+from .transformer import (FFN, MultiheadAttention, Transformer,
+                          TransformerDecoder, TransformerDecoderLayer,
+                          TransformerEncoder, TransformerEncoderLayer)
+
+__all__ = [
+    'FFN', 'MultiheadAttention', 'Transformer', 'TransformerDecoder',
+    'TransformerDecoderLayer', 'TransformerEncoder', 'TransformerEncoderLayer'
+]
