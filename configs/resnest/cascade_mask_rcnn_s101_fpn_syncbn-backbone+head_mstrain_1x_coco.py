0f359e06652f1f472487faf7f71d9a0a464223b9
Jiarui XU <xvjiarui0826@gmail.com>
2020-10-04 20:56:24 +0800

    add s101

---
+++ configs/resnest/cascade_mask_rcnn_s101_fpn_syncbn-backbone+head_mstrain_1x_coco.py
@@ -0,0 +1,4 @@
+_base_ = './cascade_mask_rcnn_s50_fpn_syncbn-backbone+head_mstrain_1x_coco.py'
+model = dict(
+    pretrained='open-mmlab://resnest101',
+    backbone=dict(stem_channels=128, depth=101))
