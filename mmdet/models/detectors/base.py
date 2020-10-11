065a479bc9cb618a0a864d778ca62ba9260b1d1c
Leonid Beynenson <Leonid.Beynenson@intel.com>
2020-10-12 01:33:00 +0300

    Remove the method BaseDetector.export
    The method ran export of the model to onnx, but now the function
    torch.onnx.export is run in the script tools/export.py directly
    -- this is required to make export of NNCF-wrapped models.

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -209,10 +209,6 @@ class BaseDetector(nn.Module, metaclass=ABCMeta):
         self.forward_backup = None
         self.img_metas = None
 
-    def export(self, img, img_metas, export_name='', **kwargs):
-        with self.forward_export_context(img_metas):
-            torch.onnx.export(self, img, export_name, **kwargs)
-
     def _parse_losses(self, losses):
         """Parse the raw outputs (losses) of the network.
 
