b0f937a640e4154a4133ba8a42d99ee0a9f3f42f
RyanXLi <xlide@connect.ust.hk>
2020-10-16 01:01:25 +0800

    change init and config

--- configs/yolo/yolov4_spp_new_param.py
+++ configs/yolo/yolov4_spp_new_param.py
@@ -70,12 +70,12 @@ train_pipeline = [
         mean=img_norm_cfg['mean'],
         to_rgb=img_norm_cfg['to_rgb'],
         ratio_range=(1, 2)),
-    dict(
-        type='MinIoURandomCrop',
-        min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
-        min_crop_size=0.3),
-    dict(type='Resize', img_scale=[(320, 320), (320, 320)], keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    # dict(
+    #     type='MinIoURandomCrop',
+    #     min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
+    #     min_crop_size=0.3),
+    dict(type='Resize', img_scale=[(448, 448), (448, 448)], keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
