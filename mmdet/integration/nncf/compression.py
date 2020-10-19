df6884ea429a5b492bc0470aa322c27a11a4bde8
Aleksei Kashapov <aleksei.kashapov@intel.com>
2020-10-19 10:25:42 +0300

    Fix bug with a lack of compression for input tensor

--- mmdet/integration/nncf/compression.py
+++ mmdet/integration/nncf/compression.py
@@ -7,6 +7,7 @@ from mmdet.utils import get_root_logger
 if is_nncf_enabled():
     try:
         from nncf.initialization import InitializingDataLoader
+        from nncf.dynamic_graph.patch_pytorch import nncf_model_input
 
         from nncf import NNCFConfig
         from nncf import create_compressed_model, register_default_init_args
@@ -119,6 +120,7 @@ def wrap_nncf_model(model, cfg, data_loader_for_init=None, get_fake_input_func=N
         # and on the script tools/export.py
         fake_data = _get_fake_data_for_forward(cfg, nncf_config, get_fake_input_func)
         img, img_metas = fake_data["img"], fake_data["img_metas"]
+        img = nncf_model_input(img)
         with model.forward_dummy_context(img_metas):
             model(img)
 
@@ -127,6 +129,7 @@ def wrap_nncf_model(model, cfg, data_loader_for_init=None, get_fake_input_func=N
         # and on the script tools/export.py
         fake_data = _get_fake_data_for_forward(cfg, nncf_config, get_fake_input_func)
         img, img_metas = fake_data["img"], fake_data["img_metas"]
+        img = nncf_model_input(img)
         with model.forward_export_context(img_metas):
             model(img)
 
