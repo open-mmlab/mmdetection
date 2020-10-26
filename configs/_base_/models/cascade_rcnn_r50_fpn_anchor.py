e3e0b3f7fff3f91fc51412eff04a75204d219ec7
jahwan oh <jahwan.oh@bepro11.com>
2020-10-26 20:07:25 +0100

    Update cascade_rcnn_r50_fpn_anchor.py
    iou_threshold 0.10 --> 0.60

--- configs/_base_/models/cascade_rcnn_r50_fpn_anchor.py
+++ configs/_base_/models/cascade_rcnn_r50_fpn_anchor.py
@@ -183,5 +183,5 @@ test_cfg = dict(
         min_bbox_size=0),
     rcnn=dict(
         score_thr=0.60,
-        nms=dict(type='nms', iou_threshold=0.10),
+        nms=dict(type='nms', iou_threshold=0.60),
         max_per_img=100))
