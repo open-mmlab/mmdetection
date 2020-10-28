d1cedb72d08467ad876a2c02c51e40f960ee9bfc
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-10-28 12:56:53 +0100

    Use mmcv tensor2imgs (#4010)

--- mmdet/core/utils/__init__.py
+++ mmdet/core/utils/__init__.py
@@ -1,7 +1,4 @@
 from .dist_utils import DistOptimizerHook, allreduce_grads
-from .misc import multi_apply, tensor2imgs, unmap
+from .misc import multi_apply, unmap
 
-__all__ = [
-    'allreduce_grads', 'DistOptimizerHook', 'tensor2imgs', 'multi_apply',
-    'unmap'
-]
+__all__ = ['allreduce_grads', 'DistOptimizerHook', 'multi_apply', 'unmap']
