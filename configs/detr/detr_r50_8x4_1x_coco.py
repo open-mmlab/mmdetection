57f38e8a80c63ace9e82e7114fc6e371bbf1ee8b
v-qjqs <qjliqiaofei@gmail.com>
2020-11-02 14:00:37 +0800

    toy commit

--- configs/detr/detr_r50_8x4_1x_coco.py
+++ configs/detr/detr_r50_8x4_1x_coco.py
@@ -55,13 +55,15 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='Resize',
-        img_scale=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
-                   (608, 1333), (640, 1333), (672, 1333), (704, 1333),
-                   (736, 1333), (768, 1333), (800, 1333)],
-        multiscale_mode='value',
-        keep_ratio=True),
+    # dict(
+    #     type='Resize',
+    #     img_scale=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+    #                (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+    #                (736, 1333), (768, 1333), (800, 1333)],
+    #     # img_scale=[(800, 1333)],
+    #     multiscale_mode='value',
+    #     keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(600, 600), allow_negative_crop=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=1),
