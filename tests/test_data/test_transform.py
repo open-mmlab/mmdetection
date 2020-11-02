a719e3ee6ddae11cecae9d5fc4a05f143a25b537
v-qjqs <qjliqiaofei@gmail.com>
2020-11-02 15:20:33 +0800

    add unit test

--- tests/test_data/test_transform.py
+++ tests/test_data/test_transform.py
@@ -675,3 +675,57 @@ def test_cutout():
     cutout_module = build_from_cfg(transform, PIPELINES)
     cutout_result = cutout_module(copy.deepcopy(results))
     assert cutout_result['img'].sum() > img.sum()
+
+
+def test_random_crop_warpper():
+    # test assertion for invalid crop_type
+    with pytest.raises(AssertionError):
+        transform = dict(
+            type='RandomCropWarpper', crop_type='unknow', crop_size=(1, 1))
+        build_from_cfg(transform, PIPELINES)
+
+    def _construct_toy_data():
+        img = np.array([[1, 2, 3, 4], [5, 6, 7, 8]], dtype=np.uint8)
+        img = np.stack([img, img, img], axis=-1)
+        results = dict()
+        # image
+        results['img'] = img
+        results['img_shape'] = img.shape
+        results['img_fields'] = ['img']
+        # bboxes
+        results['bbox_fields'] = ['gt_bboxes', 'gt_bboxes_ignore']
+        results['gt_bboxes'] = np.array([[0., 0., 2., 1.]], dtype=np.float32)
+        results['gt_bboxes_ignore'] = np.array([[2., 0., 3., 1.]],
+                                               dtype=np.float32)
+        # labels
+        results['gt_labels'] = np.array([1], dtype=np.int64)
+        return results
+
+    # test crop_type "relative_range"
+    results = _construct_toy_data()
+    transform = dict(
+        type='RandomCropWarpper',
+        crop_type='relative_range',
+        crop_size=(0.3, 0.7))
+    transform_module = build_from_cfg(transform, PIPELINES)
+    transform_module(copy.deepcopy(results))
+
+    # test crop_type "relative"
+    transform = dict(
+        type='RandomCropWarpper', crop_type='relative', crop_size=(0.3, 0.7))
+    transform_module = build_from_cfg(transform, PIPELINES)
+    transform_module(copy.deepcopy(results))
+
+    # test crop_type "absolute"
+    transform = dict(
+        type='RandomCropWarpper', crop_type='absolute', crop_size=(1, 2))
+    transform_module = build_from_cfg(transform, PIPELINES)
+    transform_module(copy.deepcopy(results))
+
+    # test crop_type "absolute_range"
+    transform = dict(
+        type='RandomCropWarpper',
+        crop_type='absolute_range',
+        crop_size=(1, 20))
+    transform_module = build_from_cfg(transform, PIPELINES)
+    transform_module(copy.deepcopy(results))
