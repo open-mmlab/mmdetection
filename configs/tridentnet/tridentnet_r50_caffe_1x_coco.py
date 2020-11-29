25aef509a49517a0d542d72f91ca7ddf4cb9d71e
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2020-11-29 11:55:53 +0800

    add mstrain config

--- configs/tridentnet/tridentnet_r50_caffe_1x_coco.py
+++ configs/tridentnet/tridentnet_r50_caffe_1x_coco.py
@@ -1,4 +1,5 @@
 _base_ = [
+    '../_base_/models/faster_rcnn_r50_caffe_c4.py',
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
@@ -10,119 +11,30 @@ model = dict(
     pretrained='open-mmlab://detectron2/resnet50_caffe',
     backbone=dict(
         type='TridentResNet',
-        depth=50,
-        num_stages=3,
-        strides=(1, 2, 2),
-        dilations=(1, 1, 1),
-        out_indices=(2, ),
-        trident_dilations=(1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=False),
-        norm_eval=True,
-        style='caffe'),
-    rpn_head=dict(
-        type='RPNHead',
-        in_channels=1024,
-        feat_channels=1024,
-        anchor_generator=dict(
-            type='AnchorGenerator',
-            scales=[2, 4, 8, 16, 32],
-            ratios=[0.5, 1.0, 2.0],
-            strides=[16]),
-        bbox_coder=dict(
-            type='DeltaXYWHBBoxCoder',
-            target_means=[.0, .0, .0, .0],
-            target_stds=[1.0, 1.0, 1.0, 1.0]),
-        loss_cls=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+        trident_dilations=(1, 2, 3)
+    ),
     roi_head=dict(
-        type='TridentRoIHead',
-        shared_head=dict(
-            type='ResLayer',
-            depth=50,
-            stage=3,
-            stride=2,
-            dilation=1,
-            style='caffe',
-            norm_cfg=dict(type='BN', requires_grad=False),
-            norm_eval=True),
-        bbox_roi_extractor=dict(
-            type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=14, sample_num=0),
-            out_channels=1024,
-            featmap_strides=[16]),
-        bbox_head=dict(
-            type='BBoxHead',
-            with_avg_pool=True,
-            roi_feat_size=7,
-            in_channels=2048,
-            num_classes=80,
-            bbox_coder=dict(
-                type='DeltaXYWHBBoxCoder',
-                target_means=[0., 0., 0., 0.],
-                target_stds=[0.1, 0.1, 0.2, 0.2]),
-            reg_class_agnostic=False,
-            loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-            loss_bbox=dict(type='L1Loss', loss_weight=1.0))))
+        type='TridentRoIHead'
+    )
+)
 
 train_cfg = dict(
-    rpn=dict(
-        assigner=dict(
-            type='MaxIoUAssigner',
-            pos_iou_thr=0.7,
-            neg_iou_thr=0.3,
-            min_pos_iou=0.3,
-            match_low_quality=True,
-            ignore_iof_thr=-1),
-        sampler=dict(
-            type='RandomSampler',
-            num=256,
-            pos_fraction=0.5,
-            neg_pos_ub=-1,
-            add_gt_as_proposals=False),
-        allowed_border=-1,
-        pos_weight=-1,
-        debug=False),
     rpn_proposal=dict(
-        nms_across_levels=False,
-        nms_pre=12000,
         nms_post=500,
-        max_num=500,
-        nms_thr=0.7,
-        min_bbox_size=0),
+        max_num=500
+    ),
     rcnn=dict(
-        assigner=dict(
-            type='MaxIoUAssigner',
-            pos_iou_thr=0.5,
-            neg_iou_thr=0.5,
-            min_pos_iou=0.5,
-            match_low_quality=False,
-            ignore_iof_thr=-1),
         sampler=dict(
-            type='RandomSampler',
             num=128,
             pos_fraction=0.5,
-            neg_pos_ub=-1,
-            add_gt_as_proposals=False),
-        pos_weight=-1,
-        debug=False))
+            add_gt_as_proposals=False
+        )
+    )
+)
 
-test_cfg = dict(
-    rpn=dict(
-        nms_across_levels=False,
-        nms_pre=6000,
-        nms_post=1000,
-        max_num=1000,
-        nms_thr=0.7,
-        min_bbox_size=0),
-    rcnn=dict(
-        score_thr=0.05, nms=dict(type='nms', iou_thr=0.5), max_per_img=100))
-
+# use caffe img_norm
 img_norm_cfg = dict(
     mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
-
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
@@ -149,8 +61,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=1,
     train=dict(pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
+# optimizer
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
