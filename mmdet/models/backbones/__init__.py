f579adc8085cc0cc97ce29e87070eafd019d49b4
GT9505 <gt950513@mail.ustc.edu.cn>
2020-12-03 15:09:10 +0800

    support import Bottleneck and BasicBlock in resnet

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -6,12 +6,12 @@ from .hrnet import HRNet
 from .regnet import RegNet
 from .res2net import Res2Net
 from .resnest import ResNeSt
-from .resnet import ResNet, ResNetV1d
+from .resnet import BasicBlock, Bottleneck, ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
     'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'Darknet',
-    'ResNeSt'
+    'ResNeSt', 'BasicBlock', 'Bottleneck'
 ]
