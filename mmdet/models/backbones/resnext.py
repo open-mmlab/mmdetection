4ea2da54875868ad8149976c9558c5707cec0f34
hhaAndroid <1286304229@qq.com>
2020-12-17 09:57:40 +0800

    Fix empirical attention in resnext backbone error

--- mmdet/models/backbones/resnext.py
+++ mmdet/models/backbones/resnext.py
@@ -1,6 +1,6 @@
 import math
 
-from mmcv.cnn import build_conv_layer, build_norm_layer,build_plugin_layer
+from mmcv.cnn import build_conv_layer, build_norm_layer
 
 from ..builder import BACKBONES
 from ..utils import ResLayer
@@ -84,37 +84,25 @@ class Bottleneck(_Bottleneck):
         self.add_module(self.norm3_name, norm3)
 
         if self.with_plugins:
+            self._del_block_plugins(self.after_conv1_plugin_names +
+                                    self.after_conv2_plugin_names +
+                                    self.after_conv3_plugin_names)
             self.after_conv1_plugin_names = self.make_block_plugins(
                 width, self.after_conv1_plugins)
             self.after_conv2_plugin_names = self.make_block_plugins(
                 width, self.after_conv2_plugins)
             self.after_conv3_plugin_names = self.make_block_plugins(
-                width * self.expansion, self.after_conv3_plugins)
+                self.planes * self.expansion, self.after_conv3_plugins)
 
-    def make_block_plugins(self, in_channels, plugins):
-        """make plugins for block.
+    def _del_block_plugins(self, plugin_names):
+        """delete plugins for block if exist.
 
         Args:
-            in_channels (int): Input channels of plugin.
-            plugins (list[dict]): List of plugins cfg to build.
-
-        Returns:
-            list[str]: List of the names of plugin.
+            plugin_names (list[str]): List of plugins name to delete.
         """
-        assert isinstance(plugins, list)
-        plugin_names = []
-        for plugin in plugins:
-            plugin = plugin.copy()
-            name, layer = build_plugin_layer(
-                plugin,
-                in_channels=in_channels,
-                postfix=plugin.pop('postfix', ''))
-            # fix duplicate plugin. When multiple same name plugins are added, only the last one is kept
-            if hasattr(self, name):
-                del self._modules[name]
-            self.add_module(name, layer)
-            plugin_names.append(name)
-        return plugin_names
+        assert isinstance(plugin_names, list)
+        for plugin_name in plugin_names:
+            del self._modules[plugin_name]
 
 
 @BACKBONES.register_module()
