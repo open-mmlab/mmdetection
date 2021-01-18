405c37766398608f81b8cae657914c0f15830286
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2021-01-18 01:26:50 -0800

    Move train_cfg/test_cfg into model (#4347)
    * Move train_cfg/test_cfg into model
    
    * fixed fsaf
    
    * fixed missing

--- configs/yolo/yolov3_d53_mstrain-608_273e_coco.py
+++ configs/yolo/yolov3_d53_mstrain-608_273e_coco.py
@@ -37,18 +37,21 @@ model = dict(
             use_sigmoid=True,
             loss_weight=2.0,
             reduction='sum'),
-        loss_wh=dict(type='MSELoss', loss_weight=2.0, reduction='sum')))
-# training and testing settings
-train_cfg = dict(
-    assigner=dict(
-        type='GridAssigner', pos_iou_thr=0.5, neg_iou_thr=0.5, min_pos_iou=0))
-test_cfg = dict(
-    nms_pre=1000,
-    min_bbox_size=0,
-    score_thr=0.05,
-    conf_thr=0.005,
-    nms=dict(type='nms', iou_threshold=0.45),
-    max_per_img=100)
+        loss_wh=dict(type='MSELoss', loss_weight=2.0, reduction='sum')),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='GridAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.5,
+            min_pos_iou=0)),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        conf_thr=0.005,
+        nms=dict(type='nms', iou_threshold=0.45),
+        max_per_img=100))
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
