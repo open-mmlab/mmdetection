7a3ada992cfd98aec917c6fb68799e0fff8b8153
v-qjqs <qjliqiaofei@gmail.com>
2021-01-26 13:34:20 +0800

    reformat

--- configs/detectors/detectors_htc_r50_mstrain_40e_coco.py
+++ configs/detectors/detectors_htc_r50_mstrain_40e_coco.py
@@ -26,9 +26,6 @@ optimizer_config = dict(
     _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
     warmup_ratio=1.0 / 3,
     step=[36, 39])
 total_epochs = 40
--- configs/detectors/detectors_htc_x101_32x4d_mstrain_40e_coco.py
+++ configs/detectors/detectors_htc_x101_32x4d_mstrain_40e_coco.py
@@ -1,11 +1,16 @@
-_base_ = '../htc/htc_x101_32x4d_fpn_16x1_20e_coco.py'
+_base_ = './detectors_htc_r50_mstrain_40e_coco.py'
 model = dict(
+    pretrained='open-mmlab://resnext101_32x4d',
     backbone=dict(
         type='DetectoRS_ResNeXt',
-        conv_cfg=dict(type='ConvAWS'),
-        sac=dict(type='SAC', use_deform=True),
-        stage_with_sac=(False, True, True, True),
-        output_img=True),
+        depth=101,
+        groups=32,
+        base_width=4,
+        #conv_cfg=dict(type='ConvAWS'),
+        #sac=dict(type='SAC', use_deform=True),
+        #stage_with_sac=(False, True, True, True),
+        #output_img=True
+    ),
     neck=dict(
         type='RFP',
         rfp_steps=2,
@@ -27,36 +32,4 @@ model = dict(
             stage_with_sac=(False, True, True, True),
             pretrained='open-mmlab://resnext101_32x4d',
             style='pytorch')))
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='LoadAnnotations', with_bbox=True, with_mask=True, with_seg=True),
-    dict(
-        type='Resize',
-        img_scale=[(1333, 400), (1333, 1200)],
-        multiscale_mode='range',
-        keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='SegRescale', scale_factor=1 / 8),
-    dict(type='DefaultFormatBundle'),
-    dict(
-        type='Collect',
-        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg']),
-]
-data = dict(
-    samples_per_gpu=2, workers_per_gpu=2, train=dict(pipeline=train_pipeline))
-# optimizer
-optimizer_config = dict(
-    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=1.0 / 3,
-    step=[36, 39])
-total_epochs = 40
+data = dict(samples_per_gpu=1, workers_per_gpu=1)
