b5eb61c9cddf55bb5c60e41b2c970e35e01f35a7
v-qjqs <qjliqiaofei@gmail.com>
2021-01-26 12:45:47 +0800

    add config `detectors_htc_x101_32x4d_mstrain_40e_coco.py`

---
+++ configs/detectors/detectors_htc_x101_32x4d_mstrain_40e_coco.py
@@ -0,0 +1,62 @@
+_base_ = '../htc/htc_x101_32x4d_fpn_16x1_20e_coco.py'
+model = dict(
+    backbone=dict(
+        type='DetectoRS_ResNeXt',
+        conv_cfg=dict(type='ConvAWS'),
+        sac=dict(type='SAC', use_deform=True),
+        stage_with_sac=(False, True, True, True),
+        output_img=True),
+    neck=dict(
+        type='RFP',
+        rfp_steps=2,
+        aspp_out_channels=64,
+        aspp_dilations=(1, 3, 6, 1),
+        rfp_backbone=dict(
+            rfp_inplanes=256,
+            type='DetectoRS_ResNeXt',
+            depth=101,
+            groups=32,
+            base_width=4,
+            num_stages=4,
+            out_indices=(0, 1, 2, 3),
+            frozen_stages=1,
+            norm_cfg=dict(type='BN', requires_grad=True),
+            norm_eval=True,
+            conv_cfg=dict(type='ConvAWS'),
+            sac=dict(type='SAC', use_deform=True),
+            stage_with_sac=(False, True, True, True),
+            pretrained='open-mmlab://resnext101_32x4d',
+            style='pytorch')))
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadAnnotations', with_bbox=True, with_mask=True, with_seg=True),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 400), (1333, 1200)],
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='SegRescale', scale_factor=1 / 8),
+    dict(type='DefaultFormatBundle'),
+    dict(
+        type='Collect',
+        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg']),
+]
+data = dict(
+    samples_per_gpu=2, workers_per_gpu=2, train=dict(pipeline=train_pipeline))
+# optimizer
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 3,
+    step=[36, 39])
+total_epochs = 40
