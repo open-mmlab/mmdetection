abfb685fec306940a05aafb82e7697ab87742e9f
Cao Yuhang <yhcao6@gmail.com>
2021-02-22 18:29:42 +0800

    fix pad

--- configs/fpg/retinanet_r50_fpg-cha128_crop640_50e_coco.py
+++ configs/fpg/retinanet_r50_fpg-cha128_crop640_50e_coco.py
@@ -3,3 +3,23 @@ _base_ = 'retinanet_r50_fpg_crop640_50e_coco.py'
 model = dict(
     neck=dict(out_channels=128, inter_channels=128),
     bbox_head=dict(in_channels=128))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Resize',
+        img_scale=(640, 640),
+        ratio_range=(0.8, 1.2),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(640, 640)),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=(640, 640)),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+data = dict(train=dict(pipeline=train_pipeline))
