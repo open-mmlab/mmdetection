8cdd821cb295afe6a15e6dda5465249096c242d3
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-03-05 15:18:36 +0800

    Update tridentnet_r50_caffe_1x_coco.py

--- configs/tridentnet/tridentnet_r50_caffe_1x_coco.py
+++ configs/tridentnet/tridentnet_r50_caffe_1x_coco.py
@@ -48,7 +48,6 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    workers_per_gpu=0,
     train=dict(pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
