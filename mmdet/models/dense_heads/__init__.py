75d71e7bfcba3dbb78c9f8284baa481352e6b55d
HikariTJU <clofta@gmail.com>
2021-03-13 00:32:25 +0800

    fix lint

--- mmdet/models/dense_heads/__init__.py
+++ mmdet/models/dense_heads/__init__.py
@@ -13,6 +13,7 @@ from .ga_retina_head import GARetinaHead
 from .ga_rpn_head import GARPNHead
 from .gfl_head import GFLHead
 from .guided_anchor_head import FeatureAdaption, GuidedAnchorHead
+from .ld_head import LDHead
 from .nasfcos_head import NASFCOSHead
 from .paa_head import PAAHead
 from .pisa_retinanet_head import PISARetinaHead
@@ -27,7 +28,6 @@ from .transformer_head import TransformerHead
 from .vfnet_head import VFNetHead
 from .yolact_head import YOLACTHead, YOLACTProtonet, YOLACTSegmHead
 from .yolo_head import YOLOV3Head
-from .ld_head import LDHead
 
 __all__ = [
     'AnchorFreeHead', 'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption',
