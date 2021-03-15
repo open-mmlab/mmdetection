c41fd9f8bc1aaf709cc39cbcc9066a909481270a
gtw04 <sjhong310@gmail.com>
2021-03-15 18:06:13 +0900

    Correct line < 80

--- mmdet/models/roi_heads/roi_extractors/base_roi_extractor.py
+++ mmdet/models/roi_heads/roi_extractors/base_roi_extractor.py
@@ -36,8 +36,8 @@ class BaseRoIExtractor(nn.Module, metaclass=ABCMeta):
             layer_cfg (dict): Dictionary to construct and config RoI layer
                 operation. Options are modules under ``mmcv/ops`` such as
                 ``RoIAlign``.
-            featmap_strides (List[int]): The stride of input feature map w.r.t to the
-                original image size, which would be used to scale RoI
+            featmap_strides (List[int]): The stride of input feature map w.r.t
+                to the original image size, which would be used to scale RoI
                 coordinate (original image coordinate system) to feature
                 coordinate system.
 
