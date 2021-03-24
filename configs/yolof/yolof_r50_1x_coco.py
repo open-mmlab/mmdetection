906a475d906978c7ff4fff9b86338cb01cc65363
hhaAndroid <1286304229@qq.com>
2021-03-24 17:33:40 +0800

    Update cfg

--- configs/yolof/yolof_r50_1x_coco.py
+++ configs/yolof/yolof_r50_1x_coco.py
@@ -63,6 +63,7 @@ optimizer = dict(
     momentum=0.9,
     weight_decay=0.0001,
     paramwise_cfg=dict(
+        norm_decay_mult=0.,
         custom_keys={'backbone': dict(lr_mult=0.334, decay_mult=1.0)}))
 lr_config = dict(warmup_iters=1500, warmup_ratio=0.00066667)
 
