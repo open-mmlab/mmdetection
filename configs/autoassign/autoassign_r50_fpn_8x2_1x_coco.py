8e66bf9cc430443537c323490ced5ecb0994d242
jshilong <2392587229zsl@gmail.com>
2021-03-29 14:06:22 +0800

    move train_cfg test_cfg to model

--- configs/autoassign/autoassign_r50_fpn_8x2_1x_coco.py
+++ configs/autoassign/autoassign_r50_fpn_8x2_1x_coco.py
@@ -31,14 +31,14 @@ model = dict(
         stacked_convs=4,
         feat_channels=256,
         strides=[8, 16, 32, 64, 128],
-        loss_bbox=dict(type='GIoULoss', loss_weight=5.0)))
-train_cfg = None
-test_cfg = dict(
-    nms_pre=1000,
-    min_bbox_size=0,
-    score_thr=0.05,
-    nms=dict(type='nms', iou_threshold=0.6),
-    max_per_img=100)
+        loss_bbox=dict(type='GIoULoss', loss_weight=5.0)),
+    train_cfg=None,
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.6),
+        max_per_img=100))
 img_norm_cfg = dict(
     mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
 train_pipeline = [
