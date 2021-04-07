a3c8c000a2cf2e974e6d7f01085177556d54b62b
zhangshilong <zhangshilong@sensetime.com>
2021-04-07 16:45:29 +0800

    post process pipline

---
+++ mmdet/core/post_processing/builder.py
@@ -0,0 +1,58 @@
+import collections
+
+from mmcv.utils import Registry, build_from_cfg
+
+POST_PROCESS = Registry('post_process')
+
+
+@POST_PROCESS.register_module()
+class ComposePostProcess(object):
+    """Compose multiple post process operation sequentially.
+
+    Args:
+        post_processes (Sequence[dict | callable]): Sequence
+            of post-process operation object or config
+            dict to be composed.
+    """
+
+    def __init__(self, post_processes):
+        assert isinstance(post_processes, collections.abc.Sequence)
+        self.post_processes = []
+        for process in post_processes:
+            if isinstance(process, dict):
+                operation = build_from_cfg(process, POST_PROCESS)
+                self.post_processes.append(operation)
+            elif callable(process):
+                self.post_processes.append(process)
+            else:
+                raise TypeError('post_process must be callable or a dict')
+
+    def __call__(self, results):
+        """Call function to apply post-process sequentially.
+
+        Args:
+            results (obj:`Results`): A result dict contains
+                the predictions of model.
+
+        Returns:
+            obj:`Results`
+        """
+
+        for p in self.post_processes:
+            results = p(results)
+            if results is None:
+                return None
+        return results
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for t in self.post_processes:
+            format_string += '\n'
+            format_string += f'    {t}'
+        format_string += '\n)'
+        return format_string
+
+
+def build_post_process(cfg, default_args=None):
+    post_process = build_from_cfg(cfg, POST_PROCESS, default_args)
+    return post_process
