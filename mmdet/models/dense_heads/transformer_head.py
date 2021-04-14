3ddb1ca566013917de8d9f20166a08bdbfcd4bc1
zhangshilong <zhangshilong@sensetime.com>
2021-04-14 17:22:04 +0800

    remove transformer ops

--- mmdet/models/dense_heads/transformer_head.py
+++ mmdet/models/dense_heads/transformer_head.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import Conv2d, Linear, build_activation_layer
+from mmcv.cnn.bricks.transformer import FFN, build_positional_encoding
 from mmcv.runner import force_fp32
 
 from mmdet.core import (bbox_cxcywh_to_xyxy, bbox_xyxy_to_cxcywh,
                         build_assigner, build_sampler, multi_apply,
                         reduce_mean)
-from mmdet.models.utils import (FFN, build_positional_encoding,
-                                build_transformer)
+from mmdet.models.utils import build_transformer
 from ..builder import HEADS, build_loss
 from .anchor_free_head import AnchorFreeHead
 
--- mmdet/models/utils/builder.py
+++ mmdet/models/utils/builder.py
@@ -1,32 +1,8 @@
 from mmcv.utils import Registry, build_from_cfg
 
 TRANSFORMER = Registry('Transformer')
-POSITIONAL_ENCODING = Registry('Position encoding')
-ATTENTION = Registry('Attention')
-TRANSFORMERLAYER = Registry('TransformerLayer')
-TRANSFORMERCODER = Registry('TransformerCoder')
 
 
 def build_transformer(cfg, default_args=None):
     """Builder for Transformer."""
     return build_from_cfg(cfg, TRANSFORMER, default_args)
-
-
-def build_positional_encoding(cfg, default_args=None):
-    """Builder for Position Encoding."""
-    return build_from_cfg(cfg, POSITIONAL_ENCODING, default_args)
-
-
-def build_attention(cfg, default_args=None):
-    """Builder for attention."""
-    return build_from_cfg(cfg, ATTENTION, default_args)
-
-
-def build_transformerlayer(cfg, default_args=None):
-    """Builder for transformer layer."""
-    return build_from_cfg(cfg, TRANSFORMERLAYER, default_args)
-
-
-def build_transformercoder(cfg, default_args=None):
-    """Builder for transformer coder."""
-    return build_from_cfg(cfg, TRANSFORMERCODER, default_args)
