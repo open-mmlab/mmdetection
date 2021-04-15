8f997c71d62d373c5e74f655aed4a3713bd8b7a3
Semyon Bevzyuk <semen.bevzuk@gmail.com>
2021-04-16 02:09:06 +0300

    Fix bboxes

--- mmdet/core/post_processing/bbox_nms.py
+++ mmdet/core/post_processing/bbox_nms.py
@@ -49,7 +49,7 @@ def multiclass_nms(multi_bboxes,
     labels = labels.reshape(-1)
 
     nms_cfg['score_threshold'] = score_thr
-    nms_cfg['max_num'] = max_num if max_num > 0 else boxes.shape[0]
+    nms_cfg['max_num'] = max_num if max_num > 0 else bboxes.shape[0]
     if not torch.onnx.is_in_onnx_export():
         # NonZero not supported  in TensorRT
         # remove low scoring boxes
