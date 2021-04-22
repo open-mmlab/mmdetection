00ecfd012bcc9178f710223a348e671a31f0add5
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2021-04-22 17:57:02 +0300

    fix training size divisor for effdet-like net

--- configs/efficientdet/retinanet_effd0_bifpn_1x_coco.py
+++ configs/efficientdet/retinanet_effd0_bifpn_1x_coco.py
@@ -89,7 +89,7 @@ train_pipeline = [
     dict(type='RandomCrop', crop_size=(input_size, input_size)),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+    dict(type='Pad', size_divisor=128),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
