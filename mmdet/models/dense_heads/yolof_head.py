bafadc9f27e5a57bede8d2870ec82cd0f153ce8d
hhaAndroid <1286304229@qq.com>
2021-04-22 14:57:46 +0800

    Simplify the code.

--- mmdet/models/dense_heads/yolof_head.py
+++ mmdet/models/dense_heads/yolof_head.py
@@ -4,8 +4,7 @@ from mmcv.cnn import (ConvModule, bias_init_with_prob, constant_init, is_norm,
                       normal_init)
 from mmcv.runner import force_fp32
 
-from mmdet.core import (anchor_inside_flags, images_to_levels, multi_apply,
-                        reduce_mean, unmap)
+from mmdet.core import anchor_inside_flags, multi_apply, reduce_mean, unmap
 from ..builder import HEADS
 from .anchor_head import AnchorHead
 
@@ -144,9 +143,9 @@ class YOLOFHead(AnchorHead):
 
         Args:
             cls_scores (list[Tensor]): Box scores for each scale level
-                Has shape (N, num_anchors * num_classes, H, W)
+                Has shape (batch, num_anchors * num_classes, h, w)
             bbox_preds (list[Tensor]): Box energies / deltas for each scale
-                level with shape (N, num_anchors * 4, H, W)
+                level with shape (batch, num_anchors * 4, h, w)
             gt_bboxes (list[Tensor]): Ground truth bboxes for each image with
                 shape (num_gts, 4) in [tl_x, tl_y, br_x, br_y] format.
             gt_labels (list[Tensor]): class indices corresponding to each box
@@ -158,13 +157,17 @@ class YOLOFHead(AnchorHead):
         Returns:
             dict[str, Tensor]: A dictionary of loss components.
         """
-        featmap_sizes = [featmap.size()[-2:] for featmap in cls_scores]
-        assert len(featmap_sizes) == self.anchor_generator.num_levels
+        assert len(cls_scores) == 1
+        assert self.anchor_generator.num_levels == 1
 
         device = cls_scores[0].device
-
+        featmap_sizes = [featmap.size()[-2:] for featmap in cls_scores]
         anchor_list, valid_flag_list = self.get_anchors(
             featmap_sizes, img_metas, device=device)
+
+        # The output level is always 1
+        anchor_list = [anchors[0] for anchors in anchor_list]
+        valid_flag_list = [valid_flags[0] for valid_flags in valid_flag_list]
 
         cls_scores_list = levels_to_images(cls_scores)
         bbox_preds_list = levels_to_images(bbox_preds)
@@ -182,29 +185,35 @@ class YOLOFHead(AnchorHead):
             label_channels=label_channels)
         if cls_reg_targets is None:
             return None
-        (labels_list, label_weights_list, bbox_targets_list, bbox_weights_list,
-         num_total_pos, num_total_neg, pos_idx_list, pos_predicted_boxes_list,
-         target_boxes_list) = cls_reg_targets
+        (batch_labels, batch_label_weights, num_total_pos, num_total_neg,
+         batch_bbox_weights, batch_pos_predicted_boxes,
+         batch_target_boxes) = cls_reg_targets
 
-        flatten_labels = torch.cat(labels_list).reshape(-1)
-        pos_inds = ((flatten_labels >= 0)
-                    & (flatten_labels < self.num_classes)).nonzero(
-                        as_tuple=False).reshape(-1)
-        num_pos = torch.tensor(
-            len(pos_inds), dtype=torch.float, device=bbox_preds[0].device)
-        num_total_samples = max(reduce_mean(num_pos), 1.0)
+        flatten_labels = batch_labels.reshape(-1)
+        batch_label_weights = batch_label_weights.reshape(-1)
+        cls_score = cls_scores[0].permute(0, 2, 3,
+                                          1).reshape(-1, self.cls_out_channels)
 
-        losses_cls, losses_bbox = multi_apply(
-            self.loss_single,
-            cls_scores,
-            bbox_preds,
-            labels_list,
-            label_weights_list,
-            pos_idx_list,
-            pos_predicted_boxes_list,
-            target_boxes_list,
-            num_total_samples=num_total_samples)
-        return dict(loss_cls=losses_cls, loss_bbox=losses_bbox)
+        num_total_samples = (num_total_pos +
+                             num_total_neg) if self.sampling else num_total_pos
+        num_total_samples = max(
+            reduce_mean(cls_score.new_tensor(num_total_samples)).item(), 1.0)
+
+        # classification loss
+        loss_cls = self.loss_cls(
+            cls_score,
+            flatten_labels,
+            batch_label_weights,
+            avg_factor=num_total_samples)
+
+        # regression loss
+        loss_bbox = self.loss_bbox(
+            batch_pos_predicted_boxes,
+            batch_target_boxes,
+            batch_bbox_weights.float(),
+            avg_factor=num_total_samples)
+
+        return dict(loss_cls=loss_cls, loss_bbox=loss_bbox)
 
     def get_targets(self,
                     cls_scores_list,
@@ -216,24 +225,20 @@ class YOLOFHead(AnchorHead):
                     gt_bboxes_ignore_list=None,
                     gt_labels_list=None,
                     label_channels=1,
-                    unmap_outputs=True,
-                    return_sampling_results=False):
+                    unmap_outputs=True):
         """Compute regression and classification targets for anchors in
         multiple images.
 
         Args:
-            cls_scores_list (list[list[Tensor]])： Multi level cls scores of
-                each image.
-            bbox_preds_list (list[list[Tensor]])： Multi level bbox preds of
-                each image.
-            anchor_list (list[list[Tensor]]): Multi level anchors of each
-                image. The outer list indicates images, and the inner list
-                corresponds to feature levels of the image. Each element of
-                the inner list is a tensor of shape (num_anchors, 4).
-            valid_flag_list (list[list[Tensor]]): Multi level valid flags of
-                each image. The outer list indicates images, and the inner list
-                corresponds to feature levels of the image. Each element of
-                the inner list is a tensor of shape (num_anchors, )
+            cls_scores_list (list[Tensor])： Classification scores of
+                each image. each is a 4D-tensor, the shape is
+                (h * w, num_anchors * num_classes).
+            bbox_preds_list (list[Tensor])： Bbox preds of each image.
+                each is a 4D-tensor, the shape is (h * w, num_anchors * 4).
+            anchor_list (list[Tensor]): Anchors of each image. Each element of
+                is a tensor of shape (h * w * num_anchors, 4).
+            valid_flag_list (list[Tensor]): Valid flags of each image. Each
+               element of is a tensor of shape (h * w * num_anchors, )
             gt_bboxes_list (list[Tensor]): Ground truth bboxes of each image.
             img_metas (list[dict]): Meta info of each image.
             gt_bboxes_ignore_list (list[Tensor]): Ground truth bboxes to be
@@ -246,11 +251,10 @@ class YOLOFHead(AnchorHead):
         Returns:
             tuple: Usually returns a tuple containing learning targets.
 
-                - labels_list (list[Tensor]): Labels of each level.
-                - label_weights_list (list[Tensor]): Label weights of each \
-                    level.
-                - bbox_targets_list (list[Tensor]): BBox targets of each level.
-                - bbox_weights_list (list[Tensor]): BBox weights of each level.
+                - batch_labels (Tensor): Label of all images. Each element \
+                    of is a tensor of shape (batch, h * w * num_anchors)
+                - batch_label_weights (Tensor): Label weights of all images \
+                    of is a tensor of shape (batch, h * w * num_anchors)
                 - num_total_pos (int): Number of positive samples in all \
                     images.
                 - num_total_neg (int): Number of negative samples in all \
@@ -263,16 +267,6 @@ class YOLOFHead(AnchorHead):
         num_imgs = len(img_metas)
         assert len(anchor_list) == len(valid_flag_list) == num_imgs
 
-        # anchor number of multi levels
-        num_level_anchors = [anchors.size(0) for anchors in anchor_list[0]]
-        # concat all level anchors to a single tensor
-        concat_anchor_list = []
-        concat_valid_flag_list = []
-        for i in range(num_imgs):
-            assert len(anchor_list[i]) == len(valid_flag_list[i])
-            concat_anchor_list.append(torch.cat(anchor_list[i]))
-            concat_valid_flag_list.append(torch.cat(valid_flag_list[i]))
-
         # compute targets for each image
         if gt_bboxes_ignore_list is None:
             gt_bboxes_ignore_list = [None for _ in range(num_imgs)]
@@ -281,38 +275,30 @@ class YOLOFHead(AnchorHead):
         results = multi_apply(
             self._get_targets_single,
             bbox_preds_list,
-            concat_anchor_list,
-            concat_valid_flag_list,
+            anchor_list,
+            valid_flag_list,
             gt_bboxes_list,
             gt_bboxes_ignore_list,
             gt_labels_list,
             img_metas,
             label_channels=label_channels,
             unmap_outputs=unmap_outputs)
-        (all_labels, all_label_weights, all_bbox_targets, all_bbox_weights,
-         pos_inds_list, neg_inds_list, sampling_results_list) = results[:7]
-        rest_results = list(results[7:])  # user-added return values
+        (all_labels, all_label_weights, pos_inds_list, neg_inds_list,
+         sampling_results_list) = results[:5]
+        rest_results = list(results[5:])  # user-added return values
         # no valid anchors
         if any([labels is None for labels in all_labels]):
             return None
         # sampled anchors of all images
         num_total_pos = sum([max(inds.numel(), 1) for inds in pos_inds_list])
         num_total_neg = sum([max(inds.numel(), 1) for inds in neg_inds_list])
-        # split targets to a list w.r.t. multiple levels
-        labels_list = images_to_levels(all_labels, num_level_anchors)
-        label_weights_list = images_to_levels(all_label_weights,
-                                              num_level_anchors)
-        bbox_targets_list = images_to_levels(all_bbox_targets,
-                                             num_level_anchors)
-        bbox_weights_list = images_to_levels(all_bbox_weights,
-                                             num_level_anchors)
-        res = (labels_list, label_weights_list, bbox_targets_list,
-               bbox_weights_list, num_total_pos, num_total_neg)
-        if return_sampling_results:
-            res = res + (sampling_results_list, )
 
+        batch_labels = torch.stack(all_labels, 0)
+        batch_label_weights = torch.stack(all_label_weights, 0)
+
+        res = (batch_labels, batch_label_weights, num_total_pos, num_total_neg)
         for i, r in enumerate(rest_results):  # user-added return values
-            rest_results[i] = [torch.cat(r, 0)]
+            rest_results[i] = torch.cat(r, 0)
 
         return res + tuple(rest_results)
 
@@ -326,11 +312,50 @@ class YOLOFHead(AnchorHead):
                             img_meta,
                             label_channels=1,
                             unmap_outputs=True):
+        """Compute regression and classification targets for anchors in a
+        single image.
+
+        Args:
+            bbox_preds (Tensor): Bbox prediction of the image, which
+                shape is (h * w ,4)
+            flat_anchors (Tensor): Anchors of the image, which shape is
+                (h * w * num_anchors ,4)
+            valid_flags (Tensor): Valid flags of the image, which shape is
+                (h * w * num_anchors,).
+            gt_bboxes (Tensor): Ground truth bboxes of the image,
+                shape (num_gts, 4).
+            gt_bboxes_ignore (Tensor): Ground truth bboxes to be
+                ignored, shape (num_ignored_gts, 4).
+            img_meta (dict): Meta info of the image.
+            gt_labels (Tensor): Ground truth labels of each box,
+                shape (num_gts,).
+            label_channels (int): Channel of label.
+            unmap_outputs (bool): Whether to map outputs back to the original
+                set of anchors.
+
+        Returns:
+            tuple:
+                labels (Tensor): Labels of image, which shape is
+                    (h * w * num_anchors, ).
+                label_weights (Tensor): Label weights of image, which shape is
+                    (h * w * num_anchors, ).
+                pos_inds (Tensor): Pos index of image.
+                neg_inds (Tensor): Neg index of image.
+                sampling_result (obj:`SamplingResult`): Sampling result.
+                pos_bbox_weights (Tensor): The Weight of using to calculate
+                    the bbox branch loss, which shape is (num, ).
+                pos_predicted_boxes (Tensor): boxes predicted value of
+                    using to calculate the bbox branch loss, which shape is
+                    (num, 4).
+                pos_target_boxes (Tensor): boxes target value of
+                    using to calculate the bbox branch loss, which shape is
+                    (num, 4).
+        """
         inside_flags = anchor_inside_flags(flat_anchors, valid_flags,
                                            img_meta['img_shape'][:2],
                                            self.train_cfg.allowed_border)
         if not inside_flags.any():
-            return (None, ) * 10
+            return (None, ) * 8
         # assign gt and sample anchors
         anchors = flat_anchors[inside_flags, :]
         bbox_preds = bbox_preds.reshape(-1, 4)
@@ -338,21 +363,18 @@ class YOLOFHead(AnchorHead):
 
         # decoded bbox
         decoder_bbox_preds = self.bbox_coder.decode(anchors, bbox_preds)
-
         assign_result = self.assigner.assign(
             decoder_bbox_preds, anchors, gt_bboxes, gt_bboxes_ignore,
             None if self.sampling else gt_labels)
 
-        pos_idx = assign_result.get_extra_property('pos_idx')
+        pos_bbox_weights = assign_result.get_extra_property('pos_idx')
         pos_predicted_boxes = assign_result.get_extra_property(
             'pos_predicted_boxes')
-        target_boxes = assign_result.get_extra_property('target_boxes')
+        pos_target_boxes = assign_result.get_extra_property('target_boxes')
 
         sampling_result = self.sampler.sample(assign_result, anchors,
                                               gt_bboxes)
         num_valid_anchors = anchors.shape[0]
-        bbox_targets = torch.zeros_like(anchors)
-        bbox_weights = torch.zeros_like(anchors)
         labels = anchors.new_full((num_valid_anchors, ),
                                   self.num_classes,
                                   dtype=torch.long)
@@ -361,9 +383,6 @@ class YOLOFHead(AnchorHead):
         pos_inds = sampling_result.pos_inds
         neg_inds = sampling_result.neg_inds
         if len(pos_inds) > 0:
-            pos_bbox_targets = sampling_result.pos_gt_bboxes
-            bbox_targets[pos_inds, :] = pos_bbox_targets
-            bbox_weights[pos_inds, :] = 1.0
             if gt_labels is None:
                 # Only rpn gives gt_labels as None
                 # Foreground is the first class since v2.5.0
@@ -386,53 +405,6 @@ class YOLOFHead(AnchorHead):
                 fill=self.num_classes)  # fill bg label
             label_weights = unmap(label_weights, num_total_anchors,
                                   inside_flags)
-            bbox_targets = unmap(bbox_targets, num_total_anchors, inside_flags)
-            bbox_weights = unmap(bbox_weights, num_total_anchors, inside_flags)
 
-        return (labels, label_weights, bbox_targets, bbox_weights, pos_inds,
-                neg_inds, sampling_result, pos_idx, pos_predicted_boxes,
-                target_boxes)
-
-    def loss_single(self, cls_score, bbox_pred, labels, label_weights,
-                    bbox_weights, pos_predicted_boxes, bbox_targets,
-                    num_total_samples):
-        """Compute loss of a single scale level.
-
-        Args:
-            cls_score (Tensor): Box scores for each scale level
-                Has shape (N, num_anchors * num_classes, H, W).
-            bbox_pred (Tensor): Box energies / deltas for each scale
-                level with shape (N, num_anchors * 4, H, W).
-            labels (Tensor): Labels of each anchors with shape
-                (N, num_total_anchors).
-            label_weights (Tensor): Label weights of each anchor with shape
-                (N, num_total_anchors)
-            bbox_weights (Tensor): BBox regression loss weights of each anchor
-                with shape (N, num, 4).
-            pos_predicted_boxes (Tensor): BBox regression predicted.
-            bbox_targets (Tensor): BBox regression targets of each anchor wight
-                shape (N, num_total_anchors, 4).
-            num_total_samples (int): If sampling, num total samples equal to
-                the number of total anchors; Otherwise, it is the number of
-                positive anchors.
-
-        Returns:
-            tuple[Tensor]: A tuple of loss components.
-        """
-        # classification loss
-        labels = labels.reshape(-1)
-        label_weights = label_weights.reshape(-1)
-        cls_score = cls_score.permute(0, 2, 3,
-                                      1).reshape(-1, self.cls_out_channels)
-
-        loss_cls = self.loss_cls(
-            cls_score, labels, label_weights, avg_factor=num_total_samples)
-
-        # regression loss
-        loss_bbox = self.loss_bbox(
-            pos_predicted_boxes,
-            bbox_targets,
-            bbox_weights.float(),
-            avg_factor=num_total_samples)
-
-        return loss_cls, loss_bbox
+        return (labels, label_weights, pos_inds, neg_inds, sampling_result,
+                pos_bbox_weights, pos_predicted_boxes, pos_target_boxes)
