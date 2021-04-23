3a3c48c3c5681e0f2ce7e6b68b18fc2bf1d6a114
maningsheng <maningsheng@sensetime.com>
2021-04-23 10:43:30 +0800

    move CUDAExecutionProvider to first place

--- mmdet/core/export/model_wrappers.py
+++ mmdet/core/export/model_wrappers.py
@@ -31,8 +31,8 @@ class ONNXRuntimeDetector(BaseDetector):
         options = [{}]
         is_cuda_available = ort.get_device() == 'GPU'
         if is_cuda_available:
-            providers.append('CUDAExecutionProvider')
-            options.append({'device_id': device_id})
+            providers.insert(0, 'CUDAExecutionProvider')
+            options.insert(0, {'device_id': device_id})
 
         sess.set_providers(providers, options)
 
