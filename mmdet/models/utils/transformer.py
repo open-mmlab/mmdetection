45e9e868705ce38624d4ed1104174a87ec04d633
zhangshilong <zhangshilong@sensetime.com>
2021-04-25 22:55:07 +0800

    pull detr

--- mmdet/models/utils/transformer.py
+++ mmdet/models/utils/transformer.py
@@ -170,11 +170,11 @@ class Transformer(BaseModule):
 
     Args:
         encoder (`mmcv.ConfigDict` | Dict): Config of
-            TransformerEncoder.
+            TransformerEncoder. Defaults to None.
         decoder ((`mmcv.ConfigDict` | Dict)): Config of
-            TransformerDecoder.
+            TransformerDecoder. Defaults to None
         init_cfg (obj:`mmcv.ConfigDict`): The Config for initialization.
-            Default: None.
+            Defaults to None.
     """
 
     def __init__(self, encoder=None, decoder=None, init_cfg=None):
