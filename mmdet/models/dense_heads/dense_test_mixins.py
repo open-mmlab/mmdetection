5d9138d4480361484e36d3a3acae3c6a1599a86b
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2021-04-26 00:29:19 +0000

    Move test methods of RPN

--- mmdet/models/dense_heads/dense_test_mixins.py
+++ mmdet/models/dense_heads/dense_test_mixins.py
@@ -1,12 +1,17 @@
+import sys
 from inspect import signature
 
 import torch
 
-from mmdet.core import bbox2result, bbox_mapping_back, multiclass_nms
+from mmdet.core import (bbox2result, bbox_mapping_back, merge_aug_proposals,
+                        multiclass_nms)
+
+if sys.version_info >= (3, 7):
+    from mmdet.utils.contextmanagers import completed
 
 
 class BBoxTestMixin(object):
-    """Mixin class for test time augmentation of bboxes."""
+    """Mixin class for testing det bboxes."""
 
     def simple_test_bboxes(self,
                            feats,
@@ -130,3 +135,52 @@ class BBoxTestMixin(object):
                 img_metas[0][0]['scale_factor'])
         bbox_results = bbox2result(_det_bboxes, det_labels, self.num_classes)
         return bbox_results
+
+    if sys.version_info >= (3, 7):
+
+        async def async_simple_test_rpn(self, x, img_metas):
+            sleep_interval = self.test_cfg.pop('async_sleep_interval', 0.025)
+            async with completed(
+                    __name__, 'rpn_head_forward',
+                    sleep_interval=sleep_interval):
+                rpn_outs = self(x)
+
+            proposal_list = self.get_bboxes(*rpn_outs, img_metas)
+            return proposal_list
+
+    def simple_test_rpn(self, x, img_metas):
+        """Test without augmentation.
+
+        Args:
+            x (tuple[Tensor]): Features from the upstream network, each is
+                a 4D-tensor.
+            img_metas (list[dict]): Meta info of each image.
+
+        Returns:
+            list[Tensor]: Proposals of each image.
+        """
+        rpn_outs = self(x)
+        proposal_list = self.get_bboxes(*rpn_outs, img_metas)
+        return proposal_list
+
+    def aug_test_rpn(self, feats, img_metas):
+        samples_per_gpu = len(img_metas[0])
+        aug_proposals = [[] for _ in range(samples_per_gpu)]
+        for x, img_meta in zip(feats, img_metas):
+            proposal_list = self.simple_test_rpn(x, img_meta)
+            for i, proposals in enumerate(proposal_list):
+                aug_proposals[i].append(proposals)
+        # reorganize the order of 'img_metas' to match the dimensions
+        # of 'aug_proposals'
+        aug_img_metas = []
+        for i in range(samples_per_gpu):
+            aug_img_meta = []
+            for j in range(len(img_metas)):
+                aug_img_meta.append(img_metas[j][i])
+            aug_img_metas.append(aug_img_meta)
+        # after merging, proposals will be rescaled to the original image size
+        merged_proposals = [
+            merge_aug_proposals(proposals, aug_img_meta, self.test_cfg)
+            for proposals, aug_img_meta in zip(aug_proposals, aug_img_metas)
+        ]
+        return merged_proposals
