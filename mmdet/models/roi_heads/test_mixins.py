458ffc14caafbd53995b1664a773cb6df141d7e5
Haian Huang(深度眸) <1286304229@qq.com>
2021-04-26 15:49:26 +0800

    Fix empty bbox error in mask_head of carafe (#5062)
    * Fix empty bbox error in mask_head of carafe
    
    * add onnx error tips

--- mmdet/models/roi_heads/test_mixins.py
+++ mmdet/models/roi_heads/test_mixins.py
@@ -254,6 +254,14 @@ class MaskTestMixin(object):
         ori_shapes = tuple(meta['ori_shape'] for meta in img_metas)
         scale_factors = tuple(meta['scale_factor'] for meta in img_metas)
 
+        if all(det_bbox.shape[0] == 0 for det_bbox in det_bboxes):
+            if torch.onnx.is_in_onnx_export():
+                raise RuntimeError('[ONNX Error] Can not record MaskHead '
+                                   'as it has not been executed this time')
+            segm_results = [[[] for _ in range(self.mask_head.num_classes)]
+                            for _ in range(len(det_bboxes))]
+            return segm_results
+
         # The length of proposals of different batches may be different.
         # In order to form a batch, a padding operation is required.
         if isinstance(det_bboxes, list):
