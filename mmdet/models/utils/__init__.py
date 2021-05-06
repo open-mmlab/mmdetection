87fe4e740faa942c55b30faf882fe278a3d8eda1
RangiLyu <lyuchqi@gmail.com>
2021-05-06 20:05:48 +0800

    add mobilenet_v2

--- mmdet/models/utils/__init__.py
+++ mmdet/models/utils/__init__.py
@@ -1,6 +1,7 @@
 from .builder import build_transformer
 from .gaussian_target import gaussian_radius, gen_gaussian_target
 from .inverted_residual import InvertedResidual
+from .make_divisible import make_divisible
 from .positional_encoding import (LearnedPositionalEncoding,
                                   SinePositionalEncoding)
 from .res_layer import ResLayer, SimplifiedBasicBlock
@@ -11,6 +12,7 @@ from .transformer import (DetrTransformerDecoder, DetrTransformerDecoderLayer,
 __all__ = [
     'ResLayer', 'gaussian_radius', 'gen_gaussian_target',
     'DetrTransformerDecoderLayer', 'DetrTransformerDecoder', 'Transformer',
-    'build_transformer', 'SinePositionalEncoding', 'LearnedPositionalEncoding',
-    'DynamicConv', 'SimplifiedBasicBlock', 'InvertedResidual', 'SELayer'
+    'build_transformer', 'SinePositionalEncoding', 'make_divisible',
+    'LearnedPositionalEncoding', 'DynamicConv', 'SimplifiedBasicBlock',
+    'InvertedResidual', 'SELayer'
 ]
