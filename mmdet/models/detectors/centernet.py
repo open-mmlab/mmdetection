61b7c539ab0ad31e06f7ae8eee671d906939995b
kellenf <984677144@qq.com>
2021-05-10 11:45:18 +0800

    fix flip test

--- mmdet/models/detectors/centernet.py
+++ mmdet/models/detectors/centernet.py
@@ -23,42 +23,69 @@ class CenterNet(CornerNet):
         super(CenterNet, self).__init__(backbone, neck, bbox_head, train_cfg,
                                         test_cfg, pretrained, init_cfg)
 
-        def aug_test(self, imgs, img_metas, rescale=False):
-            """Augment testing of CornerNet.
+    def merge_aug_results(self, aug_results, with_nms):
+        """Merge augmented detection bboxes and score.
 
-            Args:
-                imgs (list[Tensor]): Augmented images.
-                img_metas (list[list[dict]]): Meta information of each
-                image, e.g.,
-                    image size, scaling factor, etc.
-                rescale (bool): If True, return boxes in original image space.
-                    Default: False.
+        Args:
+            aug_results (list[list[Tensor]]): Det_bboxes and det_labels of each
+                image.
+            img_metas (list[list[dict]]): Meta information of each image, e.g.,
+                image size, scaling factor, etc.
 
-            Note:
-                ``imgs`` must including flipped image pairs.
+        Returns:
+            tuple: (bboxes, labels)
+        """
+        recovered_bboxes, aug_labels = [], []
+        for single_result in aug_results:
+            recovered_bboxes.append(single_result[0][0])
+            aug_labels.append(single_result[0][1])
 
-            Returns:
-                list[list[np.ndarray]]: BBox results of each image and classes.
-                    The outer list corresponds to each image. The inner list
-                    corresponds to each class.
-            """
-            img_inds = list(range(len(imgs)))
+        bboxes = torch.cat(recovered_bboxes, dim=0).contiguous()
+        labels = torch.cat(aug_labels).contiguous()
+        if with_nms:
+            out_bboxes, out_labels = self.bbox_head._bboxes_nms(
+                bboxes, labels, self.bbox_head.test_cfg)
+        else:
+            out_bboxes, out_labels = bboxes, labels
 
-            assert img_metas[0][0]['flip'] + img_metas[1][0]['flip'], (
-                'aug test must have flipped image pair')
-            aug_results = []
-            for ind, flip_ind in zip(img_inds[0::2], img_inds[1::2]):
-                img_pair = torch.cat([imgs[ind], imgs[flip_ind]])
-                x = self.extract_feat(img_pair)
-                outs = self.bbox_head(x)
-                bbox_list = self.bbox_head.get_bboxes(
-                    *outs, [img_metas[ind], img_metas[flip_ind]],
-                    rescale=rescale,
-                    with_nms=False)
-                aug_results.append(bbox_list)
-            bbox_list = [self.merge_aug_results(aug_results)]
-            bbox_results = [
-                bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
-                for det_bboxes, det_labels in bbox_list
-            ]
-            return bbox_results
+        return out_bboxes, out_labels
+
+    def aug_test(self, imgs, img_metas, rescale=False):
+        """Augment testing of CornerNet.
+
+        Args:
+            imgs (list[Tensor]): Augmented images.
+            img_metas (list[list[dict]]): Meta information of each image, e.g.,
+                image size, scaling factor, etc.
+            rescale (bool): If True, return boxes in original image space.
+                Default: False.
+
+        Note:
+            ``imgs`` must including flipped image pairs.
+
+        Returns:
+            list[list[np.ndarray]]: BBox results of each image and classes.
+                The outer list corresponds to each image. The inner list
+                corresponds to each class.
+        """
+        img_inds = list(range(len(imgs)))
+        # TODO: add multi_scale test, use nms if use multi_scale test
+        with_nms = True if len(imgs) > 2 else False
+        assert img_metas[0][0]['flip'] + img_metas[1][0]['flip'], (
+            'aug test must have flipped image pair')
+        aug_results = []
+        for ind, flip_ind in zip(img_inds[0::2], img_inds[1::2]):
+            img_pair = torch.cat([imgs[ind], imgs[flip_ind]])
+            x = self.extract_feat(img_pair)
+            outs = self.bbox_head(x)
+            bbox_list = self.bbox_head.get_bboxes(
+                *outs, [img_metas[ind], img_metas[flip_ind]],
+                rescale=rescale,
+                with_nms=with_nms)
+            aug_results.append(bbox_list)
+        bbox_list = [self.merge_aug_results(aug_results, with_nms)]
+        bbox_results = [
+            bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
+            for det_bboxes, det_labels in bbox_list
+        ]
+        return bbox_results
