32bb326f509b6143495e1a5b8337ecb072fd3516
q.yao <yaoqian@sensetime.com>
2021-05-17 17:51:28 +0800

    convert with wrapper

--- mmdet/core/export/pytorch2onnx.py
+++ mmdet/core/export/pytorch2onnx.py
@@ -1,3 +1,4 @@
+import copy
 from functools import partial
 
 import mmcv
@@ -5,6 +6,8 @@ import numpy as np
 import torch
 from mmcv.runner import load_checkpoint
 
+from mmdet.apis.inference import LoadImage
+from mmdet.datasets.pipelines import Compose
 from ..evaluation import get_classes
 
 
@@ -99,6 +102,38 @@ def build_model_from_cfg(config_path, checkpoint_path, cfg_options=None):
         model.CLASSES = get_classes('coco')
     model.cpu().eval()
     return model
+
+
+def prepare_inputs(img_path,
+                   pipeline,
+                   shape=None,
+                   keep_ratio=False,
+                   rescale=False):
+    test_pipeline = copy.deepcopy(pipeline)
+    # build the data pipeline
+    if shape is not None:
+        test_pipeline[1]['img_scale'] = (shape[1], shape[0])
+
+    if not keep_ratio:
+        # update parameters in transforms
+        for trans in test_pipeline[1]['transforms']:
+            if 'keep_ratio' in trans:
+                trans['keep_ratio'] = False
+            if 'size_divisor' in trans:
+                trans['size_divisor'] = 1
+    test_pipeline = [LoadImage()] + test_pipeline[1:]
+    test_pipeline = Compose(test_pipeline)
+    # prepare data
+    data = dict(img=img_path)
+    data = test_pipeline(data)
+    imgs = data['img']
+    img_metas = [[i.data] for i in data['img_metas']]
+    imgs = [img[None, :] for img in imgs]
+    if not rescale:
+        for img_meta in img_metas:
+            img_meta[0]['scale_factor'] = [1.0] * 4
+            img_meta[0]['flip'] = False
+    return imgs, img_metas
 
 
 def preprocess_example_input(input_config):
--- tools/deployment/onnx2tensorrt.py
+++ tools/deployment/onnx2tensorrt.py
@@ -2,17 +2,17 @@ import argparse
 import os
 import os.path as osp
 
+import mmcv
 import numpy as np
 import onnx
-import onnxruntime as ort
 import torch
-from mmcv.ops import get_onnxruntime_op_path
-from mmcv.tensorrt import (TRTWraper, is_tensorrt_plugin_loaded, onnx2trt,
-                           save_trt_engine)
+from mmcv import Config
+from mmcv.tensorrt import is_tensorrt_plugin_loaded, onnx2trt, save_trt_engine
 
 from mmdet.core import get_classes
-from mmdet.core.export import preprocess_example_input
-from mmdet.core.visualization.image import imshow_det_bboxes
+from mmdet.core.export import prepare_inputs
+from mmdet.core.export.model_wrappers import (ONNXRuntimeDetector,
+                                              TensorRTDetector)
 
 
 def get_GiB(x: int):
@@ -22,7 +22,9 @@ def get_GiB(x: int):
 
 def onnx2tensorrt(onnx_file,
                   trt_file,
-                  input_config,
+                  input_img,
+                  input_shape,
+                  test_pipeline,
                   verify=False,
                   show=False,
                   dataset='coco',
@@ -30,7 +32,6 @@ def onnx2tensorrt(onnx_file,
                   verbose=False):
     import tensorrt as trt
     onnx_model = onnx.load(onnx_file)
-    input_shape = input_config['input_shape']
     # create trt engine and wraper
     opt_shape_dict = {'input': [input_shape, input_shape, input_shape]}
     max_workspace_size = get_GiB(workspace_size)
@@ -47,76 +48,57 @@ def onnx2tensorrt(onnx_file,
     print(f'Successfully created TensorRT engine: {trt_file}')
 
     if verify:
-        one_img, one_meta = preprocess_example_input(input_config)
-        input_img_cpu = one_img.detach().cpu().numpy()
-        input_img_cuda = one_img.cuda()
-        img = one_meta['show_img']
+        # prepare inputs
+        img_list, img_meta_list = prepare_inputs(
+            input_img,
+            test_pipeline,
+            shape=input_shape[2:],
+            keep_ratio=False,
+            rescale=True)
+        img_list = [_.cuda().contiguous() for _ in img_list]
 
-        # Get results from ONNXRuntime
-        ort_custom_op_path = get_onnxruntime_op_path()
-        session_options = ort.SessionOptions()
-        if osp.exists(ort_custom_op_path):
-            session_options.register_custom_ops_library(ort_custom_op_path)
-        sess = ort.InferenceSession(onnx_file, session_options)
-        output_names = [_.name for _ in sess.get_outputs()]
-        ort_outputs = sess.run(None, {
-            'input': input_img_cpu,
-        })
-        with_mask = len(output_names) == 3
-        ort_outputs = [_.squeeze(0) for _ in ort_outputs]
-        ort_dets, ort_labels = ort_outputs[:2]
-        ort_masks = ort_outputs[2] if with_mask else None
-        ort_shapes = [_.shape for _ in ort_outputs]
-        print(f'ONNX Runtime output names: {output_names}, \
-            output shapes: {ort_shapes}')
+        # wrap ONNX and TensorRT model
+        CLASSES = get_classes(dataset)
+        onnx_model = ONNXRuntimeDetector(onnx_file, CLASSES, device_id=0)
+        trt_model = TensorRTDetector(trt_file, CLASSES, device_id=0)
 
-        # Get results from TensorRT
-        trt_model = TRTWraper(trt_file, ['input'], output_names)
+        # inference with wrapped model
         with torch.no_grad():
-            trt_outputs = trt_model({'input': input_img_cuda})
-        trt_outputs = [
-            trt_outputs[_].detach().cpu().numpy().squeeze(0)
-            for _ in output_names
-        ]
-        trt_dets, trt_labels = trt_outputs[:2]
-        trt_shapes = [_.shape for _ in trt_outputs]
-        print(f'TensorRT output names: {output_names}, \
-            output shapes: {trt_shapes}')
-        trt_masks = trt_outputs[2] if with_mask else None
+            onnx_results = onnx_model(
+                img_list, img_metas=img_meta_list, return_loss=False)[0]
+            trt_results = trt_model(
+                img_list, img_metas=img_meta_list, return_loss=False)[0]
 
-        # Show detection outputs
         if show:
-            CLASSES = get_classes(dataset)
-            score_thr = 0.35
-            imshow_det_bboxes(
-                img.copy(),
-                trt_dets,
-                trt_labels,
-                segms=trt_masks,
-                class_names=CLASSES,
-                score_thr=score_thr,
-                win_name='TensorRT')
-            imshow_det_bboxes(
-                img.copy(),
-                ort_dets,
-                ort_labels,
-                segms=ort_masks,
-                class_names=CLASSES,
-                score_thr=score_thr,
-                win_name='ONNXRuntime')
-        # Compare results
-        np.testing.assert_allclose(ort_dets, trt_dets, rtol=1e-03, atol=1e-05)
-        np.testing.assert_allclose(ort_labels, trt_labels)
+            # visualize
+            from mmdet.apis import show_result_pyplot
+            show_img = mmcv.imread(input_img)
+            show_result_pyplot(
+                onnx_model, show_img, onnx_results, title='ONNXRuntime')
+            show_result_pyplot(
+                trt_model, show_img, trt_results, title='TensorRT')
+
+        with_mask = trt_model.with_masks
+        # compare a part of result
         if with_mask:
-            np.testing.assert_allclose(
-                ort_masks, trt_masks, rtol=1e-03, atol=1e-05)
-        print('The numerical values are the same ' +
-              'between ONNXRuntime and TensorRT')
+            compare_pairs = list(zip(onnx_results, trt_results))
+        else:
+            compare_pairs = [(onnx_results, trt_results)]
+        err_msg = 'The numerical values are different between Pytorch' + \
+                  ' and ONNX, but it does not necessarily mean the' + \
+                  ' exported ONNX model is problematic.'
+        # check the numerical value
+        for onnx_res, pytorch_res in compare_pairs:
+            for o_res, p_res in zip(onnx_res, pytorch_res):
+                np.testing.assert_allclose(
+                    o_res, p_res, rtol=1e-03, atol=1e-05, err_msg=err_msg)
+        print('The numerical values are the same between Pytorch and ONNX')
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Convert MMDetection models from ONNX to TensorRT')
+    parser.add_argument('config', help='test config file path')
     parser.add_argument('model', help='Filename of input ONNX model')
     parser.add_argument(
         '--trt-file',
@@ -139,27 +121,11 @@ def parse_args():
         help='Whether to verbose logging messages while creating \
                 TensorRT engine. Defaults to False.')
     parser.add_argument(
-        '--to-rgb',
-        action='store_false',
-        help='Feed model with RGB or BGR image. Default is RGB.')
-    parser.add_argument(
         '--shape',
         type=int,
         nargs='+',
         default=[400, 600],
         help='Input size of the model')
-    parser.add_argument(
-        '--mean',
-        type=float,
-        nargs='+',
-        default=[123.675, 116.28, 103.53],
-        help='Mean value used for preprocess input data')
-    parser.add_argument(
-        '--std',
-        type=float,
-        nargs='+',
-        default=[58.395, 57.12, 57.375],
-        help='Variance value used for preprocess input data')
     parser.add_argument(
         '--workspace-size',
         type=int,
@@ -177,6 +143,8 @@ if __name__ == '__main__':
     if not args.input_img:
         args.input_img = osp.join(osp.dirname(__file__), '../demo/demo.jpg')
 
+    cfg = Config.fromfile(args.config)
+
     if len(args.shape) == 1:
         input_shape = (1, 3, args.shape[0], args.shape[0])
     elif len(args.shape) == 2:
@@ -184,21 +152,13 @@ if __name__ == '__main__':
     else:
         raise ValueError('invalid input shape')
 
-    assert len(args.mean) == 3
-    assert len(args.std) == 3
-
-    normalize_cfg = {'mean': args.mean, 'std': args.std, 'to_rgb': args.to_rgb}
-    input_config = {
-        'input_shape': input_shape,
-        'input_path': args.input_img,
-        'normalize_cfg': normalize_cfg
-    }
-
     # Create TensorRT engine
     onnx2tensorrt(
         args.model,
         args.trt_file,
-        input_config,
+        args.input_img,
+        input_shape,
+        cfg.test_pipeline,
         verify=args.verify,
         show=args.show,
         dataset=args.dataset,
