077a01b177b036bc587acb8faf266cca21143c1d
Jiaqi Wang <wj017@ie.cuhk.edu.hk>
2021-05-18 15:24:52 +0800

    Seesaw Loss for Long-Tailed Instance Segmentation (CVPR 2021) (#5128)
    * init seesaw loss
    
    * clean code
    
    * update
    
    * update
    
    * update
    
    * test bug fix
    
    * update
    
    * minor update
    
    * add readme and cfgs
    
    * minor update
    
    * reformat build predictor
    
    * minor update
    
    * minor update
    
    * add model urls
    
    * update doc
    
    * minor fix
    
    * minor fix
    
    * update unit test
    
    * update unit tests
    
    * update
    
    * minor update
    
    * fix CI
    
    * fix pytorch 1.8 _conv_forward inference
    
    * update
    
    * minor fix
    
    * update

---
+++ configs/seesaw_loss/cascade_mask_rcnn_r101_fpn_sample1e-3_seesaw_loss_mstrain_2x_lvis_v1.py
@@ -0,0 +1,96 @@
+_base_ = [
+    '../_base_/models/cascade_mask_rcnn_r50_fpn.py',
+    '../_base_/datasets/lvis_v1_instance.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+
+model = dict(
+    pretrained='torchvision://resnet101',
+    backbone=dict(depth=101),
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1203,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                cls_predictor_cfg=dict(type='NormedLinear', tempearture=20),
+                loss_cls=dict(
+                    type='SeesawLoss',
+                    p=0.8,
+                    q=2.0,
+                    num_classes=1203,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1203,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                cls_predictor_cfg=dict(type='NormedLinear', tempearture=20),
+                loss_cls=dict(
+                    type='SeesawLoss',
+                    p=0.8,
+                    q=2.0,
+                    num_classes=1203,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1203,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                cls_predictor_cfg=dict(type='NormedLinear', tempearture=20),
+                loss_cls=dict(
+                    type='SeesawLoss',
+                    p=0.8,
+                    q=2.0,
+                    num_classes=1203,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        ],
+        mask_head=dict(num_classes=1203)),
+    test_cfg=dict(
+        rcnn=dict(
+            score_thr=0.0001,
+            # LVIS allows up to 300
+            max_per_img=300)))
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+data = dict(train=dict(dataset=dict(pipeline=train_pipeline)))
+evaluation = dict(interval=24, metric=['bbox', 'segm'])
---
+++ configs/seesaw_loss/mask_rcnn_r50_fpn_sample1e-3_seesaw_loss_normed_mask_mstrain_2x_lvis_v1.py
@@ -0,0 +1,5 @@
+_base_ = './mask_rcnn_r50_fpn_sample1e-3_seesaw_loss_mstrain_2x_lvis_v1.py'
+model = dict(
+    roi_head=dict(
+        mask_head=dict(
+            predictor_cfg=dict(type='NormedConv2d', tempearture=20))))
