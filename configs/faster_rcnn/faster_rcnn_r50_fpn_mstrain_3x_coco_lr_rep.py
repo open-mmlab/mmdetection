fb90f857fc2add913250336143c1cf5957694201
hhaAndroid <1286304229@qq.com>
2021-05-21 14:08:03 +0800

    add RepeatDataset config

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_mstrain_3x_coco_lr_rep.py
@@ -0,0 +1,37 @@
+_base_ = './faster_rcnn_r50_fpn_1x_coco.py'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+
+data = dict(train=dict(
+    _delete_=True,
+    type='RepeatDataset',
+    times=3,
+    dataset=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline)))
+
+# learning policy
+lr_config = dict(step=[9, 11])
+runner = dict(max_epochs=12)
