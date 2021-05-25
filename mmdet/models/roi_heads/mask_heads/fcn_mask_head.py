fix scale factor of two stage

mmdet/models/roi_heads/mask_heads/fcn_mask_head.py
@@ -188,7 +188,7 @@ class FCNMaskHead(BaseModule):
             det_labels (Tensor): shape (n, )
             rcnn_test_cfg (dict): rcnn testing config
             ori_shape (Tuple): original image height and width, shape (2,)
-            scale_factor(float | Tensor): If ``rescale is True``, box
+            scale_factor(ndarry | Tensor): If ``rescale is True``, box
                 coordinates are divided by this scale factor to fit
                 ``ori_shape``.
             rescale (bool): If True, the resulting masks will be rescaled to
@@ -227,6 +227,7 @@ class FCNMaskHead(BaseModule):
         if isinstance(mask_pred, torch.Tensor):
             mask_pred = mask_pred.sigmoid()
         else:
+            # In AugTest, has been activated before
             mask_pred = det_bboxes.new_tensor(mask_pred)
 
         device = mask_pred.device
@@ -234,25 +235,19 @@ class FCNMaskHead(BaseModule):
                      ]  # BG is not included in num_classes
         bboxes = det_bboxes[:, :4]
         labels = det_labels
-        # No need to consider rescale and scale_factor while exporting to ONNX
+
+        # In most cases, scale_factor shoule have been
+        # converted to Tensor when rescale the bbox
+        if not isinstance(scale_factor, torch.Tensor):
+            assert isinstance(scale_factor, np.ndarray)
+            scale_factor = torch.from_numpy(scale_factor)
 
         if rescale:
             img_h, img_w = ori_shape[:2]
         else:
-            if isinstance(scale_factor, float):
-                img_h = np.round(ori_shape[0] * scale_factor).astype(np.int32)
-                img_w = np.round(ori_shape[1] * scale_factor).astype(np.int32)
-            else:
-                w_scale, h_scale = scale_factor[0], scale_factor[1]
-                img_h = np.round(ori_shape[0] * h_scale.item()).astype(
-                    np.int32)
-                img_w = np.round(ori_shape[1] * w_scale.item()).astype(
-                    np.int32)
-            scale_factor = 1.0
-
-        if not isinstance(scale_factor, (float, torch.Tensor)):
-            scale_factor = bboxes.new_tensor(scale_factor)
-        bboxes = bboxes / scale_factor
+            w_scale, h_scale = scale_factor[0], scale_factor[1]
+            img_h = np.round(ori_shape[0] * h_scale.item()).astype(np.int32)
+            img_w = np.round(ori_shape[1] * w_scale.item()).astype(np.int32)
 
         N = len(mask_pred)
         # The actual implementation split the input into chunks,

mmdet/models/roi_heads/test_mixins.py
@@ -7,7 +7,6 @@ from mmdet.core import (bbox2roi, bbox_mapping, merge_aug_bboxes,
                         merge_aug_masks, multiclass_nms)
 
 logger = logging.getLogger(__name__)
-
 if sys.version_info >= (3, 7):
     from mmdet.utils.contextmanagers import completed
 
@@ -22,8 +21,7 @@ class BBoxTestMixin:
                                     proposals,
                                     rcnn_test_cfg,
                                     rescale=False,
-                                    bbox_semaphore=None,
-                                    global_lock=None):
+                                    **kwargs):
             """Asynchronized test for box head without augmentation."""
             rois = bbox2roi(proposals)
             roi_feats = self.bbox_roi_extractor(
@@ -209,8 +207,7 @@ class MaskTestMixin:
             if det_bboxes.shape[0] == 0:
                 segm_result = [[] for _ in range(self.mask_head.num_classes)]
             else:
-                if rescale and not isinstance(scale_factor,
-                                              (float, torch.Tensor)):
+                if rescale:
                     scale_factor = det_bboxes.new_tensor(scale_factor)
                 _bboxes = (
                     det_bboxes[:, :4] *
@@ -273,8 +270,7 @@ class MaskTestMixin:
         # rescale it back to the testing scale to obtain RoIs.
         det_bboxes = det_bboxes[..., :4]
         if rescale:
-            if not isinstance(scale_factors[0], float):
-                scale_factors = det_bboxes.new_tensor(scale_factors)
+            scale_factors = det_bboxes.new_tensor(scale_factors)
             det_bboxes = det_bboxes * scale_factors.unsqueeze(1)
 
         batch_index = torch.arange(
@@ -334,12 +330,13 @@ class MaskTestMixin:
             merged_masks = merge_aug_masks(aug_masks, img_metas, self.test_cfg)
 
             ori_shape = img_metas[0][0]['ori_shape']
+            scale_factor = det_bboxes.new_ones(4)
             segm_result = self.mask_head.get_seg_masks(
                 merged_masks,
                 det_bboxes,
                 det_labels,
                 self.test_cfg,
                 ori_shape,
-                scale_factor=1.0,
+                scale_factor=scale_factor,
                 rescale=False)
         return segm_result