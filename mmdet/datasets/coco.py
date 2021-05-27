f08ee28ab3e09da6845df4f233e2df02ac2b2e05
zhangshilong <zhangshilong@sensetime.com>
2021-05-27 16:39:18 +0800

    move to coco wapper

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -45,12 +45,7 @@ class CocoDataset(CustomDataset):
         """
 
         self.coco = COCO(ann_file)
-        self.cat_ids = []
-
-        # make cat_ids consistent with the order of class_name
-        for class_name in self.CLASSES:
-            self.cat_ids.append(self.coco.get_cat_ids(cat_names=class_name)[0])
-
+        self.cat_ids = self.coco.get_cat_ids(cat_names=self.CLASSES)
         self.cat2label = {cat_id: i for i, cat_id in enumerate(self.cat_ids)}
         self.img_ids = self.coco.get_img_ids()
         data_infos = []
