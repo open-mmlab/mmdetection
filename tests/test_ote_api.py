6cfa2b7f89703ff2883d1dd6f35307b4a8918665
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2021-06-09 17:20:55 +0300

    move and expand OTE API tests

--- tests/test_ote_api.py
+++ tests/test_ote_api.py
@@ -3,6 +3,7 @@ import os.path as osp
 import random
 import time
 import unittest
+import warnings
 from concurrent.futures import ThreadPoolExecutor
 
 from flaky import flaky
@@ -25,20 +26,21 @@ from sc_sdk.utils.project_factory import ProjectFactory
 from mmdet.apis.ote.apis.detection import MMObjectDetectionTask, MMDetectionParameters, configurable_parameters
 
 
-class TestOTEDetection(unittest.TestCase):
+class TestOTEAPI(unittest.TestCase):
     """
-    Collection of OTEDetection tests for the MMObjectDetectionTask
+    Collection of tests for OTE API and OTE Model Templates
     """
 
     def init_environment(self, configurable_parameters, number_of_images=500):
         project = ProjectFactory.create_project_single_task(name='OTEDetectionTestProject',
                                                             description='OTEDetectionTestProject',
-                                                            label_names=["rectangle", "ellipse", "triangle"],
-                                                            task_name="OTEDetectionTestTask",
+                                                            label_names=['rectangle', 'ellipse', 'triangle'],
+                                                            task_name='OTEDetectionTestTask',
                                                             configurable_parameters=configurable_parameters)
         self.addCleanup(lambda: ProjectFactory.delete_project_with_id(project.id))
         labels = project.get_labels()
 
+        warnings.filterwarnings('ignore', message='.* coordinates .* are out of bounds.*')
         items = []
         for i in range(0, number_of_images):
             image_numpy, shapes = generate_random_annotated_image(image_width=640,
@@ -59,11 +61,12 @@ class TestOTEDetection(unittest.TestCase):
                 box = box.clip(0, 1)
                 box_shapes.append(Box(x1=box[0], y1=box[1], x2=box[2], y2=box[3], labels=shape_labels))
 
-            image = Image(name=f"image_{i}", project=project, numpy=image_numpy)
+            image = Image(name=f'image_{i}', project=project, numpy=image_numpy)
             image_identifier = ImageIdentifier(image.id)
             annotation = Annotation(kind=AnnotationKind.ANNOTATION, media_identifier=image_identifier)
             annotation.append_shapes(box_shapes)
             items.append(DatasetItem(media=image, annotation=annotation))
+        warnings.resetwarnings()
 
         rng = random.Random()
         rng.shuffle(items)
@@ -83,15 +86,11 @@ class TestOTEDetection(unittest.TestCase):
         return project, environment, dataset
 
     @staticmethod
-    def setup_configurable_parameters(num_epochs=10):
+    def setup_configurable_parameters(template_dir, num_epochs=10):
         configurable_parameters = MMDetectionParameters()
-        template_dir = osp.join(osp.dirname(osp.abspath(__file__)), '..', '..', '..', '..', '..',
-                                'configs', 'ote', 'custom-object-detection', 'mobilenet_v2-2s_ssd-256x256')
-        configurable_parameters.algo_backend.model.value = osp.join(template_dir, 'model.py')
         configurable_parameters.algo_backend.template.value = osp.join(template_dir, 'template.yaml')
+        configurable_parameters.algo_backend.model.value = 'model.py'
         configurable_parameters.algo_backend.model_name.value = 'some_detection_model'
-        configurable_parameters.learning_parameters.learning_rate_schedule.value = 'cyclic'
-        configurable_parameters.learning_parameters.batch_size.value = 32
         configurable_parameters.learning_parameters.num_epochs.value = num_epochs
         return configurable_parameters
 
@@ -101,19 +100,20 @@ class TestOTEDetection(unittest.TestCase):
         Tests starting and cancelling training.
 
         Flow of the test:
-        - Creates a randomly annotated project with 100 images and a video with 25 frames. Shapes in the project are
-            ['rectangle', 'triangle', 'circle']. Annotations are converted to bounding box annotations.
-        - Start training the mmdetection task and give cancel training signal after 10 seconds. Assert that training
+        - Creates a randomly annotated project with a small dataset containing 3 classes:
+            ['rectangle', 'triangle', 'circle'].
+        - Start training and give cancel training signal after 10 seconds. Assert that training
             stops within 35 seconds after that
-        - Start training the task and give cancel signal immediately. Assert that training stops within 25 seconds.
+        - Start training and give cancel signal immediately. Assert that training stops within 25 seconds.
 
         This test should be finished in under one minute on a workstation.
         """
-        configurable_parameters = self.setup_configurable_parameters(num_epochs=100)
+        template_dir = osp.join('configs', 'ote', 'custom-object-detection', 'mobilenet_v2-2s_ssd-256x256')
+        configurable_parameters = self.setup_configurable_parameters(template_dir, num_epochs=100)
         _, detection_environment, dataset = self.init_environment(configurable_parameters, 250)
         detection_task = MMObjectDetectionTask(task_environment=detection_environment)
 
-        executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix="train_thread")
+        executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix='train_thread')
 
         # Test stopping after some time
         start_time = time.time()
@@ -122,7 +122,7 @@ class TestOTEDetection(unittest.TestCase):
         detection_task.cancel_training()
 
         # stopping process has to happen in less than 35 seconds
-        self.assertLess(time.time() - start_time, 35, "Expected to stop within 35 seconds [flaky].")
+        self.assertLess(time.time() - start_time, 35, 'Expected to stop within 35 seconds [flaky].')
         train_future.result()
 
         # Test stopping immediately
@@ -139,7 +139,7 @@ class TestOTEDetection(unittest.TestCase):
         start_time = time.time()
         result_dataset = task.analyse(dataset.with_empty_annotations())
         end_time = time.time()
-        print(f"{len(dataset)} analysed in {end_time - start_time} seconds")
+        print(f'{len(dataset)} analysed in {end_time - start_time} seconds')
         result_set = ResultSet(
             model=environment.model,
             ground_truth_dataset=dataset,
@@ -148,60 +148,76 @@ class TestOTEDetection(unittest.TestCase):
         performance = task.compute_performance(result_set)
         return performance
 
-    @flaky(max_runs=1, rerun_filter=rerun_on_flaky_assert())
-    def test_training_and_analyse(self):
+    @flaky(max_runs=2, rerun_filter=rerun_on_flaky_assert())
+    def train_and_eval(self, template_dir):
         """
-        Tests for training, analysis, evaluation, model optimization for the task
+        Run training, analysis, evaluation and model optimization
 
         Flow of the test:
-        - Creates a randomly annotated project with 100 images and a video with 25 frames. Shapes in the project are
-            ['rectangle', 'triangle', 'circle']. Annotations are converted to bounding box annotations.
-        - Trains a model for 5 epochs. Asserts that the returned model is not a NullModel, that
-            validation F-measure is larger than 0.5 and also that OpenVINO optimization runs successfully.
+        - Creates a randomly annotated project with a small dataset containing 3 classes:
+            ['rectangle', 'triangle', 'circle'].
+        - Trains a model for 10 epochs. Asserts that the returned model is not a NullModel, that
+            validation F-measure is larger than the threshold and also that OpenVINO optimization runs successfully.
         - Reloads the model in the task and recompute the performance. Asserts that the performance
             difference between the original and the reloaded model is smaller than 1e-4. Ideally there should be no
             difference at all.
         """
-        configurable_parameters = self.setup_configurable_parameters()
+        configurable_parameters = self.setup_configurable_parameters(template_dir)
         _, detection_environment, dataset = self.init_environment(configurable_parameters, 250)
         task = MMObjectDetectionTask(task_environment=detection_environment)
         self.addCleanup(task._delete_scratch_space)
 
-        print("MMDetection task initialized, model training starts.")
+        print('Task initialized, model training starts.')
         # Train the task.
         # train_task checks that the returned model is not a NullModel, that the task returns an OptimizedModel and that
-        # validation f-measure is higher than 0.5, which is a pretty low bar considering that the dataset is so easy
+        # validation f-measure is higher than the threshold, which is a pretty low bar
+        # considering that the dataset is so easy
 
         model = task.train(dataset=dataset)
         self.assertFalse(isinstance(model, NullModel))
 
         if isinstance(task, IModelOptimizer):
             optimized_models = task.optimize_loaded_model()
-            self.assertGreater(len(optimized_models), 0, "Task must return an Optimised model.")
+            self.assertGreater(len(optimized_models), 0, 'Task must return an Optimised model.')
             for m in optimized_models:
                 self.assertIsInstance(m, OptimizedModel,
-                                      "Optimised model must be an Openvino or DeployableTensorRT model.")
+                                      'Optimised model must be an Openvino or DeployableTensorRT model.')
 
         # Run inference
         validation_performance = self.eval(task, detection_environment, dataset)
-        print(f"Evaluated model to have a performance of {validation_performance}")
-        self.assertGreater(validation_performance.score.value, 0.5, "Expected F-measure to be higher than 0.5 [flaky]")
+        print(f'Evaluated model to have a performance of {validation_performance}')
+        score_threshold = 0.5
+        self.assertGreater(validation_performance.score.value, score_threshold,
+            f'Expected F-measure to be higher than {score_threshold} [flaky]')
 
-        print("Reloading model.")
+        print('Reloading model.')
         # Re-load the model
         task.load_model(task.task_environment)
 
-        print("Reevaluating model.")
+        print('Reevaluating model.')
         # Performance should be the same after reloading
         performance_after_reloading = self.eval(task, detection_environment, dataset)
         performance_delta = performance_after_reloading.score.value - validation_performance.score.value
         perf_delta_tolerance = 0.0001
 
         self.assertLess(np.abs(performance_delta), perf_delta_tolerance,
-                        msg=f"Expected no or very small performance difference after reloading. Performance delta "
-                            f"({validation_performance.score.value} vs {performance_after_reloading.score.value}) was "
-                            f"larger than the tolerance of {perf_delta_tolerance}")
+                        msg=f'Expected no or very small performance difference after reloading. Performance delta '
+                            f'({validation_performance.score.value} vs {performance_after_reloading.score.value}) was '
+                            f'larger than the tolerance of {perf_delta_tolerance}')
 
-        print(f"Performance: {validation_performance.score.value:.4f}")
-        print(f"Performance after reloading: {performance_after_reloading.score.value:.4f}")
-        print(f"Performance delta after reloading: {performance_delta:.6f}")
+        print(f'Performance: {validation_performance.score.value:.4f}')
+        print(f'Performance after reloading: {performance_after_reloading.score.value:.4f}')
+        print(f'Performance delta after reloading: {performance_delta:.6f}')
+
+    @flaky(max_runs=2, rerun_filter=rerun_on_flaky_assert())
+    def test_training_custom_mobilenetssd_256(self):
+        self.train_and_eval(osp.join('configs', 'ote', 'custom-object-detection', 'mobilenet_v2-2s_ssd-256x256'))
+
+    @flaky(max_runs=2, rerun_filter=rerun_on_flaky_assert())
+    def test_training_custom_mobilenetssd_384(self):
+        self.train_and_eval(osp.join('configs', 'ote', 'custom-object-detection', 'mobilenet_v2-2s_ssd-384x384'))
+
+    @flaky(max_runs=2, rerun_filter=rerun_on_flaky_assert())
+    def test_training_custom_mobilenetssd_512(self):
+        self.train_and_eval(osp.join('configs', 'ote', 'custom-object-detection', 'mobilenet_v2-2s_ssd-512x512'))
+
