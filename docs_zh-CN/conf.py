2b98130e1d2145842668604402a1c438a48c2a28
Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>
2021-06-09 11:46:30 +0800

    add chinese docs framework (#5299)

--- docs_zh-CN/conf.py
+++ docs_zh-CN/conf.py
@@ -81,6 +81,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+language = 'zh_CN'
+
 
 def builder_inited_handler(app):
     subprocess.run(['./stat.py'])
