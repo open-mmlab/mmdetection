e084958d2691a6eeb3000203105cd059897554c1
zhangshilong <2392587229zsl@gmail.com>
2021-06-14 23:17:51 +0800

    revert __init__ of core

---
+++ mmdet/core/__init__.py
@@ -0,0 +1,6 @@
+from .anchor import *  # noqa: F401, F403
+from .bbox import *  # noqa: F401, F403
+from .evaluation import *  # noqa: F401, F403
+from .mask import *  # noqa: F401, F403
+from .post_processing import *  # noqa: F401, F403
+from .utils import *  # noqa: F401, F403
