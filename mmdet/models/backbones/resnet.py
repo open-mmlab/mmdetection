a0daccef1f07a53c5547fbbf360dbcede6016a35
RangiLyu <lyuchqi@gmail.com>
2021-06-15 17:39:41 +0800

    fix typo

--- mmdet/models/backbones/resnet.py
+++ mmdet/models/backbones/resnet.py
@@ -397,7 +397,7 @@ class ResNet(BaseModule):
         assert not (init_cfg and pretrained), \
             'init_cfg and pretrained cannot be setting at the same time'
         if isinstance(pretrained, str):
-            warnings.warn('DeprecationWarning: pretrained is a deprecated, '
+            warnings.warn('DeprecationWarning: pretrained is deprecated, '
                           'please use "init_cfg" instead')
             self.init_cfg = dict(type='Pretrained', checkpoint=pretrained)
         elif pretrained is None:
