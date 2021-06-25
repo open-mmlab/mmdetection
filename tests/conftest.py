026fa929085393b354980c42ac5adc5067ecfbb4
Leonid Beynenson <Leonid.Beynenson@intel.com>
2021-06-25 12:11:33 +0300

    Add the first version of ote training test; WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,16 @@ try:
     import e2e.fixtures
 
     from e2e.conftest_utils import * # noqa
+    from e2e.conftest_utils import pytest_addoption as _e2e_pytest_addoption # noqa
     from e2e import config # noqa
     from e2e.utils import get_plugins_from_packages
     pytest_plugins = get_plugins_from_packages([e2e])
 except ImportError:
+    _e2e_pytest_addoption = None
     pass
 
+def pytest_addoption(parser):
+    if _e2e_pytest_addoption:
+        _e2e_pytest_addoption(parser)
+    parser.addoption('--dataset-definitions', action='store', default=None,
+                     help='Path to the dataset_definitions.yml file for tests that require datasets.')
---
+++ tests/test_ote_training.py
@@ -0,0 +1,185 @@
+#import functools
+#import numpy as np
+#import os.path as osp
+#import pytest
+#import random
+#import time
+#import warnings
+#from concurrent.futures import ThreadPoolExecutor
+#
+#from flaky import flaky
+#from sc_sdk.entities.annotation import Annotation, AnnotationScene, AnnotationSceneKind
+#from sc_sdk.entities.dataset_item import DatasetItem
+#from sc_sdk.entities.datasets import Dataset, Subset
+#from sc_sdk.entities.image import Image
+#from sc_sdk.entities.media_identifier import ImageIdentifier
+#from sc_sdk.entities.model import NullModel
+#from sc_sdk.entities.optimized_model import OptimizedModel
+#from sc_sdk.entities.resultset import ResultSet
+#from sc_sdk.entities.shapes.box import Box
+#from sc_sdk.entities.shapes.ellipse import Ellipse
+#from sc_sdk.entities.shapes.polygon import Polygon
+#from sc_sdk.entities.task_environment import TaskEnvironment
+#from sc_sdk.tests.test_helpers import generate_random_annotated_image, rerun_on_flaky_assert
+#from sc_sdk.usecases.tasks.interfaces.model_optimizer import IModelOptimizer
+#from sc_sdk.utils.project_factory import ProjectFactory
+#
+#from mmdet.apis.ote.apis.detection import MMObjectDetectionTask, MMDetectionParameters, configurable_parameters
+
+#######
+import importlib
+import os.path as osp
+import pytest
+import sys
+import yaml
+
+from collections import namedtuple
+
+from sc_sdk.entities.analyse_parameters import AnalyseParameters
+from sc_sdk.entities.datasets import Subset
+from sc_sdk.entities.resultset import ResultSet
+from sc_sdk.entities.task_environment import TaskEnvironment
+from sc_sdk.logging import logger_factory
+from sc_sdk.utils.project_factory import ProjectFactory
+
+from mmdet.apis.ote.extension.datasets.mmdataset import MMDatasetAdapter
+
+from e2e_test_system import e2e_pytest
+
+
+logger = logger_factory.get_logger('Sample')
+
+def TRAINING_PARAMETERS_FIELDS():
+    return ['annotations_train',
+            'images_train_dir',
+            'annotations_val',
+            'images_val_dir',
+            'annotations_test',
+            'images_test_dir',
+            'template_file_path',
+            'data_dir',
+            ]
+TrainingParameters = namedtuple('TrainingParameters', TRAINING_PARAMETERS_FIELDS())
+
+@pytest.fixture
+def dataset_definitions(request):
+    path = request.config.getoption('--dataset-definitions')
+    assert path is not None, (f'The command line parameter "--dataset-definitions" is not set, '
+                             f'whereas it is required for the test {request.node.originalname or request.node.name}')
+    with open(path) as f:
+        data = yaml.safe_load(f)
+    return data
+
+def load_template(path):
+    with open(path) as f:
+        template = yaml.full_load(f)
+    # Save path to template file, to resolve relative paths later.
+    template['hyper_parameters']['params'].setdefault('algo_backend', {})['template'] = path
+    return template
+
+def get_task_class(path):
+    module_name, class_name = path.rsplit('.', 1)
+    module = importlib.import_module(module_name)
+    return getattr(module, class_name)
+
+
+def run_ote_training(params: TrainingParameters):
+#    params.annotations_train = None
+#    params.images_train_dir = None
+#    params.annotations_val = None
+#    params.images_val_dir = None
+#    params.template_file_path = 'configs/ote/custom-object-detection/mobilenet_v2-2s_ssd-256x256/template.yaml'
+#    params.data_dir = '/home/lbeynens/_Work/ICV/Deep/OTE_Validation/Experiments/experiment_003_run_pytest_ote_training/COCO/'
+    if not (params.annotations_train or params.images_train_dir or params.annotations_val or params.images_val_dir):
+        print('Using COCO val both for training and validation')
+        train_ann_file = osp.join(params.data_dir, 'coco/annotations/instances_val2017.json')
+        train_data_root = osp.join(params.data_dir, 'coco/val2017/')
+        val_ann_file = osp.join(params.data_dir, 'coco/annotations/instances_val2017.json')
+        val_data_root = osp.join(params.data_dir, 'coco/val2017/')
+        test_ann_file = osp.join(params.data_dir, 'coco/annotations/instances_val2017.json')
+        test_data_root = osp.join(params.data_dir, 'coco/val2017/')
+    else:
+        #
+        # check consistency
+        if not all([params.annotations_train, params.images_train_dir, params.annotations_val, params.images_val_dir]):
+            raise RuntimeError('If not default COCO dataset is used, the following parametrers should be set: '
+                               '--annotations-train, --images-train, --annotations-val, --images-val')
+        print(f'Using for train annotation file {params.annotations_train}')
+        print(f'Using for val annotation file {params.annotations_val}')
+        train_ann_file = params.annotations_train
+        train_data_root = params.images_train_dir
+        val_ann_file = params.annotations_val
+        val_data_root = params.images_val_dir
+        test_ann_file = params.annotations_test
+        test_data_root = params.images_test_dir
+
+    dataset = MMDatasetAdapter(
+        train_ann_file=train_ann_file,
+        train_data_root=train_data_root,
+        val_ann_file=val_ann_file,
+        val_data_root=val_data_root,
+        test_ann_file=test_ann_file,
+        test_data_root=test_data_root)
+
+    template = load_template(params.template_file_path)
+    task_impl_path = template['task']['impl']
+    task_cls = get_task_class(task_impl_path)
+
+
+    project = ProjectFactory().create_project_single_task(
+        name='otedet-sample-project',
+        description='otedet-sample-project',
+        label_names=dataset.get_labels(),
+        task_name='otedet-task')
+
+    dataset.set_project_labels(project.get_labels())
+
+    print(f'train dataset: {len(dataset.get_subset(Subset.TRAINING))} items')
+    print(f'validation dataset: {len(dataset.get_subset(Subset.VALIDATION))} items')
+
+    environment = TaskEnvironment(project=project, task_node=project.tasks[-1])
+    params = task_cls.get_configurable_parameters(environment)
+    task_cls.apply_template_configurable_parameters(params, template)
+    environment.set_configurable_parameters(params)
+
+    task = task_cls(task_environment=environment)
+
+    # Tweak parameters.
+    params = task.get_configurable_parameters(environment)
+    if True:
+        from pprint import pformat
+        print(f'params before training=\n{pformat(params.serialize())}')
+    params.learning_parameters.nncf_quantization.value = False
+    # params.learning_parameters.learning_rate_warmup_iters.value = 0
+    params.learning_parameters.batch_size.value = 32
+    params.learning_parameters.num_epochs.value = 1
+    params.postprocessing.result_based_confidence_threshold.value = False
+    params.postprocessing.confidence_threshold.value = 0.025
+    environment.set_configurable_parameters(params)
+    task.update_configurable_parameters(environment)
+
+    logger.info('Start model training... [ROUND 0]')
+    model = task.train(dataset=dataset)
+    logger.info('Model training finished [ROUND 0]')
+
+
+@pytest.fixture
+#def training_params_coco_shortened_500():
+def training_params_coco_shortened_500(dataset_definitions):
+    cur_dataset_definition = dataset_definitions['coco_shortened_500']
+    training_parameters_fields = {k: v for k, v in cur_dataset_definition.items() if k in TRAINING_PARAMETERS_FIELDS()}
+    params = TrainingParameters(**training_parameters_fields)
+#    params = TrainingParameters(annotations_train=None,
+#                                images_train_dir=None,
+#                                annotations_val=None,
+#                                images_val_dir=None,
+#                                annotations_test=None,
+#                                images_test_dir=None,
+#                                template_file_path='configs/ote/custom-object-detection/mobilenet_v2-2s_ssd-256x256/template.yaml',
+#                                data_dir='/home/lbeynens/_Work/ICV/Deep/OTE_Validation/Experiments/experiment_003_run_pytest_ote_training/COCO/',)
+    return params
+
+@e2e_pytest
+def test_training_coco_shortened_500(training_params_coco_shortened_500):
+    run_ote_training(training_params_coco_shortened_500)
+
