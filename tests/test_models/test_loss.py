c538f050eae85c2892ed290feda450b3a4d83cb3
BIGWangYuDong <yudongwang@tju.edu.cn>
2021-06-29 13:50:17 +0800

    add loss test unit

--- tests/test_models/test_loss.py
+++ tests/test_models/test_loss.py
@@ -1,8 +1,11 @@
 import pytest
 import torch
 
-from mmdet.models.losses import (BoundedIoULoss, CIoULoss, DIoULoss, GIoULoss,
-                                 IoULoss)
+from mmdet.models.losses import (BalancedL1Loss, BoundedIoULoss, CIoULoss,
+                                 DIoULoss, DistributionFocalLoss, FocalLoss,
+                                 GaussianFocalLoss, GIoULoss, IoULoss, L1Loss,
+                                 MSELoss, QualityFocalLoss, SmoothL1Loss,
+                                 VarifocalLoss)
 
 
 @pytest.mark.parametrize(
@@ -14,3 +17,18 @@ def test_iou_type_loss_zeros_weight(loss_class):
 
     loss = loss_class()(pred, target, weight)
     assert loss == 0.
+
+
+@pytest.mark.parametrize('loss_class', [
+    IoULoss, BoundedIoULoss, GIoULoss, DIoULoss, CIoULoss, MSELoss, L1Loss,
+    SmoothL1Loss, BalancedL1Loss, FocalLoss, QualityFocalLoss,
+    GaussianFocalLoss, DistributionFocalLoss, VarifocalLoss
+])
+def test_loss_with_reduction_override(loss_class):
+    pred = torch.rand((10, 4))
+    target = torch.rand((10, 4))
+
+    with pytest.raises(AssertionError):
+        # assert whether reduction_override in loss function
+        reduction_override = True
+        loss_class()(pred, target, reduction_override=reduction_override)
