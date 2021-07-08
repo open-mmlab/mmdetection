88111276f3fd188071e250a7b3c43585eb7e2b38
Haian Huang(深度眸) <1286304229@qq.com>
2021-07-08 19:29:55 +0800

    Fix valid_mask logic error of rpn_head (#5562)

--- mmdet/models/dense_heads/cascade_rpn_head.py
+++ mmdet/models/dense_heads/cascade_rpn_head.py
@@ -631,14 +631,11 @@ class StageCascadeRPNHead(RPNHead):
         if cfg.min_bbox_size >= 0 and (not torch.onnx.is_in_onnx_export()):
             w = proposals[:, 2] - proposals[:, 0]
             h = proposals[:, 3] - proposals[:, 1]
-            valid_inds = torch.nonzero(
-                (w > cfg.min_bbox_size)
-                & (h > cfg.min_bbox_size),
-                as_tuple=False).squeeze()
-            if valid_inds.sum().item() != len(proposals):
-                proposals = proposals[valid_inds, :]
-                scores = scores[valid_inds]
-                ids = ids[valid_inds]
+            valid_mask = (w > cfg.min_bbox_size) & (h > cfg.min_bbox_size)
+            if not valid_mask.all():
+                proposals = proposals[valid_mask]
+                scores = scores[valid_mask]
+                ids = ids[valid_mask]
 
         # deprecate arguments warning
         if 'nms' not in cfg or 'max_num' in cfg or 'nms_thr' in cfg:
