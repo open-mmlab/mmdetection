efd9b92fc88e4d92f36ca754884302a00afdd73b
zhangshilong <2392587229zsl@gmail.com>
2021-07-12 19:10:02 +0800

    add multi scale

--- configs/condinst/condinst_coco_r50_pytorch_fpn_gn_mstrain_640-800_1x.py
+++ configs/condinst/condinst_coco_r50_pytorch_fpn_gn_mstrain_640-800_1x.py
@@ -60,6 +60,25 @@ model = dict(
         nms=dict(type='nms', iou_thr=0.5),
         max_per_img=100))
 # optimizer
+scale_values = (640, 672, 704, 736, 768, 800)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='Resize',
+        img_scale=[(scale, 1333) for scale in scale_values],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+data = dict(train=dict(pipeline=train_pipeline))
+
 optimizer = dict(
     type='SGD',
     lr=0.01,
