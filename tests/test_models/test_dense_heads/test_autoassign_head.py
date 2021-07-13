b9546b26845a4c3a5912238d066ca7f760a9a2c4
PJLAB\huanghaian <1286304229@qq.com>
2021-07-13 15:50:35 +0800

    fix unittest error

--- tests/test_models/test_dense_heads/test_autoassign_head.py
+++ tests/test_models/test_dense_heads/test_autoassign_head.py
@@ -69,9 +69,6 @@ def test_autoassign_head_loss():
     cls_scores = [torch.ones(2, 4, 5, 5)]
     bbox_preds = [torch.ones(2, 4, 5, 5)]
     iou_preds = [torch.ones(2, 1, 5, 5)]
-    mlvl_anchors = [torch.ones(5 * 5, 2)]
-    img_shape = None
-    scale_factor = [0.5, 0.5]
     cfg = mmcv.Config(
         dict(
             nms_pre=1000,
@@ -80,12 +77,5 @@ def test_autoassign_head_loss():
             nms=dict(type='nms', iou_threshold=0.6),
             max_per_img=100))
     rescale = False
-    self._get_bboxes(
-        cls_scores,
-        bbox_preds,
-        iou_preds,
-        mlvl_anchors,
-        img_shape,
-        scale_factor,
-        cfg,
-        rescale=rescale)
+    self.get_bboxes(
+        cls_scores, bbox_preds, iou_preds, img_metas, cfg, rescale=rescale)
