e2612a3e596cc286e94385ebeaf3db867a249eea
PJLAB\huanghaian <1286304229@qq.com>
2021-07-21 19:54:47 +0800

    add ema

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -2,9 +2,10 @@ from .inference import (async_inference_detector, inference_detector,
                         init_detector, show_result_pyplot)
 from .test import multi_gpu_test, single_gpu_test
 from .train import get_root_logger, set_random_seed, train_detector
+from .ema import EMAHook
 
 __all__ = [
     'get_root_logger', 'set_random_seed', 'train_detector', 'init_detector',
     'async_inference_detector', 'inference_detector', 'show_result_pyplot',
-    'multi_gpu_test', 'single_gpu_test'
+    'multi_gpu_test', 'single_gpu_test', 'EMAHook'
 ]
