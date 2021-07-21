d6036e83593bf8ef89ac897076d3a384de81fe41
mmz <mingze.miao@outlook.com>
2021-07-21 02:38:32 +0000

    move some parameters from function to class_int

--- mmdet/models/dense_heads/custom_centernet_head.py
+++ mmdet/models/dense_heads/custom_centernet_head.py
@@ -87,7 +87,13 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
                  init_cfg=None):
         super(CustomCenterNetHead, self).__init__(init_cfg)
         self.num_classes = num_classes
+        self.INF = 100000000
         self.strides = [8, 16, 32, 64, 128]
+        self.hm_min_overlap=0.8
+        self.delta=(1-self.hm_min_overlap)/(1+self.hm_min_overlap)
+        self.sizes_of_interest=[[0, 80], [64, 160], [128, 320], [256, 640], [512, 10000000]]
+        self.min_radius=4
+        self.with_agn_hm=True
 
         self.train_cfg = train_cfg
         self.test_cfg = test_cfg
@@ -96,8 +102,6 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
         self.out_kernel = 3
         norm = "GN"
         self.only_proposal = True
-
-
 
         ##########  initialize the   1.<cls_tower>   2.<bbox_tower>   3.<share_tower>[no]  4.<bbox_pred>  5.<agn_hm>  6.<cls_logits>[no]
 
@@ -380,8 +384,7 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
             reduction='sum') / reg_norm
         losses['loss_centernet_loc'] = reg_loss
 
-        # if self.with_agn_hm:
-        if True:
+        if self.with_agn_hm:
             cat_agn_heatmap = flattened_hms.max(dim=1)[0] # M
             agn_pos_loss, agn_neg_loss = self.binary_heatmap_focal_loss(
                 agn_hm_pred, cat_agn_heatmap, pos_inds,
@@ -440,25 +443,16 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
         '''
 
         # get positive pixel index
-
-        INF = 100000000
-        num_classes=80
-        sizes_of_interest=[[0, 80], [64, 160], [128, 320], [256, 640], [512, 10000000]]
-        only_proposal=True
-        hm_min_overlap=0.8
-        delta=(1-hm_min_overlap)/(1+hm_min_overlap)
-        min_radius=4
-
         pos_inds = self._get_label_inds(gt_bboxes, shapes_per_level) 
 
-        heatmap_channels = num_classes
+        heatmap_channels = self.num_classes
         L = len(grids)
         num_loc_list = [len(loc) for loc in grids]
         strides = torch.cat([
             shapes_per_level.new_ones(num_loc_list[l]) * self.strides[l] \
             for l in range(L)]).float() # M
         reg_size_ranges = torch.cat([
-            shapes_per_level.new_tensor(sizes_of_interest[l]).float().view(
+            shapes_per_level.new_tensor(self.sizes_of_interest[l]).float().view(
             1, 2).expand(num_loc_list[l], 2) for l in range(L)]) # M x 2
         grids = torch.cat(grids, dim=0) # M x 2
         M = grids.shape[0]
@@ -476,7 +470,7 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
                 reg_targets.append(grids.new_zeros((M, 4)) - self.INF)
                 flattened_hms.append(
                     grids.new_zeros((
-                        M, 1 if only_proposal else heatmap_channels)))
+                        M, 1 if self.only_proposal else heatmap_channels)))
                 continue
             
             l = grids[:, 0].view(M, 1) - boxes[:, 0].view(1, N) # M x N
@@ -503,14 +497,14 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
             dist2 = ((grids.view(M, 1, 2).expand(M, N, 2) - \
                 centers_expanded) ** 2).sum(dim=2) # M x N
             dist2[is_peak] = 0
-            radius2 = delta ** 2 * 2 * area # N
+            radius2 = self.delta ** 2 * 2 * area # N
             radius2 = torch.clamp(
-                radius2, min=min_radius ** 2)
+                radius2, min=self.min_radius ** 2)
             weighted_dist2 = dist2 / radius2.view(1, N).expand(M, N) # M x N            
             reg_target = self._get_reg_targets(
                 reg_target, weighted_dist2.clone(), reg_mask, area) # M x 4
 
-            if only_proposal:
+            if self.only_proposal:
                 flattened_hm = self._create_agn_heatmaps_from_dist(
                     weighted_dist2.clone()) # M x 1
             # else:
@@ -609,10 +603,8 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
         Return:
             is_cared_in_the_level: n x L
         '''
-
-        sizes_of_interest=[[0, 80], [64, 160], [128, 320], [256, 640], [512, 10000000]]
         size_ranges = boxes.new_tensor(
-            sizes_of_interest).view(len(sizes_of_interest), 2) # L x 2
+            self.sizes_of_interest).view(len(self.sizes_of_interest), 2) # L x 2
         crit = ((boxes[:, 2:] - boxes[:, :2]) **2).sum(dim=1) ** 0.5 / 2 # n
         n, L = crit.shape[0], size_ranges.shape[0]
         crit = crit.view(n, 1).expand(n, L)
@@ -670,13 +662,11 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
           dist (M x N)
           is_*: M x N
         '''
-        INF=100000000
-
-        dist[mask == 0] = INF * 1.0
+        dist[mask == 0] = self.INF * 1.0
         min_dist, min_inds = dist.min(dim=1) # M
         reg_targets_per_im = reg_targets[
             range(len(reg_targets)), min_inds] # M x N x 4 --> M x 4
-        reg_targets_per_im[min_dist == INF] = - INF
+        reg_targets_per_im[min_dist == self.INF] = - self.INF
         return reg_targets_per_im
 
     def assign_reg_fpn(self, reg_targets_per_im, size_ranges):
@@ -708,14 +698,14 @@ class CustomCenterNetHead(BaseDenseHead, BBoxTestMixin):
 
     def _flatten_outputs(self, clss, reg_pred, agn_hm_pred):
         # Reshape: (N, F, Hl, Wl) -> (N, Hl, Wl, F) -> (sum_l N*Hl*Wl, F)
-        with_agn_hm=True
+
 
         clss = torch.cat([x.permute(0, 2, 3, 1).reshape(-1, x.shape[1]) \
             for x in clss], 0) if clss[0] is not None else None
         reg_pred = torch.cat(
             [x.permute(0, 2, 3, 1).reshape(-1, 4) for x in reg_pred], 0)            
         agn_hm_pred = torch.cat([x.permute(0, 2, 3, 1).reshape(-1) \
-            for x in agn_hm_pred], 0) if with_agn_hm else None
+            for x in agn_hm_pred], 0) if self.with_agn_hm else None
         return clss, reg_pred, agn_hm_pred
 
     def reduce_sum(self, tensor):
