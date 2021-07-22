5bc1bda0e8280528a39fce5cee63efc965ff2b15
Eugene Liu <eugene.liu@intel.com>
2021-07-22 12:50:08 +0100

    add distributed support for early stopping without creating empty file

--- mmdet/apis/ote/extension/utils/hooks.py
+++ mmdet/apis/ote/extension/utils/hooks.py
@@ -12,53 +12,10 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-import os
 from math import inf
 
-from mmcv.runner import BaseRunner, EpochBasedRunner, IterBasedRunner
+from mmcv.runner import LrUpdaterHook
 from mmcv.runner.hooks import HOOKS, Hook
-
-
-@HOOKS.register_module()
-class CancelTrainingHook(Hook):
-    def __init__(self, interval: int = 5):
-        """
-        Periodically check whether whether a stop signal is sent to the runner during model training.
-        Every 'check_interval' iterations, the work_dir for the runner is checked to see if a file '.stop_training'
-        is present. If it is, training is stopped.
-
-        :param interval: Period for checking for stop signal, given in iterations.
-
-        """
-        self.interval = interval
-
-    def before_run(self, runner):
-        """ Remove .stop_training file before training begins if it exists
-
-        :param runner:
-        """
-        work_dir = runner.work_dir
-        stop_filepath = os.path.join(work_dir, '.stop_training')
-        if os.path.exists(stop_filepath):
-            os.remove(stop_filepath)
-
-    @staticmethod
-    def _check_for_stop_signal(runner: BaseRunner):
-        work_dir = runner.work_dir
-        stop_filepath = os.path.join(work_dir, '.stop_training')
-        if os.path.exists(stop_filepath):
-            if isinstance(runner, EpochBasedRunner):
-                epoch = runner.epoch
-                runner._max_epochs = epoch  # Force runner to stop by pretending it has reached it's max_epoch
-            elif isinstance(runner, IterBasedRunner):
-                iter = runner.iter
-                runner._max_iters = iter
-            runner.should_stop = True  # Set this flag to true to stop the current training epoch
-
-    def after_train_iter(self, runner: BaseRunner):
-        if not self.every_n_iters(runner, self.interval):
-            return
-        self._check_for_stop_signal(runner)
 
 
 @HOOKS.register_module()
@@ -80,7 +37,6 @@ class EarlyStoppingHook(Hook):
 
     def _init_rule(self, rule, key_indicator):
         """Initialize rule, key_indicator, comparison_func, and best score.
-
         Here is the rule to determine which rule is used for key indicator
         when the rule is not specific:
         1. If the key indicator is in ``self.greater_keys``, the rule will be
@@ -91,7 +47,6 @@ class EarlyStoppingHook(Hook):
            in ``self.greater_keys``, the rule will be specified as 'greater'.
         4. Or if the key indicator is equal to the substring in any one item
            in ``self.less_keys``, the rule will be specified as 'less'.
-
         Args:
             rule (str | None): Comparison rule for best score.
             key_indicator (str | None): Key indicator to determine the
@@ -122,6 +77,10 @@ class EarlyStoppingHook(Hook):
 
     def before_run(self, runner):
         self.by_epoch = False if runner.max_epochs is None else True
+        for hook in runner.hooks:
+            if isinstance(hook, LrUpdaterHook):
+                self.warmup_iters = hook.warmup_iters
+                break
 
     def after_train_iter(self, runner):
         """Called after every training iter to evaluate the results."""
@@ -134,7 +93,8 @@ class EarlyStoppingHook(Hook):
             self._do_check_stopping(runner)
 
     def _do_check_stopping(self, runner):
-        if not self._should_check_stopping(runner):
+        # skip early stopping checks before model fully warms up
+        if not self._should_check_stopping(runner) or self.warmup_iters > runner.iter:
             return
 
         if runner.rank == 0:
@@ -147,7 +107,7 @@ class EarlyStoppingHook(Hook):
                 if self.wait_count >= self.patience:
                     stop_point = runner.epoch if self.by_epoch else runner.iter
                     print(f"Early Stopping at :{stop_point} with best {self.key_indicator}: {self.best_score}")
-                    self._create_stop_file(runner)
+                    runner.should_stop = True
 
     def _should_check_stopping(self, runner):
         check_time = self.every_n_epochs if self.by_epoch else self.every_n_iters
@@ -155,12 +115,3 @@ class EarlyStoppingHook(Hook):
             # No evaluation during the interval.
             return False
         return True
-
-    def _create_stop_file(self, runner):
-        """ Create an empty stop file. CancelTrainingHook check this file under work_dir in order to stop training
-        across multiple training processes
-
-        :param runner: MMDetection runner
-        """
-        stop_filepath = os.path.join(runner.work_dir, '.stop_training')
-        open(stop_filepath, 'a').close()
--- mmdet/apis/ote/extension/utils/runner.py
+++ mmdet/apis/ote/extension/utils/runner.py
@@ -14,7 +14,9 @@
 
 import time
 
+import torch.distributed as dist
 from mmcv.runner import RUNNERS, EpochBasedRunner
+from mmcv.runner import get_dist_info
 
 
 @RUNNERS.register_module()
@@ -25,10 +27,27 @@ class EpochRunnerWithCancel(EpochBasedRunner):
     # TODO: Implement cancelling of training via keyboard interrupt signal, instead of should_stop
     """
 
+    def __init__(self, *args, **kwargs):
+        super(EpochRunnerWithCancel, self).__init__(*args, **kwargs)
+        self.should_stop = False
+        _, world_size = get_dist_info()
+        self.distributed = True if world_size > 1 else False
+
+    def stop(self):
+        if self.rank == 0 and self.should_stop:
+            broadcast_obj = [True]
+        else:
+            broadcast_obj = [None]
+
+        if self.distributed:
+            dist.broadcast_object_list(broadcast_obj, src=0)
+        if broadcast_obj[0]:
+            self._max_epochs = self.epoch
+        return broadcast_obj[0]
+
     def train(self, data_loader, **kwargs):
         self.model.train()
         self.mode = 'train'
-        self.should_stop = False
         self.data_loader = data_loader
         self._max_iters = self._max_epochs * len(self.data_loader)
         self.call_hook('before_train_epoch')
@@ -36,12 +55,12 @@ class EpochRunnerWithCancel(EpochBasedRunner):
         for i, data_batch in enumerate(self.data_loader):
             self._inner_iter = i
             self.call_hook('before_train_iter')
-            self.run_iter(data_batch, train_mode=True)
+            self.run_iter(data_batch, train_mode=True, **kwargs)
             self.call_hook('after_train_iter')
-            self._iter += 1
-            if self.should_stop:
-                self.should_stop = False
+            if self.stop():
                 break
+            self._iter += 1
 
         self.call_hook('after_train_epoch')
+        self.stop()
         self._epoch += 1
