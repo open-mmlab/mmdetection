8e4c4a83df20483310c6dfa2c581665eb9bcafa4
RangiLyu <lyuchqi@gmail.com>
2021-07-23 21:19:52 +0800

    rename yolox neck

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -12,7 +12,7 @@ from .pafpn import PAFPN
 from .rfp import RFP
 from .ssd_neck import SSDNeck
 from .yolo_neck import YOLOV3Neck
-from .yolo_pafpn import YOLOXPAFPN
+from .yolox_pafpn import YOLOXPAFPN
 
 __all__ = [
     'FPN', 'BFP', 'ChannelMapper', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
