a8d35afb1a4c022d0415a16488af6dec307a9f23
haochenye <21724054@zju.edu.cn>
2021-07-27 09:15:48 +0800

    mosaic_1280_1280

--- mmdet/datasets/dataset_wrappers.py
+++ mmdet/datasets/dataset_wrappers.py
@@ -1,14 +1,15 @@
 import bisect
 import math
 from collections import defaultdict
-
+from mmcv import build_from_cfg
 import numpy as np
 from mmcv.utils import print_log
 from torch.utils.data.dataset import ConcatDataset as _ConcatDataset
-
+import random
 from .builder import DATASETS
 from .coco import CocoDataset
-
+from .pipelines import Compose
+import mmcv
 
 @DATASETS.register_module()
 class ConcatDataset(_ConcatDataset):
@@ -84,7 +85,7 @@ class ConcatDataset(_ConcatDataset):
         # Check whether all the datasets support evaluation
         for dataset in self.datasets:
             assert hasattr(dataset, 'evaluate'), \
-                    f'{type(dataset)} does not implement evaluate function'
+                f'{type(dataset)} does not implement evaluate function'
 
         if self.separate_eval:
             dataset_idx = -1
@@ -280,3 +281,130 @@ class ClassBalancedDataset:
     def __len__(self):
         """Length after repetition."""
         return len(self.repeat_indices)
+
+
+@DATASETS.register_module()
+class MosaicMixUpDataset():
+    def __init__(self,
+                 dataset,
+                 pipeline=None,
+                 mosaic=True,
+                 mixup=True,
+                 degrees=10.0,
+                 translate=0.1,
+                 scale=(0.1, 2),
+                 shear=2.0,
+                 perspective=0.0,
+                 mixup_scale=(0.5, 1.5),
+                 input_size=(640, 640),
+                 center_ratio=(0.5, 1.5)
+                 ):
+        self.dataset = build_from_cfg(dataset, DATASETS)
+        self.pipeline = Compose(pipeline)
+        self.mosaic = mosaic
+        self.mixup = mixup
+
+        self.degrees = degrees
+        self.translate = translate
+        self.scale = scale
+        self.shear = shear
+        self.perspective = perspective
+        self.mixup_scale = mixup_scale
+        self.input_size = input_size
+        self.center_ratio = center_ratio
+
+        self.flag = self.dataset.flag
+        self.CLASSES = self.dataset.CLASSES
+
+    def __getitem__(self, idx):
+        if self.mosaic:
+            input_h, input_w = self.input_size[0], self.input_size[1]
+            mosaic_labels = []
+            mosaic_img = np.full((input_h * 2, input_w * 2, 3), 114, dtype=np.uint8)
+
+            input_dim = self.input_size
+            # yc, xc = s, s  # mosaic center x, y
+            xc = int(random.uniform(0.5 * input_dim[0], 1.5 * input_dim[0]))
+            yc = int(random.uniform(0.5 * input_dim[1], 1.5 * input_dim[1]))
+
+            indices = [idx] + [random.randint(0, len(self.dataset) - 1) for _ in range(3)]
+
+            for i, loc in enumerate(('top_left', 'top_right', 'bottom_left', 'bottom_right')):
+                results = self.dataset[indices[i]]
+
+                img = results["img"]
+                gt_labels = results["gt_labels"]
+                gt_bboxes = results["gt_bboxes"]
+
+                _labels = np.concatenate([gt_bboxes, gt_labels[:, None]], axis=1)
+                # adaptive for dynamic size
+                h0, w0 = img.shape[:2]  # orig hw
+                scale = min(1. * input_dim[0] / h0, 1. * input_dim[1] / w0)
+                img = mmcv.imresize(img, (int(w0 * scale), int(h0 * scale)), return_scale=False)
+                h, w, c = img.shape
+
+                x1a, y1a, x2a, y2a, x1b, y1b, x2b, y2b = \
+                    self.get_mosaic_coordinate(loc, xc, yc, w, h, input_h, input_w)
+
+                mosaic_img[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]  # img4[ymin:ymax, xmin:xmax]
+                padw = x1a - x1b
+                padh = y1a - y1b
+
+                labels = _labels.copy()
+                if _labels.size > 0:
+                    labels[:, 0] = scale * _labels[:, 0] + padw
+                    labels[:, 1] = scale * _labels[:, 1] + padh
+                    labels[:, 2] = scale * _labels[:, 2] + padw
+                    labels[:, 3] = scale * _labels[:, 3] + padh
+                mosaic_labels.append(labels)
+
+            if len(mosaic_labels):
+                mosaic_labels = np.concatenate(mosaic_labels, 0)
+                np.clip(mosaic_labels[:, 0], 0, 2 * input_dim[1], out=mosaic_labels[:, 0])
+                np.clip(mosaic_labels[:, 1], 0, 2 * input_dim[0], out=mosaic_labels[:, 1])
+                np.clip(mosaic_labels[:, 2], 0, 2 * input_dim[1], out=mosaic_labels[:, 2])
+                np.clip(mosaic_labels[:, 3], 0, 2 * input_dim[0], out=mosaic_labels[:, 3])
+
+            img, img_shape, gt_bboxes, gt_labels = mosaic_img, mosaic_img.shape, mosaic_labels[:, :4], mosaic_labels[:, 4]
+
+            results["img"] = img
+            results["img_shape"] = img_shape
+            results["ori_shape"] = img_shape
+            results["gt_labels"] = gt_labels
+
+            import cv2
+            for i in range(len(gt_bboxes)):
+                box = gt_bboxes[i]
+                x1, y1, x2, y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+            cv2.imwrite(f"mosaic/mosaic/{idx}.jpg", img)
+
+        else:
+            results = self.dataset[idx]
+            results = self.pipeline(results)
+
+
+        if self.mixup:
+            pass
+
+        return results
+
+    def get_mosaic_coordinate(self, loc, xc, yc, w, h, input_h, input_w):
+        # TODO update doc
+        # index0 to top left part of image
+        if loc == "top_left":
+            x1, y1, x2, y2 = max(xc - w, 0), max(yc - h, 0), xc, yc
+            small_coord = w - (x2 - x1), h - (y2 - y1), w, h
+        # index1 to top right part of image
+        elif loc == "top_right":
+            x1, y1, x2, y2 = xc, max(yc - h, 0), min(xc + w, input_w * 2), yc
+            small_coord = 0, h - (y2 - y1), min(w, x2 - x1), h
+        # index2 to bottom left part of image
+        elif loc == "bottom_left":
+            x1, y1, x2, y2 = max(xc - w, 0), yc, xc, min(input_h * 2, yc + h)
+            small_coord = w - (x2 - x1), 0, w, min(y2 - y1, h)
+        # index2 to bottom right part of image
+        elif loc == "bottom_right":
+            x1, y1, x2, y2 = xc, yc, min(xc + w, input_w * 2), min(input_h * 2, yc + h)  # noqa
+            small_coord = 0, 0, min(w, x2 - x1), min(y2 - y1, h)
+        return [x1, y1, x2, y2, *small_coord]
