515bc5f0b4264a6c1a5e5bddee34d430d83d8697
RangiLyu <lyuchqi@gmail.com>
2021-07-28 16:45:18 +0800

    fix head init

--- mmdet/models/dense_heads/yolox_head.py
+++ mmdet/models/dense_heads/yolox_head.py
@@ -156,6 +156,7 @@ class YOLOXHead(BaseDenseHead, BBoxTestMixin):
         return conv_cls, conv_reg, conv_obj
 
     def init_weights(self):
+        super(YOLOXHead, self).init_weights()
         # Use prior in model initialization to improve stability
         bias_init = bias_init_with_prob(0.01)
         for conv_cls, conv_obj in zip(self.multi_level_conv_cls,
