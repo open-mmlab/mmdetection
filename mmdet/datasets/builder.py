dd9988bf037b661edb4a9965ca954fb6fd7bc068
PJLAB\huanghaian <1286304229@qq.com>
2021-07-29 18:46:26 +0800

    revert

--- mmdet/datasets/builder.py
+++ mmdet/datasets/builder.py
@@ -14,10 +14,10 @@ from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
 if platform.system() != 'Windows':
     # https://github.com/pytorch/pytorch/issues/973
     import resource
-
     rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+    base_soft_limit = rlimit[0]
     hard_limit = rlimit[1]
-    soft_limit = min(4096, hard_limit)
+    soft_limit = min(max(4096, base_soft_limit), hard_limit)
     resource.setrlimit(resource.RLIMIT_NOFILE, (soft_limit, hard_limit))
 
 DATASETS = Registry('dataset')
