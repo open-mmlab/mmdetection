c23da6ab4ce5dcc2ab9360a204109ed32b9a530c
RangiLyu <lyuchqi@gmail.com>
2021-07-29 16:03:13 +0800

    [Update]: Update some configs to new settings.

--- configs/yolox/yolox_nano.py
+++ configs/yolox/yolox_nano.py
@@ -2,9 +2,10 @@ _base_ = './yolox_tiny.py'
 
 # model settings
 model = dict(
-    backbone=dict(depth=0.33, width=0.25, depthwise=True),
-    bbox_head=dict(width=0.25, depthwise=True)
-)
-
-
-
+    backbone=dict(deepen_factor=0.33, widen_factor=0.25, depthwise=True),
+    neck=dict(
+        in_channels=[64, 128, 256],
+        out_channels=64,
+        csp_num_blocks=1,
+        depthwise=True),
+    bbox_head=dict(in_channels=64, feat_channels=64, depthwise=True))
