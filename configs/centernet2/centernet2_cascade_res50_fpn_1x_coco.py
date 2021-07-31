9e318e4feccf43ab6f32ea50652f08d6a87a221f
mmz <mingze.miao@outlook.com>
2021-07-31 13:23:20 +0000

    lr v2

--- configs/centernet2/centernet2_cascade_res50_fpn_1x_coco.py
+++ configs/centernet2/centernet2_cascade_res50_fpn_1x_coco.py
@@ -6,14 +6,14 @@ _base_ = [
 optimizer_config = dict(
     _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.00025, momentum=0.9, weight_decay=0.0001)
 
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[1, 3])
-runner = dict(type='EpochBasedRunner', max_epochs=6)
+    warmup_iters=1000,
+    warmup_ratio=0.00001,
+    step=[18])
+runner = dict(type='EpochBasedRunner', max_epochs=20)
 
 find_unused_parameters=True
