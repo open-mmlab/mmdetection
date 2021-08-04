595873c5f461b403c79b910c25530774a575ce23
mmz <mingze.miao@outlook.com>
2021-08-04 08:08:12 +0000

    none

--- configs/_base_/models/centernet2_cascade_r50_fpn.py
+++ configs/_base_/models/centernet2_cascade_r50_fpn.py
@@ -27,7 +27,7 @@ model = dict(
         num_box_convs=4,
         num_share_convs=0,
         use_deformable=False,
-        # loss_center_heatmap=dict(type='CustomGaussianFocalLoss', alpha=0.25,ignore_high_fp=0.85,loss_weight=0.5),
+        loss_center_heatmap=dict(type='CustomGaussianFocalLoss', alpha=0.25,ignore_high_fp=0.85,loss_weight=0.5),
         ),
     roi_head=dict(
         type='CascadeRoIHead',
