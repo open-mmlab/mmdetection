35a79c0dbff4ac681b1644ec91783902eee80d54
Ilya Krylov <ilya.krylov@intel.com>
2021-08-05 14:33:28 +0300

    mmdataset fix of Leonid

--- mmdet/apis/ote/extension/datasets/mmdataset.py
+++ mmdet/apis/ote/extension/datasets/mmdataset.py
@@ -84,17 +84,77 @@ class OTEDataset(CustomDataset):
     named like that and not dataset_items.
 
     """
+
+    class _DataInfoProxy:
+        """
+        This class is intended to be a wrapper to use it in CustomDataset-derived class as `self.data_infos`.
+        Instead of using list `data_infos` as in CustomDataset, our implementation of dataset OTEDataset
+        uses this proxy class with overriden __len__ and __getitem__; this proxy class
+        forwards data access operations to ote_dataset and converts the dataset items to the view
+        convenient for mmdetection.
+        """
+        def __init__(self, ote_dataset, classes):
+            self.ote_dataset = ote_dataset
+            self.CLASSES = classes
+
+        def __len__(self):
+            return len(self.ote_dataset)
+
+        def __getitem__(self, index):
+            """
+            Prepare a dict 'data_info' that is expected by the mmdet pipeline to handle images and annotations
+            :return data_info: dictionary that contains the image and image metadata, as well as the labels of the objects
+                in the image
+            """
+
+            dataset = self.ote_dataset
+            item = dataset[index]
+
+            height, width = item.height, item.width
+
+            data_info = dict(dataset_item=item, width=width, height=height, dataset_id=dataset.id, index=index,
+                            ann_info=dict(label_list=self.CLASSES))
+
+            return data_info
+
     def __init__(self, ote_dataset: Dataset, pipeline, classes=None, test_mode: bool = False):
         self.ote_dataset = ote_dataset
         self.test_mode = test_mode
         self.CLASSES = self.get_classes(classes)
-        self.data_infos = self.load_dataset_items()
+
+        # Instead of using list data_infos as in CustomDataset, this implementation of dataset
+        # uses a proxy class with overriden __len__ and __getitem__; this proxy class
+        # forwards data access operations to ote_dataset.
+        # Note that list `data_infos` cannot be used here, since OTE dataset class does not have interface to
+        # get only annotation of a data item, so we would load the whole data item (including image)
+        # even if we need only checking aspect ratio of the image; due to it
+        # this implementation of dataset does not uses such tricks as skipping images with wrong aspect ratios or
+        # small image size, since otherwise reading the whole dataset during initialization will be required.
+        self.data_infos = OTEDataset._DataInfoProxy(ote_dataset, self.CLASSES)
+
         self.proposals = None  # Attribute expected by mmdet but not used for OTE datasets
 
         if not test_mode:
             self._set_group_flag()
 
         self.pipeline = Compose(pipeline)
+
+    def _set_group_flag(self):
+        """Set flag for grouping images.
+
+        Originally, in Custom dataset, images with aspect ratio greater than 1 will be set as group 1,
+        otherwise group 0.
+        This implementation will set group 0 for every image.
+        """
+        self.flag = np.zeros(len(self), dtype=np.uint8)
+
+    def _rand_another(self, idx):
+        return np.random.choice(len(self))
+
+    # In contrast with CustomDataset this implementation of dataset
+    # does not filter images w.r.t. the min size
+    def _filter_imgs(self, min_size=32):
+        raise NotImplementedError
 
     def prepare_train_img(self, idx: int) -> dict:
         """Get training data and annotations after pipeline.
@@ -124,25 +184,6 @@ class OTEDataset(CustomDataset):
         results['bbox_fields'] = []
         results['mask_fields'] = []
         results['seg_fields'] = []
-
-    def load_dataset_items(self) -> List:
-        """
-        Prepare a dict 'data_infos' that is expected by the mmdet pipeline to handle images and annotations
-
-        :return data_infos: dictionary that contains the image and image metadata, as well as the labels of the objects
-            in the image
-        """
-        dataset = self.ote_dataset
-        dataset_items = []
-
-        for ii, item in enumerate(dataset):
-            height, width = item.height, item.width
-
-            img_info = dict(dataset_item=item, width=width, height=height, dataset_id=dataset.id, index=ii,
-                            ann_info=dict(label_list=self.CLASSES))
-            dataset_items.append(img_info)
-
-        return dataset_items
 
     def get_ann_info(self, idx):
         """
