3e34f1d225fc0faa0b9447484fa72ca6791dcc8e
mmz <mingze.miao@outlook.com>
2021-08-05 04:43:51 +0000

    centernet v1.1

--- configs/centernet2/centernet2_cascade_res50_fpn_1x_coco.py
+++ configs/centernet2/centernet2_cascade_res50_fpn_1x_coco.py
@@ -6,9 +6,6 @@ _base_ = [
 optimizer_config = dict(
     _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 
-<<<<<<< HEAD
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-=======
 optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 
 # # lr_config = dict(
@@ -18,7 +15,6 @@ optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 # #     warmup_ratio=0.00001,
 # #     step=[18])
 # # runner = dict(type='EpochBasedRunner', max_epochs=20)
->>>>>>> dev3
 
 data = dict(
     samples_per_gpu=4,
