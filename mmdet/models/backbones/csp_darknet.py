329b03948e106b1e409290463623ce0d0587cce1
zhangshilong <2392587229zsl@gmail.com>
2021-08-06 14:10:21 +0800

    fix doc str

--- mmdet/models/backbones/csp_darknet.py
+++ mmdet/models/backbones/csp_darknet.py
@@ -126,9 +126,9 @@ class CSPDarknet(BaseModule):
     Args:
         arch (str): Architechture of CSP-Darknet, from {P5, P6}.
             Default: P5.
-        deepen_factor (float): Width multiplier, multiply number of
+        deepen_factor (float): Depth multiplier, multiply number of
             channels in each layer by this amount. Default: 1.0.
-        widen_factor (float): Depth multiplier, multiply number of
+        widen_factor (float): Width multiplier, multiply number of
             blocks in CSP layer by this amount. Default: 1.0.
         out_indices (Sequence[int]): Output from which stages.
             Default: (2, 3, 4).
