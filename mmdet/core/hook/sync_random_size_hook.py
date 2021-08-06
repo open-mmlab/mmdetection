c181587714c78144e62086610e34f46921852b6a
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-08-06 14:21:42 +0800

    [Feature] Add SyncRandomSizeHook, YOLOXModeSwitchHook and SyncNormHook (#5773)
    * Add yolox_process_hook from yolox-dev
    
    * Split the YOLOX Hooks
    
    * Fix docs
    
    * Applied suggestions from reviewer
    
    * Apply suggestions from reviewer
    
    * Add docstr and applied suggestions
    
    * update
    
    * update doc
    
    * Applied suggestions about the docs
    
    * Fixing docs
    
    * Applied suggestions from reviewer
    
    * rename
    
    * rename and add unittest
    
    * fix lint
    
    Co-authored-by: PJLAB\huanghaian <1286304229@qq.com>

---
+++ mmdet/core/hook/sync_random_size_hook.py
@@ -0,0 +1,52 @@
+import random
+
+import torch
+from mmcv.runner import get_dist_info
+from mmcv.runner.hooks import HOOKS, Hook
+from torch import distributed as dist
+
+
+@HOOKS.register_module()
+class SyncRandomSizeHook(Hook):
+    """Change and synchronize the random image size across ranks, currently
+    used in YOLOX.
+
+    Args:
+        ratio_range (tuple[int]): Random ratio range. It will be multiplied
+            by 32, and then change the dataset output image size.
+            Default: (14, 26).
+        img_scale (tuple[int]): Size of input image. Default: (640, 640).
+        interval (int): The interval of change image size. Default: 10.
+    """
+
+    def __init__(self,
+                 ratio_range=(14, 26),
+                 img_scale=(640, 640),
+                 interval=10):
+        self.rank, world_size = get_dist_info()
+        self.is_distributed = world_size > 1
+        self.ratio_range = ratio_range
+        self.img_scale = img_scale
+        self.interval = interval
+
+    def after_train_iter(self, runner):
+        """Change the dataset output image size."""
+        if self.ratio_range is not None and (runner.iter +
+                                             1) % self.interval == 0:
+            tensor = torch.LongTensor(2)
+            if torch.cuda.is_available():
+                tensor = tensor.to(runner.model.device)
+
+            if self.rank == 0:
+                size_factor = self.img_scale[1] * 1. / self.img_scale[0]
+                size = random.randint(*self.ratio_range)
+                size = (int(32 * size), 32 * int(size * size_factor))
+                tensor[0] = size[0]
+                tensor[1] = size[1]
+
+            if self.is_distributed:
+                dist.barrier()
+                dist.broadcast(tensor, 0)
+
+            runner.data_loader.dataset.dynamic_scale = (tensor[0].item(),
+                                                        tensor[1].item())
---
+++ mmdet/core/hook/yolox_mode_switch_hook.py
@@ -0,0 +1,34 @@
+from mmcv.parallel import is_module_wrapper
+from mmcv.runner.hooks import HOOKS, Hook
+
+
+@HOOKS.register_module()
+class YOLOXModeSwitchHook(Hook):
+    """Switch the mode of YOLOX during training.
+
+    This hook turns off the mosaic and mixup data augmentation and switches
+    to use L1 loss in bbox_head.
+
+    Args:
+        num_last_epochs (int): The number of latter epochs in the end of the
+            training to close the data augmentation and switch to L1 loss.
+            Default: 15.
+    """
+
+    def __init__(self, num_last_epochs=15):
+        self.num_last_epochs = num_last_epochs
+
+    def before_train_epoch(self, runner):
+        """Close mosaic and mixup augmentation and switches to use L1 loss."""
+        epoch = runner.epoch
+        train_loader = runner.data_loader
+        model = runner.model
+        if is_module_wrapper(model):
+            model = model.module
+        if (epoch + 1) == runner.max_epochs - self.num_last_epochs:
+            runner.logger.info('No mosaic and mixup aug now!')
+            # TODO
+            train_loader.dataset.enable_mosaic = False
+            train_loader.dataset.enable_mixup = False
+            runner.logger.info('Add additional L1 loss now!')
+            model.bbox_head.use_l1 = True
