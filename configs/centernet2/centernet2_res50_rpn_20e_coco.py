621816b2575a84ad7553e782c8183b461dd1cea5
yyz561 <yong_zheng@icloud.com>
2021-08-07 20:42:04 +0800

    fix dist train bug when use reduced mean

--- configs/centernet2/centernet2_res50_rpn_20e_coco.py
+++ configs/centernet2/centernet2_res50_rpn_20e_coco.py
@@ -53,7 +53,7 @@ model = dict(
         type='CascadeRoIHead',
         num_stages=3,
         stage_loss_weights=[1, 0.5, 0.25],
-        mult_proposal_score=True,
+        mult_proposal_score=False,
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
