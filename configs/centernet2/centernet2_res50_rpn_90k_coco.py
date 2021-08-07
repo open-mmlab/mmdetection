8bebddc9a86f37b256090f1907c42fb57137c401
yyz561 <yong_zheng@icloud.com>
2021-08-07 08:53:46 +0800

    fix to suit flake8 format

--- configs/centernet2/centernet2_res50_rpn_90k_coco.py
+++ configs/centernet2/centernet2_res50_rpn_90k_coco.py
@@ -191,7 +191,7 @@ train_pipeline = [
     dict(type='Resize',
          img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
                     (1333, 768), (1333, 800)],
-         multiscale_mode = 'value',
+         multiscale_mode='value',
          keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
