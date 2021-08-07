7c2b9b3495a19942345ed0a5e8d387edc5147e8a
mmz <mingze.miao@outlook.com>
2021-08-07 05:01:49 +0000

    mstrain

--- configs/centernet2/centernet2_cascade_res50_fpn_1x_coco.py
+++ configs/centernet2/centernet2_cascade_res50_fpn_1x_coco.py
@@ -4,19 +4,16 @@ _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
 optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 
-# # lr_config = dict(
-# #     policy='step',
-# #     warmup='linear',
-# #     warmup_iters=5000,
-# #     warmup_ratio=0.00001,
-# #     step=[18])
-# # runner = dict(type='EpochBasedRunner', max_epochs=20)
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=0.0001,
+    step=[8, 11])
+runner = dict(type='EpochBasedRunner', max_epochs=12)
 
 data = dict(
     samples_per_gpu=4,
@@ -28,14 +25,19 @@ train_pipeline = [
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='Resize',
-        img_scale=[(1333, 640), (1333, 800)],
-        multiscale_mode='range',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
         keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
 
 # add by jack for tersorboard
