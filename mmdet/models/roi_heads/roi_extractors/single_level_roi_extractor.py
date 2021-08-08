d73019e54a8e5df039714d8c55d8686fdb025889
mmz <mingze.miao@outlook.com>
2021-08-08 14:14:45 +0000

    map roi level modified

--- mmdet/models/roi_heads/roi_extractors/single_level_roi_extractor.py
+++ mmdet/models/roi_heads/roi_extractors/single_level_roi_extractor.py
@@ -32,6 +32,27 @@ class SingleRoIExtractor(BaseRoIExtractor):
                                                  featmap_strides, init_cfg)
         self.finest_scale = finest_scale
 
+    # def map_roi_levels(self, rois, num_levels):
+    #     """Map rois to corresponding feature levels by scales.
+
+    #     - scale < finest_scale * 2: level 0
+    #     - finest_scale * 2 <= scale < finest_scale * 4: level 1
+    #     - finest_scale * 4 <= scale < finest_scale * 8: level 2
+    #     - scale >= finest_scale * 8: level 3
+
+    #     Args:
+    #         rois (Tensor): Input RoIs, shape (k, 5).
+    #         num_levels (int): Total level number.
+
+    #     Returns:
+    #         Tensor: Level index (0-based) of each RoI, shape (k, )
+    #     """
+    #     scale = torch.sqrt(
+    #         (rois[:, 3] - rois[:, 1]) * (rois[:, 4] - rois[:, 2]))
+    #     target_lvls = torch.floor(torch.log2(scale / self.finest_scale + 1e-6))
+    #     target_lvls = target_lvls.clamp(min=0, max=num_levels - 1).long()
+    #     return target_lvls
+
     def map_roi_levels(self, rois, num_levels):
         """Map rois to corresponding feature levels by scales.
 
@@ -49,9 +70,9 @@ class SingleRoIExtractor(BaseRoIExtractor):
         """
         scale = torch.sqrt(
             (rois[:, 3] - rois[:, 1]) * (rois[:, 4] - rois[:, 2]))
-        target_lvls = torch.floor(torch.log2(scale / self.finest_scale + 1e-6))
-        target_lvls = target_lvls.clamp(min=0, max=num_levels - 1).long()
-        return target_lvls
+        target_lvls = torch.floor(4 + torch.log2(scale / 224 + 1e-8))
+        target_lvls = target_lvls.clamp(3, 7).long()
+        return target_lvls.to(torch.int64) - 3
 
     @force_fp32(apply_to=('feats', ), out_fp16=True)
     def forward(self, feats, rois, roi_scale_factor=None):
