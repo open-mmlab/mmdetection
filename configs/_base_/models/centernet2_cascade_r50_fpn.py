81310d68f1f6fd4682895f23000c37341f82dca2
mmz <mingze.miao@outlook.com>
2021-08-08 15:16:23 +0000

    merge jack and mmz

--- configs/_base_/models/centernet2_cascade_r50_fpn.py
+++ configs/_base_/models/centernet2_cascade_r50_fpn.py
@@ -37,7 +37,7 @@ model = dict(
     roi_head=dict(
         type='CustomCascadeRoIHead',
         num_stages=3,
-        stage_loss_weights=[1, 0.5, 0.25],
+        stage_loss_weights=[1, 1, 1],
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
