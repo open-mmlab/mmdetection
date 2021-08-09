45a3a194899cc4074c246740e970e32c7fcee2e1
PJLAB\huanghaian <1286304229@qq.com>
2021-08-09 19:00:55 +0800

    add comment

--- configs/yolox/yolox_s_300e_coco.py
+++ configs/yolox/yolox_s_300e_coco.py
@@ -12,7 +12,8 @@ model = dict(
     bbox_head=dict(
         type='YOLOXHead', num_classes=80, in_channels=128, feat_channels=128),
     train_cfg=dict(assigner=dict(type='SimOTAAssigner', center_radius=2.5)),
-    # TODO test 0.001 val 0.01
+    # In order to align the source code, the threshold of the val phase is
+    # 0.01, and the threshold of the test phase is 0.001.
     test_cfg=dict(score_thr=0.01, nms=dict(type='nms', iou_threshold=0.65)))
 
 # dataset settings
