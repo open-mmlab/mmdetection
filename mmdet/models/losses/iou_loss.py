[Fix]: Alpha of ciou loss does not have gradient.

mmdet/models/losses/iou_loss.py
@@ -213,9 +213,12 @@ def ciou_loss(pred, target, eps=1e-7):
     factor = 4 / math.pi**2
     v = factor * torch.pow(torch.atan(w2 / h2) - torch.atan(w1 / h1), 2)
 
+    with torch.no_grad():
+        alpha = (ious > 0.5).float() * v / (1 - ious + v)
+
     # CIoU
-    cious = ious - (rho2 / c2 + v**2 / (1 - ious + v))
-    loss = 1 - cious
+    cious = ious - (rho2 / c2 + alpha * v)
+    loss = 1 - cious.clamp(min=-1.0, max=1.0)
     return loss
 
 