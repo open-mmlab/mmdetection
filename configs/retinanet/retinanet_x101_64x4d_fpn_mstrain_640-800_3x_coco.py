036072f6500fff79e6c24a61c9880a8016d4e357
zhangshilong <2392587229zsl@gmail.com>
2021-08-10 14:26:00 +0800

    remove rebudent args

--- configs/retinanet/retinanet_x101_64x4d_fpn_mstrain_640-800_3x_coco.py
+++ configs/retinanet/retinanet_x101_64x4d_fpn_mstrain_640-800_3x_coco.py
@@ -4,14 +4,5 @@ _base_ = [
 # optimizer
 model = dict(
     pretrained='open-mmlab://resnext101_64x4d',
-    backbone=dict(
-        type='ResNeXt',
-        depth=101,
-        groups=64,
-        base_width=4,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        style='pytorch'))
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+    backbone=dict(type='ResNeXt', depth=101, groups=64, base_width=4))
+optimizer = dict(type='SGD', lr=0.01)
