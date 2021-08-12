1ee759649272ecdcd3fd93e54f7901e55d17d5e6
Ilya Krylov <ilya.krylov@intel.com>
2021-08-12 17:32:34 +0300

    added some dependencies

--- init_venv.sh
+++ init_venv.sh
@@ -90,6 +90,10 @@ pip install -e . -c ${CONSTRAINTS_FILE}
 MMDETECTION_DIR=`realpath .`
 echo "export MMDETECTION_DIR=${MMDETECTION_DIR}" >> ${venv_dir}/bin/activate
 
+pip install -e $SC_SDK_REPO/src/ote_sdk -c ${CONSTRAINTS_FILE}
+pip install -e $SC_SDK_REPO/src/sc_sdk -c ${CONSTRAINTS_FILE}
+pip install -e $SC_SDK_REPO/src/common/users_handler -c ${CONSTRAINTS_FILE}
+
 deactivate
 
 echo
