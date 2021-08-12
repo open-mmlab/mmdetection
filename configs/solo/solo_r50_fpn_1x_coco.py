cc5f863787a7947b7e79bdd35486a18964554532
zhangshilong <2392587229zsl@gmail.com>
2021-08-12 20:28:08 +0800

    format loss

--- configs/solo/solo_r50_fpn_1x_coco.py
+++ configs/solo/solo_r50_fpn_1x_coco.py
@@ -28,7 +28,7 @@ model = dict(
         feat_channels=256,
         strides=[8, 8, 16, 32, 32],
         scale_ranges=((1, 96), (48, 192), (96, 384), (192, 768), (384, 2048)),
-        sigma=0.2,
+        pos_scale=0.2,
         num_grids=[40, 36, 24, 16, 12],
         cls_down_index=0,
         loss_mask=dict(type='DiceLoss', use_sigmoid=True, loss_weight=3.0),
