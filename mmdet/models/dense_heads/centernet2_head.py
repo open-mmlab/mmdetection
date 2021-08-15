8a75f6b1bfdee6578bcce222ed5cd6f93711be6a
yyz561 <yong_zheng@icloud.com>
2021-08-15 18:58:15 +0800

    flake8 yapf controversy

--- mmdet/models/dense_heads/centernet2_head.py
+++ mmdet/models/dense_heads/centernet2_head.py
@@ -345,7 +345,7 @@ class CenterNet2Head(BaseDenseHead, BBoxTestMixin):
 
             # Use half diagonal length to assign boxes into different level
             # of feature maps.
-            half_diagonal = ((reg_target[:, :, :2] + reg_target[:, :, 2:])**
+            half_diagonal = ((reg_target[:, :, :2] + reg_target[:, :, 2:]) **
                              2).sum(dim=2)**0.5 / 2
             is_cared_in_the_level = (
                 (half_diagonal >= reg_size_ranges[:, [0]])
