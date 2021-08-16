682f03db0c0516324ec4e1e5d97e1d93c0a2485a
Yiwei Zhang <30398809+zywvvd@users.noreply.github.com>
2021-08-16 09:47:51 +0800

    make output result of ImageToTensor contiguous (#5756)
    In order to reduce the series of hidden dangers caused by discontinuous
    menory

--- mmdet/datasets/pipelines/formating.py
+++ mmdet/datasets/pipelines/formating.py
@@ -92,7 +92,7 @@ class ImageToTensor:
             img = results[key]
             if len(img.shape) < 3:
                 img = np.expand_dims(img, -1)
-            results[key] = to_tensor(img.transpose(2, 0, 1))
+            results[key] = (to_tensor(img.transpose(2, 0, 1))).contiguous()
         return results
 
     def __repr__(self):
