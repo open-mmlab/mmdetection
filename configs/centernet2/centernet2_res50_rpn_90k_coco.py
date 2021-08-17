a94c0b86ad044de57ac2799f41bf785d3cb2d7cf
yyz561 <yong_zheng@icloud.com>
2021-08-17 20:50:12 +0800

    set bs to 2

--- configs/centernet2/centernet2_res50_rpn_90k_coco.py
+++ configs/centernet2/centernet2_res50_rpn_90k_coco.py
@@ -208,8 +208,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=20,
-    workers_per_gpu=4,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
