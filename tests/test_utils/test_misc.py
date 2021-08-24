b9bb2a6408e9074452da70d26bd05bf1cb862165
BIGWangYuDong <yudongwang@tju.edu.cn>
2021-08-24 17:07:17 +0800

    add center of mass test unit

--- tests/test_utils/test_misc.py
+++ tests/test_utils/test_misc.py
@@ -4,7 +4,7 @@ import torch
 
 from mmdet.core.bbox import distance2bbox
 from mmdet.core.mask.structures import BitmapMasks, PolygonMasks
-from mmdet.core.utils import mask2ndarray
+from mmdet.core.utils import center_of_mass, mask2ndarray
 
 
 def dummy_raw_polygon_masks(size):
@@ -90,3 +90,20 @@ def test_distance2bbox():
     deltas = torch.zeros((2, 0, 4))
     out = distance2bbox(rois, deltas, max_shape=(120, 100))
     assert rois.shape == out.shape
+
+
+@pytest.mark.parametrize('mask', [
+    torch.ones((28, 28)),
+    torch.zeros((28, 28)),
+    torch.rand(28, 28) > 0.5,
+    torch.tensor([[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]])
+])
+def test_center_of_mass(mask):
+    center_h, center_w = center_of_mass(mask)
+    if mask.shape[0] == 4:
+        assert center_h == 1.5
+        assert center_w == 1.5
+    assert isinstance(center_h, torch.Tensor) \
+           and isinstance(center_w, torch.Tensor)
+    assert 0 <= center_h <= 28 \
+           and 0 <= center_w <= 28
