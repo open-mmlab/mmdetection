163af1242d05b15dbb21477ca2082e3fc529cde4
zhangshilong <2392587229zsl@gmail.com>
2021-08-24 20:06:55 +0800

    add vis

--- mmdet/models/detectors/solo.py
+++ mmdet/models/detectors/solo.py
@@ -5,15 +5,21 @@ from .single_stage_instance_seg import SingleStageInstanceSegmentor
 @DETECTORS.register_module()
 class SOLO(SingleStageInstanceSegmentor):
 
-    def show_result(self, data, result, **kwargs):
-        """Show prediction results of the detector.
-
-        Args:
-            data (str or np.ndarray): Image filename or loaded image.
-            result (Tensor or tuple): The results to draw over `img`
-                bbox_result or (bbox_result, segm_result).
-
-        Returns:
-            np.ndarray: The image with bboxes drawn on it.
-        """
-        raise NotImplementedError
+    def __init__(self,
+                 backbone,
+                 neck=None,
+                 bbox_head=None,
+                 mask_head=None,
+                 train_cfg=None,
+                 test_cfg=None,
+                 init_cfg=None,
+                 pretrained=None):
+        super(SOLO, self).__init__(
+            backbone,
+            neck=neck,
+            bbox_head=bbox_head,
+            mask_head=mask_head,
+            train_cfg=train_cfg,
+            test_cfg=test_cfg,
+            init_cfg=init_cfg,
+            pretrained=pretrained)
