1ffc669e59e54ea564926d7679235aae65c8626a
RangiLyu <lyuchqi@gmail.com>
2021-08-25 11:52:51 +0800

    update loss weight

--- configs/faster_rcnn/faster_rcnn_r50_fpn_ciou_1x_coco.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_ciou_1x_coco.py
@@ -3,4 +3,4 @@ model = dict(
     roi_head=dict(
         bbox_head=dict(
             reg_decoded_bbox=True,
-            loss_bbox=dict(type='CIoULoss', loss_weight=10.0))))
+            loss_bbox=dict(type='CIoULoss', loss_weight=12.0))))
