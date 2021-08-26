3f548b388d706facf905d2c8fdc78492badf62a3
Leonid Beynenson <leonid.beynenson@intel.com>
2021-08-26 13:29:45 +0300

    Update init_venv.sh w.r.t. PR comment
    Co-authored-by: Pavel Druzhkov <druzhkov.paul@gmail.com>

--- init_venv.sh
+++ init_venv.sh
@@ -90,8 +90,8 @@ echo numpy==${NUMPY_VERSION} >> ${CONSTRAINTS_FILE}
 cat constraints.txt >> ${CONSTRAINTS_FILE}
 
 pip install --upgrade pip || exit 1
-pip install wheel -c ${CONSTRAINTS_FILE}|| exit 1
-pip install --upgrade setuptools -c ${CONSTRAINTS_FILE}|| exit 1
+pip install wheel -c ${CONSTRAINTS_FILE} || exit 1
+pip install --upgrade setuptools -c ${CONSTRAINTS_FILE} || exit 1
 
 if [[ -z $CUDA_VERSION_CODE ]]; then
   echo torch==${TORCH_VERSION}+cpu >> ${CONSTRAINTS_FILE}
