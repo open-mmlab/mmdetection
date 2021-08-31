ec8ac3a810318b5590bcf459e5c00618e119cc97
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-09-01 02:04:13 +0300

    imports

--- mmdet/apis/ote/apis/detection/configuration.py
+++ mmdet/apis/ote/apis/detection/configuration.py
@@ -12,18 +12,17 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-# import attr
 from attr import attrs
-from sc_sdk.configuration import ModelConfig, ModelLifecycle, UIRules
-# from sc_sdk.configuration.config_element_type import ElementCategory
-from ote_sdk.configuration.elements import (ParameterGroup, add_parameter_group,
-                                           configurable_boolean,
-                                           configurable_float,
-                                           configurable_integer,
-                                           string_attribute,
-                                           boolean_attribute)
-# from sc_sdk.configuration.elements.primitive_parameters import set_common_metadata
-# from sc_sdk.configuration.ui_rules import NullUIRules, UIRules
+from sc_sdk.configuration import ModelConfig, ModelLifecycle
+from ote_sdk.configuration.elements import (
+    ParameterGroup,
+    add_parameter_group,
+    configurable_boolean,
+    configurable_float,
+    configurable_integer,
+    string_attribute,
+    boolean_attribute
+)
 
 
 @attrs
