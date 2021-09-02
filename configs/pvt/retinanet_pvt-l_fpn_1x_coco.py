1ddeeb7d67fac85dbff616598e1fcc868b1816e6
zhe chen <wztxy89@163.com>
2021-09-02 21:07:46 +0800

    update pvt-l config

--- configs/pvt/retinanet_pvt-l_fpn_1x_coco.py
+++ configs/pvt/retinanet_pvt-l_fpn_1x_coco.py
@@ -6,4 +6,4 @@ model = dict(
         num_layers=[3, 8, 27, 3],
         init_cfg=dict(checkpoint='https://github.com/whai362/PVT/'
                       'releases/download/v2/pvt_large.pth')))
-fp16 = dict(loss_scale=512.)
+fp16 = dict(loss_scale=dict(init_scale=512))
