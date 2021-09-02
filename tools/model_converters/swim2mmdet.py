1035f6ea5371845a21d50d37b5dd4f0cd434ce33
wwqgtxx <wwqgtxx@gmail.com>
2021-09-02 20:41:52 +0800

    reuse the ckpt_convert.py's swin_converter in swim2mmdet.py

--- tools/model_converters/swim2mmdet.py
+++ tools/model_converters/swim2mmdet.py
@@ -1,61 +1,8 @@
 import argparse
-from collections import OrderedDict
 
 import torch
 
-
-def convert_swin(ckpt):
-    new_ckpt = OrderedDict()
-
-    def correct_unfold_reduction_order(x):
-        out_channel, in_channel = x.shape
-        x = x.reshape(out_channel, 4, in_channel // 4)
-        x = x[:, [0, 2, 1, 3], :].transpose(1,
-                                            2).reshape(out_channel, in_channel)
-        return x
-
-    def correct_unfold_norm_order(x):
-        in_channel = x.shape[0]
-        x = x.reshape(4, in_channel // 4)
-        x = x[[0, 2, 1, 3], :].transpose(0, 1).reshape(in_channel)
-        return x
-
-    for k, v in ckpt.items():
-        if k.startswith('backbone.head'):
-            continue
-        elif k.startswith('backbone.layers'):
-            new_v = v
-            if 'attn.' in k:
-                new_k = k.replace('attn.', 'attn.w_msa.')
-            elif 'mlp.' in k:
-                if 'mlp.fc1.' in k:
-                    new_k = k.replace('mlp.fc1.', 'ffn.layers.0.0.')
-                elif 'mlp.fc2.' in k:
-                    new_k = k.replace('mlp.fc2.', 'ffn.layers.1.')
-                else:
-                    new_k = k.replace('mlp.', 'ffn.')
-            elif 'downsample' in k:
-                new_k = k
-                if 'reduction.' in k:
-                    new_v = correct_unfold_reduction_order(v)
-                elif 'norm.' in k:
-                    new_v = correct_unfold_norm_order(v)
-            else:
-                new_k = k
-            new_k = new_k.replace('layers', 'stages', 1)
-        elif k.startswith('backbone.patch_embed'):
-            new_v = v
-            if 'proj' in k:
-                new_k = k.replace('proj', 'projection')
-            else:
-                new_k = k
-        else:
-            new_v = v
-            new_k = k
-
-        new_ckpt[new_k] = new_v
-
-    return new_ckpt
+from mmdet.models.utils.ckpt_convert import swin_converter
 
 
 def main():
@@ -75,7 +22,7 @@ def main():
     else:
         state_dict = checkpoint
 
-    weight = convert_swin(state_dict)
+    weight = swin_converter(state_dict,prefix='backbone.')
 
     if 'state_dict' in checkpoint:
         checkpoint['state_dict'] = weight
