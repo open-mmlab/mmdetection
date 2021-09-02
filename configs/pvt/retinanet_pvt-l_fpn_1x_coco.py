76a1c051beb240bb1bde5108f1cac30cfd883779
zhe chen <wztxy89@163.com>
2021-09-02 20:59:21 +0800

    update pvt-l config

--- configs/pvt/retinanet_pvt-l_fpn_1x_coco.py
+++ configs/pvt/retinanet_pvt-l_fpn_1x_coco.py
@@ -6,8 +6,4 @@ model = dict(
         num_layers=[3, 8, 27, 3],
         init_cfg=dict(checkpoint='https://github.com/whai362/PVT/'
                       'releases/download/v2/pvt_large.pth')))
-# optimizer
-optimizer = dict(
-    _delete_=True, type='AdamW', lr=0.0001 / 1.4, weight_decay=0.0001)
-# dataset settings
-data = dict(samples_per_gpu=1, workers_per_gpu=1)
+fp16 = dict(loss_scale=512.)
