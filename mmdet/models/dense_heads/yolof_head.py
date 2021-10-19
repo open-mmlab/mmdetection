17fee9977880137c0f5de3205ae9ddfd457b946d
zhangshilong <2392587229zsl@gmail.com>
2021-10-19 21:42:36 +0800

    support all

--- mmdet/models/dense_heads/yolof_head.py
+++ mmdet/models/dense_heads/yolof_head.py
@@ -88,19 +88,19 @@ class YOLOFHead(AnchorHead):
         self.bbox_subnet = nn.Sequential(*bbox_subnet)
         self.cls_score = nn.Conv2d(
             self.in_channels,
-            self.num_anchors * self.num_classes,
+            self.num_base_priors * self.num_classes,
             kernel_size=3,
             stride=1,
             padding=1)
         self.bbox_pred = nn.Conv2d(
             self.in_channels,
-            self.num_anchors * 4,
+            self.num_base_priors * 4,
             kernel_size=3,
             stride=1,
             padding=1)
         self.object_pred = nn.Conv2d(
             self.in_channels,
-            self.num_anchors,
+            self.num_base_priors,
             kernel_size=3,
             stride=1,
             padding=1)
