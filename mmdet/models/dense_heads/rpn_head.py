313b5f9bd46a82166a6106fa97c0447391dcc25d
zhangshilong <2392587229zsl@gmail.com>
2021-10-20 17:25:54 +0800

    fix unintest of onnx

--- mmdet/models/dense_heads/rpn_head.py
+++ mmdet/models/dense_heads/rpn_head.py
@@ -237,7 +237,6 @@ class RPNHead(AnchorHead):
 
         return dets[:cfg.max_per_img]
 
-    # TODO: waiting for refactor the anchor_head and anchor_free head
     def onnx_export(self, x, img_metas):
         """Test without augmentation.
 
@@ -245,89 +244,22 @@ class RPNHead(AnchorHead):
             x (tuple[Tensor]): Features from the upstream network, each is
                 a 4D-tensor.
             img_metas (list[dict]): Meta info of each image.
-
         Returns:
-            tuple[Tensor, Tensor]: dets of shape [N, num_det, 5]
-                and class labels of shape [N, num_det].
+            Tensor: dets of shape [N, num_det, 5].
         """
         cls_scores, bbox_preds = self(x)
 
         assert len(cls_scores) == len(bbox_preds)
-        num_levels = len(cls_scores)
 
-        device = cls_scores[0].device
-        featmap_sizes = [cls_scores[i].shape[-2:] for i in range(num_levels)]
-        mlvl_anchors = self.prior_generator.grid_anchors(
-            featmap_sizes, device=device)
-
-        cls_scores = [cls_scores[i].detach() for i in range(num_levels)]
-        bbox_preds = [bbox_preds[i].detach() for i in range(num_levels)]
-
-        assert len(
-            img_metas
-        ) == 1, 'Only support one input image while in exporting to ONNX'
-        img_shapes = img_metas[0]['img_shape_for_onnx']
-
-        cfg = copy.deepcopy(self.test_cfg)
-
-        mlvl_scores = []
-        mlvl_bbox_preds = []
-        mlvl_valid_anchors = []
-        batch_size = cls_scores[0].shape[0]
-        nms_pre_tensor = torch.tensor(
-            cfg.nms_pre, device=cls_scores[0].device, dtype=torch.long)
-        for idx in range(len(cls_scores)):
-            rpn_cls_score = cls_scores[idx]
-            rpn_bbox_pred = bbox_preds[idx]
-            assert rpn_cls_score.size()[-2:] == rpn_bbox_pred.size()[-2:]
-            rpn_cls_score = rpn_cls_score.permute(0, 2, 3, 1)
-            if self.use_sigmoid_cls:
-                rpn_cls_score = rpn_cls_score.reshape(batch_size, -1)
-                scores = rpn_cls_score.sigmoid()
-            else:
-                rpn_cls_score = rpn_cls_score.reshape(batch_size, -1, 2)
-                # We set FG labels to [0, num_class-1] and BG label to
-                # num_class in RPN head since mmdet v2.5, which is unified to
-                # be consistent with other head since mmdet v2.0. In mmdet v2.0
-                # to v2.4 we keep BG label as 0 and FG label as 1 in rpn head.
-                scores = rpn_cls_score.softmax(-1)[..., 0]
-            rpn_bbox_pred = rpn_bbox_pred.permute(0, 2, 3, 1).reshape(
-                batch_size, -1, 4)
-            anchors = mlvl_anchors[idx]
-            anchors = anchors.expand_as(rpn_bbox_pred)
-            # Get top-k prediction
-            from mmdet.core.export import get_k_for_topk
-            nms_pre = get_k_for_topk(nms_pre_tensor, rpn_bbox_pred.shape[1])
-            if nms_pre > 0:
-                _, topk_inds = scores.topk(nms_pre)
-                batch_inds = torch.arange(batch_size).view(
-                    -1, 1).expand_as(topk_inds)
-                # Avoid onnx2tensorrt issue in https://github.com/NVIDIA/TensorRT/issues/1134 # noqa: E501
-                # Mind k<=3480 in TensorRT for TopK
-                transformed_inds = scores.shape[1] * batch_inds + topk_inds
-                scores = scores.reshape(-1, 1)[transformed_inds].reshape(
-                    batch_size, -1)
-                rpn_bbox_pred = rpn_bbox_pred.reshape(
-                    -1, 4)[transformed_inds, :].reshape(batch_size, -1, 4)
-                anchors = anchors.reshape(-1, 4)[transformed_inds, :].reshape(
-                    batch_size, -1, 4)
-            mlvl_scores.append(scores)
-            mlvl_bbox_preds.append(rpn_bbox_pred)
-            mlvl_valid_anchors.append(anchors)
-
-        batch_mlvl_scores = torch.cat(mlvl_scores, dim=1)
-        batch_mlvl_anchors = torch.cat(mlvl_valid_anchors, dim=1)
-        batch_mlvl_rpn_bbox_pred = torch.cat(mlvl_bbox_preds, dim=1)
-        batch_mlvl_proposals = self.bbox_coder.decode(
-            batch_mlvl_anchors, batch_mlvl_rpn_bbox_pred, max_shape=img_shapes)
-
+        batch_bboxes, batch_scores = super(RPNHead, self).onnx_export(
+            cls_scores, bbox_preds, img_metas=img_metas, with_nms=False)
         # Use ONNX::NonMaxSuppression in deployment
         from mmdet.core.export import add_dummy_nms_for_onnx
-        batch_mlvl_scores = batch_mlvl_scores.unsqueeze(2)
+        cfg = copy.deepcopy(self.test_cfg)
         score_threshold = cfg.nms.get('score_thr', 0.0)
         nms_pre = cfg.get('deploy_nms_pre', -1)
-        dets, _ = add_dummy_nms_for_onnx(batch_mlvl_proposals,
-                                         batch_mlvl_scores, cfg.max_per_img,
+        dets, _ = add_dummy_nms_for_onnx(batch_bboxes, batch_scores,
+                                         cfg.max_per_img,
                                          cfg.nms.iou_threshold,
                                          score_threshold, nms_pre,
                                          cfg.max_per_img)
