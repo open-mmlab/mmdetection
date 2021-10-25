73ea47705aa53df63157b9b05e38ee73031772c6
Zalesskaya, Galina <galina.zalesskaya@intel.com>
2021-10-25 18:59:55 +0300

    SyncBN -> BN to enable CPU inference

--- configs/ote/custom-object-detection/gen3_resnet50_VFNet/model.py
+++ configs/ote/custom-object-detection/gen3_resnet50_VFNet/model.py
@@ -7,7 +7,7 @@ model = dict(
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch'),
     neck=dict(
