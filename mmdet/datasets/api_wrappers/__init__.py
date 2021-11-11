d5c834919b99d21df13533db93979cc02514329a
AronLin <347630870@qq.com>
2021-11-11 20:28:50 +0800

    Replace with our api

--- mmdet/datasets/api_wrappers/__init__.py
+++ mmdet/datasets/api_wrappers/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .coco_api import COCO, COCOeval
+from .panoptic_evaluation import pq_compute_multi_core
 
-__all__ = ['COCO', 'COCOeval']
+__all__ = ['COCO', 'COCOeval', 'pq_compute_multi_core']
