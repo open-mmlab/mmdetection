be174ab41dc61cc38f55430e2b1aa4e0733ed73c
Andrea Panizza <8233615+AndreaPi@users.noreply.github.com>
2021-11-15 12:21:02 +0100

    Fix type error in 2_new_data_mode (#6469)
    * Always map location to cpu when load checkpoint (#6405)
    
    * configs: update groie README (#6401)
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>
    
    * [Fix] fix config path in docs (#6396)
    
    * [Enchance] Set a random seed when the user does not set a seed. (#6457)
    
    * fix random seed bug
    
    * add comment
    
    * enchance random seed
    
    * rename
    
    Co-authored-by: Haobo Yuan <yuanhaobo@whu.edu.cn>
    
    * [BugFixed] fix wrong trunc_normal_init use (#6432)
    
    * fix wrong trunc_normal_init use
    
    * fix wrong trunc_normal_init use
    
    * fix #6446
    
    Co-authored-by: Uno Wu <st9007a@gmail.com>
    Co-authored-by: Leonardo Rossi <leonardo.rossi@unipr.it>
    Co-authored-by: BigDong <yudongwang@tju.edu.cn>
    Co-authored-by: Haian Huang(深度眸) <1286304229@qq.com>
    Co-authored-by: Haobo Yuan <yuanhaobo@whu.edu.cn>
    Co-authored-by: Shusheng Yang <shusheng.yang@qq.com>

--- mmdet/models/backbones/pvt.py
+++ mmdet/models/backbones/pvt.py
@@ -9,6 +9,7 @@ from mmcv.cnn import (Conv2d, build_activation_layer, build_norm_layer,
                       constant_init, normal_init, trunc_normal_init)
 from mmcv.cnn.bricks.drop import build_dropout
 from mmcv.cnn.bricks.transformer import MultiheadAttention
+from mmcv.cnn.utils.weight_init import trunc_normal_
 from mmcv.runner import (BaseModule, ModuleList, Sequential, _load_checkpoint,
                          load_state_dict)
 from torch.nn.modules.utils import _pair as to_2tuple
@@ -315,7 +316,7 @@ class AbsolutePositionEmbedding(BaseModule):
         self.drop = nn.Dropout(p=drop_rate)
 
     def init_weights(self):
-        trunc_normal_init(self.pos_embed, std=0.02)
+        trunc_normal_(self.pos_embed, std=0.02)
 
     def resize_pos_embed(self, pos_embed, input_shape, mode='bilinear'):
         """Resize pos_embed weights.
@@ -526,9 +527,7 @@ class PyramidVisionTransformer(BaseModule):
                         f'training start from scratch')
             for m in self.modules():
                 if isinstance(m, nn.Linear):
-                    trunc_normal_init(m.weight, std=.02)
-                    if m.bias is not None:
-                        constant_init(m.bias, 0)
+                    trunc_normal_init(m, std=.02, bias=0.)
                 elif isinstance(m, nn.LayerNorm):
                     constant_init(m.bias, 0)
                     constant_init(m.weight, 1.0)
