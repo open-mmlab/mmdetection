2d26a9e253551750226277fe61a6843704a44f6b
Han <zh950713@gmail.com>
2021-11-15 10:12:49 +0000

    print log_vars keys in assertion msg

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -202,8 +202,13 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         if dist.is_available() and dist.is_initialized():
             log_var_length = torch.tensor(len(log_vars), device=loss.device)
             dist.all_reduce(log_var_length)
+            message = (
+                f'rank {dist.get_rank()}'
+                + f' len(log_vars): {len(log_vars)}'
+                + ' keys: ' + ','.join(log_vars.keys())
+            )
             assert log_var_length == len(log_vars) * dist.get_world_size(), \
-                'loss log variables are different across GPUs!'
+                'loss log variables are different across GPUs!\n' + message
 
         log_vars['loss'] = loss
         for loss_name, loss_value in log_vars.items():
