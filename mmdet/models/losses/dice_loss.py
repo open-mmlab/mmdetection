c8d423a7cde5f17ceda464dde806d0573e07dc24
luochunhua <luochunhua1996@outlook.com>
2021-11-28 12:52:35 +0000

    update dice loss

--- mmdet/models/losses/dice_loss.py
+++ mmdet/models/losses/dice_loss.py
@@ -92,7 +92,7 @@ class DiceLoss(nn.Module):
                  use_second_power=True,
                  loss_weight=1.0,
                  eps=1e-3):
-        """`Dice Loss, there are two form dice losses is supported:
+        """`Dice Loss, there are two forms of dice loss is supported:
 
             - the one proposed in `V-Net: Fully Convolutional Neural
                 Networks for Volumetric Medical Image Segmentation
