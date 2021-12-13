47b5f1d29ec4141ff1150ee3e88d57e0bc715f0b
Czm369 <1341807343@qq.com>
2021-12-13 11:44:15 +0800

    pre-commit

--- mmdet/utils/misc.py
+++ mmdet/utils/misc.py
@@ -1,21 +1,32 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import glob
 import os.path as osp
+import warnings
 
 
-def find_latest_checkpoint(path, ext='pth'):
-    """
+def find_latest_checkpoint(path, suffix='pth'):
+    """Find the latest checkpoint from the working directory.
+
+    Args:
+        path(str): The path to find checkpoints.
+        suffix(str): File extension.
+            Defaults to pth.
+
+    Returns:
+        latest_path(str | None): File path of the latest checkpoint.
     References:
         .. [1] https://github.com/microsoft/SoftTeacher
                   /blob/main/ssod/utils/patch.py
     """
     if not osp.exists(path):
+        warnings.warn('The path of checkpoints does not exist.')
         return None
-    if osp.exists(osp.join(path, f'latest.{ext}')):
-        return osp.join(path, f'latest.{ext}')
+    if osp.exists(osp.join(path, f'latest.{suffix}')):
+        return osp.join(path, f'latest.{suffix}')
 
-    checkpoints = glob.glob(osp.join(path, f'*.{ext}'))
+    checkpoints = glob.glob(osp.join(path, f'*.{suffix}'))
     if len(checkpoints) == 0:
+        warnings.warn('There are no checkpoints in the path.')
         return None
     latest = -1
     latest_path = None
--- tests/test_utils/test_misc.py
+++ tests/test_utils/test_misc.py
@@ -167,8 +167,14 @@ def test_filter_scores_and_topk():
 
 def test_find_latest_checkpoint():
     with tempfile.TemporaryDirectory() as tmpdir:
+        path = tmpdir
+        latest = find_latest_checkpoint(path)
+        # There are no checkpoints in the path.
+        assert latest is None
+
         path = tmpdir + '/none'
         latest = find_latest_checkpoint(path)
+        # The path does not exist.
         assert latest is None
 
     with tempfile.TemporaryDirectory() as tmpdir:
