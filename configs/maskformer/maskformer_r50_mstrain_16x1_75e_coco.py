51a46957631e30ab4292d8031000f0cb41826d2e
luochunhua <luochunhua1996@outlook.com>
2021-12-16 07:47:18 +0000

    add a config file

--- configs/maskformer/maskformer_r50_mstrain_16x1_75e_coco.py
+++ configs/maskformer/maskformer_r50_mstrain_16x1_75e_coco.py
@@ -1,6 +1,7 @@
 """This version was mentioned in Table XI, in paper `Masked-attention Mask
 Transformer for Universal Image
 Segmentation<https://arxiv.org/abs/2112.01527>`_"""
+
 _base_ = './maskformer_r50_mstrain_64x1_300e_coco.py'
 
 # optimizer
