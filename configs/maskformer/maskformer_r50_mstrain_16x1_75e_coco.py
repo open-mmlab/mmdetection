cf05a4201557c3d1841013c628a7c1271bd24606
luochunhua <luochunhua1996@outlook.com>
2021-12-16 07:46:51 +0000

    add a config file

---
+++ configs/maskformer/maskformer_r50_mstrain_16x1_75e_coco.py
@@ -0,0 +1,11 @@
+"""This version was mentioned in Table XI, in paper `Masked-attention Mask
+Transformer for Universal Image
+Segmentation<https://arxiv.org/abs/2112.01527>`_"""
+_base_ = './maskformer_r50_mstrain_64x1_300e_coco.py'
+
+# optimizer
+optimizer = dict(weight_decay=0.0005)
+
+# learning policy
+lr_config = dict(step=[50])
+runner = dict(type='EpochBasedRunner', max_epochs=75)
