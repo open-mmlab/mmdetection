c89ee72a4302b84b09d568b0f8b08ec9ddf32e0e
BIGWangYuDong <yudongwang@tju.edu.cn>
2021-12-22 01:21:54 +0800

    minor fix

--- configs/openimage/faster_rcnn_r50_fpn_1x_openimages_challenge.py
+++ configs/openimage/faster_rcnn_r50_fpn_1x_openimages_challenge.py
@@ -10,27 +10,30 @@ data_root = 'data/OpenImages/'
 data = dict(
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'challenge2019/train_small.txt',
+        ann_file=data_root +
+        'challenge2019/challenge-2019-train-detection-bbox.txt',
         img_prefix=data_root + 'OpenImages/',
         label_description_file=data_root +
         'challenge2019/cls-label-description.csv',
         hierarchy_file=data_root + 'challenge2019/class_label_tree.np'),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'challenge2019/train_small.txt',
+        ann_file=data_root +
+        'challenge2019/challenge-2019-validation-detection-bbox.txt',
         img_prefix=data_root + 'OpenImages/',
         label_description_file=data_root +
         'challenge2019/cls-label-description.csv',
         hierarchy_file=data_root + 'challenge2019/class_label_tree.np',
         meta_file=data_root +
-        'challenge2019/challenge-2019-validation-metas-small.pkl'),
+        'challenge2019/challenge-2019-validation-metas.pkl'),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'challenge2019/train_small.txt',
+        ann_file=data_root +
+        'challenge2019/challenge-2019-validation-detection-bbox.txt',
         img_prefix=data_root + 'OpenImages/',
         label_description_file=data_root +
         'challenge2019/cls-label-description.csv',
         hierarchy_file=data_root + 'challenge2019/class_label_tree.np',
         meta_file=data_root +
-        'challenge2019/challenge-2019-validation-metas-small.pkl'))
+        'challenge2019/challenge-2019-validation-metas.pkl'))
 evaluation = dict(interval=1, metric='mAP')
