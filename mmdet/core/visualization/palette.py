4709db30ec5092b67bb1da57c9f7f23578c36efe
jbwang1997 <jbwang1997@gmail.com>
2021-12-29 12:39:34 +0800

    Fix bugs

--- mmdet/core/visualization/palette.py
+++ mmdet/core/visualization/palette.py
@@ -1,24 +1,7 @@
-from enum import Enum
-
 import mmcv
 import numpy as np
 
 import mmdet
-
-
-class Color(Enum):
-    """An enum that defines common colors.
-
-    Contains red, green ,blue, cyan, yellow, magenta, white and black.
-    """
-    red = (255, 0, 0)
-    green = (0, 255, 0)
-    blue = (0, 0, 255)
-    cyan = (0, 255, 255)
-    yellow = (255, 255, 0)
-    magenta = (255, 0, 255)
-    white = (255, 255, 255)
-    black = (0, 0, 0)
 
 
 def palette_val(palette):
@@ -48,28 +31,25 @@ def get_palette(palette, num_classes=None):
     """
     if isinstance(palette, list):
         return palette
+    elif isinstance(palette, tuple):
+        assert isinstance(num_classes, int)
+        return [palette] * num_classes
     elif palette == 'coco':
-        palette = mmdet.datasets.CocoDataset.PALETTE
+        return mmdet.datasets.CocoDataset.PALETTE
     elif palette == 'voc':
-        palette = mmdet.datasets.VOCDataset.PALETTE
+        return mmdet.datasets.VOCDataset.PALETTE
     elif palette == 'citys':
-        palette = mmdet.datasets.CityscapesDataset.PALETTE
+        return mmdet.datasets.CityscapesDataset.PALETTE
     elif palette == 'random' or palette is None:
         assert isinstance(num_classes, int)
         state = np.random.get_state()
         # random color
         np.random.seed(42)
         palette = np.random.randint(0, 256, size=(num_classes, 3))
-        palette = [tuple(c) for c in palette]
         np.random.set_state(state)
+        return [tuple(c) for c in palette]
     elif mmcv.is_str(palette):
         assert isinstance(num_classes, int)
-        return [Color[palette].value] * num_classes
-    elif isinstance(palette, Color):
-        assert isinstance(num_classes, int)
-        return [palette.value] * num_classes
-    elif isinstance(palette, tuple):
-        assert isinstance(num_classes, int)
-        return [palette] * num_classes
+        return [mmcv.color_val(palette)[::-1]] * num_classes
     else:
         raise TypeError(f'Invalid type for palette: {type(palette)}')
