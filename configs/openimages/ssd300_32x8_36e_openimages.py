73204543a6049ca5d9931387030881d554455e60
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-01-09 21:14:56 +0800

    fully support openimages

--- configs/openimages/ssd300_32x8_36e_openimages.py
+++ configs/openimages/ssd300_32x8_36e_openimages.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/ssd300.py', '../_base_/datasets/openimages_detection.py',
-    '../_base_/default_runtime.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_1x.py'
 ]
 model = dict(
     bbox_head=dict(
