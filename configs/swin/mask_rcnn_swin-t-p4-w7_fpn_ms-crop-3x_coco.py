19f70192a2b1fc823359f83fc38891ff270a599c
Zac Todd <zactodd0@gmail.com>
2022-01-10 13:26:52 +1300

    Current reviving error with config.
    When running this config it is missing a policy resulting in AsserionError.

--- configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
+++ configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
@@ -87,5 +87,5 @@ optimizer = dict(
             'relative_position_bias_table': dict(decay_mult=0.),
             'norm': dict(decay_mult=0.)
         }))
-lr_config = dict(warmup_iters=1000, step=[27, 33])
+lr_config = dict(policy='step', warmup_iters=1000, step=[27, 33])
 runner = dict(max_epochs=36)
