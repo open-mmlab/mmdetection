b6f21e3a9f642b07ca6ad3411960a7cb97530178
jbwang1997 <jbwang1997@gmail.com>
2022-01-17 13:23:36 +0800

    [Enhance] Upgrage visualization to custom colors of different classes. (#6716)
    * Update visualization
    
    * Fix a help text mistake
    
    * short function docs
    
    * Fix for iosrt
    
    * Back to master
    
    * Check by pre-commit
    
    * Update dataset_warppers palette
    
    * Custom colors for visualizaton
    
    * Update visualization in image_demo.py
    
    * align palette with panopticapi
    
    * delete palette from checkpoint meta
    
    * Use tuple to represent color
    
    * Update visualization
    
    * Modify image_demo.py
    
    * Check test error
    
    * Check test error
    
    * Fix error for empty case
    
    * Delete redundant functions
    
    * Check visualization
    
    * Add default case
    
    * Fix bug
    
    * Fix bugs
    
    * Add unit tests
    
    * Change coments
    
    * Avoid break using positional arguments.
    
    * Add copyright
    
    * Modify comments
    
    * fix lint
    
    Co-authored-by: Guangchen Lin <347630870@qq.com>

--- mmdet/core/visualization/__init__.py
+++ mmdet/core/visualization/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .image import (color_val_matplotlib, imshow_det_bboxes,
                     imshow_gt_det_bboxes)
+from .palette import get_palette, palette_val
 
-__all__ = ['imshow_det_bboxes', 'imshow_gt_det_bboxes', 'color_val_matplotlib']
+__all__ = [
+    'imshow_det_bboxes', 'imshow_gt_det_bboxes', 'color_val_matplotlib',
+    'palette_val', 'get_palette'
+]
