35f3f96144b38bb51b15083ac10d8e2746fbc757
RangiLyu <lyuchqi@gmail.com>
2022-01-17 16:10:33 +0800

    [Enhance] Continue to speed up training. (#6974)
    * [Enhance] Speed up training time.
    
    * set in cfg

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -14,3 +14,8 @@ log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
