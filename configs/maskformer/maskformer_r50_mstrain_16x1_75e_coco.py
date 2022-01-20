112cc95c4c5b6031d4ed72690f1116afd838db81
luochunhua <luochunhua1996@outlook.com>
2022-01-20 09:13:46 +0000

    update config

--- configs/maskformer/maskformer_r50_mstrain_16x1_75e_coco.py
+++ configs/maskformer/maskformer_r50_mstrain_16x1_75e_coco.py
@@ -13,7 +13,7 @@ model = dict(
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=3,
+        frozen_stages=-1,
         norm_cfg=dict(type='BN', requires_grad=False),
         norm_eval=True,
         style='pytorch',
