6a9dc09428318e0f02e6ddcabfd6db27e2af0b34
RangiLyu <lyuchqi@gmail.com>
2022-01-25 16:40:15 +0800

    [Enhance] Add setup multi-processing both in train and test. (#7036)
    * [Enhance] Add setup multi-processing both in train and test.
    
    * switch to torch mp

--- tools/train.py
+++ tools/train.py
@@ -1,14 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
 import copy
-import multiprocessing as mp
 import os
 import os.path as osp
-import platform
 import time
 import warnings
 
-import cv2
 import mmcv
 import torch
 from mmcv import Config, DictAction
@@ -19,7 +16,7 @@ from mmdet import __version__
 from mmdet.apis import init_random_seed, set_random_seed, train_detector
 from mmdet.datasets import build_dataset
 from mmdet.models import build_detector
-from mmdet.utils import collect_env, get_root_logger
+from mmdet.utils import collect_env, get_root_logger, setup_multi_processes
 
 
 def parse_args():
@@ -89,38 +86,6 @@ def parse_args():
         args.cfg_options = args.options
 
     return args
-
-
-def setup_multi_processes(cfg):
-    # set multi-process start method as `fork` to speed up the training
-    if platform.system() != 'Windows':
-        mp_start_method = cfg.get('mp_start_method', 'fork')
-        mp.set_start_method(mp_start_method)
-
-    # disable opencv multithreading to avoid system being overloaded
-    opencv_num_threads = cfg.get('opencv_num_threads', 0)
-    cv2.setNumThreads(opencv_num_threads)
-
-    # setup OMP threads
-    # This code is referred from https://github.com/pytorch/pytorch/blob/master/torch/distributed/run.py  # noqa
-    if ('OMP_NUM_THREADS' not in os.environ and cfg.data.workers_per_gpu > 1):
-        omp_num_threads = 1
-        warnings.warn(
-            f'Setting OMP_NUM_THREADS environment variable for each process '
-            f'to be {omp_num_threads} in default, to avoid your system being '
-            f'overloaded, please further tune the variable for optimal '
-            f'performance in your application as needed.')
-        os.environ['OMP_NUM_THREADS'] = str(omp_num_threads)
-
-    # setup MKL threads
-    if 'MKL_NUM_THREADS' not in os.environ and cfg.data.workers_per_gpu > 1:
-        mkl_num_threads = 1
-        warnings.warn(
-            f'Setting MKL_NUM_THREADS environment variable for each process '
-            f'to be {mkl_num_threads} in default, to avoid your system being '
-            f'overloaded, please further tune the variable for optimal '
-            f'performance in your application as needed.')
-        os.environ['MKL_NUM_THREADS'] = str(mkl_num_threads)
 
 
 def main():
