5a34dcd2f6f19dd6bd5c95ce276ef8830f570034
PJLAB\huanghaian <1286304229@qq.com>
2022-02-11 11:13:56 +0800

    update

--- configs/retinanet/retinanet_r50_caffe_fpn_caffe2xavier_mstrain_loss-ema-norm_1x_coco.py
+++ configs/retinanet/retinanet_r50_caffe_fpn_caffe2xavier_mstrain_loss-ema-norm_1x_coco.py
@@ -1,4 +1,7 @@
 _base_ = './retinanet_r50_caffe_fpn_mstrain_loss-ema-norm_1x_coco.py'
 
-model = dict(neck=dict(init_cfg=dict(type='Caffe2Xavier', layer='Conv2d')))
+model = dict(
+    neck=dict(
+        relu_before_extra_convs=True,
+        init_cfg=dict(type='Caffe2Xavier', layer='Conv2d')))
 lr_config = dict(warmup_iters=1000)
--- configs/retinanet/retinanet_r50_caffe_fpn_mstrain_loss-ema-norm_1x_coco.py
+++ configs/retinanet/retinanet_r50_caffe_fpn_mstrain_loss-ema-norm_1x_coco.py
@@ -1,7 +1,6 @@
 _base_ = './retinanet_r50_caffe_fpn_mstrain_1x_coco.py'
 
 model = dict(
-    neck=dict(relu_before_extra_convs=True),
     bbox_head=dict(
         loss_normalizer_momentum=0.9,
         loss_normalizer=100,
