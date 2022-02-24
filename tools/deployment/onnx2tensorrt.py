5675dacdde67a8ee85221f7b6f76f80a47323cd9
Cedric Luo <luochunhua1996@outlook.com>
2022-02-24 14:05:07 +0800

    Add deprecation message for deploy tools (#7242)

--- tools/deployment/onnx2tensorrt.py
+++ tools/deployment/onnx2tensorrt.py
@@ -252,3 +252,15 @@ if __name__ == '__main__':
         show=args.show,
         workspace_size=args.workspace_size,
         verbose=args.verbose)
+
+    # Following strings of text style are from colorama package
+    bright_style, reset_style = '\x1b[1m', '\x1b[0m'
+    red_text, blue_text = '\x1b[31m', '\x1b[34m'
+    white_background = '\x1b[107m'
+
+    msg = white_background + bright_style + red_text
+    msg += 'DeprecationWarning: This tool will be deprecated in future. '
+    msg += blue_text + 'Welcome to use the unified model deployment toolbox '
+    msg += 'MMDeploy: https://github.com/open-mmlab/mmdeploy'
+    msg += reset_style
+    warnings.warn(msg)
