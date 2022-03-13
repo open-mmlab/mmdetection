3b581f1e46c9b2a1022f9dafc7804ac2a7b9b506
Irvingao <1729854488@qq.com>
2022-03-13 20:56:21 +0800

    [Feature] modify ddod code0313

--- configs/ddod/ddod_r50_fpn_fp16_1x_coco.py
+++ configs/ddod/ddod_r50_fpn_fp16_1x_coco.py
@@ -61,6 +61,4 @@ model = dict(
         nms=dict(type='nms', iou_threshold=0.6),
         max_per_img=100))
 
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
-
 fp16 = dict(loss_scale=512.)
