5347a8a8c17191307540305b5ae0e88c855e7035
Czm369 <1341807343@qq.com>
2022-03-22 10:04:54 +0800

    Support replacing config

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -3,8 +3,10 @@ from .collect_env import collect_env
 from .logger import get_caller_name, get_root_logger, log_img_scale
 from .misc import find_latest_checkpoint
 from .setup_env import setup_multi_processes
+from .replace import replace_config
 
 __all__ = [
     'get_root_logger', 'collect_env', 'find_latest_checkpoint',
-    'setup_multi_processes', 'get_caller_name', 'log_img_scale'
+    'setup_multi_processes', 'get_caller_name', 'log_img_scale', 
+    'replace_config'
 ]
