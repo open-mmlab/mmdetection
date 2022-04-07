7de4694b639349ab2537b7934fcc602f9cd349c3
Czm369 <1341807343@qq.com>
2022-04-07 21:56:22 +0800

    use mmcv.track_parallel_progress

--- tools/misc/split_coco.py
+++ tools/misc/split_coco.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
-import multiprocessing as mul
 import os.path as osp
 
 import mmcv
@@ -18,65 +17,55 @@ def parse_args():
     parser.add_argument(
         '--data-root',
         type=str,
-        help='data root of coco dataset',
+        help='The data root of coco dataset.',
         default='./data/coco/')
     parser.add_argument(
         '--out-dir',
         type=str,
-        help='the output directory of coco semi-supervised annotations',
+        help='The output directory of coco semi-supervised annotations.',
         default='./data/coco_semi_annos/')
-    parser.add_argument(
-        '--fold',
-        type=int,
-        help='k-fold cross validation for semi-supervised object detection',
-        default=5)
     parser.add_argument(
         '--labeled-percent',
         type=float,
         nargs='+',
-        help='the percent of labeled data in train set',
+        help='The percentage of labeled data in the training set.',
         default=[1, 2, 5, 10])
+    parser.add_argument(
+        '--fold',
+        type=int,
+        help='K-fold cross validation for semi-supervised object detection.',
+        default=5)
     args = parser.parse_args()
     return args
 
 
-def split_coco(mul_args):
+def split_coco(data_root, out_dir, percent, fold):
     """Split COCO data for Semi-supervised object detection.
 
     Args:
-        mul_args(data_root, out_dir, fold, percent):
-            args for multiprocessing.
-        data_root: root of dataset.
-        out_dir: output directory of the semi-supervised annotations.
-        fold: the fold of dataset and set as random seed for data split.
-        percent: percentage of labeled data.
+        data_root(str): The data root of coco dataset.
+        out_dir(str): The output directory of coco semi-supervised annotations.
+        percent(float): The percentage of labeled data in the training set.
+        fold(int): The fold of dataset and set as random seed for data split.
     """
 
-    data_root, out_dir, fold, percent = mul_args
-
-    def save_anno(name, images, annotations):
-        print(f'Starting to split data {name}.json '
-              f'saved {len(images)} images and {len(annotations)}annotations')
-
-        sub_annos = {}
-        sub_annos['images'] = images
-        sub_annos['annotations'] = annotations
-        sub_annos['licenses'] = annos['licenses']
-        sub_annos['categories'] = annos['categories']
-        sub_annos['info'] = annos['info']
+    def save_anns(name, images, annotations):
+        sub_anns = dict()
+        sub_anns['images'] = images
+        sub_anns['annotations'] = annotations
+        sub_anns['licenses'] = anns['licenses']
+        sub_anns['categories'] = anns['categories']
+        sub_anns['info'] = anns['info']
 
         mmcv.mkdir_or_exist(out_dir)
-        mmcv.dump(sub_annos, f'{out_dir}/{name}.json')
-
-        print(f'Finishing to split data {name}.json '
-              f'saved {len(images)} images and {len(annotations)} annotations')
+        mmcv.dump(sub_anns, f'{out_dir}/{name}.json')
 
     # set random seed with the fold
     np.random.seed(fold)
     ann_file = osp.join(data_root, 'annotations/instances_train2017.json')
-    annos = mmcv.load(ann_file)
+    anns = mmcv.load(ann_file)
 
-    image_list = annos['images']
+    image_list = anns['images']
     labeled_tot = int(percent / 100. * len(image_list))
     labeled_ind = set(
         np.random.choice(range(len(image_list)), size=labeled_tot))
@@ -93,25 +82,26 @@ def split_coco(mul_args):
     labeled_id = set(labeled_id)
     labeled_annotations, unlabeled_annotations = [], []
 
-    for anno in annos['annotations']:
-        if anno['image_id'] in labeled_id:
-            labeled_annotations.append(anno)
+    for ann in anns['annotations']:
+        if ann['image_id'] in labeled_id:
+            labeled_annotations.append(ann)
         else:
-            unlabeled_annotations.append(anno)
+            unlabeled_annotations.append(ann)
 
     # save labeled and unlabeled
     labeled_name = f'instances_train2017.{fold}@{percent}'
     unlabeled_name = f'instances_train2017.{fold}@{percent}-unlabeled'
 
-    save_anno(labeled_name, labeled_images, labeled_annotations)
-    save_anno(unlabeled_name, unlabeled_images, unlabeled_annotations)
+    save_anns(labeled_name, labeled_images, labeled_annotations)
+    save_anns(unlabeled_name, unlabeled_images, unlabeled_annotations)
+
+
+def multi_wrapper(args):
+    return split_coco(*args)
 
 
 if __name__ == '__main__':
     args = parse_args()
-    pool = mul.Pool(args.fold)
-    for percent in args.labeled_percent:
-        pool.map(split_coco, [
-            *zip([args.data_root] * args.fold, [args.out_dir] * args.fold,
-                 list(range(1, args.fold + 1)), [percent] * args.fold)
-        ])
+    pf = [(args.data_root, args.out_dir, p, f)
+          for f in range(1, args.fold + 1) for p in args.labeled_percent]
+    mmcv.track_parallel_progress(multi_wrapper, pf, args.fold)
