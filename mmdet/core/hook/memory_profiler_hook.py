33cdc652437550878cf7e4c539b77d0de91aefe8
luochunhua <luochunhua1996@outlook.com>
2022-04-07 08:25:29 +0000

    update

--- mmdet/core/hook/memory_profiler_hook.py
+++ mmdet/core/hook/memory_profiler_hook.py
@@ -2,10 +2,14 @@
 from mmcv.runner.hooks import HOOKS, Hook
 
 try:
-    import psutil
+    from psutil import swap_memory, virtual_memory
+except ImportError:
+    virtual_memory = None
+    swap_memory = None
+
+try:
     from memory_profiler import memory_usage
 except ImportError:
-    psutil = None
     memory_usage = None
 
 
@@ -20,31 +24,36 @@ class MemoryProfilerHook(Hook):
     """
 
     def __init__(self, interval=50):
-        if psutil is None and memory_usage is None:
-            raise RuntimeError('Please install psutil and memory_profiler.')
-        elif psutil is None:
-            raise RuntimeError('Please install psutil.')
+        if virtual_memory is None and memory_usage is None:
+            raise RuntimeError('psutil and memory_profiler are not installed, '
+                               'please install them by:'
+                               'pip install psutil memory_profiler')
+        elif virtual_memory is None:
+            raise RuntimeError('psutil is not installed, please install it by:'
+                               'pip install psutil')
         elif memory_usage is None:
-            raise RuntimeError('Please install memory_profiler.')
+            raise RuntimeError(
+                'memory_profiler is not installed, please install it by:'
+                'pip install memory_profiler')
 
         self.interval = interval
 
     def after_iter(self, runner):
         if self.every_n_iters(runner, self.interval):
-            virtual_memory = psutil.virtual_memory()
-            swap_memory = psutil.swap_memory()
+            virtual_memory_ = virtual_memory()
+            swap_memory_ = swap_memory()
             factor = 1024 * 1024
             runner.logger.info(
                 'Memory information '
                 'available_memory: '
-                f'{round(virtual_memory.available / factor)} MB, '
+                f'{round(virtual_memory_.available / factor)} MB, '
                 'used_memory: '
-                f'{round(virtual_memory.used / factor)} MB, '
-                f'memory_utilization: {virtual_memory.percent} %, '
+                f'{round(virtual_memory_.used / factor)} MB, '
+                f'memory_utilization: {virtual_memory_.percent} %, '
                 'available_swap_memory: '
-                f'{round((swap_memory.total - swap_memory.used) / factor)}'
+                f'{round((swap_memory_.total - swap_memory_.used) / factor)}'
                 'MB, '
-                f'used_swap_memory: {round(swap_memory.used / factor)} MB, '
-                f'swap_memory_utilization: {swap_memory.percent} %, '
+                f'used_swap_memory: {round(swap_memory_.used / factor)} MB, '
+                f'swap_memory_utilization: {swap_memory_.percent} %, '
                 'current_process_memory: '
                 f'{round(memory_usage()[0] / factor)} MB')
