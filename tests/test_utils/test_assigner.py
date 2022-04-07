5a3a301874fa798d64059cc14b6a203d8a3f5362
fms <2635540050@qq.com>
2022-04-07 07:57:26 +0000

    remove the test of no gt masks for the naive_dice option of DiceCost is False

--- tests/test_utils/test_assigner.py
+++ tests/test_utils/test_assigner.py
@@ -560,7 +560,7 @@ def test_mask_hungarian_match_assigner():
     assert torch.all(assign_result.gt_inds == 0)
     assert torch.all(assign_result.labels == -1)
 
-    # test with gt masks
+    # test with gt masks of naive_dice is True
     gt_labels = torch.LongTensor([10, 100])
     gt_masks = torch.zeros((2, 50, 50)).long()
     gt_masks[0, :25] = 1
@@ -595,11 +595,28 @@ def test_mask_hungarian_match_assigner():
     assert (assign_result.gt_inds > 0).sum() == gt_labels.size(0)
     assert (assign_result.labels > -1).sum() == gt_labels.size(0)
 
-    # test with mask dice mode
+    # test with mask dice mode that naive_dice is True
     assigner_cfg = dict(
         cls_cost=dict(type='ClassificationCost', weight=0.0),
         mask_cost=dict(type='FocalLossCost', weight=0.0, binary_input=True),
         dice_cost=dict(type='DiceCost', weight=1.0, pred_act=True, eps=1.0))
+    self = MaskHungarianAssigner(**assigner_cfg)
+    assign_result = self.assign(cls_pred, mask_pred, gt_labels, gt_masks,
+                                img_meta)
+    assert torch.all(assign_result.gt_inds > -1)
+    assert (assign_result.gt_inds > 0).sum() == gt_labels.size(0)
+    assert (assign_result.labels > -1).sum() == gt_labels.size(0)
+
+    # test with mask dice mode that naive_dice is False
+    assigner_cfg = dict(
+        cls_cost=dict(type='ClassificationCost', weight=0.0),
+        mask_cost=dict(type='FocalLossCost', weight=0.0, binary_input=True),
+        dice_cost=dict(
+            type='DiceCost',
+            weight=1.0,
+            pred_act=True,
+            eps=1.0,
+            naive_dice=True))
     self = MaskHungarianAssigner(**assigner_cfg)
     assign_result = self.assign(cls_pred, mask_pred, gt_labels, gt_masks,
                                 img_meta)
@@ -628,27 +645,3 @@ def test_mask_hungarian_match_assigner():
         dice_cost=dict(type='DiceCost', weight=0.0, pred_act=True, eps=1.0))
     with pytest.raises(AssertionError):
         self = MaskHungarianAssigner(**assigner_cfg)
-
-    # test with gt masks that naive_dice is False
-    assigner_cfg = dict(
-        cls_cost=dict(type='ClassificationCost', weight=1.0),
-        mask_cost=dict(type='FocalLossCost', weight=20.0, binary_input=True),
-        dice_cost=dict(
-            type='DiceCost',
-            weight=1.0,
-            pred_act=True,
-            eps=1.0,
-            naive_dice=False))
-    self = MaskHungarianAssigner(**assigner_cfg)
-    cls_pred = torch.rand((10, 133))
-    mask_pred = torch.rand((10, 50, 50))
-
-    gt_labels = torch.LongTensor([10, 100])
-    gt_masks = torch.zeros((2, 50, 50)).long()
-    gt_masks[0, :25] = 1
-    gt_masks[0, 25:] = 1
-    assign_result = self.assign(cls_pred, mask_pred, gt_labels, gt_masks,
-                                img_meta)
-    assert torch.all(assign_result.gt_inds > -1)
-    assert (assign_result.gt_inds > 0).sum() == gt_labels.size(0)
-    assert (assign_result.labels > -1).sum() == gt_labels.size(0)
