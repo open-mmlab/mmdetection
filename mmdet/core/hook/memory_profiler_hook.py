2a48fd474fca5e7a7a7ef9be0197945a151d289c
luochunhua <luochunhua1996@outlook.com>
2022-04-10 07:40:02 +0000

    update

--- mmdet/core/hook/memory_profiler_hook.py
+++ mmdet/core/hook/memory_profiler_hook.py
@@ -1,17 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmcv.runner.hooks import HOOKS, Hook
 
-try:
-    from psutil import swap_memory, virtual_memory
-except ImportError:
-    virtual_memory = None
-    swap_memory = None
-
-try:
-    from memory_profiler import memory_usage
-except ImportError:
-    memory_usage = None
-
 
 @HOOKS.register_module()
 class MemoryProfilerHook(Hook):
@@ -24,36 +13,43 @@ class MemoryProfilerHook(Hook):
     """
 
     def __init__(self, interval=50):
-        if virtual_memory is None and memory_usage is None:
-            raise RuntimeError('psutil and memory_profiler are not installed, '
-                               'please install them by:'
-                               'pip install psutil memory_profiler')
-        elif virtual_memory is None:
-            raise RuntimeError('psutil is not installed, please install it by:'
-                               'pip install psutil')
-        elif memory_usage is None:
-            raise RuntimeError(
-                'memory_profiler is not installed, please install it by:'
+        try:
+            from psutil import swap_memory, virtual_memory
+            self._swap_memory = swap_memory
+            self._virtual_memory = virtual_memory
+        except ImportError:
+            raise ImportError('psutil is not installed, please install it by: '
+                              'pip install psutil')
+
+        try:
+            from memory_profiler import memory_usage
+            self._memory_usage = memory_usage
+        except ImportError:
+            raise ImportError(
+                'memory_profiler is not installed, please install it by: '
                 'pip install memory_profiler')
 
         self.interval = interval
 
     def after_iter(self, runner):
         if self.every_n_iters(runner, self.interval):
-            virtual_memory_ = virtual_memory()
-            swap_memory_ = swap_memory()
+            # in Byte
+            virtual_memory = self._virtual_memory()
+            swap_memory = self._swap_memory()
+            # in MB
+            process_memory = self._memory_usage()[0]
             factor = 1024 * 1024
             runner.logger.info(
                 'Memory information '
                 'available_memory: '
-                f'{round(virtual_memory_.available / factor)} MB, '
+                f'{round(virtual_memory.available / factor)} MB, '
                 'used_memory: '
-                f'{round(virtual_memory_.used / factor)} MB, '
-                f'memory_utilization: {virtual_memory_.percent} %, '
+                f'{round(virtual_memory.used / factor)} MB, '
+                f'memory_utilization: {virtual_memory.percent} %, '
                 'available_swap_memory: '
-                f'{round((swap_memory_.total - swap_memory_.used) / factor)}'
+                f'{round((swap_memory.total - swap_memory.used) / factor)}'
                 'MB, '
-                f'used_swap_memory: {round(swap_memory_.used / factor)} MB, '
-                f'swap_memory_utilization: {swap_memory_.percent} %, '
+                f'used_swap_memory: {round(swap_memory.used / factor)} MB, '
+                f'swap_memory_utilization: {swap_memory.percent} %, '
                 'current_process_memory: '
-                f'{round(memory_usage()[0] / factor)} MB')
+                f'{round(process_memory)} MB')
