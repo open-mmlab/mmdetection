a7bdb5f5ae5c18afb5b6d14cb2410436637e30d4
ccoding <ccoding04@gmail.com>
2022-04-14 12:45:01 +0800

    support auto_scale_lr

--- configs/retinanet/retinanet_r18_fpn_1x_coco.py
+++ configs/retinanet/retinanet_r18_fpn_1x_coco.py
@@ -11,3 +11,8 @@ model = dict(
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18')),
     neck=dict(in_channels=[64, 128, 256, 512]))
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (2 samples per GPU)
+auto_scale_lr = dict(base_batch_size=16)
