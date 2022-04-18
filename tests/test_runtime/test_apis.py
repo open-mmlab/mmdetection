923d3949cec0424df55d01a9af0fd77f7b64f315
nijkah <nijkah@gmail.com>
2022-04-18 17:40:15 +0900

    Fix bug in test code

--- tests/test_runtime/test_apis.py
+++ tests/test_runtime/test_apis.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import urllib
 from pathlib import Path
 
 import pytest
@@ -16,6 +18,19 @@ def test_init_detector():
     checkpoint_file = os.path.join(
         project_dir,
         'checkpoints/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth')
+
+    if not os.path.exists(checkpoint_file):
+        url = ('https://download.openmmlab.com/mmdetection/v2.0'
+               '/mask_rcnn/mask_rcnn_r50_fpn_1x_coco'
+               '/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth')
+        print(f'Downloading {url} ...')
+        local_filename, _ = urllib.request.urlretrieve(url)
+        os.makedirs(os.path.dirname(checkpoint_file), exist_ok=True)
+        shutil.move(local_filename, checkpoint_file)
+        print(f'Saved as {checkpoint_file}')
+    else:
+        print(f'Using existing checkpoint {checkpoint_file}')
+
     # test init_detector with config_file: str and checkpoint
     model = init_detector(
         config_file, checkpoint=checkpoint_file, device='cpu')
@@ -27,12 +42,11 @@ def test_init_detector():
                 depth=101,
                 init_cfg=dict(
                     type='Pretrained', checkpoint='torchvision://resnet101'))))
-    model = init_detector(
-        config_file, checkpoint=checkpoint_file, cfg_options=cfg_options)
+    model = init_detector(config_file, cfg_options=cfg_options)
 
     # test init_detector with :obj:`Path`
     config_path_object = Path(config_file)
-    model = init_detector(config_path_object, checkpoint=checkpoint_file)
+    model = init_detector(config_path_object)
 
     # test init_detector with undesirable type
     with pytest.raises(TypeError):
