b0cd194491e7559ac4cb12d0eb3b623d375d531d
nijkah <nijkah@gmail.com>
2022-04-18 23:48:05 +0900

    Remove redundant checkpoint download

--- tests/test_runtime/test_apis.py
+++ tests/test_runtime/test_apis.py
@@ -1,6 +1,4 @@
 import os
-import shutil
-import urllib
 from pathlib import Path
 
 import pytest
@@ -15,25 +13,8 @@ def test_init_detector():
     config_file = os.path.join(
         project_dir, 'configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py')
 
-    checkpoint_file = os.path.join(
-        project_dir,
-        'checkpoints/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth')
-
-    if not os.path.exists(checkpoint_file):
-        url = ('https://download.openmmlab.com/mmdetection/v2.0'
-               '/mask_rcnn/mask_rcnn_r50_fpn_1x_coco'
-               '/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth')
-        print(f'Downloading {url} ...')
-        local_filename, _ = urllib.request.urlretrieve(url)
-        os.makedirs(os.path.dirname(checkpoint_file), exist_ok=True)
-        shutil.move(local_filename, checkpoint_file)
-        print(f'Saved as {checkpoint_file}')
-    else:
-        print(f'Using existing checkpoint {checkpoint_file}')
-
-    # test init_detector with config_file: str and checkpoint
-    model = init_detector(
-        config_file, checkpoint=checkpoint_file, device='cpu')
+    # test init_detector with config_file: str
+    model = init_detector(config_file, device='cpu')
 
     # test init_detector with config_file: str and cfg_options
     cfg_options = dict(
