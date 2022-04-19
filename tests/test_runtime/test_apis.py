92e7ff4f4c754cf526553ea46cb52c2cf5785a00
nijkah <nijkah@gmail.com>
2022-04-19 14:12:44 +0900

    Remove redundant tests

--- tests/test_runtime/test_apis.py
+++ tests/test_runtime/test_apis.py
@@ -13,16 +13,13 @@ def test_init_detector():
     config_file = os.path.join(
         project_dir, 'configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py')
 
-    # test init_detector with config_file: str
-    model = init_detector(config_file, device='cpu')
-
     # test init_detector with config_file: str and cfg_options
     cfg_options = dict(
         model=dict(
             backbone=dict(
-                depth=101,
+                depth=18,
                 init_cfg=dict(
-                    type='Pretrained', checkpoint='torchvision://resnet101'))))
+                    type='Pretrained', checkpoint='torchvision://resnet18'))))
     model = init_detector(config_file, device='cpu', cfg_options=cfg_options)
 
     # test init_detector with :obj:`Path`
