d79d648d09b48cddd83d227039e06fdb7d9b4625
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-04-21 16:14:57 +0800

    minor fix

--- configs/openimages/faster_rcnn_r50_fpn_32x2_cas_1x_openimages.py
+++ configs/openimages/faster_rcnn_r50_fpn_32x2_cas_1x_openimages.py
@@ -2,5 +2,4 @@ _base_ = ['faster_rcnn_r50_fpn_32x2_1x_openimages.py']
 
 # Use ClassAwareSampler
 data = dict(
-    workers_per_gpu=0,
     train_dataloader=dict(class_aware_sampler=dict(num_sample_class=1)))
