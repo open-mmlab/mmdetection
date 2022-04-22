3d221641af1377fc98b122df2a712f76ad9626fb
zhangsanfeng2022 <ludehui@cambricon.com>
2022-04-22 04:11:17 -0400

    refactor(mlu): refactor build_dp and build_ddp.

--- mmdet/utils/util_distribution.py
+++ mmdet/utils/util_distribution.py
@@ -11,17 +11,16 @@ ddp_factory = {
         'cuda' : MMDistributedDataParallel
     }
 
-def build_dp(model, device='cuda', device_ids=None):
+def build_dp(model, device='cuda', dim=0, *args, **kwargs):
     """build DataParallel module by device type.
     
     if device is cuda, return a MMDataParallel model; if device is mlu,
     return a MLUDataParallel model.
 
     Args:
-        model(nn.Moudle): model to be parallelized.
+        model(:class:`nn.Module`): model to be parallelized.
         device(str): device type, cuda, cpu or mlu. Defaults to cuda.
-        device_ids(int): device ids of modules to be scattered to.
-            Defaults to None when GPU or MLU is not available.
+        dim(int): Dimension used to scatter the data. Defaults to 0.
 
     Returns:
         model(nn.Module): the model to be parallelized.
@@ -33,35 +32,21 @@ def build_dp(model, device='cuda', device_ids=None):
         dp_factory['mlu'] = MLUDataParallel
         model = model.mlu()
 
-    return dp_factory[device](model, device_ids=device_ids)
+    return dp_factory[device](model, dim=dim, *args, **kwargs)
 
 
-def build_ddp(model, device='cuda', device_ids=None,
-        broadcast_buffers=False, find_unused_parameters=False):
+def build_ddp(model, device='cuda', *args, **kwargs):
     """Build DistributedDataParallel module by device type.
 
     If device is cuda, return a MMDistributedDataParallel model; if device is mlu,
     return a MLUDistributedDataParallel model.
 
     Args:
-        model(:class:`nn.Moudle`): module to be parallelized.
+        model(:class:`nn.Module`): module to be parallelized.
         device(str): device type, mlu or cuda.
-        device_ids(int): which represents the only device where the input module
-            corresponding to this process resides. Defaults to None.
-        broadcast_buffers(bool): Flag that enables syncing (broadcasting) buffers of
-            the module at beginning of the forward function. Defaults to True.
-        find_unused_parameters(bool): Traverse the autograd graph of all tensors
-            contained in the return value of the wrapped module's ``forward`` function.
-            Parameters that don't receive gradients as part of this graph are preemptively
-            marked as being ready to be reduced. Note that all ``forward`` outputs that
-            are derived from module parameters must participate in calculating loss and
-            later the gradient computation. If they don't, this wrapper will hang waiting
-            for autograd to produce gradients for those parameters. Any outputs derived from
-            module parameters that are otherwise unused can be detached from the autograd
-            graph using ``torch.Tensor.detach``. Defaults to False.
 
     Returns:
-        model(nn.Module): the module to be parallelized
+        model(:class:`nn.Module`): the module to be parallelized
 
     References:
         .. [1] https://pytorch.org/docs/stable/generated/torch.nn.parallel.
@@ -75,9 +60,7 @@ def build_ddp(model, device='cuda', device_ids=None,
         ddp_factory['mlu'] = MLUDistributedDataParallel
         model = model.mlu()
 
-    return ddp_factory[device](model, device_ids=device_ids,
-                               broadcast_buffers=broadcast_buffers,
-                               find_unused_parameters=find_unused_parameters)
+    return ddp_factory[device](model, *args, **kwargs)
 
 def is_mlu_available():
     """ Returns a bool indicating if MLU is currently available. """
