368e7fcd7b9b7154ee07033d5e33ef4407bfb947
Kai Chen <chenkaidev@gmail.com>
2022-05-09 21:31:19 +0800

    update config to fix the error with anchors

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -50,6 +50,7 @@ extensions = [
 ]
 
 myst_enable_extensions = ['colon_fence']
+myst_heading_anchors = 3
 
 autodoc_mock_imports = [
     'matplotlib', 'pycocotools', 'terminaltables', 'mmdet.version', 'mmcv.ops'
