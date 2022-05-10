f4a662d2d11eea05aa2769a5a721d09430a64b40
lvchengqi.vendor <lvchengqi@pjlab.org.cn>
2022-05-10 02:33:22 +0000

    [Feature] Support CocoMetric.

--- mmdet/core/mask/utils.py
+++ mmdet/core/mask/utils.py
@@ -39,30 +39,18 @@ def encode_mask_results(mask_results):
     """Encode bitmap mask to RLE code.
 
     Args:
-        mask_results (list | tuple[list]): bitmap mask results.
-            In mask scoring rcnn, mask_results is a tuple of (segm_results,
-            segm_cls_score).
+        mask_results (list): bitmap mask results.
 
     Returns:
         list | tuple: RLE encoded mask.
     """
-    if isinstance(mask_results, tuple):  # mask scoring
-        cls_segms, cls_mask_scores = mask_results
-    else:
-        cls_segms = mask_results
-    num_classes = len(cls_segms)
-    encoded_mask_results = [[] for _ in range(num_classes)]
-    for i in range(len(cls_segms)):
-        for cls_segm in cls_segms[i]:
-            encoded_mask_results[i].append(
-                mask_util.encode(
-                    np.array(
-                        cls_segm[:, :, np.newaxis], order='F',
-                        dtype='uint8'))[0])  # encoded with RLE
-    if isinstance(mask_results, tuple):
-        return encoded_mask_results, cls_mask_scores
-    else:
-        return encoded_mask_results
+    encoded_mask_results = []
+    for mask in mask_results:
+        encoded_mask_results.append(
+            mask_util.encode(
+                np.array(mask[:, :, np.newaxis], order='F',
+                         dtype='uint8'))[0])  # encoded with RLE
+    return encoded_mask_results
 
 
 def mask2bbox(masks):
--- mmdet/metrics/__init__.py
+++ mmdet/metrics/__init__.py
@@ -1,1 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .coco_metric import CocoMetric
+
+__all__ = ['CocoMetric']
