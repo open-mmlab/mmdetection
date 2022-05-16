e5b88b8adce55b98f2f5f74724c1d624061ac2f0
Czm369 <1341807343@qq.com>
2022-05-16 20:58:26 +0800

    Support sampling from different datasets

--- mmdet/datasets/samplers/__init__.py
+++ mmdet/datasets/samplers/__init__.py
@@ -3,8 +3,11 @@ from .class_aware_sampler import ClassAwareSampler
 from .distributed_sampler import DistributedSampler
 from .group_sampler import DistributedGroupSampler, GroupSampler
 from .infinite_sampler import InfiniteBatchSampler, InfiniteGroupBatchSampler
+from .multi_sampler import (GroupMultiSourceInfiniteSampler,
+                            MultiSourceInfiniteSampler)
 
 __all__ = [
     'DistributedSampler', 'DistributedGroupSampler', 'GroupSampler',
-    'InfiniteGroupBatchSampler', 'InfiniteBatchSampler', 'ClassAwareSampler'
+    'InfiniteGroupBatchSampler', 'InfiniteBatchSampler', 'ClassAwareSampler',
+    'MultiSourceInfiniteSampler', 'GroupMultiSourceInfiniteSampler'
 ]
--- mmdet/datasets/samplers/multi_sampler.py
+++ mmdet/datasets/samplers/multi_sampler.py
@@ -1,186 +1,197 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import itertools
+
 import numpy as np
 import torch
 from mmcv.runner import get_dist_info
-from torch.utils.data import Sampler, WeightedRandomSampler
+from torch.utils.data import Sampler
+
+from mmdet.core.utils import sync_random_seed
 
 
-class DistributedGroupSemiBalanceSampler(Sampler):
+class MultiSourceInfiniteSampler(Sampler):
+    r"""Multi-Source Infinite Sampler.
+
+        According to the sampling ratio, sample data from different
+        datasets to form batches for ``IterBasedRunner``.
+
+    Args:
+        dataset: Dataset used for sampling.
+        sample_ratio (list[int | float]): The sampling ratio of different
+            datasets in a batch.
+        samples_per_gpu (int): When model is :obj:`DistributedDataParallel`,
+            it is the number of training samples on each GPU.
+            When model is :obj:`DataParallel`, it is
+            `num_gpus * samples_per_gpu`.
+            Default : 1.
+        num_replicas (int, optional): Number of processes participating in
+            distributed training.
+        rank (int, optional): Rank of the current process
+            within ``num_replicas``.
+        seed (int, optional): random seed used to shuffle the sampler if
+            ``shuffle=True``. This number should be identical across all
+            processes in the distributed group. Default: 0.
+        shuffle (bool): Whether shuffle the dataset or not. Default: True.
+    """
 
     def __init__(self,
                  dataset,
-                 by_prob=False,
-                 epoch_length=7330,
-                 sample_ratio=None,
+                 sample_ratio,
                  samples_per_gpu=1,
                  num_replicas=None,
                  rank=None,
-                 **kwargs):
-        # check to avoid some problem
-        assert samples_per_gpu > 1, 'samples_per_gpu should be greater than 1.'
+                 seed=0,
+                 shuffle=True):
         _rank, _num_replicas = get_dist_info()
         if num_replicas is None:
             num_replicas = _num_replicas
         if rank is None:
             rank = _rank
 
-        self.dataset = dataset
-        self.samples_per_gpu = samples_per_gpu
-        self.num_replicas = num_replicas
         self.rank = rank
-        self.epoch = 0
-        self.by_prob = by_prob
+        self.num_replicas = num_replicas
 
-        assert hasattr(self.dataset, 'flag')
-        self.flag = self.dataset.flag
-        self.group_sizes = np.bincount(self.flag)
-        self.num_samples = 0
-        self.cumulative_sizes = dataset.cumulative_sizes
-        # decide the frequency to sample each kind of datasets
-        if not isinstance(sample_ratio, list):
-            sample_ratio = [sample_ratio] * len(self.cumulative_sizes)
+        assert hasattr(dataset, 'cumulative_sizes')
+        self.dataset = dataset
+        self.cumulative_sizes = [0] + dataset.cumulative_sizes
+        assert isinstance(sample_ratio, list)
+        assert len(sample_ratio) == len(dataset.cumulative_sizes)
         self.sample_ratio = sample_ratio
-        self.sample_ratio = [
-            int(sr / min(self.sample_ratio)) for sr in self.sample_ratio
+        self.samples_per_gpu = samples_per_gpu
+        self.sample_num = [
+            int(samples_per_gpu * sr / sum(sample_ratio))
+            for sr in sample_ratio
         ]
-        self.size_of_dataset = []
-        cumulative_sizes = [0] + self.cumulative_sizes
+        self.sample_num[0] = samples_per_gpu - sum(self.sample_num[1:])
+        assert sum(self.sample_num) == self.samples_per_gpu, \
+            f'The sum of sample_num must be equal to ' \
+            f'samples_per_gpu, but get {self.sample_num}'
 
-        for i, _ in enumerate(self.group_sizes):
-            size_of_dataset = 0
-            cur_group_inds = np.where(self.flag == i)[0]
-            for j in range(len(self.cumulative_sizes)):
-                cur_group_cur_dataset = np.where(
-                    np.logical_and(
-                        cur_group_inds > cumulative_sizes[j],
-                        cur_group_inds < cumulative_sizes[j + 1],
-                    ))[0]
-                size_per_dataset = len(cur_group_cur_dataset)
-                size_of_dataset = max(
-                    size_of_dataset,
-                    np.ceil(size_per_dataset / self.sample_ratio[j]))
+        self.seed = sync_random_seed(seed)
+        self.shuffle = shuffle
+        self.indices_per_source = {
+            source: self._indices_of_rank(len(ds))
+            for source, ds in enumerate(dataset.datasets)
+        }
 
-            self.size_of_dataset.append(
-                int(
-                    np.ceil(size_of_dataset / self.samples_per_gpu /
-                            self.num_replicas)) * self.samples_per_gpu)
-            for j in range(len(self.cumulative_sizes)):
-                self.num_samples += self.size_of_dataset[
-                    -1] * self.sample_ratio[j]
+    def _infinite_indices(self, sample_size):
+        """Infinitely yield a sequence of indices."""
+        g = torch.Generator()
+        g.manual_seed(self.seed)
+        while True:
+            if self.shuffle:
+                yield from torch.randperm(sample_size, generator=g).tolist()
+            else:
+                yield from torch.arange(sample_size).tolist()
 
-        self.total_size = self.num_samples * self.num_replicas
-        group_factor = [g / sum(self.group_sizes) for g in self.group_sizes]
-        self.epoch_length = [
-            int(np.round(gf * epoch_length)) for gf in group_factor
-        ]
-        self.epoch_length[-1] = epoch_length - sum(self.epoch_length[:-1])
+    def _indices_of_rank(self, sample_size):
+        """Slice the infinite indices by rank."""
+        yield from itertools.islice(
+            self._infinite_indices(sample_size), self.rank, None,
+            self.num_replicas)
 
     def __iter__(self):
-        # deterministically shuffle based on epoch
-        g = torch.Generator()
-        g.manual_seed(self.epoch)
-        indices = []
-        cumulative_sizes = [0] + self.cumulative_sizes
-        for i, size in enumerate(self.group_sizes):
-            if size > 0:
-                indice = np.where(self.flag == i)[0]
-                assert len(indice) == size
-                indice_per_dataset = []
-
-                for j in range(len(self.cumulative_sizes)):
-                    indice_per_dataset.append(indice[np.where(
-                        np.logical_and(
-                            indice >= cumulative_sizes[j],
-                            indice < cumulative_sizes[j + 1],
-                        ))[0]])
-
-                shuffled_indice_per_dataset = [
-                    s[list(
-                        torch.randperm(int(s.shape[0]), generator=g).numpy())]
-                    for s in indice_per_dataset
-                ]
-                # split into
-                total_indice = []
-                batch_idx = 0
-                # pdb.set_trace()
-                while batch_idx < self.epoch_length[i] * self.num_replicas:
-                    ratio = [
-                        x / sum(self.sample_ratio) for x in self.sample_ratio
-                    ]
-                    if self.by_prob:
-                        indicator = list(
-                            WeightedRandomSampler(
-                                ratio,
-                                self.samples_per_gpu,
-                                replacement=True,
-                                generator=g,
-                            ))
-                        unique, counts = np.unique(
-                            indicator, return_counts=True)
-                        ratio = [0] * len(shuffled_indice_per_dataset)
-                        for u, c in zip(unique, counts):
-                            ratio[u] = c
-                        assert len(ratio) == 2, 'Only two set is supported'
-                        if ratio[0] == 0:
-                            ratio[0] = 1
-                            ratio[1] -= 1
-                        elif ratio[1] == 0:
-                            ratio[1] = 1
-                            ratio[0] -= 1
-
-                        ratio = [r / sum(ratio) for r in ratio]
-
-                    # num of each dataset
-                    ratio = [int(r * self.samples_per_gpu) for r in ratio]
-
-                    ratio[-1] = self.samples_per_gpu - sum(ratio[:-1])
-                    selected = []
-                    # print(ratio)
-                    for j in range(len(shuffled_indice_per_dataset)):
-                        if len(shuffled_indice_per_dataset[j]) < ratio[j]:
-                            shuffled_indice_per_dataset[j] = np.concatenate((
-                                shuffled_indice_per_dataset[j],
-                                indice_per_dataset[j][list(
-                                    torch.randperm(
-                                        int(indice_per_dataset[j].shape[0]),
-                                        generator=g,
-                                    ).numpy())],
-                            ))
-
-                        selected.append(
-                            shuffled_indice_per_dataset[j][:ratio[j]])
-                        shuffled_indice_per_dataset[
-                            j] = shuffled_indice_per_dataset[j][ratio[j]:]
-                    selected = np.concatenate(selected)
-                    total_indice.append(selected)
-                    batch_idx += 1
-                    # print(self.size_of_dataset)
-                indice = np.concatenate(total_indice)
-                indices.append(indice)
-        indices = np.concatenate(indices)  # k
-        indices = [
-            indices[j] for i in list(
-                torch.randperm(
-                    len(indices) // self.samples_per_gpu,
-                    generator=g,
-                )) for j in range(
-                    i * self.samples_per_gpu,
-                    (i + 1) * self.samples_per_gpu,
-                )
-        ]
-
-        offset = len(self) * self.rank
-        indices = indices[offset:offset + len(self)]
-        assert len(indices) == len(self)
-        return iter(indices)
+        batch_buffer = []
+        while True:
+            for source, num in enumerate(self.sample_num):
+                batch_buffer_per_source = []
+                for idx in self.indices_per_source[source]:
+                    idx += self.cumulative_sizes[source]
+                    batch_buffer_per_source.append(idx)
+                    if len(batch_buffer_per_source) == num:
+                        batch_buffer += batch_buffer_per_source
+                        break
+            yield from batch_buffer
+            batch_buffer = []
 
     def __len__(self):
-        return sum(self.epoch_length) * self.samples_per_gpu
+        return len(self.dataset)
 
     def set_epoch(self, epoch):
-        self.epoch = epoch
+        """Not supported in `IterationBased` runner."""
+        raise NotImplementedError
 
-    # duplicated, implement it by weight instead of sampling
-    # def update_sample_ratio(self):
-    #     if self.dynamic_step is not None:
-    #         self.sample_ratio = [d(self.epoch) for d in self.dynamic]
+
+class GroupMultiSourceInfiniteSampler(MultiSourceInfiniteSampler):
+    r"""Group Multi-Source Infinite Sampler.
+
+        According to the sampling ratio, sample data from different
+        datasets but the same group to form batches for ``IterBasedRunner``.
+
+    Args:
+        dataset: Dataset used for sampling.
+        sample_ratio (list[int | float]): The sampling ratio of different
+            datasets in a batch.
+        samples_per_gpu (int): When model is :obj:`DistributedDataParallel`,
+            it is the number of training samples on each GPU.
+            When model is :obj:`DataParallel`, it is
+            `num_gpus * samples_per_gpu`.
+            Default : 1.
+        num_replicas (int, optional): Number of processes participating in
+            distributed training.
+        rank (int, optional): Rank of the current process
+            within ``num_replicas``.
+        seed (int, optional): random seed used to shuffle the sampler if
+            ``shuffle=True``. This number should be identical across all
+            processes in the distributed group. Default: 0.
+        shuffle (bool): Whether shuffle the indices of a dummy ``epoch``
+            for different datasets, it should be noted that ``shuffle``
+            can not guarantee that you can generate sequential indices
+            because it need to ensure that all indices in a batch is
+            in a group. Default: True.
+
+    """
+
+    def __init__(self,
+                 dataset,
+                 sample_ratio,
+                 samples_per_gpu=1,
+                 num_replicas=None,
+                 rank=None,
+                 seed=0,
+                 shuffle=True):
+        super(GroupMultiSourceInfiniteSampler, self).__init__(
+            dataset=dataset,
+            sample_ratio=sample_ratio,
+            samples_per_gpu=samples_per_gpu,
+            num_replicas=num_replicas,
+            rank=rank,
+            seed=seed,
+            shuffle=shuffle)
+
+        assert hasattr(dataset, 'flag')
+        self.group_sizes = np.bincount(self.dataset.flag)
+        self.group_ratio = self.group_sizes / sum(self.group_sizes)
+
+        self.indices_source_group = {
+            group: {
+                source: np.where(ds.flag == group)[0]
+                for source, ds in enumerate(dataset.datasets)
+            }
+            for group in range(len(self.group_ratio))
+        }
+
+        self.indices_per_source_per_group = {
+            group: {
+                source: self._indices_of_rank(sum(ds.flag == group))
+                for source, ds in enumerate(dataset.datasets)
+            }
+            for group in range(len(self.group_ratio))
+        }
+
+    def __iter__(self):
+        batch_buffer = []
+        while True:
+            group = np.random.choice(
+                list(range(len(self.group_ratio))), p=self.group_ratio)
+            for source, num in enumerate(self.sample_num):
+                batch_buffer_per_source_per_group = []
+                for idx in self.indices_per_source_per_group[group][source]:
+                    idx = self.indices_source_group[group][source][
+                        idx] + self.cumulative_sizes[source]
+                    batch_buffer_per_source_per_group.append(idx)
+                    if len(batch_buffer_per_source_per_group) == num:
+                        batch_buffer += batch_buffer_per_source_per_group
+                        break
+            yield from batch_buffer
+            batch_buffer = []
