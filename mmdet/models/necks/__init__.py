c51c67e2c65cf5b05a01ae21e287d41c215b9c3c
FANG-MING <2635540050@qq.com>
2022-05-22 23:17:27 +0800

    remove mmdet/models/necks/semantic_fpn_wrapprer.py

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -12,7 +12,6 @@ from .nas_fpn import NASFPN
 from .nasfcos_fpn import NASFCOS_FPN
 from .pafpn import PAFPN
 from .rfp import RFP
-from .semantic_fpn_wrapper import SemanticFPNWrapper
 from .ssd_neck import SSDNeck
 from .yolo_neck import YOLOV3Neck
 from .yolox_pafpn import YOLOXPAFPN
@@ -20,5 +19,5 @@ from .yolox_pafpn import YOLOXPAFPN
 __all__ = [
     'FPN', 'BFP', 'ChannelMapper', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
     'NASFCOS_FPN', 'RFP', 'YOLOV3Neck', 'FPG', 'DilatedEncoder',
-    'CTResNetNeck', 'SSDNeck', 'YOLOXPAFPN', 'DyHead', 'SemanticFPNWrapper'
+    'CTResNetNeck', 'SSDNeck', 'YOLOXPAFPN', 'DyHead',
 ]
