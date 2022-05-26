1c20efa15b1587dc5c1ccc371bde9531df4e89e9
saul <saul.goldblat@sagarobotics.com>
2022-05-26 12:27:25 +0100

    updated Dockerfile with latest pytorch and cuda versions

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -3,7 +3,7 @@ VOLUME=$1
 
 docker run --gpus all -it --name=mmdetection \
     --shm-size=8gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  -p 8082:22 -p 8083:6006 \
+  -p 8084:22 -p 8085:6006 \
   -v $VOLUME:$VOLUME:rw \
   mmdetection_image
 
