b95ccac9af8f2e4261a5c1ec2fac154c0daef2ee
luochunhua.vendor <luochunhua@pjlab.org.cn>
2022-05-26 11:05:59 +0000

    coco panoptic metric

--- mmdet/datasets/coco_panoptic.py
+++ mmdet/datasets/coco_panoptic.py
@@ -1,94 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os.path as osp
-from collections import defaultdict
 from typing import Callable, List, Optional, Sequence, Union
 
 from mmdet.registry import DATASETS
-from .api_wrappers import COCO
+from .api_wrappers import COCOPanoptic
 from .coco import CocoDataset
-
-__all__ = ['CocoPanopticDataset']
-
-
-class COCOPanoptic(COCO):
-    """This wrapper is for loading the panoptic style annotation file.
-
-    The format is shown in the CocoPanopticDataset class.
-
-    Args:
-        annotation_file (str, optional): Path of annotation file.
-            Defaults to None.
-    """
-
-    def __init__(self, annotation_file: Optional[str] = None) -> None:
-        super(COCOPanoptic, self).__init__(annotation_file)
-
-    def createIndex(self) -> None:
-        """Create index."""
-        # create index
-        print('creating index...')
-        # anns stores 'segment_id -> annotation'
-        anns, cats, imgs = {}, {}, {}
-        img_to_anns, cat_to_imgs = defaultdict(list), defaultdict(list)
-        if 'annotations' in self.dataset:
-            for ann, img_info in zip(self.dataset['annotations'],
-                                     self.dataset['images']):
-                img_info['segm_file'] = ann['file_name']
-                for seg_ann in ann['segments_info']:
-                    # to match with instance.json
-                    seg_ann['image_id'] = ann['image_id']
-                    seg_ann['height'] = img_info['height']
-                    seg_ann['width'] = img_info['width']
-                    img_to_anns[ann['image_id']].append(seg_ann)
-                    # segment_id is not unique in coco dataset orz...
-                    if seg_ann['id'] in anns.keys():
-                        anns[seg_ann['id']].append(seg_ann)
-                    else:
-                        anns[seg_ann['id']] = [seg_ann]
-
-        if 'images' in self.dataset:
-            for img in self.dataset['images']:
-                imgs[img['id']] = img
-
-        if 'categories' in self.dataset:
-            for cat in self.dataset['categories']:
-                cats[cat['id']] = cat
-
-        if 'annotations' in self.dataset and 'categories' in self.dataset:
-            for ann in self.dataset['annotations']:
-                for seg_ann in ann['segments_info']:
-                    cat_to_imgs[seg_ann['category_id']].append(ann['image_id'])
-
-        print('index created!')
-
-        self.anns = anns
-        self.imgToAnns = img_to_anns
-        self.catToImgs = cat_to_imgs
-        self.imgs = imgs
-        self.cats = cats
-
-    def load_anns(self, ids: Union[List[int], int] = []) -> List[dict]:
-        """Load anns with the specified ids.
-
-        ``self.anns`` is a list of annotation lists instead of a
-        list of annotations.
-
-        Args:
-            ids (Union[List[int], int]): Integer ids specifying anns.
-
-        Returns:
-            anns (List[dict]): Loaded ann objects.
-        """
-        anns = []
-
-        if hasattr(ids, '__iter__') and hasattr(ids, '__len__'):
-            # self.anns is a list of annotation lists instead of
-            # a list of annotations
-            for id in ids:
-                anns += self.anns[id]
-            return anns
-        elif type(ids) == int:
-            return self.anns[ids]
 
 
 @DATASETS.register_module()
--- mmdet/metrics/__init__.py
+++ mmdet/metrics/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .coco_metric import CocoMetric
+from .coco_panoptic_metric import CocoPanopticMetric
 
-__all__ = ['CocoMetric']
+__all__ = ['CocoMetric', 'CocoPanopticMetric']
