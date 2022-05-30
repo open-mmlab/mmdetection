aa77bd0860364cbdadc834d9c4432cd2ff81f366
wangjiabao1.vendor <wangjiabao@pjlab.org.cn>
2022-05-30 09:11:39 +0000

    [Enhance] Collecting commonly used type hint in one file.

---
+++ mmdet/core/utils/typing.py
@@ -0,0 +1,22 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+"""Collecting some commonly used type hint in mmdetection."""
+
+from typing import List, Optional, Union
+
+from mmengine.config import ConfigDict
+from mmengine.data import InstanceData
+
+from mmdet.core import DetDataSample
+
+# Type hint of config data
+ConfigType = Union[ConfigDict, dict]
+OptConfigType = Optional[ConfigType]
+# Type hint of one or more config data
+MultiConfig = Union[ConfigType, List[ConfigType]]
+OptMultiConfig = Optional[MultiConfig]
+# List of InstanceData
+InstanceList = List[InstanceData]
+OptInstanceList = Optional[InstanceList]
+# List of DetDataSample
+SampleList = List[DetDataSample]
+OptSampleList = Optional[SampleList]
