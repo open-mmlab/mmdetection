51d8bc019d94c436a01f77f0aa9b57325f633e4e
jbwang1997 <jbwang1997@gmail.com>
2022-06-01 22:49:37 +0800

    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4 author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800 committer zytx121 <592267829@qq.com> 1654144462 +0800
    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4
    author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800
    committer zytx121 <592267829@qq.com> 1654144408 +0800
    
    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4
    author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800
    committer zytx121 <592267829@qq.com> 1654144401 +0800
    
    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4
    author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800
    committer zytx121 <592267829@qq.com> 1654144359 +0800
    
    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4
    author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800
    committer zytx121 <592267829@qq.com> 1654144352 +0800
    
    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4
    author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800
    committer zytx121 <592267829@qq.com> 1654144344 +0800
    
    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4
    author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800
    committer zytx121 <592267829@qq.com> 1654144307 +0800
    
    parent a12833b614358951fbec2bb7b6ce3f3d0ae462c4
    author jbwang1997 <jbwang1997@gmail.com> 1654094977 +0800
    committer zytx121 <592267829@qq.com> 1654144297 +0800
    
    Fix optimizer_wrapper

--- configs/paa/paa_r50_fpn_2x_coco.py
+++ configs/paa/paa_r50_fpn_2x_coco.py
@@ -1,3 +1,18 @@
 _base_ = './paa_r50_fpn_1x_coco.py'
-lr_config = dict(step=[16, 22])
-runner = dict(type='EpochBasedRunner', max_epochs=24)
+max_epochs = 24
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[16, 22],
+        gamma=0.1)
+]
+
+# training schedule for 2x
+train_cfg = dict(max_epochs=max_epochs)
--- mmdet/core/utils/__init__.py
+++ mmdet/core/utils/__init__.py
@@ -2,8 +2,8 @@
 from .dist_utils import (DistOptimizerHook, all_reduce_dict, allreduce_grads,
                          reduce_mean, sync_random_seed)
 from .misc import (center_of_mass, filter_scores_and_topk, flip_tensor,
-                   generate_coordinate, mask2ndarray, multi_apply,
-                   select_single_mlvl, stack_batch, unmap)
+                   generate_coordinate, levels_to_images, mask2ndarray,
+                   multi_apply, select_single_mlvl, stack_batch, unmap)
 from .typing import (ConfigType, InstanceList, MultiConfig, OptConfigType,
                      OptInstanceList, OptMultiConfig, OptSampleList,
                      SampleList)
@@ -12,7 +12,8 @@ __all__ = [
     'allreduce_grads', 'DistOptimizerHook', 'reduce_mean', 'multi_apply',
     'unmap', 'mask2ndarray', 'flip_tensor', 'all_reduce_dict',
     'center_of_mass', 'generate_coordinate', 'select_single_mlvl',
-    'filter_scores_and_topk', 'sync_random_seed', 'stack_batch', 'ConfigType',
-    'OptConfigType', 'MultiConfig', 'OptMultiConfig', 'InstanceList',
-    'OptInstanceList', 'SampleList', 'OptSampleList'
+    'filter_scores_and_topk', 'sync_random_seed', 'stack_batch',
+    'levels_to_images', 'ConfigType', 'OptConfigType', 'MultiConfig',
+    'OptMultiConfig', 'InstanceList', 'OptInstanceList', 'SampleList',
+    'OptSampleList'
 ]
