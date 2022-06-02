69ef3f1cfa07988af8df4735d6a68a1c5e03673b
jbwang1997 <jbwang1997@gmail.com>
2022-06-02 05:11:45 +0000

    Refactor AutoAssign

--- configs/autoassign/autoassign_r50_caffe_fpn_8x2_1x_coco.py
+++ configs/autoassign/autoassign_r50_caffe_fpn_8x2_1x_coco.py
@@ -4,8 +4,15 @@ _base_ = [
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+# model settings
+preprocess_cfg = dict(
+    mean=[102.9801, 115.9465, 122.7717],
+    std=[1.0, 1.0, 1.0],
+    to_rgb=False,
+    pad_size_divisor=32)
 model = dict(
     type='AutoAssign',
+    preprocess_cfg=preprocess_cfg,
     backbone=dict(
         type='ResNet',
         depth=50,
@@ -42,44 +49,21 @@ model = dict(
         score_thr=0.05,
         nms=dict(type='nms', iou_threshold=0.6),
         max_per_img=100))
-img_norm_cfg = dict(
-    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
+
+# learning rate
+param_scheduler = [
     dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
-        ])
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
+        end=1000),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=12,
+        by_epoch=True,
+        milestones=[8, 11],
+        gamma=0.1)
 ]
-data = dict(
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+
 # optimizer
-optimizer = dict(lr=0.01, paramwise_cfg=dict(norm_decay_mult=0.))
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=1000,
-    warmup_ratio=1.0 / 1000,
-    step=[8, 11])
-total_epochs = 12
+optim_wrapper = dict(
+    optimizer=dict(lr=0.01, paramwise_cfg=dict(norm_decay_mult=0.)))
