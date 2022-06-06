147af76a661cfddc0acc0c509b0cd378a733bcf1
huanghaian.vendor <huanghaian@pjlab.org.cn>
2022-06-06 07:48:01 +0000

    Support VisualizationHook

--- mmdet/core/hook/__init__.py
+++ mmdet/core/hook/__init__.py
@@ -4,9 +4,11 @@ from .memory_profiler_hook import MemoryProfilerHook
 from .num_class_check_hook import NumClassCheckHook
 from .set_epoch_info_hook import SetEpochInfoHook
 from .sync_norm_hook import SyncNormHook
+from .visualization_hook import DetVisualizationHook
 from .yolox_mode_switch_hook import YOLOXModeSwitchHook
 
 __all__ = [
     'YOLOXModeSwitchHook', 'SyncNormHook', 'CheckInvalidLossHook',
-    'SetEpochInfoHook', 'MemoryProfilerHook', 'NumClassCheckHook'
+    'SetEpochInfoHook', 'MemoryProfilerHook', 'DetVisualizationHook',
+    'NumClassCheckHook'
 ]
