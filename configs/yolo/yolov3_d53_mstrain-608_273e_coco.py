d372f289805567037e5347d08f11a0c25975a4a0
lvchengqi.vendor <lyuchqi@gmail.com>
2022-06-06 09:02:52 +0000

    [Refactor] Refactor YOLOv3

--- configs/yolo/yolov3_d53_mstrain-608_273e_coco.py
+++ configs/yolo/yolov3_d53_mstrain-608_273e_coco.py
@@ -1,7 +1,11 @@
-_base_ = '../_base_/default_runtime.py'
+_base_ = ['../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py']
+
+preprocess_cfg = dict(
+    mean=[0, 0, 0], std=[255., 255., 255.], to_rgb=True, pad_size_divisor=32)
 # model settings
 model = dict(
     type='YOLOV3',
+    preprocess_cfg=preprocess_cfg,
     backbone=dict(
         type='Darknet',
         depth=53,
@@ -58,75 +62,96 @@ model = dict(
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
-img_norm_cfg = dict(mean=[0, 0, 0], std=[255., 255., 255.], to_rgb=True)
+
+# file_client_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/detection/',
+#         'data/': 's3://openmmlab/datasets/detection/'
+#     }))
+file_client_args = dict(backend='disk')
+
 train_pipeline = [
-    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='Expand',
-        mean=img_norm_cfg['mean'],
-        to_rgb=img_norm_cfg['to_rgb'],
+        mean=preprocess_cfg['mean'],
+        to_rgb=preprocess_cfg['to_rgb'],
         ratio_range=(1, 2)),
     dict(
         type='MinIoURandomCrop',
         min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
         min_crop_size=0.3),
-    dict(type='Resize', img_scale=[(320, 320), (608, 608)], keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='RandomResize', scale=[(320, 320), (608, 608)]),
+    dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+    dict(type='PackDetInputs')
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='Resize', scale=(608, 608), keep_ratio=True),
     dict(
-        type='MultiScaleFlipAug',
-        img_scale=(608, 608),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
-        ])
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
 ]
-data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=4,
-    train=dict(
+
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
-        pipeline=train_pipeline),
-    val=dict(
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='train2017/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        data_root=data_root,
+        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img='val2017/'),
+        test_mode=True,
         pipeline=test_pipeline))
+test_dataloader = val_dataloader
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/instances_val2017.json',
+    metric='bbox')
+test_evaluator = val_evaluator
+
+train_cfg = dict(max_epochs=273, val_interval=7)
+
 # optimizer
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005))
+
 # learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=2000,  # same as burn-in in darknet
-    warmup_ratio=0.1,
-    step=[218, 246])
-# runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=273)
-evaluation = dict(interval=1, metric=['bbox'])
+param_scheduler = [
+    dict(type='LinearLR', start_factor=0.1, by_epoch=False, begin=0, end=2000),
+    dict(type='MultiStepLR', by_epoch=True, milestones=[218, 246], gamma=0.1)
+]
+
+default_hooks = dict(
+    optimizer=dict(
+        _delete_=True,
+        type='OptimizerHook',
+        grad_clip=dict(max_norm=35, norm_type=2)),
+    checkpoint=dict(type='CheckpointHook', interval=7))
 
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
 # base_batch_size = (8 GPUs) x (8 samples per GPU)
-auto_scale_lr = dict(base_batch_size=64)
+# auto_scale_lr = dict(base_batch_size=64)
--- mmdet/core/bbox/assigners/grid_assigner.py
+++ mmdet/core/bbox/assigners/grid_assigner.py
@@ -1,7 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import Optional, Tuple, Union
+
 import torch
+from mmengine.data import InstanceData
 
 from mmdet.registry import TASK_UTILS
+from ...utils.typing import ConfigType
 from .assign_result import AssignResult
 from .base_assigner import BaseAssigner
 
@@ -19,27 +23,37 @@ class GridAssigner(BaseAssigner):
 
     Args:
         pos_iou_thr (float): IoU threshold for positive bboxes.
-        neg_iou_thr (float or tuple): IoU threshold for negative bboxes.
+        neg_iou_thr (float or tuple[float, float]): IoU threshold for negative
+        bboxes.
         min_pos_iou (float): Minimum iou for a bbox to be considered as a
             positive bbox. Positive samples can have smaller IoU than
             pos_iou_thr due to the 4th step (assign max IoU sample to each gt).
+            Defaults to 0.
         gt_max_assign_all (bool): Whether to assign all bboxes with the same
             highest overlap with some gt to that gt.
+        iou_calculator (:obj:`ConfigDict` or dict): Config of overlaps
+            Calculator.
     """
 
-    def __init__(self,
-                 pos_iou_thr,
-                 neg_iou_thr,
-                 min_pos_iou=.0,
-                 gt_max_assign_all=True,
-                 iou_calculator=dict(type='BboxOverlaps2D')):
+    def __init__(
+        self,
+        pos_iou_thr: float,
+        neg_iou_thr: Union[float, Tuple[float, float]],
+        min_pos_iou: float = .0,
+        gt_max_assign_all: bool = True,
+        iou_calculator: ConfigType = dict(type='BboxOverlaps2D')
+    ) -> None:
         self.pos_iou_thr = pos_iou_thr
         self.neg_iou_thr = neg_iou_thr
         self.min_pos_iou = min_pos_iou
         self.gt_max_assign_all = gt_max_assign_all
         self.iou_calculator = TASK_UTILS.build(iou_calculator)
 
-    def assign(self, bboxes, box_responsible_flags, gt_bboxes, gt_labels=None):
+    def assign(self,
+               pred_instances: InstanceData,
+               gt_instances: InstanceData,
+               gt_instances_ignore: Optional[InstanceData] = None,
+               **kwargs) -> AssignResult:
         """Assign gt to bboxes. The process is very much like the max iou
         assigner, except that positive samples are constrained within the cell
         that the gt boxes fell in.
@@ -59,37 +73,49 @@ class GridAssigner(BaseAssigner):
             gt bbox falls in to itself.
 
         Args:
-            bboxes (Tensor): Bounding boxes to be assigned, shape(n, 4).
-            box_responsible_flags (Tensor): flag to indicate whether box is
-                responsible for prediction, shape(n, )
-            gt_bboxes (Tensor): Groundtruth boxes, shape (k, 4).
-            gt_labels (Tensor, optional): Label of gt_bboxes, shape (k, ).
+            pred_instances (:obj:`InstanceData`): Instances of model
+                predictions. It includes ``priors``, and the priors can
+                be anchors or points, or the bboxes predicted by the
+                previous stage, has shape (n, 4). The bboxes predicted by
+                the current model or stage will be named ``bboxes``,
+                ``labels``, and ``scores``, the same as the ``InstanceData``
+                in other places.
+            gt_instances (:obj:`InstanceData`): Ground truth of instance
+                annotations. It usually includes ``bboxes``, with shape (k, 4),
+                and ``labels``, with shape (k, ).
+            gt_instances_ignore (:obj:`InstanceData`, optional): Instances
+                to be ignored during training. It includes ``bboxes``
+                attribute data that is ignored during training and testing.
+                Defaults to None.
 
         Returns:
             :obj:`AssignResult`: The assign result.
         """
-        num_gts, num_bboxes = gt_bboxes.size(0), bboxes.size(0)
+        gt_bboxes = gt_instances.bboxes
+        gt_labels = gt_instances.labels
 
-        # compute iou between all gt and bboxes
-        overlaps = self.iou_calculator(gt_bboxes, bboxes)
+        priors = pred_instances.priors
+        responsible_flags = pred_instances.responsible_flags
+
+        num_gts, num_priors = gt_bboxes.size(0), priors.size(0)
+
+        # compute iou between all gt and priors
+        overlaps = self.iou_calculator(gt_bboxes, priors)
 
         # 1. assign -1 by default
-        assigned_gt_inds = overlaps.new_full((num_bboxes, ),
+        assigned_gt_inds = overlaps.new_full((num_priors, ),
                                              -1,
                                              dtype=torch.long)
 
-        if num_gts == 0 or num_bboxes == 0:
-            # No ground truth or boxes, return empty assignment
-            max_overlaps = overlaps.new_zeros((num_bboxes, ))
+        if num_gts == 0 or num_priors == 0:
+            # No ground truth or priors, return empty assignment
+            max_overlaps = overlaps.new_zeros((num_priors, ))
             if num_gts == 0:
                 # No truth, assign everything to background
                 assigned_gt_inds[:] = 0
-            if gt_labels is None:
-                assigned_labels = None
-            else:
-                assigned_labels = overlaps.new_full((num_bboxes, ),
-                                                    -1,
-                                                    dtype=torch.long)
+            assigned_labels = overlaps.new_full((num_priors, ),
+                                                -1,
+                                                dtype=torch.long)
             return AssignResult(
                 num_gts,
                 assigned_gt_inds,
@@ -99,7 +125,7 @@ class GridAssigner(BaseAssigner):
         # 2. assign negative: below
         # for each anchor, which gt best overlaps with it
         # for each anchor, the max iou of all gts
-        # shape of max_overlaps == argmax_overlaps == num_bboxes
+        # shape of max_overlaps == argmax_overlaps == num_priors
         max_overlaps, argmax_overlaps = overlaps.max(dim=0)
 
         if isinstance(self.neg_iou_thr, float):
@@ -113,8 +139,8 @@ class GridAssigner(BaseAssigner):
         # 3. assign positive: falls into responsible cell and above
         # positive IOU threshold, the order matters.
         # the prior condition of comparison is to filter out all
-        # unrelated anchors, i.e. not box_responsible_flags
-        overlaps[:, ~box_responsible_flags.type(torch.bool)] = -1.
+        # unrelated anchors, i.e. not responsible_flags
+        overlaps[:, ~responsible_flags.type(torch.bool)] = -1.
 
         # calculate max_overlaps again, but this time we only consider IOUs
         # for anchors responsible for prediction
@@ -125,8 +151,8 @@ class GridAssigner(BaseAssigner):
         # shape of gt_max_overlaps == gt_argmax_overlaps == num_gts
         gt_max_overlaps, gt_argmax_overlaps = overlaps.max(dim=1)
 
-        pos_inds = (max_overlaps >
-                    self.pos_iou_thr) & box_responsible_flags.type(torch.bool)
+        pos_inds = (max_overlaps > self.pos_iou_thr) & responsible_flags.type(
+            torch.bool)
         assigned_gt_inds[pos_inds] = argmax_overlaps[pos_inds] + 1
 
         # 4. assign positive to max overlapped anchors within responsible cell
@@ -134,22 +160,18 @@ class GridAssigner(BaseAssigner):
             if gt_max_overlaps[i] > self.min_pos_iou:
                 if self.gt_max_assign_all:
                     max_iou_inds = (overlaps[i, :] == gt_max_overlaps[i]) & \
-                         box_responsible_flags.type(torch.bool)
+                         responsible_flags.type(torch.bool)
                     assigned_gt_inds[max_iou_inds] = i + 1
-                elif box_responsible_flags[gt_argmax_overlaps[i]]:
+                elif responsible_flags[gt_argmax_overlaps[i]]:
                     assigned_gt_inds[gt_argmax_overlaps[i]] = i + 1
 
         # assign labels of positive anchors
-        if gt_labels is not None:
-            assigned_labels = assigned_gt_inds.new_full((num_bboxes, ), -1)
-            pos_inds = torch.nonzero(
-                assigned_gt_inds > 0, as_tuple=False).squeeze()
-            if pos_inds.numel() > 0:
-                assigned_labels[pos_inds] = gt_labels[
-                    assigned_gt_inds[pos_inds] - 1]
-
-        else:
-            assigned_labels = None
+        assigned_labels = assigned_gt_inds.new_full((num_priors, ), -1)
+        pos_inds = torch.nonzero(
+            assigned_gt_inds > 0, as_tuple=False).squeeze()
+        if pos_inds.numel() > 0:
+            assigned_labels[pos_inds] = gt_labels[assigned_gt_inds[pos_inds] -
+                                                  1]
 
         return AssignResult(
             num_gts, assigned_gt_inds, max_overlaps, labels=assigned_labels)
