a10168d09b7bc5e305f7601b65b491e392325c64
lvchengqi.vendor <lyuchqi@gmail.com>
2022-06-08 04:55:08 +0000

    [Fix] Fix iter-based config.

--- configs/retinanet/retinanet_r50_fpn_90k_coco.py
+++ configs/retinanet/retinanet_r50_fpn_90k_coco.py
@@ -2,7 +2,10 @@ _base_ = 'retinanet_r50_fpn_1x_coco.py'
 
 # training schedule for 90k
 train_cfg = dict(
-    type='IterBasedTrainLoop', max_iters=90000, val_interval=10000)
+    _delete_=True,
+    type='IterBasedTrainLoop',
+    max_iters=90000,
+    val_interval=10000)
 # learning rate policy
 param_scheduler = [
     dict(
@@ -15,4 +18,7 @@ param_scheduler = [
         milestones=[60000, 80000],
         gamma=0.1)
 ]
-default_hooks = dict(checkpoint=dict(interval=10000))
+train_dataloader = dict(sampler=dict(type='InfiniteSampler'))
+default_hooks = dict(checkpoint=dict(by_epoch=False, interval=10000))
+
+log_processor = dict(by_epoch=False)
