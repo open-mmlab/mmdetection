0762ea92c1311a2c571a80e1821c275ccc9468db
chhluo <luochunhua1996@outlook.com>
2022-06-13 12:22:40 +0000

    CityScapes Metric

--- configs/cityscapes/faster_rcnn_r50_fpn_1x_cityscapes.py
+++ configs/cityscapes/faster_rcnn_r50_fpn_1x_cityscapes.py
@@ -1,28 +1,18 @@
 _base_ = [
     '../_base_/models/faster_rcnn_r50_fpn.py',
     '../_base_/datasets/cityscapes_detection.py',
-    '../_base_/default_runtime.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_1x.py'
 ]
 model = dict(
     backbone=dict(init_cfg=None),
     roi_head=dict(
         bbox_head=dict(
-            type='Shared2FCBBoxHead',
-            in_channels=256,
-            fc_out_channels=1024,
-            roi_feat_size=7,
             num_classes=8,
-            bbox_coder=dict(
-                type='DeltaXYWHBBoxCoder',
-                target_means=[0., 0., 0., 0.],
-                target_stds=[0.1, 0.1, 0.2, 0.2]),
-            reg_class_agnostic=False,
-            loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
             loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))))
+
 # optimizer
 # lr is set for a batch size of 8
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optim_wrapper = dict(optimizer=dict(lr=0.01))
 
 # learning rate
 param_scheduler = [
@@ -31,16 +21,15 @@ param_scheduler = [
     dict(
         type='MultiStepLR',
         begin=0,
-        end=12,
+        end=8,
         by_epoch=True,
         # [7] yields higher performance than [6]
         milestones=[7],
         gamma=0.1)
 ]
 
-train_cfg = dict(by_epoch=True, max_epochs=8)  # actual epoch = 8 * 8 = 64
-val_cfg = dict(interval=1)
-test_cfg = dict()
+# actual epoch = 8 * 8 = 64
+train_cfg = dict(max_epochs=8)
 
 # For better, more stable performance initialize from COCO
 load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'  # noqa
