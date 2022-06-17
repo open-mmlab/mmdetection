6d2a6b265293de60b52cf2b6b5a57fe2c7c3be9f
zytx121 <592267829@qq.com>
2022-06-17 11:12:24 +0800

    init

--- tests/test_models/test_dense_heads/test_paa_head.py
+++ tests/test_models/test_dense_heads/test_paa_head.py
@@ -37,6 +37,7 @@ class TestPAAHead(TestCase):
         s = 256
         img_metas = [{
             'img_shape': (s, s, 3),
+            'pad_shape': (s, s, 3),
             'scale_factor': 1,
         }]
         train_cfg = Config(
@@ -77,8 +78,8 @@ class TestPAAHead(TestCase):
         gt_instances = InstanceData()
         gt_instances.bboxes = torch.empty((0, 4))
         gt_instances.labels = torch.LongTensor([])
-        empty_gt_losses = paa.loss(cls_scores, bbox_preds, iou_preds,
-                                   [gt_instances], img_metas)
+        empty_gt_losses = paa.loss_by_feat(cls_scores, bbox_preds, iou_preds,
+                                           [gt_instances], img_metas)
         # When there is no truth, the cls loss should be nonzero but there
         # should be no box loss.
         empty_cls_loss = empty_gt_losses['loss_cls']
@@ -99,8 +100,8 @@ class TestPAAHead(TestCase):
         gt_instances.bboxes = torch.Tensor(
             [[23.6667, 23.8757, 238.6326, 151.8874]])
         gt_instances.labels = torch.LongTensor([2])
-        one_gt_losses = paa.loss(cls_scores, bbox_preds, iou_preds,
-                                 [gt_instances], img_metas)
+        one_gt_losses = paa.loss_by_feat(cls_scores, bbox_preds, iou_preds,
+                                         [gt_instances], img_metas)
         onegt_cls_loss = one_gt_losses['loss_cls']
         onegt_box_loss = one_gt_losses['loss_bbox']
         onegt_iou_loss = one_gt_losses['loss_iou']
@@ -143,5 +144,5 @@ class TestPAAHead(TestCase):
                 nms=dict(type='nms', iou_threshold=0.6),
                 max_per_img=100))
         rescale = False
-        paa.get_results(
+        paa.predict_by_feat(
             cls_scores, bbox_preds, iou_preds, img_metas, cfg, rescale=rescale)
