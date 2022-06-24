a377f445d5c6b6a6e9a7e9a7c715daaf0de0f778
czm369 <1341807343@qq.com>
2022-06-24 05:30:56 +0000

    Support MultiSourceSampler and GroupMultiSourceSampler

--- mmdet/datasets/samplers/__init__.py
+++ mmdet/datasets/samplers/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .batch_sampler import AspectRatioBatchSampler
 from .class_aware_sampler import ClassAwareSampler
+from .multi_source_sampler import GroupMultiSourceSampler, MultiSourceSampler
 
-__all__ = ['ClassAwareSampler', 'AspectRatioBatchSampler']
+__all__ = [
+    'ClassAwareSampler', 'AspectRatioBatchSampler', 'MultiSourceSampler',
+    'GroupMultiSourceSampler'
+]
