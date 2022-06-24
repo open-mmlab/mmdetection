e9b0ab130511ceb71ff3f9b2ba208a3c9c892488
hhaAndroid <1286304229@qq.com>
2022-06-24 05:25:26 +0000

    Refactor Albu+Corrupt+Instaboost

--- configs/albu_example/mask_rcnn_r50_fpn_albu_1x_coco.py
+++ configs/albu_example/mask_rcnn_r50_fpn_albu_1x_coco.py
@@ -1,6 +1,5 @@
 _base_ = '../mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
 albu_train_transforms = [
     dict(
         type='ShiftScaleRotate',
@@ -42,17 +41,18 @@ albu_train_transforms = [
         p=0.1),
 ]
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadImageFromFile',
+        file_client_args={{_base_.file_client_args}}),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='Pad', size_divisor=32),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
     dict(
         type='Albu',
         transforms=albu_train_transforms,
         bbox_params=dict(
             type='BboxParams',
             format='pascal_voc',
-            label_fields=['gt_labels'],
+            label_fields=['gt_bboxes_labels', 'gt_ignore_flags'],
             min_visibility=0.0,
             filter_lost_elements=True),
         keymap={
@@ -60,14 +60,9 @@ train_pipeline = [
             'gt_masks': 'masks',
             'gt_bboxes': 'bboxes'
         },
-        update_pad_shape=False,
         skip_img_without_anno=True),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='DefaultFormatBundle'),
-    dict(
-        type='Collect',
-        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks'],
-        meta_keys=('filename', 'ori_shape', 'img_shape', 'img_norm_cfg',
-                   'pad_shape', 'scale_factor'))
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
 ]
-data = dict(train=dict(pipeline=train_pipeline))
+
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
