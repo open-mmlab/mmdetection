c627392bab71bb03f10166218777102ff4b3927c
luochunhua.vendor <luochunhua@pjlab.org.cn>
2022-06-28 02:51:23 +0000

    RepPoints

--- mmdet/models/detectors/reppoints_detector.py
+++ mmdet/models/detectors/reppoints_detector.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from mmdet.core.utils import ConfigType, OptConfigType, OptMultiConfig
 from mmdet.registry import MODELS
 from .single_stage import SingleStageDetector
 
@@ -12,13 +13,18 @@ class RepPointsDetector(SingleStageDetector):
     """
 
     def __init__(self,
-                 backbone,
-                 neck,
-                 bbox_head,
-                 train_cfg=None,
-                 test_cfg=None,
-                 pretrained=None,
-                 init_cfg=None):
-        super(RepPointsDetector,
-              self).__init__(backbone, neck, bbox_head, train_cfg, test_cfg,
-                             pretrained, init_cfg)
+                 backbone: ConfigType,
+                 neck: ConfigType,
+                 bbox_head: ConfigType,
+                 train_cfg: OptConfigType = None,
+                 test_cfg: OptConfigType = None,
+                 data_preprocessor: OptConfigType = None,
+                 init_cfg: OptMultiConfig = None):
+        super().__init__(
+            backbone=backbone,
+            neck=neck,
+            bbox_head=bbox_head,
+            train_cfg=train_cfg,
+            test_cfg=test_cfg,
+            data_preprocessor=data_preprocessor,
+            init_cfg=init_cfg)
