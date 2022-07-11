141733369c83aa9c49bde6d56ee4d160c45d66f0
zhouyue.vendor <zhouyue@pjlab.org.cn>
2022-07-11 11:21:53 +0000

    Refactor FoveaBox

--- configs/foveabox/fovea_align_r101_fpn_gn-head_4x4_2x_coco.py
+++ configs/foveabox/fovea_align_r101_fpn_gn-head_4x4_2x_coco.py
@@ -8,5 +8,16 @@ model = dict(
         with_deform=True,
         norm_cfg=dict(type='GN', num_groups=32, requires_grad=True)))
 # learning policy
-lr_config = dict(step=[16, 22])
-runner = dict(type='EpochBasedRunner', max_epochs=24)
+max_epochs = 24
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[16, 22],
+        gamma=0.1)
+]
+train_cfg = dict(max_epochs=max_epochs)
--- configs/foveabox/fovea_align_r50_fpn_gn-head_4x4_2x_coco.py
+++ configs/foveabox/fovea_align_r50_fpn_gn-head_4x4_2x_coco.py
@@ -4,7 +4,17 @@ model = dict(
         with_deform=True,
         norm_cfg=dict(type='GN', num_groups=32, requires_grad=True)))
 # learning policy
-lr_config = dict(step=[16, 22])
-runner = dict(type='EpochBasedRunner', max_epochs=24)
-optimizer_config = dict(
-    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+max_epochs = 24
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[16, 22],
+        gamma=0.1)
+]
+train_cfg = dict(max_epochs=max_epochs)
+optim_wrapper = dict(clip_grad=dict(max_norm=35, norm_type=2))
