ea7ec6766944662e6f8368d3f3e04b4b7a517a78
jbwang1997 <jbwang1997@gmail.com>
2022-07-11 11:20:11 +0000

    Refactor DeepFashion

--- configs/deepfashion/mask_rcnn_r50_fpn_15e_deepfashion.py
+++ configs/deepfashion/mask_rcnn_r50_fpn_15e_deepfashion.py
@@ -7,4 +7,17 @@ model = dict(
     roi_head=dict(
         bbox_head=dict(num_classes=15), mask_head=dict(num_classes=15)))
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=15)
+max_epochs = 15
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[8, 11],
+        gamma=0.1)
+]
