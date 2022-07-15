7a0bcd9a7c4260389f81e4961e7c262fd5dd2cdb
zytx121 <sjtu_zy@sjtu.edu.cn>
2022-07-15 15:07:55 +0000

    Update FPG//HRNet/RegNet configs

--- configs/fpg/mask_rcnn_r50_fpn_crop640_50e_coco.py
+++ configs/fpg/mask_rcnn_r50_fpn_crop640_50e_coco.py
@@ -4,7 +4,11 @@ _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 norm_cfg = dict(type='BN', requires_grad=True)
+image_size = (640, 640)
+batch_augments = [dict(type='BatchFixedSizePad', size=image_size)]
+
 model = dict(
+    data_preprocessor=dict(pad_size_divisor=64, batch_augments=batch_augments),
     backbone=dict(norm_cfg=norm_cfg, norm_eval=False),
     neck=dict(
         type='FPN',
@@ -16,62 +20,62 @@ model = dict(
         bbox_head=dict(norm_cfg=norm_cfg), mask_head=dict(norm_cfg=norm_cfg)))
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadImageFromFile',
+        file_client_args={{_base_.file_client_args}}),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(
-        type='Resize',
-        img_scale=(640, 640),
+        type='RandomResize',
+        scale=image_size,
         ratio_range=(0.8, 1.2),
         keep_ratio=True),
-    dict(type='RandomCrop', crop_size=(640, 640)),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=(640, 640)),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
     dict(
-        type='MultiScaleFlipAug',
-        img_scale=(640, 640),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=64),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
+        type='RandomCrop',
+        crop_type='absolute_range',
+        crop_size=image_size,
+        allow_negative_crop=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
 ]
-data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=4,
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+
+test_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        file_client_args={{_base_.file_client_args}}),
+    dict(type='Resize', scale=image_size, keep_ratio=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=8, num_workers=4, dataset=dict(pipeline=train_pipeline))
+val_dataloader = dict(dataset=dict(pipeline=test_pipeline))
+test_dataloader = val_dataloader
+
 # learning policy
-optimizer = dict(
-    type='SGD',
-    lr=0.08,
-    momentum=0.9,
-    weight_decay=0.0001,
-    paramwise_cfg=dict(norm_decay_mult=0, bypass_duplicate=True))
-optimizer_config = dict(grad_clip=None)
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=1000,
-    warmup_ratio=0.1,
-    step=[30, 40])
-# runtime settings
-runner = dict(max_epochs=50)
-evaluation = dict(interval=2)
+max_epochs = 50
+train_cfg = dict(max_epochs=max_epochs, val_interval=2)
+param_scheduler = [
+    dict(type='LinearLR', start_factor=0.1, by_epoch=False, begin=0, end=1000),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[30, 40],
+        gamma=0.1)
+]
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=0.0001),
+    paramwise_cfg=dict(norm_decay_mult=0, bypass_duplicate=True),
+    clip_grad=None)
 
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
