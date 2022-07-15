329c90a48a760dcba24fce0bb1b59e1f9a107454
huanghaian <huanghaian@sensetime.com>
2022-07-15 06:41:47 +0000

    Update Res2Net/ResNeSt/Scratch config

--- configs/res2net/htc_r2_101_fpn_20e_coco.py
+++ configs/res2net/htc_r2_101_fpn_20e_coco.py
@@ -1,4 +1,4 @@
-_base_ = '../htc/htc_r50_fpn_1x_coco.py'
+_base_ = '../htc/htc_r50_fpn_20e_coco.py'
 model = dict(
     backbone=dict(
         type='Res2Net',
@@ -8,6 +8,3 @@ model = dict(
         init_cfg=dict(
             type='Pretrained',
             checkpoint='open-mmlab://res2net101_v1d_26w_4s')))
-# learning policy
-lr_config = dict(step=[16, 19])
-runner = dict(type='EpochBasedRunner', max_epochs=20)
--- configs/resnest/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_1x_coco.py
+++ configs/resnest/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_1x_coco.py
@@ -1,6 +1,11 @@
 _base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
+    # use ResNeSt img_norm
+    data_preprocessor=dict(
+        mean=[123.68, 116.779, 103.939],
+        std=[58.393, 57.12, 57.375],
+        bgr_to_rgb=True),
     backbone=dict(
         type='ResNeSt',
         stem_channels=64,
@@ -20,43 +25,17 @@ model = dict(
             type='Shared4Conv1FCBBoxHead',
             conv_out_channels=256,
             norm_cfg=norm_cfg)))
-# # use ResNeSt img_norm
-img_norm_cfg = dict(
-    mean=[123.68, 116.779, 103.939], std=[58.393, 57.12, 57.375], to_rgb=True)
+
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
     dict(
-        type='LoadAnnotations',
-        with_bbox=True,
-        with_mask=False,
-        poly2mask=False),
+        type='LoadImageFromFile',
+        file_client_args={{_base_.file_client_args}}),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(
-        type='Resize',
-        img_scale=[(1333, 640), (1333, 800)],
-        multiscale_mode='range',
+        type='RandomResize', scale=[(1333, 640), (1333, 800)],
         keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
 ]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
-]
-data = dict(
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
