ca02c12832e94787cc0c215338003034417f94ca
zhouyue.vendor <zhouyue@pjlab.org.cn>
2022-07-15 15:07:55 +0000

    Update FPG//HRNet/RegNet configs

--- configs/hrnet/fcos_hrnetv2p_w32_gn-head_4x4_1x_coco.py
+++ configs/hrnet/fcos_hrnetv2p_w32_gn-head_4x4_1x_coco.py
@@ -1,5 +1,9 @@
 _base_ = '../fcos/fcos_r50_caffe_fpn_gn-head_4x4_1x_coco.py'
 model = dict(
+    data_preprocessor=dict(
+        mean=[103.53, 116.28, 123.675],
+        std=[57.375, 57.12, 58.395],
+        bgr_to_rgb=False),
     backbone=dict(
         _delete_=True,
         type='HRNet',
@@ -37,34 +41,3 @@ model = dict(
         out_channels=256,
         stride=2,
         num_outs=5))
-img_norm_cfg = dict(
-    mean=[103.53, 116.28, 123.675], std=[57.375, 57.12, 58.395], to_rgb=False)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
-]
-data = dict(
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
