1b7560ccf42e98b1bc4a4057a6e47c6adc5ea9ed
hhaAndroid <1286304229@qq.com>
2022-07-18 06:32:25 +0000

    Add benchmark training scripts

--- mmdet/testing/__init__.py
+++ mmdet/testing/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from ._fast_stop_training_hook import FastStopTrainingHook  # noqa: F401,F403
 from ._utils import (demo_mm_inputs, demo_mm_proposals,
                      demo_mm_sampling_results, get_detector_cfg,
                      get_roi_head_cfg)
