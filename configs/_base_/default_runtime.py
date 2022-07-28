fabf64db2a94713ea746dd6527770e31d6e8b994
luochunhua <luochunhua1996@outlook.com>
2022-07-28 09:14:28 +0000

    move auto_scale_lr to schedule

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -22,9 +22,3 @@ log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
 log_level = 'INFO'
 load_from = None
 resume = False
-
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=16)
