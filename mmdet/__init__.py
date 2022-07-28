e4a0cd6c62c978a1d6a6ab6f7c2195be9efca9cd
BigDong <yudongwang1226@gmail.com>
2022-07-28 19:06:50 +0800

    [Enhance] Update mmdet, mmcv, and mmdet version in MMDetection (#8417)
    * Update dev-3.x circleci (#8396)
    
    * update circleci
    
    * update test config
    
    * tmp delete github action
    
    * update
    
    * tmp reduce the coverage requirement
    
    * update branch
    
    * update branch
    
    * [Fix] Fix metafile 3.x (#8404)
    
    * update reppoints readme and metafile
    
    * update openimages metafile
    
    * update faster rcnn readme and metafile
    
    * update convnext readme and metafile
    
    * update guided_anchoring metafile
    
    * update groie metafile and readme
    
    * update reppoints readme and metafile
    
    * update metafile
    
    * update metafile
    
    * release ld and mask_rcnn models
    
    * update metafile
    
    * update regnet metafile
    
    * fix markdown format
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * update md format
    
    * release lad
    
    * rename
    
    * rename
    
    * update solov2 metafile
    
    * update cascase rcnn metafile
    
    * [Doc]: fix markdown version (#8408)
    
    * [Enhance] Update mmdet, mmcv, and mmdet version in MMDetection
    
    * Fix anchor_free load_from_state_dict
    
    Co-authored-by: RangiLyu <lyuchqi@gmail.com>
    Co-authored-by: Cedric Luo <luochunhua1996@outlook.com>
    Co-authored-by: Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -1,29 +1,27 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import mmcv
+import mmengine
+from mmengine.utils import digit_version
 
-from .version import __version__, short_version
-
-
-def digit_version(version_str):
-    digit_version = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            digit_version.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            digit_version.append(int(patch_version[0]) - 1)
-            digit_version.append(int(patch_version[1]))
-    return digit_version
-
+from .version import __version__, version_info
 
 mmcv_minimum_version = '2.0.0rc0'
 mmcv_maximum_version = '2.0.0'
 mmcv_version = digit_version(mmcv.__version__)
 
+mmengine_minimum_version = '0.0.0'
+mmengine_maximum_version = '0.2.0'
+mmengine_version = digit_version(mmengine.__version__)
 
 assert (mmcv_version >= digit_version(mmcv_minimum_version)
         and mmcv_version <= digit_version(mmcv_maximum_version)), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
 
-__all__ = ['__version__', 'short_version']
+assert (mmengine_version >= digit_version(mmengine_minimum_version)
+        and mmengine_version <= digit_version(mmengine_maximum_version)), \
+    f'MMEngine=={mmengine.__version__} is used but incompatible. ' \
+    f'Please install mmengine>={mmengine_minimum_version}, ' \
+    f'<={mmengine_maximum_version}.'
+
+__all__ = ['__version__', 'version_info', 'digit_version']
