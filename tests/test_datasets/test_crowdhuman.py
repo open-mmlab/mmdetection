6fa1d61289dcbef3b108615ff4df50a8252b80af
322Desktop <254693273@qq.com>
2022-07-28 23:26:13 +0800

    [WIP] Add CrowdHumanDataset

--- tests/test_datasets/test_crowdhuman.py
+++ tests/test_datasets/test_crowdhuman.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 # Copyright (c) OpenMMLab. All rights reserved.
-=======
->>>>>>> [WIP] Add CrowdHumanDataset
 import unittest
 
 from mmdet.datasets import CrowdHumanDataset
@@ -11,18 +8,9 @@ class TestCrowdHumanDataset(unittest.TestCase):
 
     def test_crowdhuman_init(self):
         dataset = CrowdHumanDataset(
-<<<<<<< HEAD
             data_root='tests/data/crowdhuman_dataset/',
             ann_file='test_annotation_train.odgt',
             data_prefix=dict(img='Images/'),
             pipeline=[])
         self.assertEqual(len(dataset), 1)
         self.assertEqual(dataset.metainfo['CLASSES'], ('person', ))
-=======
-            data_root='tests/data/CrowdHuman/',
-            ann_file='test_annotation_train.odgt',
-            data_prefix=dict(img='Images/'),
-            pipeline=[])
-        self.assertEqual(len(dataset), 2)
-        self.assertEqual(dataset.metainfo['CLASSES'], ['person'])
->>>>>>> [WIP] Add CrowdHumanDataset
