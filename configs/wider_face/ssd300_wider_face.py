374940669896b55e956e96c31eaf5fe9b5b209a0
luochunhua <luochunhua1996@outlook.com>
2022-08-05 14:27:20 +0800

    update config

--- configs/wider_face/ssd300_wider_face.py
+++ configs/wider_face/ssd300_wider_face.py
@@ -4,7 +4,7 @@ _base_ = [
 ]
 model = dict(bbox_head=dict(num_classes=1))
 # optimizer
-optimizer = dict(type='SGD', lr=0.012, momentum=0.9, weight_decay=5e-4)
+optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=5e-4)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(
