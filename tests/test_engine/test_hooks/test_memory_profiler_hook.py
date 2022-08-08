114a014d90d0498934fc36f966ce9859f3ef6b89
luochunhua <luochunhua1996@outlook.com>
2022-08-08 04:22:12 +0000

    update ut

--- tests/test_engine/test_hooks/test_memory_profiler_hook.py
+++ tests/test_engine/test_hooks/test_memory_profiler_hook.py
@@ -1,55 +1,38 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from collections import namedtuple
 from unittest import TestCase
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
+
+from mmdet.engine.hooks import MemoryProfilerHook
 
 
 class TestMemoryProfilerHook(TestCase):
 
-    def test_memory_profiler_hook(self):
-        from mmdet.engine.hooks import MemoryProfilerHook
+    def test_after_train_iter(self):
+        hook = MemoryProfilerHook(2)
+        runner = Mock()
+        runner.logger = Mock()
+        runner.logger.info = Mock()
+        hook.after_train_iter(runner, 0)
+        runner.logger.info.assert_not_called()
+        hook.after_train_iter(runner, 1)
+        runner.logger.info.assert_called_once()
 
-        def _mock_virtual_memory():
-            virtual_memory_type = namedtuple(
-                'virtual_memory', ['total', 'available', 'percent', 'used'])
-            return virtual_memory_type(
-                total=270109085696,
-                available=250416816128,
-                percent=7.3,
-                used=17840881664)
+    def test_after_val_iter(self):
+        hook = MemoryProfilerHook(2)
+        runner = Mock()
+        runner.logger = Mock()
+        runner.logger.info = Mock()
+        hook.after_val_iter(runner, 0)
+        runner.logger.info.assert_not_called()
+        hook.after_val_iter(runner, 1)
+        runner.logger.info.assert_called_once()
 
-        def _mock_swap_memory():
-            swap_memory_type = namedtuple('swap_memory', [
-                'total',
-                'used',
-                'percent',
-            ])
-            return swap_memory_type(total=8589930496, used=0, percent=0.0)
-
-        def _mock_memory_usage():
-            return [40.22265625]
-
-        mock_virtual_memory = Mock(return_value=_mock_virtual_memory())
-        mock_swap_memory = Mock(return_value=_mock_swap_memory())
-        mock_memory_usage = Mock(return_value=_mock_memory_usage())
-
-        @patch('psutil.swap_memory', mock_swap_memory)
-        @patch('psutil.virtual_memory', mock_virtual_memory)
-        @patch('memory_profiler.memory_usage', mock_memory_usage)
-        def _test_after_iter():
-            hook = MemoryProfilerHook(2)
-            runner = Mock()
-
-            assert not mock_memory_usage.called
-            assert not mock_swap_memory.called
-            assert not mock_memory_usage.called
-            hook._after_iter(runner, 0)
-            assert not mock_memory_usage.called
-            assert not mock_swap_memory.called
-            assert not mock_memory_usage.called
-            hook._after_iter(runner, 1)
-            assert mock_memory_usage.called
-            assert mock_swap_memory.called
-            assert mock_memory_usage.called
-
-        _test_after_iter()
+    def test_after_test_iter(self):
+        hook = MemoryProfilerHook(2)
+        runner = Mock()
+        runner.logger = Mock()
+        runner.logger.info = Mock()
+        hook.after_test_iter(runner, 0)
+        runner.logger.info.assert_not_called()
+        hook.after_test_iter(runner, 1)
+        runner.logger.info.assert_called_once()
