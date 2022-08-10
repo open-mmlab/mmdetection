b3bb245db0b3d970da09207111c0feebeb49243c
QingyunLi <962537281@qq.com>
2022-08-10 08:53:50 +0800

    update: modify configs, delete configs of neck conv and FrozenBatchNorm2d

--- configs/dino/dino_4scale_r50_8x2_12e_coco.py
+++ configs/dino/dino_4scale_r50_8x2_12e_coco.py
@@ -9,8 +9,7 @@ model = dict(
         num_stages=4,
         out_indices=(1, 2, 3),
         frozen_stages=1,
-        # norm_cfg=dict(type='BN', requires_grad=False),
-        norm_cfg=dict(type='FrozenBatchNorm2d'),  # should use BN instead
+        norm_cfg=dict(type='BN', requires_grad=False),
         norm_eval=True,
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
@@ -20,7 +19,6 @@ model = dict(
         kernel_size=1,
         out_channels=256,
         act_cfg=None,
-        conv_cfg=dict(type='Conv2d', bias=True),  # should be deleted
         norm_cfg=dict(type='GN', num_groups=32),
         num_outs=4),
     bbox_head=dict(
