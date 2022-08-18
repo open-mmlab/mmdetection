5d3ebb4d6bfc784cfa8398c86421cf23308da4d8
wanghonglie <wanghonglie@pjlab.org.cn>
2022-08-18 17:14:16 +0800

    rename config

--- configs/sparse-rcnn/sparse-rcnn_r101_fpn_300_proposals_crop_mstrain_480-800_3x_coco.py
+++
@@ -1,7 +0,0 @@
-_base_ = './sparse_rcnn_r50_fpn_300_proposals_crop_mstrain_480-800_3x_coco.py'
-
-model = dict(
-    backbone=dict(
-        depth=101,
-        init_cfg=dict(type='Pretrained',
-                      checkpoint='torchvision://resnet101')))
