d4d313455754fe912a047b8fb17e1eb768c26e19
Hfhstrive <61572903+Hfhstrive@users.noreply.github.com>
2022-08-22 23:32:36 +0800

    Update queryinst_r50_fpn_1x_coco.py

--- configs/queryinst/queryinst_r50_fpn_1x_coco.py
+++ configs/queryinst/queryinst_r50_fpn_1x_coco.py
@@ -7,11 +7,11 @@ num_proposals = 100
 model = dict(
     type='QueryInst',
     backbone=dict(
-        type='ResNet',
+        type='RegNet',
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
+        frozen_stages=2,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
