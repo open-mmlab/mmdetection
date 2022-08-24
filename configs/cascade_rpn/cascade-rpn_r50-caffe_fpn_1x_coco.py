4850734db0db6b5d8fb774792a95a41f2172d37b
wanghonglie <wanghonglie@pjlab.org.cn>
2022-08-24 16:54:16 +0800

    update

--- configs/cascade_rpn/cascade-rpn_r50-caffe_fpn_1x_coco.py
+++ configs/cascade_rpn/cascade-rpn_r50-caffe_fpn_1x_coco.py
@@ -1,77 +1,76 @@
-_base_ = '../fast_rcnn/fast_rcnn_r50_fpn_1x_coco.py'
+_base_ = '../rpn/rpn_r50_caffe_fpn_1x_coco.py'
 model = dict(
-    backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=False),
-        norm_eval=True,
-        style='caffe',
-        init_cfg=dict(
-            type='Pretrained',
-            checkpoint='open-mmlab://detectron2/resnet50_caffe')),
-    roi_head=dict(
-        bbox_head=dict(
-            bbox_coder=dict(target_stds=[0.04, 0.04, 0.08, 0.08]),
-            loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.5),
-            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
-    # model training and testing settings
-    train_cfg=dict(
-        rcnn=dict(
-            assigner=dict(
-                pos_iou_thr=0.65, neg_iou_thr=0.65, min_pos_iou=0.65),
-            sampler=dict(num=256))),
-    test_cfg=dict(rcnn=dict(score_thr=1e-3)))
-dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
-img_norm_cfg = dict(
-    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadProposals', num_max_proposals=300),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'proposals', 'gt_bboxes', 'gt_labels']),
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadProposals', num_max_proposals=300),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='ToTensor', keys=['proposals']),
+    rpn_head=dict(
+        _delete_=True,
+        type='CascadeRPNHead',
+        num_stages=2,
+        stages=[
             dict(
-                type='ToDataContainer',
-                fields=[dict(key='proposals', stack=False)]),
-            dict(type='Collect', keys=['img', 'proposals']),
-        ])
-]
-# TODO support proposals input
-data = dict(
-    train=dict(
-        proposal_file=data_root +
-        'proposals/crpn_r50_caffe_fpn_1x_train2017.pkl',
-        pipeline=train_pipeline),
-    val=dict(
-        proposal_file=data_root +
-        'proposals/crpn_r50_caffe_fpn_1x_val2017.pkl',
-        pipeline=test_pipeline),
-    test=dict(
-        proposal_file=data_root +
-        'proposals/crpn_r50_caffe_fpn_1x_val2017.pkl',
-        pipeline=test_pipeline))
+                type='StageCascadeRPNHead',
+                in_channels=256,
+                feat_channels=256,
+                anchor_generator=dict(
+                    type='AnchorGenerator',
+                    scales=[8],
+                    ratios=[1.0],
+                    strides=[4, 8, 16, 32, 64]),
+                adapt_cfg=dict(type='dilation', dilation=3),
+                bridged_feature=True,
+                sampling=False,
+                with_cls=False,
+                reg_decoded_bbox=True,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=(.0, .0, .0, .0),
+                    target_stds=(0.1, 0.1, 0.5, 0.5)),
+                loss_bbox=dict(type='IoULoss', linear=True, loss_weight=10.0)),
+            dict(
+                type='StageCascadeRPNHead',
+                in_channels=256,
+                feat_channels=256,
+                adapt_cfg=dict(type='offset'),
+                bridged_feature=False,
+                sampling=True,
+                with_cls=True,
+                reg_decoded_bbox=True,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=(.0, .0, .0, .0),
+                    target_stds=(0.05, 0.05, 0.1, 0.1)),
+                loss_cls=dict(
+                    type='CrossEntropyLoss', use_sigmoid=True,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='IoULoss', linear=True, loss_weight=10.0))
+        ]),
+    train_cfg=dict(rpn=[
+        dict(
+            assigner=dict(
+                type='RegionAssigner', center_ratio=0.2, ignore_ratio=0.5),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.7,
+                min_pos_iou=0.3,
+                ignore_iof_thr=-1,
+                iou_calculator=dict(type='BboxOverlaps2D')),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False)
+    ]),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type='nms', iou_threshold=0.8),
+            min_bbox_size=0)))
 optim_wrapper = dict(clip_grad=dict(max_norm=35, norm_type=2))
