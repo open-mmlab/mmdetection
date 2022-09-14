bff0bde04c696eb5282510fdeaa36977e59d9185
David Xu <davidxp@vip.126.com>
2022-09-15 07:43:23 +0800

    add get instance key default to null in loading.py
    add get instance key default to null in loading.py

--- mmdet/datasets/transforms/loading.py
+++ mmdet/datasets/transforms/loading.py
@@ -260,10 +260,9 @@ class LoadAnnotations(MMCV_LoadAnnotations):
         """
         gt_bboxes = []
         gt_ignore_flags = []
-        if 'instances' in results.keys():
-            for instance in results['instances']:
-                gt_bboxes.append(instance['bbox'])
-                gt_ignore_flags.append(instance['ignore_flag'])
+        for instance in results.get('instances', []):
+            gt_bboxes.append(instance['bbox'])
+            gt_ignore_flags.append(instance['ignore_flag'])
         if self.box_type is None:
             results['gt_bboxes'] = np.array(
                 gt_bboxes, dtype=np.float32).reshape((-1, 4))
@@ -282,9 +281,8 @@ class LoadAnnotations(MMCV_LoadAnnotations):
             dict: The dict contains loaded label annotations.
         """
         gt_bboxes_labels = []
-        if 'instances' in results.keys():
-            for instance in results['instances']:
-                gt_bboxes_labels.append(instance['bbox_label'])
+        for instance in results.get('instances', []):
+            gt_bboxes_labels.append(instance['bbox_label'])
         # TODO: Inconsistent with mmcv, consider how to deal with it later.
         results['gt_bboxes_labels'] = np.array(
             gt_bboxes_labels, dtype=np.int64)
@@ -328,7 +326,7 @@ class LoadAnnotations(MMCV_LoadAnnotations):
         """
         gt_masks = []
         gt_ignore_flags = []
-        for instance in results['instances']:
+        for instance in results.get('instances', []):
             gt_mask = instance['mask']
             # If the annotation of segmentation mask is invalid,
             # ignore the whole instance.
