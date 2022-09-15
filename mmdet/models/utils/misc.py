b283fe71e5b33e0ea84383af7dab6232551bb57a
Czm369 <1341807343@qq.com>
2022-09-15 15:49:26 +0800

    refactor some functions

--- mmdet/models/utils/misc.py
+++ mmdet/models/utils/misc.py
@@ -537,8 +537,8 @@ def get_box_tensor(boxes: Union[Tensor, BaseBoxes]) -> Tensor:
     return boxes
 
 
-def filter_gt_instances_by_score(batch_data_samples: SampleList,
-                                 score_thr: float) -> SampleList:
+def _filter_gt_instances_by_score(batch_data_samples: SampleList,
+                                  score_thr: float) -> SampleList:
     """Filter ground truth (GT) instances by score.
 
     Args:
@@ -559,8 +559,8 @@ def filter_gt_instances_by_score(batch_data_samples: SampleList,
     return batch_data_samples
 
 
-def filter_gt_instances_by_size(batch_data_samples: SampleList,
-                                wh_thr: tuple) -> SampleList:
+def _filter_gt_instances_by_size(batch_data_samples: SampleList,
+                                 wh_thr: tuple) -> SampleList:
     """Filter ground truth (GT) instances by size.
 
     Args:
@@ -599,10 +599,10 @@ def filter_gt_instances(batch_data_samples: SampleList,
     """
 
     if score_thr is not None:
-        batch_data_samples = filter_gt_instances_by_score(
+        batch_data_samples = _filter_gt_instances_by_score(
             batch_data_samples, score_thr)
     if wh_thr is not None:
-        batch_data_samples = filter_gt_instances_by_size(
+        batch_data_samples = _filter_gt_instances_by_size(
             batch_data_samples, wh_thr)
     return batch_data_samples
 
