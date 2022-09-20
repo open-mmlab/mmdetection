63f634abafd5c9901d872122aaa957f659bcbaa8
lym <letusgo126@126.com>
2022-09-20 10:56:54 +0800

    detele positional encodeing

--- mmdet/models/layers/transformer.py
+++ mmdet/models/layers/transformer.py
@@ -388,40 +388,6 @@ def inverse_sigmoid(x, eps=1e-5):
     x2 = (1 - x).clamp(min=eps)
     return torch.log(x1 / x2)
 
-def gen_sineembed_for_position(pos_tensor,
-                               num_feats,
-                               temperature=10000,
-                               scale=2 * math.pi):
-    dim_t = torch.arange(
-        num_feats, dtype=torch.float32, device=pos_tensor.device)
-    dim_t = temperature**(2 * (dim_t // 2) / num_feats)
-    x_embed = pos_tensor[:, :, 0] * scale
-    y_embed = pos_tensor[:, :, 1] * scale
-    pos_x = x_embed[:, :, None] / dim_t
-    pos_y = y_embed[:, :, None] / dim_t
-    pos_x = torch.stack((pos_x[:, :, 0::2].sin(), pos_x[:, :, 1::2].cos()),
-                        dim=3).flatten(2)  # TODO: .view()
-    pos_y = torch.stack((pos_y[:, :, 0::2].sin(), pos_y[:, :, 1::2].cos()),
-                        dim=3).flatten(2)
-    if pos_tensor.size(-1) == 2:
-        pos = torch.cat((pos_y, pos_x), dim=2)
-    elif pos_tensor.size(-1) == 4:
-        w_embed = pos_tensor[:, :, 2] * scale
-        pos_w = w_embed[:, :, None] / dim_t
-        pos_w = torch.stack((pos_w[:, :, 0::2].sin(), pos_w[:, :, 1::2].cos()),
-                            dim=3).flatten(2)
-
-        h_embed = pos_tensor[:, :, 3] * scale
-        pos_h = h_embed[:, :, None] / dim_t
-        pos_h = torch.stack((pos_h[:, :, 0::2].sin(), pos_h[:, :, 1::2].cos()),
-                            dim=3).flatten(2)
-
-        pos = torch.cat((pos_y, pos_x, pos_w, pos_h), dim=2)
-    else:
-        raise ValueError('Unknown pos_tensor shape(-1):{}'.format(
-            pos_tensor.size(-1)))
-    return pos
-
 
 class MLP(nn.Module):
     """Very simple multi-layer perceptron (also called FFN) with relu."""
