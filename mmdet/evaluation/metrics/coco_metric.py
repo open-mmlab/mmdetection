7c1aa3ddcde898aa822b99f9afd73e2d78ac9ddd
luochunhua <luochunhua1996@outlook.com>
2022-09-27 14:04:54 +0000

    update

--- mmdet/evaluation/metrics/coco_metric.py
+++ mmdet/evaluation/metrics/coco_metric.py
@@ -1,57 +1,75 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from typing import Sequence
 
-from mmeval.detection import CocoMetric
+from mmeval.detection import CocoMetric as MMEVAL_CocoMetric
 
 from mmdet.registry import METRICS
 from mmdet.structures.mask import encode_mask_results
 
 
-def coco_metric_process(metric: CocoMetric, data_batch: dict,
-                        data_samples: Sequence[dict]) -> None:
-    """Process one batch of data samples and predictions. The processed results
-    should be stored in ``self.results``, which will be used to compute the
-    metrics when all batches have been processed.
-
-    Args:
-        data_batch (dict): A batch of data from the dataloader.
-        data_samples (Sequence[dict]): A batch of data samples that
-            contain annotations and predictions.
+@METRICS.register_module()
+class CocoMetric(MMEVAL_CocoMetric):
     """
-    predictions = []
-    groundtruths = []
-    for data_sample in data_samples:
-        result = dict()
-        pred = data_sample['pred_instances']
-        result['img_id'] = data_sample['img_id']
-        result['bboxes'] = pred['bboxes'].cpu().numpy()
-        result['scores'] = pred['scores'].cpu().numpy()
-        result['labels'] = pred['labels'].cpu().numpy()
-        # encode mask to RLE
-        if 'masks' in pred:
-            result['masks'] = encode_mask_results(
-                pred['masks'].detach().cpu().numpy())
-        # some detectors use different scores for bbox and mask
-        if 'mask_scores' in pred:
-            result['mask_scores'] = pred['mask_scores'].cpu().numpy()
+    Args:
+        prefix (str): The prefix that will be added in the metric names to
+            disambiguate homonymous metrics of different evaluators.
+            Defaults to 'coco'.
+    """
 
-        # parse gt
-        gt = dict()
-        gt['width'] = data_sample['ori_shape'][1]
-        gt['height'] = data_sample['ori_shape'][0]
-        gt['img_id'] = data_sample['img_id']
-        if metric._coco_api is None:
-            assert 'instances' in data_sample, \
-                'ground truth is required for evaluation when ' \
-                '`ann_file` is not provided'
-            gt['instances'] = data_sample['instances']
+    def __init__(self, prefix: str = 'coco', **kwargs) -> None:
 
-        # pred, gt
-        predictions.append(result)
-        groundtruths.append(gt)
-    metric.add(predictions, groundtruths)
+        super().__init__(**kwargs)
+        self.prefix = prefix
 
+    def process(self, data_batch: dict, data_samples: Sequence[dict]) -> None:
+        """Process one batch of data samples and predictions. The processed
+        results should be stored in ``self.results``, which will be used to
+        compute the metrics when all batches have been processed.
 
-CocoMetric.process = coco_metric_process
+        Args:
+            data_batch (dict): A batch of data from the dataloader.
+            data_samples (Sequence[dict]): A batch of data samples that
+                contain annotations and predictions.
+        """
+        predictions = []
+        groundtruths = []
+        for data_sample in data_samples:
+            result = dict()
+            pred = data_sample['pred_instances']
+            result['img_id'] = data_sample['img_id']
+            result['bboxes'] = pred['bboxes'].cpu().numpy()
+            result['scores'] = pred['scores'].cpu().numpy()
+            result['labels'] = pred['labels'].cpu().numpy()
+            # encode mask to RLE
+            if 'masks' in pred:
+                result['masks'] = encode_mask_results(
+                    pred['masks'].detach().cpu().numpy())
+            # some detectors use different scores for bbox and mask
+            if 'mask_scores' in pred:
+                result['mask_scores'] = pred['mask_scores'].cpu().numpy()
 
-METRICS.register_module(module=CocoMetric)
+            # parse gt
+            gt = dict()
+            gt['width'] = data_sample['ori_shape'][1]
+            gt['height'] = data_sample['ori_shape'][0]
+            gt['img_id'] = data_sample['img_id']
+            if self._coco_api is None:
+                assert 'instances' in data_sample, \
+                    'ground truth is required for evaluation when ' \
+                    '`ann_file` is not provided'
+                gt['instances'] = data_sample['instances']
+
+            # pred, gt
+            predictions.append(result)
+            groundtruths.append(gt)
+        self.add(predictions, groundtruths)
+
+    def evaluate(self, *args, **kwargs) -> dict:
+        eval_results = self.compute(*args, **kwargs)
+
+        eval_results = {
+            f'{self.prefix}/{k}': v
+            for k, v in eval_results.items()
+        }
+
+        return eval_results
