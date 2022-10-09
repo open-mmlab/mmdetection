fc8947d7d813c584e089b75cf3ab0fe94b682783
huanghaian <huanghaian@sensetime.com>
2022-10-09 11:14:45 +0800

    Add DetWandbVisualizer

--- mmdet/visualization/__init__.py
+++ mmdet/visualization/__init__.py
@@ -1,5 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .local_visualizer import DetLocalVisualizer
 from .palette import get_palette, palette_val
+from .wandb_visualizer import DetWandbVisualizer
 
-__all__ = ['palette_val', 'get_palette', 'DetLocalVisualizer']
+__all__ = [
+    'palette_val', 'get_palette', 'DetLocalVisualizer', 'DetWandbVisualizer'
+]
