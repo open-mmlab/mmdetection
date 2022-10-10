6470ed6e666861c9f90939c4e423121b9693e6a7
RangiLyu <lyuchqi@gmail.com>
2022-10-10 17:21:36 +0800

    fix lint

--- mmdet/visualization/__init__.py
+++ mmdet/visualization/__init__.py
@@ -2,4 +2,4 @@
 from .local_visualizer import DetLocalVisualizer
 from .palette import get_palette, jitter_color, palette_val
 
-__all__ = ['palette_val', 'get_palette', 'DetLocalVisualizer']
+__all__ = ['palette_val', 'get_palette', 'DetLocalVisualizer', 'jitter_color']
