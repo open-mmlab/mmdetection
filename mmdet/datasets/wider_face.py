95209f35c5d07700844340f9729df3bfbd2580b2
jason_w <wongzheng@126.com>
2022-10-10 15:09:56 +0800

    update WIDERFaceDataset

--- mmdet/datasets/wider_face.py
+++ mmdet/datasets/wider_face.py
@@ -7,7 +7,7 @@ from mmengine.fileio import list_from_file
 from mmengine.utils import ProgressBar
 
 from mmdet.registry import DATASETS
-from mmdet.utils.typing import List, Union
+from mmdet.utils.typing import List
 from .xml_style import XMLDataset
 
 
@@ -20,14 +20,22 @@ class WIDERFaceDataset(XMLDataset):
     """
     METAINFO = {'classes': ('face', ), 'palette': [(0, 255, 0)]}
 
+    def __init__(self, **kwargs):
+        super(WIDERFaceDataset, self).__init__(img_subdir='', **kwargs)
+
+    @property
+    def img_root(self) -> str:
+        """Return the sub data root."""
+        return self.data_prefix.get('img', '')
+
     def load_data_list(self) -> List[dict]:
-        """Load annotation from XML style ann_file.
+        """Load annotation from WIDERFace XML style annotation file.
 
         Returns:
             list[dict]: Annotation info from XML file.
         """
         assert self._metainfo.get('classes', None) is not None, \
-            'classes in `XMLDataset` can not be None.'
+            '`classes` in `XMLDataset` can not be None.'
         self.cat2label = {
             cat: i
             for i, cat in enumerate(self._metainfo['classes'])
@@ -42,48 +50,25 @@ class WIDERFaceDataset(XMLDataset):
             prog_bar = ProgressBar(len(img_ids))
 
         for img_id in img_ids:
+            xml_path = osp.join(self.img_root, self.ann_subdir,
+                                f'{img_id}.xml')
+            with self.file_client.get_local_path(xml_path) as local_path:
+                raw_ann_info = ET.parse(local_path)
+            root = raw_ann_info.getroot()
+            folder = root.find('folder').text
+            filename = root.find('filename').text
+            assert osp.splitext(filename)[0] == img_id
+            img_path = osp.join(self.img_root, self.img_subdir, folder,
+                                filename)
+
             raw_img_info = {}
             raw_img_info['img_id'] = img_id
-            raw_img_info['file_name'] = f'{img_id}.jpg'
+            raw_img_info['img_path'] = img_path
+            raw_img_info['xml_path'] = xml_path
+
             parsed_data_info = self.parse_data_info(raw_img_info)
             data_list.append(parsed_data_info)
 
             if is_main_process():
                 prog_bar.update()
         return data_list
-
-    def parse_data_info(self, img_info: dict) -> Union[dict, List[dict]]:
-        """Parse raw annotation to target format.
-
-        Args:
-            img_info (dict): Raw image information, usually it includes
-                `img_id`, `file_name`, and `xml_path`.
-
-        Returns:
-            Union[dict, List[dict]]: Parsed annotation.
-        """
-        data_info = {}
-        img_id = img_info['img_id']
-        xml_path = osp.join(self.data_prefix['img'], 'Annotations',
-                            f'{img_id}.xml')
-        data_info['img_id'] = img_id
-        data_info['xml_path'] = xml_path
-
-        # deal with xml file
-        with self.file_client.get_local_path(xml_path) as local_path:
-            raw_ann_info = ET.parse(local_path)
-        root = raw_ann_info.getroot()
-        size = root.find('size')
-        width = int(size.find('width').text)
-        height = int(size.find('height').text)
-        folder = root.find('folder').text
-        img_path = osp.join(self.data_prefix['img'], folder,
-                            img_info['file_name'])
-        data_info['img_path'] = img_path
-
-        data_info['height'] = height
-        data_info['width'] = width
-
-        # Coordinates are in range [0, width - 1 or height - 1]
-        data_info['instances'] = self._parse_instance_info(raw_ann_info)
-        return data_info
