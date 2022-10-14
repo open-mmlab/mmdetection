cca4f789d431192993253c8bcd63b09e69dad3de
HAOCHENYE <21724054@zju.edu.cn>
2022-10-14 18:19:15 +0800

    minor refine

--- configs/_base_/datasets/tta_pipeline.py
+++ configs/_base_/datasets/tta_pipeline.py
@@ -4,7 +4,7 @@ tta_pipeline = [
         dict(type='TestTimeAug',
              transforms=[
                  [dict(type='Resize', scale=(1333, 800), keep_ratio=True),
-                  dict(type='Resize', scale=(1333, 800), keep_ratio=True)],
+                  dict(type='Resize', scale=(1333, 600), keep_ratio=True)],
                  [dict(type='RandomFlip', prob=1.),
                   dict(type='RandomFlip', prob=0.)],
                  [dict(type='LoadAnnotations', with_bbox=True)],
