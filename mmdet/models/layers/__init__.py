968a14d5cc46a83db3f36f1af9df3eb841a98bf4
QingyunLi <962537281@qq.com>
2022-10-19 21:48:54 +0800

    fix auto-merge bug

--- mmdet/models/layers/__init__.py
+++ mmdet/models/layers/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .activations import SiLU
 from .bbox_nms import fast_nms, multiclass_nms
 from .brick_wrappers import AdaptiveAvgPool2d, adaptive_avg_pool2d
 from .conv_upsample import ConvUpsample
@@ -14,20 +15,45 @@ from .positional_encoding import (LearnedPositionalEncoding,
                                   SinePositionalEncoding)
 from .res_layer import ResLayer, SimplifiedBasicBlock
 from .se_layer import ChannelAttention, DyReLU, SELayer
-from .transformer import (DetrTransformerDecoder, DetrTransformerDecoderLayer,
-                          DetrTransformerEncoder, DetrTransformerEncoderLayer,
-                          DynamicConv, PatchEmbed, PatchMerging,
-                          inverse_sigmoid, nchw_to_nlc, nlc_to_nchw, MLP)
+from .transformer import (MLP, DetrTransformerDecoder,
+                          DetrTransformerDecoderLayer, DetrTransformerEncoder,
+                          DetrTransformerEncoderLayer, DynamicConv, PatchEmbed,
+                          PatchMerging, inverse_sigmoid, nchw_to_nlc,
+                          nlc_to_nchw)
 
 __all__ = [
-    'fast_nms', 'multiclass_nms', 'mask_matrix_nms', 'DropBlock',
-    'PixelDecoder', 'TransformerEncoderPixelDecoder',
-    'MSDeformAttnPixelDecoder', 'ResLayer', 'PatchMerging',
-    'SinePositionalEncoding', 'LearnedPositionalEncoding', 'DynamicConv',
-    'SimplifiedBasicBlock', 'NormedLinear', 'NormedConv2d', 'InvertedResidual',
-    'SELayer', 'ConvUpsample', 'CSPLayer', 'adaptive_avg_pool2d',
-    'AdaptiveAvgPool2d', 'PatchEmbed', 'nchw_to_nlc', 'nlc_to_nchw', 'DyReLU',
-    'ExpMomentumEMA', 'inverse_sigmoid', 'ChannelAttention', 'SiLU', 'MLP',
-    'DetrTransformerEncoderLayer', 'DetrTransformerDecoderLayer',
-    'DetrTransformerEncoder', 'DetrTransformerDecoder',
+    'fast_nms',
+    'multiclass_nms',
+    'mask_matrix_nms',
+    'DropBlock',
+    'PixelDecoder',
+    'TransformerEncoderPixelDecoder',
+    'MSDeformAttnPixelDecoder',
+    'ResLayer',
+    'PatchMerging',
+    'SinePositionalEncoding',
+    'LearnedPositionalEncoding',
+    'DynamicConv',
+    'SimplifiedBasicBlock',
+    'NormedLinear',
+    'NormedConv2d',
+    'InvertedResidual',
+    'SELayer',
+    'ConvUpsample',
+    'CSPLayer',
+    'adaptive_avg_pool2d',
+    'AdaptiveAvgPool2d',
+    'PatchEmbed',
+    'nchw_to_nlc',
+    'nlc_to_nchw',
+    'DyReLU',
+    'ExpMomentumEMA',
+    'inverse_sigmoid',
+    'ChannelAttention',
+    'SiLU',
+    'MLP',
+    'DetrTransformerEncoderLayer',
+    'DetrTransformerDecoderLayer',
+    'DetrTransformerEncoder',
+    'DetrTransformerDecoder',
 ]
