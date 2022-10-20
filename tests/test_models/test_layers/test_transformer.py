7c9abef6e55d589957d9cf7a11d0ca4517427137
lym <letusgo126@126.com>
2022-10-20 15:03:23 +0800

    delete Transformer in ci test

--- tests/test_models/test_layers/test_transformer.py
+++ tests/test_models/test_layers/test_transformer.py
@@ -531,39 +531,3 @@ def test_detr_transformer_dencoder_encoder_layer():
         DetrTransformerEncoder(**config)
 
 
-def test_transformer():
-    config = ConfigDict(
-        dict(
-            encoder=dict(
-                type='DetrTransformerEncoder',
-                num_layers=6,
-                transformerlayers=dict(
-                    type='BaseTransformerLayer',
-                    attn_cfgs=[
-                        dict(
-                            type='MultiheadAttention',
-                            embed_dims=256,
-                            num_heads=8,
-                            dropout=0.1)
-                    ],
-                    feedforward_channels=2048,
-                    ffn_dropout=0.1,
-                    operation_order=('self_attn', 'norm', 'ffn', 'norm'))),
-            decoder=dict(
-                type='DetrTransformerDecoder',
-                return_intermediate=True,
-                num_layers=6,
-                transformerlayers=dict(
-                    type='DetrTransformerDecoderLayer',
-                    attn_cfgs=dict(
-                        type='MultiheadAttention',
-                        embed_dims=256,
-                        num_heads=8,
-                        dropout=0.1),
-                    feedforward_channels=2048,
-                    ffn_dropout=0.1,
-                    operation_order=('self_attn', 'norm', 'cross_attn', 'norm',
-                                     'ffn', 'norm')),
-            )))
-    transformer = Transformer(**config)
-    transformer.init_weights()
