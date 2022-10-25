144305205342d05fbcfb709764f77f24e96cbb86
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-10-25 13:34:49 +0800

    minor fix

--- configs/rpn/rpn_r50_fpn_1x_coco.py
+++ configs/rpn/rpn_r50_fpn_1x_coco.py
@@ -3,23 +3,23 @@ _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
-# val_evaluator = dict(metric='proposal_fast')
-# test_evaluator = val_evaluator
+val_evaluator = dict(metric='proposal_fast')
+test_evaluator = val_evaluator
 
 # inference on val dataset and dump the proposals with evaluate metric
-data_root = 'data/coco/'
-test_evaluator = [
-    dict(
-        type='DumpProposals',
-        output_dir=data_root + 'proposals/',
-        proposals_file='rpn_r50_fpn_1x_val2017.pkl'),
-    dict(
-        type='CocoMetric',
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        metric='proposal_fast',
-        file_client_args={{_base_.file_client_args}},
-        format_only=False)
-]
+# data_root = 'data/coco/'
+# test_evaluator = [
+#     dict(
+#         type='DumpProposals',
+#         output_dir=data_root + 'proposals/',
+#         proposals_file='rpn_r50_fpn_1x_val2017.pkl'),
+#     dict(
+#         type='CocoMetric',
+#         ann_file=data_root + 'annotations/instances_val2017.json',
+#         metric='proposal_fast',
+#         file_client_args={{_base_.file_client_args}},
+#         format_only=False)
+# ]
 
 # inference on training dataset and dump the proposals without evaluate metric
 # data_root = 'data/coco/'
