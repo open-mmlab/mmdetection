9c1b26726eebe4a196d213249dc22e8017761fab
BigDong <yudongwang@tju.edu.cn>
2022-11-01 16:53:33 +0800

    [Feature] Support training detection models in detectron2 (#8672)
    * [Feature]Support using mmengine to train detectron2
    
    * update
    
    * del unnecessary comments
    
    * minor fix
    
    * minor fix
    
    * Support mask rcnn and retinanet
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * chinese doc
    
    * update
    
    * minor fix
    
    * minor fix
    
    * update docs

--- configs/common/ms-90k_coco.py
+++ configs/common/ms-90k_coco.py
@@ -3,7 +3,6 @@ _base_ = '../_base_/default_runtime.py'
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
-
 # file_client_args = dict(
 #     backend='petrel',
 #     path_mapping=dict({
@@ -12,18 +11,29 @@ data_root = 'data/coco/'
 #     }))
 file_client_args = dict(backend='disk')
 
+# Align with Detectron2
+backend = 'pillow'
 train_pipeline = [
-    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(
+        type='LoadImageFromFile',
+        file_client_args=file_client_args,
+        imdecode_backend=backend),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
-        type='RandomResize', scale=[(1333, 640), (1333, 800)],
-        keep_ratio=True),
+        type='RandomChoiceResize',
+        scales=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                (1333, 768), (1333, 800)],
+        keep_ratio=True,
+        backend=backend),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PackDetInputs')
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile', file_client_args=file_client_args),
-    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(
+        type='LoadImageFromFile',
+        file_client_args=file_client_args,
+        imdecode_backend=backend),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True, backend=backend),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='PackDetInputs',
@@ -34,23 +44,22 @@ train_dataloader = dict(
     batch_size=2,
     num_workers=2,
     persistent_workers=True,
-    sampler=dict(type='DefaultSampler', shuffle=True),
+    pin_memory=True,
+    sampler=dict(type='InfiniteSampler', shuffle=True),
     batch_sampler=dict(type='AspectRatioBatchSampler'),
     dataset=dict(
-        type='RepeatDataset',
-        times=3,
-        dataset=dict(
-            type=dataset_type,
-            data_root=data_root,
-            ann_file='annotations/instances_train2017.json',
-            data_prefix=dict(img='train2017/'),
-            filter_cfg=dict(filter_empty_gt=True, min_size=32),
-            pipeline=train_pipeline)))
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='train2017/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline))
 val_dataloader = dict(
     batch_size=1,
     num_workers=2,
     persistent_workers=True,
     drop_last=False,
+    pin_memory=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type=dataset_type,
@@ -64,25 +73,28 @@ test_dataloader = val_dataloader
 val_evaluator = dict(
     type='CocoMetric',
     ann_file=data_root + 'annotations/instances_val2017.json',
-    metric='bbox')
+    metric='bbox',
+    format_only=False)
 test_evaluator = val_evaluator
 
-# training schedule for 3x with `RepeatDataset`
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=12, val_interval=1)
+# training schedule for 90k
+max_iter = 90000
+train_cfg = dict(
+    type='IterBasedTrainLoop', max_iters=max_iter, val_interval=10000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
 # learning rate
-# Experiments show that using milestones=[9, 11] has higher performance
 param_scheduler = [
     dict(
-        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
+        end=1000),
     dict(
         type='MultiStepLR',
         begin=0,
-        end=12,
-        by_epoch=True,
-        milestones=[9, 11],
+        end=max_iter,
+        by_epoch=False,
+        milestones=[60000, 80000],
         gamma=0.1)
 ]
 
@@ -90,9 +102,11 @@ param_scheduler = [
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001))
-
 # Default setting for scaling LR automatically
 #   - `enable` means enable scaling LR automatically
 #       or not by default.
 #   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
 auto_scale_lr = dict(enable=False, base_batch_size=16)
+
+default_hooks = dict(checkpoint=dict(by_epoch=False, interval=10000))
+log_processor = dict(by_epoch=False)
