cea3106a86b8d4f18bdd6f6b4836c7a24fb1e482
BigDong <yudongwang@tju.edu.cn>
2022-11-03 14:50:01 +0800

    [Feature] Refactor Fast R-CNN to align the accuracy (#9132)
    * [Feature] Refactor Fast R-CNN
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * update readme
    
    * Update comments
    
    * minor fix
    
    * fix UT
    
    * update
    
    * update

--- configs/_base_/models/fast-rcnn_r50_fpn.py
+++ configs/_base_/models/fast-rcnn_r50_fpn.py
@@ -1,13 +1,12 @@
 # model settings
-preprocess_cfg = dict(
-    mean=[103.530, 116.280, 123.675],
-    std=[1.0, 1.0, 1.0],
-    to_rgb=False,
-    pad_size_divisor=32)
-
 model = dict(
     type='FastRCNN',
-    preprocess_cfg=preprocess_cfg,
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=32),
     backbone=dict(
         type='ResNet',
         depth=50,
