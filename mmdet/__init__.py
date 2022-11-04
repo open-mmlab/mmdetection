28b4b1324246d0036abc52b6cb3507d989c489e2
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-11-04 19:37:28 +0800

    minor fix

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -9,7 +9,7 @@ mmcv_minimum_version = '2.0.0rc0'
 mmcv_maximum_version = '2.1.0'
 mmcv_version = digit_version(mmcv.__version__)
 
-mmengine_minimum_version = '0.1.0'
+mmengine_minimum_version = '0.3.0'
 mmengine_maximum_version = '1.0.0'
 mmengine_version = digit_version(mmengine.__version__)
 
