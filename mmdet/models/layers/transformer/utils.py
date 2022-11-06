aeb2d52e10c23823390fc6feefa197271f4d25ba
lym <letusgo126@126.com>
2022-11-06 17:20:54 +0800

    move gen_sine_embed_for_ref to utils

--- mmdet/models/layers/transformer/utils.py
+++ mmdet/models/layers/transformer/utils.py
@@ -419,6 +419,40 @@ class MLP(BaseModule):
         return x
 
 
+def gen_sine_embed_for_ref(reference: Tensor):
+    # n_query, bs, _ = pos_tensor.size()
+    # sineembed_tensor = torch.zeros(n_query, bs, 256)
+    scale = 2 * math.pi
+    dim_t = torch.arange(128, dtype=torch.float32, device=reference.device)
+    dim_t = 10000**(2 * (dim_t // 2) / 128)
+    x_embed = reference[:, :, 0] * scale
+    y_embed = reference[:, :, 1] * scale
+    pos_x = x_embed[:, :, None] / dim_t
+    pos_y = y_embed[:, :, None] / dim_t
+    pos_x = torch.stack((pos_x[:, :, 0::2].sin(), pos_x[:, :, 1::2].cos()),
+                        dim=3).flatten(2)
+    pos_y = torch.stack((pos_y[:, :, 0::2].sin(), pos_y[:, :, 1::2].cos()),
+                        dim=3).flatten(2)
+    if reference.size(-1) == 2:
+        pos = torch.cat((pos_y, pos_x), dim=2)
+    elif reference.size(-1) == 4:
+        w_embed = reference[:, :, 2] * scale
+        pos_w = w_embed[:, :, None] / dim_t
+        pos_w = torch.stack((pos_w[:, :, 0::2].sin(), pos_w[:, :, 1::2].cos()),
+                            dim=3).flatten(2)
+
+        h_embed = reference[:, :, 3] * scale
+        pos_h = h_embed[:, :, None] / dim_t
+        pos_h = torch.stack((pos_h[:, :, 0::2].sin(), pos_h[:, :, 1::2].cos()),
+                            dim=3).flatten(2)
+
+        pos = torch.cat((pos_y, pos_x, pos_w, pos_h), dim=2)
+    else:
+        raise ValueError('Unknown pos_tensor shape(-1):{}'.format(
+            reference.size(-1)))
+    return pos
+
+
 @MODELS.register_module()
 class DynamicConv(BaseModule):
     """Implements Dynamic Convolution.
