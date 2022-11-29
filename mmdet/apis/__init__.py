40f64eec00d10a1c28089076b8c83fb30ab6cd00
sanbuphy <physicoada@gmail.com>
2022-11-29 12:16:33 +0800

    fix init

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import (async_inference_detector, inference_detector,
-                        init_detector)
+                        init_detector, show_result_pyplot)
 
 __all__ = [
     'init_detector',
     'async_inference_detector',
     'inference_detector',
+    'show_result_pyplot',
 ]
