aaad21989b5519e99d2e0a801d4c089eac99e6ad
RangiLyu <lyuchqi@gmail.com>
2022-12-12 16:14:49 +0800

    lint

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -18,9 +18,8 @@ from .xml_style import XMLDataset
 __all__ = [
     'XMLDataset', 'CocoDataset', 'DeepFashionDataset', 'VOCDataset',
     'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset', 'LVISV1Dataset',
-    'WIDERFaceDataset',
-    'get_loading_pipeline', 'CocoPanopticDataset', 'MultiImageMixDataset',
-    'OpenImagesDataset', 'OpenImagesChallengeDataset',
+    'WIDERFaceDataset', 'get_loading_pipeline', 'CocoPanopticDataset',
+    'MultiImageMixDataset', 'OpenImagesDataset', 'OpenImagesChallengeDataset',
     'AspectRatioBatchSampler', 'ClassAwareSampler', 'MultiSourceSampler',
     'GroupMultiSourceSampler', 'BaseDetDataset', 'CrowdHumanDataset'
 ]
