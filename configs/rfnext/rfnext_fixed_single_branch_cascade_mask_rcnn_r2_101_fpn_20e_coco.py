dd2e8aa561865b2fa93c2d3f0a3f363c61e6d0d4
lzyhha <819814373@qq.com>
2022-12-14 09:51:19 +0800

    add model and log links

--- configs/rfnext/rfnext_fixed_single_branch_cascade_mask_rcnn_r2_101_fpn_20e_coco.py
+++ configs/rfnext/rfnext_fixed_single_branch_cascade_mask_rcnn_r2_101_fpn_20e_coco.py
@@ -1,14 +1,4 @@
-_base_ = '../cascade_rcnn/cascade_mask_rcnn_r50_fpn_20e_coco.py'
-
-model = dict(
-    backbone=dict(
-        type='Res2Net',
-        depth=101,
-        scales=4,
-        base_width=26,
-        init_cfg=dict(
-            type='Pretrained',
-            checkpoint='open-mmlab://res2net101_v1d_26w_4s')))
+_base_ = '../res2net/cascade_mask_rcnn_r2_101_fpn_20e_coco.py'
 
 custom_hooks = [
     dict(type='NumClassCheckHook'),
