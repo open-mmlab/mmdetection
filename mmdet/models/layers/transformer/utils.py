182dddefa5aae6ad0aa34fdfd90323a8b419f460
lym <letusgo126@126.com>
2022-12-14 13:28:26 +0800

    refine some detail

--- mmdet/models/layers/transformer/utils.py
+++ mmdet/models/layers/transformer/utils.py
@@ -445,25 +445,25 @@ def convert_coordinate_to_encoding(coord_tensor: Tensor,
     dim_t = torch.arange(
         num_feats, dtype=torch.float32, device=coord_tensor.device)
     dim_t = temperature**(2 * (dim_t // 2) / num_feats)
-    x_embed = coord_tensor[:, :, 0] * scale
-    y_embed = coord_tensor[:, :, 1] * scale
-    pos_x = x_embed[:, :, None] / dim_t
-    pos_y = y_embed[:, :, None] / dim_t
-    pos_x = torch.stack((pos_x[:, :, 0::2].sin(), pos_x[:, :, 1::2].cos()),
+    x_embed = coord_tensor[..., 0] * scale
+    y_embed = coord_tensor[..., 1] * scale
+    pos_x = x_embed[..., None] / dim_t
+    pos_y = y_embed[..., None] / dim_t
+    pos_x = torch.stack((pos_x[..., 0::2].sin(), pos_x[..., 1::2].cos()),
                         dim=3).flatten(2)
-    pos_y = torch.stack((pos_y[:, :, 0::2].sin(), pos_y[:, :, 1::2].cos()),
+    pos_y = torch.stack((pos_y[..., 0::2].sin(), pos_y[..., 1::2].cos()),
                         dim=3).flatten(2)
     if coord_tensor.size(-1) == 2:
         pos = torch.cat((pos_y, pos_x), dim=2)
     elif coord_tensor.size(-1) == 4:
-        w_embed = coord_tensor[:, :, 2] * scale
-        pos_w = w_embed[:, :, None] / dim_t
-        pos_w = torch.stack((pos_w[:, :, 0::2].sin(), pos_w[:, :, 1::2].cos()),
+        w_embed = coord_tensor[..., 2] * scale
+        pos_w = w_embed[..., None] / dim_t
+        pos_w = torch.stack((pos_w[..., 0::2].sin(), pos_w[..., 1::2].cos()),
                             dim=3).flatten(2)
 
-        h_embed = coord_tensor[:, :, 3] * scale
-        pos_h = h_embed[:, :, None] / dim_t
-        pos_h = torch.stack((pos_h[:, :, 0::2].sin(), pos_h[:, :, 1::2].cos()),
+        h_embed = coord_tensor[..., 3] * scale
+        pos_h = h_embed[..., None] / dim_t
+        pos_h = torch.stack((pos_h[..., 0::2].sin(), pos_h[..., 1::2].cos()),
                             dim=3).flatten(2)
 
         pos = torch.cat((pos_y, pos_x, pos_w, pos_h), dim=2)
