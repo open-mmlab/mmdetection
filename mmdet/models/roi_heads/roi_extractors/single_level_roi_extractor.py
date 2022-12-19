ad86eaa93d000c55dde4c7274b4d5937eeda405f
Wang.Chenyu <61480109+chenyuwang814@users.noreply.github.com>
2022-12-19 15:09:12 +0800

    [Enhancement]: Delete compat code for parrots in roi extractor (#9503)

--- mmdet/models/roi_heads/roi_extractors/single_level_roi_extractor.py
+++ mmdet/models/roi_heads/roi_extractors/single_level_roi_extractor.py
@@ -69,9 +69,6 @@ class SingleRoIExtractor(BaseRoIExtractor):
         else:
             roi_feats = feats[0].new_zeros(
                 rois.size(0), self.out_channels, *out_size)
-        # TODO: remove this when parrots supports
-        if torch.__version__ == 'parrots':
-            roi_feats.requires_grad = True
 
         if num_levels == 1:
             if len(rois) == 0:
