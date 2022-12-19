53b0c4e44f6f257cdf8ce296210b1bc130bf0368
RangiLyu <lyuchqi@gmail.com>
2022-12-19 11:04:07 +0800

    [Fix]: Only keep meta and state_dict when publishing model. (#9356)
    * Only keep meta and state_dict when publish model.
    
    * support set saved keys
    
    * resolve comments

--- tools/model_converters/publish_model.py
+++ tools/model_converters/publish_model.py
@@ -3,6 +3,7 @@ import argparse
 import subprocess
 
 import torch
+from mmengine.logging import print_log
 
 
 def parse_args():
@@ -10,15 +11,30 @@ def parse_args():
         description='Process a checkpoint to be published')
     parser.add_argument('in_file', help='input checkpoint filename')
     parser.add_argument('out_file', help='output checkpoint filename')
+    parser.add_argument(
+        '--save-keys',
+        nargs='+',
+        type=str,
+        default=['meta', 'state_dict'],
+        help='keys to save in the published checkpoint')
     args = parser.parse_args()
     return args
 
 
-def process_checkpoint(in_file, out_file):
+def process_checkpoint(in_file, out_file, save_keys=['meta', 'state_dict']):
     checkpoint = torch.load(in_file, map_location='cpu')
-    # remove optimizer for smaller file size
-    if 'optimizer' in checkpoint:
-        del checkpoint['optimizer']
+
+    # only keep `meta` and `state_dict` for smaller file size
+    ckpt_keys = list(checkpoint.keys())
+    for k in ckpt_keys:
+        if k not in save_keys:
+            print_log(
+                f'Key `{k}` will be removed because it is not in '
+                f'save_keys. If you want to keep it, '
+                f'please set --save-keys.',
+                logger='current')
+            checkpoint.pop(k, None)
+
     # if it is necessary to remove some sensitive data in checkpoint['meta'],
     # add the code here.
     if torch.__version__ >= '1.6':
@@ -32,11 +48,13 @@ def process_checkpoint(in_file, out_file):
         out_file_name = out_file
     final_file = out_file_name + f'-{sha[:8]}.pth'
     subprocess.Popen(['mv', out_file, final_file])
+    print_log(
+        f'The published model is saved at {final_file}.', logger='current')
 
 
 def main():
     args = parse_args()
-    process_checkpoint(args.in_file, args.out_file)
+    process_checkpoint(args.in_file, args.out_file, args.save_keys)
 
 
 if __name__ == '__main__':
