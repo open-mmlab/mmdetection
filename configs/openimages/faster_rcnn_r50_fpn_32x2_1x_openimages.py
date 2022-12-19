fb64919e950eaf7fcd7a6fb105438e9a0a693ccd
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-12-19 16:57:45 +0800

    minor fix

--- configs/openimages/faster_rcnn_r50_fpn_32x2_1x_openimages.py
+++ configs/openimages/faster_rcnn_r50_fpn_32x2_1x_openimages.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/faster_rcnn_r50_fpn.py',
-    '../_base_/datasets/objects365v2_detection.py',
+    '../_base_/datasets/openimages_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
