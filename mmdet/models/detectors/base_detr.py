5a678e67f6f947051d64258857ddd2d27552b893
Qingyun <962537281@qq.com>
2022-12-31 17:25:13 +0800

    refine DeformableDETR.pre_transformer()

--- mmdet/models/detectors/base_detr.py
+++ mmdet/models/detectors/base_detr.py
@@ -201,7 +201,7 @@ class DetectionTransformer(BaseDetector, metaclass=ABCMeta):
 
         Args:
             img_feats (tuple[Tensor]): Tuple of feature maps from neck. Each
-                feature map has shape (bs, dim, H, W).
+                    feature map has shape (bs, dim, H, W).
             batch_data_samples (list[:obj:`DetDataSample`], optional): The
                 batch data samples. It usually includes information such
                 as `gt_instance` or `gt_panoptic_seg` or `gt_sem_seg`.
