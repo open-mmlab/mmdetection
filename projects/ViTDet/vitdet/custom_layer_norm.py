ad8ccfa4c6982c95f3f68adae6c30bd8fc64d1d3
zytx121 <592267829@qq.com>
2023-01-06 17:54:22 +0800

    Update custom_layer_norm.py

--- projects/ViTDet/vitdet/custom_layer_norm.py
+++ projects/ViTDet/vitdet/custom_layer_norm.py
@@ -1,15 +1,17 @@
-import torch
 from mmengine.registry import MODELS
+from torch import Tensor
 from torch.nn import LayerNorm
+
 
 @MODELS.register_module()
 class CustomLN(LayerNorm):
-    '''Custom Layer Normalization.'''
-    def forward(self, x: torch.Tensor, **kwargs) -> torch.Tensor:
+    """Custom Layer Normalization."""
+
+    def forward(self, x: Tensor) -> Tensor:
         if x.dim() == 4:
             x = x.permute(0, 2, 3, 1)
-            x = super().forward(x, **kwargs)
+            x = super().forward(x)
             x = x.permute(0, 3, 1, 2).contiguous()
         else:
-            x = super().forward(x, **kwargs)
+            x = super().forward(x)
         return x
