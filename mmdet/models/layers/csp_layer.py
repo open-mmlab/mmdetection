561be9f464766534f9021ecd670e8b8c4bd51de0
Minato <82735346+minato-ellie@users.noreply.github.com>
2023-01-12 04:41:45 +0000

    feature: add c2f layer (for yolov8.)

--- mmdet/models/layers/csp_layer.py
+++ mmdet/models/layers/csp_layer.py
@@ -239,3 +239,98 @@ class CSPLayer(BaseModule):
         if self.channel_attention:
             x_final = self.attention(x_final)
         return self.final_conv(x_final)
+
+
+class C2fLayer(BaseModule):
+    """Cross Stage Partial Layer.
+
+    Args:
+        in_channels (int): The input channels of the CSP layer.
+        out_channels (int): The output channels of the CSP layer.
+        expand_ratio (float): Ratio to adjust the number of channels of the
+            hidden layer. Default: 0.5
+        num_blocks (int): Number of blocks. Default: 1
+        add_identity (bool): Whether to add identity in blocks.
+            Default: True
+        use_cspnext_block (bool): Whether to use CSPNeXt block.
+            Defaults to False.
+        use_depthwise (bool): Whether to use depthwise separable convolution in
+            blocks. Default: False
+        channel_attention (bool): Whether to add channel attention in each
+            stage. Defaults to True.
+        conv_cfg (dict, optional): Config dict for convolution layer.
+            Default: None, which means using conv2d.
+        norm_cfg (dict): Config dict for normalization layer.
+            Default: dict(type='BN')
+        act_cfg (dict): Config dict for activation layer.
+            Default: dict(type='Swish')
+        init_cfg (:obj:`ConfigDict` or dict or list[dict] or
+            list[:obj:`ConfigDict`], optional): Initialization config dict.
+            Defaults to None.
+    """
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 expand_ratio: float = 0.5,
+                 num_blocks: int = 1,
+                 add_identity: bool = True,
+                 use_depthwise: bool = False,
+                 use_cspnext_block: bool = False,
+                 channel_attention: bool = False,
+                 conv_cfg: OptConfigType = None,
+                 norm_cfg=None,
+                 act_cfg=None,
+                 init_cfg: OptMultiConfig = None) -> None:
+        if act_cfg is None:
+            act_cfg = dict(type='Swish')
+        if norm_cfg is None:
+            norm_cfg = dict(
+                type='BN', momentum=0.03, eps=0.001)
+
+        super().__init__(init_cfg=init_cfg)
+
+        block = CSPNeXtBlock if use_cspnext_block else DarknetBottleneck
+
+        self.hidden_channels = int(out_channels * expand_ratio)
+
+        self.channel_attention = channel_attention
+        self.main_conv = ConvModule(
+            in_channels,
+            2 * self.hidden_channels,
+            1,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=act_cfg)
+
+        self.final_conv = ConvModule(
+            (2 + num_blocks) * self.hidden_channels,
+            out_channels,
+            1,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=act_cfg)
+
+        self.blocks = nn.ModuleList(
+            block(
+                self.hidden_channels,
+                self.hidden_channels,
+                1.0,
+                add_identity,
+                use_depthwise,
+                conv_cfg=conv_cfg,
+                norm_cfg=norm_cfg,
+                act_cfg=act_cfg) for _ in range(num_blocks)
+        )
+        if channel_attention:
+            self.attention = ChannelAttention(2 * self.hidden_channels)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = list(self.main_conv(x).split((self.hidden_channels, self.hidden_channels), dim=1))
+        x.extend(block(x[-1]) for block in self.blocks)
+
+        x = torch.cat(x, dim=1)
+
+        if self.channel_attention:
+            x = self.attention(x)
+        return self.final_conv(x)
