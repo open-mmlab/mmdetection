023aae1d2af9eddf0c28950653b506b01343a4ff
tjdhg456 <hogili89@gist.ac.kr>
2023-01-17 03:46:13 +0000

    feat: remove unneccessary parts

--- msdet/__init__.py
+++ msdet/__init__.py
@@ -2,9 +2,9 @@
 from .roi_heads import ContRoIHead
 from .coco import CocoContDataset
 from .smdp import SmdpDataset, SmdpContDataset
-from .faster_rcnn import FasterRCNNCont
+from .faster_rcnn import FasterRCNN_TS
 from .transforms import Resize_Student
 
 __all__ = [
-    'ContRoIHead', 'CocoContDataset', 'SmdpDataset', 'SmdpContDataset', 'FasterRCNNCont', 'Resize_Student'
+    'ContRoIHead', 'CocoContDataset', 'SmdpDataset', 'SmdpContDataset', 'FasterRCNN_TS', 'Resize_Student'
 ]
