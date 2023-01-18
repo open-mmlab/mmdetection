c8c0be417b5466e309b6564b999590738c62085f
akstt <13146229470@163.com>
2023-01-18 14:07:47 +0800

    resolve view comments

--- tests/test_utils/test_assigner.py
+++ tests/test_utils/test_assigner.py
@@ -669,28 +669,21 @@ def test_ascend_max_iou_assigner():
         pos_iou_thr=0.5,
         neg_iou_thr=0.5,
     )
-    batch_bboxes = torch.FloatTensor([
-        [
-            [0, 0, 10, 10],
-            [10, 10, 20, 20],
-            [5, 5, 15, 15],
-            [32, 32, 38, 42],
-        ]
-    ])
-    batch_gt_bboxes = torch.FloatTensor([
-        [
-            [0, 0, 10, 9],
-            [0, 10, 10, 19],
-        ]
-    ])
+    batch_bboxes = torch.FloatTensor([[
+        [0, 0, 10, 10],
+        [10, 10, 20, 20],
+        [5, 5, 15, 15],
+        [32, 32, 38, 42],
+    ]])
+    batch_gt_bboxes = torch.FloatTensor([[
+        [0, 0, 10, 9],
+        [0, 10, 10, 19],
+    ]])
     batch_gt_labels = torch.LongTensor([[2, 3]])
-    batch_bboxes_ignore_mask = torch.IntTensor(
-        [
-            [1, 1, 1, 1]
-        ]
-    )
+    batch_bboxes_ignore_mask = torch.IntTensor([[1, 1, 1, 1]])
     assign_result = self.assign(
-        batch_bboxes, batch_gt_bboxes,
+        batch_bboxes,
+        batch_gt_bboxes,
         batch_gt_labels=batch_gt_labels,
         batch_bboxes_ignore_mask=batch_bboxes_ignore_mask)
     expected_batch_pos_mask = torch.IntTensor([1, 0, 1, 0])
@@ -699,8 +692,8 @@ def test_ascend_max_iou_assigner():
 
     assert torch.all(assign_result.batch_pos_mask == expected_batch_pos_mask)
     assert torch.all(
-        assign_result.batch_anchor_gt_indes * assign_result.batch_pos_mask
-        == expected_batch_anchor_gt_indes)
+        assign_result.batch_anchor_gt_indes *
+        assign_result.batch_pos_mask == expected_batch_anchor_gt_indes)
     assert torch.all(
-        assign_result.batch_anchor_gt_labels * assign_result.batch_pos_mask ==
-        expected_batch_anchor_gt_labels)
+        assign_result.batch_anchor_gt_labels *
+        assign_result.batch_pos_mask == expected_batch_anchor_gt_labels)
