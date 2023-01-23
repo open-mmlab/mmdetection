72e8bb38de4ca65fa7cf18aed6843ff4c63c848f
tjdhg456 <hogili89@gist.ac.kr>
2023-01-23 07:17:48 +0000

    --

--- scripts/ablation.sh
+++ scripts/ablation.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 # Teacher
-MODEL_NAME=faster_rcnn_r50_c4_1x
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
-                                    --nproc_per_node=4 \
-                                    --master_port 905 \
-                                    tools/train.py \
-                                    --config configs/faster_rcnn/coco_$MODEL_NAME.py \
-                                    --seed 0 \
-                                    --work-dir result/coco/$MODEL_NAME \
-                                    --launcher pytorch
+# MODEL_NAME=faster_rcnn_r50_c4_1x
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+#                                     --nproc_per_node=4 \
+#                                     --master_port 905 \
+#                                     tools/train.py \
+#                                     --config configs/faster_rcnn/coco_$MODEL_NAME.py \
+#                                     --seed 0 \
+#                                     --work-dir result/coco/$MODEL_NAME \
+#                                     --launcher pytorch
 
 
 # Ablation
 MODEL_NAME=faster_rcnn_r50_c4_1x_fskd
 for RATIO in 0.5
 do
-    for MIN_LR in 0.3 0.4 0.5
+    for MIN_LR in 0.3 0.4
     do
         CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                     --nproc_per_node=4 \
