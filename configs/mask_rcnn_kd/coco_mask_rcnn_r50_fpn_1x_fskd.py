5557076d7b3b9c3a06d564550796d6d90d32b1db
RhoHeeSeon <rhogmltjs@naver.com>
2023-01-26 23:05:33 +0900

    --

--- configs/mask_rcnn_kd/coco_mask_rcnn_r50_fpn_1x_fskd.py
+++ configs/mask_rcnn_kd/coco_mask_rcnn_r50_fpn_1x_fskd.py
@@ -39,10 +39,7 @@ pre_train_pipeline = [
     dict(type='RandomFlip', flip_ratio=0.5),
 ]
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
