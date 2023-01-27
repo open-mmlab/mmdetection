fb7b01d387cf6c7d65c0a6b2e2f1001fcb8fe34d
RhoHeeSeon <rhogmltjs@naver.com>
2023-01-27 05:18:59 +0000

    feat: add sparse rcnn cfg

--- configs/mask_rcnn_kd/coco_mask_rcnn_r50_fpn_1x_fskd.py
+++ configs/mask_rcnn_kd/coco_mask_rcnn_r50_fpn_1x_fskd.py
@@ -25,7 +25,7 @@ model = dict(type='MaskRCNN_TS',
 
 
 # Distillation Params
-teacher_config_path = 'result/coco/mask_rcnn_r50_fpn_1x/'
+teacher_config_path = 'result/coco/mask_rcnn_r50_fpn_1x/coco_mask_rcnn_r50_fpn_1x.py'
 teacher_weight_path = 'result/coco/mask_rcnn_r50_fpn_1x/epoch_12.pth'
 backbone_pretrain = False
 
--- tools/hs_train.sh
+++ tools/hs_train.sh
@@ -1,4 +1,4 @@
-#--------------------------------   FASTER RCNN TEACHER   --------------------------------#
+# #--------------------------------   FASTER RCNN TEACHER   --------------------------------#
 
 # MODEL_NAME='coco_faster_rcnn_r50_dc5_1x'
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch \
@@ -67,7 +67,7 @@
 #                                 --launcher pytorch
 
 
-#--------------------------------   MASK RCNN TEACHER   --------------------------------#
+# #--------------------------------   MASK RCNN TEACHER   --------------------------------#
 
 # MODEL_NAME='coco_mask_rcnn_r101_fpn_1x'
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch \
@@ -93,8 +93,19 @@
 
 #--------------------------------   MASK RCNN STUDENT   --------------------------------#
 
+MODEL_NAME='mask_rcnn_r50_fpn_1x_fskd'
+CUDA_VISIBLE_DEVICES=2,3,4,5 python -m torch.distributed.launch \
+                                --nproc_per_node=4 \
+                                --master_port 1027 \
+                                train.py \
+                                --config configs/mask_rcnn_kd/coco_${MODEL_NAME}.py \
+                                --seed 0 \
+                                --work-dir result/coco/${MODEL_NAME} \
+                                --launcher pytorch
+
+
 MODEL_NAME='mask_rcnn_r101_fpn_1x_fskd'
-CUDA_VISIBLE_DEVICES=3,4,5,6 python -m torch.distributed.launch \
+CUDA_VISIBLE_DEVICES=2,3,4,5 python -m torch.distributed.launch \
                                 --nproc_per_node=4 \
                                 --master_port 1028 \
                                 train.py \
@@ -104,13 +115,25 @@ CUDA_VISIBLE_DEVICES=3,4,5,6 python -m torch.distributed.launch \
                                 --launcher pytorch
 
 
-# MODEL_NAME='coco_mask_rcnn_r50_fpn_1x_fskd'
-# CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch \
-#                                 --nproc_per_node=4 \
-#                                 --master_port 1027 \
-#                                 train.py \
-#                                 --config configs/mask_rcnn/${MODEL_NAME}.py \
-#                                 --seed 0 \
-#                                 --work-dir result/coco/${MODEL_NAME} \
-#                                 --launcher pytorch
+#--------------------------------   SPARSE RCNN TEACHER   --------------------------------#
 
+MODEL_NAME='sparse_rcnn_r50_fpn_300_proposals_crop_mstrain_480-800_3x'
+CUDA_VISIBLE_DEVICES=2,3,4,5 python -m torch.distributed.launch \
+                                --nproc_per_node=4 \
+                                --master_port 1027 \
+                                train.py \
+                                --config configs/sparse_rcnn/coco_${MODEL_NAME}.py \
+                                --seed 0 \
+                                --work-dir result/coco/${MODEL_NAME} \
+                                --launcher pytorch
+
+
+MODEL_NAME='sparse_rcnn_r101_fpn_300_proposals_crop_mstrain_480-800_3x'
+CUDA_VISIBLE_DEVICES=2,3,4,5 python -m torch.distributed.launch \
+                                --nproc_per_node=4 \
+                                --master_port 1027 \
+                                train.py \
+                                --config configs/sparse_rcnn/coco_${MODEL_NAME}.py \
+                                --seed 0 \
+                                --work-dir result/coco/${MODEL_NAME} \
+                                --launcher pytorch
