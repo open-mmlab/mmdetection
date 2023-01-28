3acc46bb346165205a72356e6bfb598b1073b6b6
RangiLyu <lyuchqi@gmail.com>
2023-01-28 12:31:33 +0800

    [Projects] Support Detic inference. (#9645)

--- mmdet/models/roi_heads/bbox_heads/bbox_head.py
+++ mmdet/models/roi_heads/bbox_heads/bbox_head.py
@@ -87,8 +87,9 @@ class BBoxHead(BaseModule):
             out_dim_reg = box_dim if reg_class_agnostic else \
                 box_dim * num_classes
             reg_predictor_cfg_ = self.reg_predictor_cfg.copy()
-            reg_predictor_cfg_.update(
-                in_features=in_channels, out_features=out_dim_reg)
+            if isinstance(reg_predictor_cfg_, (dict, ConfigDict)):
+                reg_predictor_cfg_.update(
+                    in_features=in_channels, out_features=out_dim_reg)
             self.fc_reg = MODELS.build(reg_predictor_cfg_)
         self.debug_imgs = None
         if init_cfg is None:
