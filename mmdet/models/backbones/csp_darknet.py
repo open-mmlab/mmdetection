c55cb1e1a27bac04e3b54bb8ad1953fa6c5d3dc1
zylo117 <zylo117@users.noreply.github.com>
2023-01-29 15:48:41 +0800

    Update mmdet/models/backbones/csp_darknet.py
    Co-authored-by: Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>

--- mmdet/models/backbones/csp_darknet.py
+++ mmdet/models/backbones/csp_darknet.py
@@ -115,10 +115,7 @@ class SPPBottleneck(BaseModule):
 
     def forward(self, x):
         x = self.conv1(x)
-        if x.dtype == torch.float16:
-            x = x.float()
-            x = torch.cat([x] + [pooling(x) for pooling in self.poolings], dim=1).half()
-        else:
+        with torch.autocast(enabled=False):
             x = torch.cat([x] + [pooling(x) for pooling in self.poolings], dim=1)
         x = self.conv2(x)
         return x
