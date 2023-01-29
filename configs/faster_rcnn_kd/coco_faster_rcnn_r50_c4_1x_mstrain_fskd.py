a8b3b446af7a3e6d19612245e8fb4e4a2c8cc1ae
tjdhg456 <hogili89@gist.ac.kr>
2023-01-29 11:06:35 +0000

    feat: mstrain fskd settings

---
+++ configs/faster_rcnn_kd/coco_faster_rcnn_r50_c4_1x_mstrain_fskd.py
@@ -0,0 +1,66 @@
+_base_ = '../faster_rcnn/coco_faster_rcnn_r50_c4_1x_mstrain.py'
+
+# model
+model = dict(type='FasterRCNN_TS',
+             roi_head=dict(
+                 type='ContRoIHead'
+                ),
+             distill_type='positive' # positive or both
+            )
+
+# Distillation Params
+teacher_config_path = 'result/coco/faster_rcnn_r50_c4_1x_mstrain/coco_faster_rcnn_r50_c4_1x_mstrain.py'
+teacher_weight_path = 'result/coco/faster_rcnn_r50_c4_1x_mstrain/epoch_12.pth'
+backbone_pretrain = False
+
+# use caffe img_norm
+img_norm_cfg = dict(
+    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+
+pre_train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+]
+
+train_pipeline = [
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(type="CocoContDataset",
+               pipeline=train_pipeline,
+               pre_pipeline=pre_train_pipeline,
+               multiscale_mode_student='range', # range
+               ratio_hr_lr_student=0.5,
+               min_lr_student=0.6),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
--- scripts/train_student_coco.sh
+++ scripts/train_student_coco.sh
@@ -33,3 +33,12 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                     --launcher pytorch
 
 
+MODEL_NAME=faster_rcnn_r50_c4_1x_mstrain_fskd
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+                                    --nproc_per_node=4 \
+                                    --master_port 815 \
+                                    train.py \
+                                    --config configs/faster_rcnn_kd/coco_$MODEL_NAME.py \
+                                    --seed 0 \
+                                    --work-dir result/coco/$MODEL_NAME \
+                                    --launcher pytorch
