fix amp pooling overflow

mmdet/models/backbones/csp_darknet.py
@@ -115,7 +115,11 @@ class SPPBottleneck(BaseModule):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = torch.cat([x] + [pooling(x) for pooling in self.poolings], dim=1)
+        if x.dtype == torch.float16:
+            x = x.float()
+            x = torch.cat([x] + [pooling(x) for pooling in self.poolings], dim=1).half()
+        else:
+            x = torch.cat([x] + [pooling(x) for pooling in self.poolings], dim=1)
         x = self.conv2(x)
         return x
 

mmdet/models/layers/se_layer.py
@@ -152,7 +152,10 @@ class ChannelAttention(BaseModule):
 
     def forward(self, x: Tensor) -> Tensor:
         """Forward function for ChannelAttention."""
-        out = self.global_avgpool(x)
+        if x.dtype == torch.float16:
+            out = self.global_avgpool(x.float()).half()
+        else:
+            out = self.global_avgpool(x)
         out = self.fc(out)
         out = self.act(out)
         return x * out