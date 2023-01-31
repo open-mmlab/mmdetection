985966c65c4a98e17b77cdd17d591a8ced232d28
RhoHeeSeon <rhogmltjs@naver.com>
2023-01-31 07:59:07 +0000

    update for debug sparse rcnn kd

--- msdet/__init__.py
+++ msdet/__init__.py
@@ -4,6 +4,7 @@ from .coco import CocoContDataset
 from .smdp import SmdpDataset, SmdpContDataset
 from .faster_rcnn import FasterRCNN_TS, FasterRCNNCont, FasterRCNN_RPN
 from .mask_rcnn import MaskRCNN_TS, MaskRCNNCont
+from .sparse_rcnn import SparseRCNN_TS, SparseRCNNCont
 from .fcos import FCOS_Cont, FCOS_TS
 from .fcos_heads import FCOSHead_Cont
 from .transforms import Resize_Student
@@ -13,5 +14,6 @@ __all__ = [
     'CocoContDataset', 'SmdpDataset', 'SmdpContDataset', 
     'FasterRCNN_TS', 'FasterRCNNCont', 'FasterRCNN_RPN',
     'MaskRCNN_TS', 'MaskRCNNCont',
+    'SparseRCNN_TS', 'SparseRCNNCont',
     'FCOS_Cont', 'FCOS_TS', 'FCOSHead_Cont', 'Resize_Student', 
 ]
