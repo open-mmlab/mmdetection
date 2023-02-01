0dc7fe706ff67b735039413906366e3dda22ba48
huanghaian <huanghaian@sensetime.com>
2023-02-01 18:13:56 +0800

    update

--- .dev_scripts/benchmark_options.py
+++ .dev_scripts/benchmark_options.py
@@ -1,8 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
 third_part_libs = [
-    'pip install -r ../requirements/albu.txt', 'pip install instaboostfast',
-    'pip install git+https://github.com/cocodataset/panopticapi.git'
+    'pip install -r ../requirements/albu.txt',
+    'pip install instaboostfast',
+    'pip install git+https://github.com/cocodataset/panopticapi.git',
+    'pip install timm',
+    'pip install mmcls>=1.0.0rc0',
 ]
 
 default_floating_range = 0.5
