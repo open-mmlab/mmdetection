2a509ccdefda5e7965cfa7d0e8a25f67d772b6ef
zytx121 <592267829@qq.com>
2023-02-08 19:13:47 +0800

    move tta config

--- configs/centernet/centernet_r18-dcnv2_8xb16-crop512-140e_coco.py
+++ configs/centernet/centernet_r18-dcnv2_8xb16-crop512-140e_coco.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py',
-    '../_base_/tta/centernet_tta.py'
+    './centernet_tta.py'
 ]
 
 dataset_type = 'CocoDataset'
--- configs/yolox/yolox_s_8xb8-300e_coco.py
+++ configs/yolox/yolox_s_8xb8-300e_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py',
-    '../_base_/tta/yolox_tta.py'
+    './yolox_tta.py'
 ]
 
 img_scale = (640, 640)  # width, height
