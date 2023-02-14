99e02c7aca89e42b3ce55a8d0eced08bc68f78f1
Yue Zhou <592267829@qq.com>
2023-02-14 15:03:24 +0800

    [Feature] Support test-time augmentation for one-stage models  (#9452)
    Co-authored-by: RangiLyu <lyuchqi@gmail.com>
    Co-authored-by: Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>

--- configs/rtmdet/rtmdet_l_8xb32-300e_coco.py
+++ configs/rtmdet/rtmdet_l_8xb32-300e_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_1x.py',
-    '../_base_/datasets/coco_detection.py'
+    '../_base_/datasets/coco_detection.py', './rtmdet_tta.py'
 ]
 model = dict(
     type='RTMDet',
