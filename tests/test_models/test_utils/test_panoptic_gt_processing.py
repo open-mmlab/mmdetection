cece85e5cfb159236831afe1572561d341ed1eb2
luochunhua <luochunhua1996@outlook.com>
2023-02-18 21:05:37 +0800

    update ut

--- tests/test_models/test_utils/test_panoptic_gt_processing.py
+++ tests/test_models/test_utils/test_panoptic_gt_processing.py
@@ -56,7 +56,8 @@ def test_preprocess_panoptic_gt():
     assert out[2] is None and out[3] is None
 
     # empty stuff
-    gt_semantic_seg = torch.full((1, ) + img_metas['batch_input_shape'], 255)
+    gt_semantic_seg = torch.full(
+        (1, ) + img_metas['batch_input_shape'], 255, dtype=int)
     gt_semantic_seg[0, :5, :5] = 0
     gt_semantic_seg[0, 5:] = 1
 
