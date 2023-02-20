f2b944cfccee3bc1be32ebdee9983e62fd5d4c42
huanghaian <huanghaian@sensetime.com>
2023-02-20 16:19:15 +0800

    update

--- .dev_scripts/benchmark_options.py
+++ .dev_scripts/benchmark_options.py
@@ -6,6 +6,7 @@ third_part_libs = [
     'pip install git+https://github.com/cocodataset/panopticapi.git',
     'pip install timm',
     'pip install mmcls>=1.0.0rc0',
+    'pip install git+https://github.com/lvis-dataset/lvis-api.git',
 ]
 
 default_floating_range = 0.5
