52a5c6d6dca4fa002cbceec6b19280bd94b2dbbe
lym <letusgo126@126.com>
2023-02-20 22:05:23 +0800

    fix config

--- projects/Group_detr/group_detr/__init__.py
+++ projects/Group_detr/group_detr/__init__.py
@@ -1,5 +1,5 @@
 from .group_assigner import GroupHungarianAssigner
-from .group_conditional_attention import GroupAttention
+from .group_conditional_attention import GroupConditionalAttention
 from .group_conditional_detr_decoder import (GroupConditionalDetrTransformerDecoder,
                                              GroupConditionalDetrTransformerDecoderLayer)
 from .group_conditional_detr import GroupConditionalDETR
