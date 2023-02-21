77b8eae66bea9f8a4baf673a6787def98f9dff80
RhoHeeSeon <rhogmltjs@naver.com>
2023-02-21 07:24:02 +0000

    update

--- scripts/hs_train.sh
+++ scripts/hs_train.sh
@@ -44,7 +44,7 @@
 # MODEL_NAME='sparse_rcnn_r101_fpn_300_proposals_crop_mstrain_480-800_3x'
 # MODEL_NAME='sparse_rcnn_r101_fpn_300_proposals_crop_mstrain_480-800_3x'
 # MODEL_NAME='sparse_rcnn_r50_fpn_mstrain_480-800_3x'
-MODEL_NAME='sparse_rcnn_r50_fpn_mstrain_480-800_3x_fskd'
+MODEL_NAME='sparse_rcnn_r101_fpn_mstrain_480-800_3x_fskd'
 FOLDER_NAME='sparse_rcnn_kd'
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                 --nproc_per_node=4 \
