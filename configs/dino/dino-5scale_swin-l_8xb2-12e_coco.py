4c97e7e96f9036274c36a8840c663211e031c3ea
okotaku <to78314910@gmail.com>
2023-03-04 03:55:57 +0000

    fix base

--- configs/dino/dino-5scale_swin-l_8xb2-12e_coco.py
+++ configs/dino/dino-5scale_swin-l_8xb2-12e_coco.py
@@ -28,4 +28,4 @@ model = dict(
         init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
     neck=dict(in_channels=[192, 384, 768, 1536], num_outs=num_levels),
     encoder=dict(layer_cfg=dict(self_attn_cfg=dict(num_levels=num_levels))),
-    decoder=dict(layer_cfg=dict(cross_attn_cfg=dict(num_levels=num_levels), )))
+    decoder=dict(layer_cfg=dict(cross_attn_cfg=dict(num_levels=num_levels))))
