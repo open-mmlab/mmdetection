bfb62b9a265181edb9384e1860f4dab4419dcd28
RhoHeeSeon <rhogmltjs@naver.com>
2023-03-06 07:19:13 +0000

    update: test script

--- scripts/test_coco.sh
+++ scripts/test_coco.sh
@@ -1,19 +1,26 @@
 
-FOLDER_NAME='sparse_rcnn_kd'
-MODEL_NAME='sparse_rcnn_r50_fpn_mstrain_480-800_3x_fskd'
-
-CONFIG_FILE='configs/'${FOLDER_NAME}'/coco_'${MODEL_NAME}'.py'
-CHECKPOINT_FILE='result/coco/'${MODEL_NAME}'/epoch_36.pth'
+FOLDER_NAMES=('mask_rcnn_kd' 'mask_rcnn_kd'  'sparse_rcnn_kd'  'sparse_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd'  'faster_rcnn_kd')
+MODEL_NAMES=('mask_rcnn_r101_fpn_1x_fskd'  'mask_rcnn_r50_fpn_1x_fskd'  'sparse_rcnn_r101_fpn_mstrain_480-800_3x_fskd'  'sparse_rcnn_r50_fpn_mstrain_480-800_3x_fskd'  'faster_rcnn_r50_fpn_3x_fskd'  'faster_rcnn_r101_fpn_1x_fskd'  'faster_rcnn_r101_fpn_2x_fskd'  'faster_rcnn_r101_fpn_3x_fskd'  'faster_rcnn_x101_32x4d_fpn_1x_fskd'  'faster_rcnn_x101_32x4d_fpn_2x_fskd'  'faster_rcnn_x101_32x4d_fpn_3x_fskd'  'faster_rcnn_x101_64x4d_fpn_1x_fskd'  'faster_rcnn_x101_64x4d_fpn_2x_fskd'  'faster_rcnn_x101_64x4d_fpn_3x_fskd'  'faster_rcnn_r50_dc5_1x_fskd'  'faster_rcnn_r50_fpn_1x_fskd'  'faster_rcnn_r50_fpn_2x_fskd')
+EPOCH_NUMBERS=('12'  '12'  '36'  '36'  '36'  '12'  '24'  '36'  '12'  '24'  '24'  '12'  '24'  '36'  '12'  '12'  '24')
 
 
-# test with single gpu
-CUDA_VISIBLE_DEVICES=5 python test.py \
-    ${CONFIG_FILE} \
-    ${CHECKPOINT_FILE} \
-    --format-only \
-    --cfg-options data.test.ann_file=/SSDb/sung/dataset/coco/annotations/image_info_test-dev2017.json data.test.img_prefix=/SSDb/sung/dataset/coco/test2017 \
-    --eval-options jsonfile_prefix=result/coco_test/${MODEL_NAME}
-    # --eval-options jsonfile_prefix=${WORK_DIR}/results
+for (( i = 0 ; i < ${#FOLDER_NAMES[@]} ; i++ )) ; do
+    FOLDER_NAME=${FOLDER_NAMES[$i]}
+    MODEL_NAME=${MODEL_NAMES[$i]}
+    EPOCH_NUMBER=${EPOCH_NUMBERS[$i]}
+
+    CONFIG_FILE='configs/'${FOLDER_NAME}'/coco_'${MODEL_NAME}'.py'
+    CHECKPOINT_FILE='/ailab_mat/checkpoints/sung/msdet/coco_student/'${MODEL_NAME}'/epoch_'${EPOCH_NUMBER}'.pth'
+
+    # test with single gpu
+    CUDA_VISIBLE_DEVICES=4 python test.py \
+        ${CONFIG_FILE} \
+        ${CHECKPOINT_FILE} \
+        --format-only \
+        --cfg-options data.test.ann_file=/SSDb/sung/dataset/coco/annotations/image_info_test-dev2017.json data.test.img_prefix=/SSDb/sung/dataset/coco/test2017 \
+        --eval-options jsonfile_prefix=/ailab_mat/checkpoints/sung/msdet/test-dev/${MODEL_NAME}
+done
+
 
 
 # # test with four gpus
---
+++ test_path.py
@@ -0,0 +1,29 @@
+import os
+
+
+models = os.listdir('/ailab_mat/checkpoints/sung/msdet/coco_student')
+MODEL_NAME = []
+FOLDER_NAME = []
+EPOCH_NUMBER = []
+
+for m in models:
+    model = m.split('_')[0] + '_' + m.split('_')[1]
+    f = model + '_kd'
+    if '1x' in m:
+        e = str(12)
+    elif '2x' in m:
+        e = str(24)
+    elif '3x' in m:
+        e = str(36)
+    else:
+        print('error: epoch')
+    MODEL_NAME.append(m)
+    FOLDER_NAME.append(f)
+    EPOCH_NUMBER.append(e)
+    print(m, '\t\t', f,'\t\t', e)
+print('########')
+print(MODEL_NAME)
+print(FOLDER_NAME)
+print(EPOCH_NUMBER)
+
+print(len(MODEL_NAME))
