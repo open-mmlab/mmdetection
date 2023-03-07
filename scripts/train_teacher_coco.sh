d703ec0f5cd918400a7d3a8dd0b5f71ec723ac00
tjdhg456 <hogili89@gist.ac.kr>
2023-03-07 04:26:43 +0000

    feat: data folder

--- scripts/train_teacher_coco.sh
+++ scripts/train_teacher_coco.sh
@@ -34,7 +34,8 @@
 
 
 for MODEL_NAME in faster_rcnn_r50_dc5_1x_mstrain faster_rcnn_r50_fpn_1x_mstrain faster_rcnn_r50_fpn_2x_mstrain faster_rcnn_r101_fpn_1x_mstrain faster_rcnn_r101_fpn_2x_mstrain faster_rcnn_x101_32x4d_fpn_1x_mstrain faster_rcnn_x101_32x4d_fpn_2x_mstrain faster_rcnn_x101_64x4d_fpn_1x_mstrain faster_rcnn_x101_64x4d_fpn_2x_mstrain
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+do
+    CUDA_VISIBLE_DEVICES=1,5,6,7 python -m torch.distributed.launch \
                                         --nproc_per_node=4 \
                                         --master_port=911 \
                                         train.py \
