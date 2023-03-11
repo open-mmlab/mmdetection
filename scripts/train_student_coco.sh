8e801c89bd7eed06cf3f162bf3b1ff08579d15be
tjdhg456 <hogili89@gist.ac.kr>
2023-03-11 02:18:47 +0000

    --

--- scripts/train_student_coco.sh
+++ scripts/train_student_coco.sh
@@ -71,3 +71,16 @@ do
                                         --work-dir result/coco/$MODEL_NAME \
                                         --launcher pytorch
 done
+
+
+for MODEL_NAME in faster_rcnn_x101_64x4d_fpn_3x_mstrain_fskd
+do
+    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+                                        --nproc_per_node=4 \
+                                        --master_port=899 \
+                                        train.py \
+                                        --config configs/faster_rcnn_kd/coco_$MODEL_NAME.py \
+                                        --seed 0 \
+                                        --work-dir result/coco/$MODEL_NAME \
+                                        --launcher pytorch
+done
