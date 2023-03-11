8112d7c21101cd1c9ab2ca3a2662fdd66879e75e
tjdhg456 <hogili89@gist.ac.kr>
2023-03-11 04:07:16 +0000

    --

--- scripts/train_msaug.sh
+++ scripts/train_msaug.sh
@@ -1,23 +1,10 @@
 # !/usr/bin/env bash
 
-for MODEL_NAME in faster_rcnn_r50_c4_1x_mstrain_aug
+for MODEL_NAME in faster_rcnn_r50_c4_1x_mstrain_aug faster_rcnn_r50_fpn_3x_mstrain_aug
 do
     CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                         --nproc_per_node=4 \
                                         --master_port=899 \
-                                        train.py \
-                                        --config configs/faster_rcnn_msaug/coco_$MODEL_NAME.py \
-                                        --seed 0 \
-                                        --work-dir result/coco/$MODEL_NAME \
-                                        --launcher pytorch
-done
-
-
-for MODEL_NAME in faster_rcnn_r50_fpn_3x_mstrain_aug
-do
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
-                                        --nproc_per_node=4 \
-                                        --master_port=909 \
                                         train.py \
                                         --config configs/faster_rcnn_msaug/coco_$MODEL_NAME.py \
                                         --seed 0 \
