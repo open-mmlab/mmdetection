d55a46145957de57cd8b951a550be4a5b6f9de52
tjdhg456 <hogili89@gist.ac.kr>
2023-03-11 07:19:51 +0000

    --

--- scripts/train_student_coco.sh
+++ scripts/train_student_coco.sh
@@ -15,7 +15,7 @@ done
 
 
 
-for MODEL_NAME in faster_rcnn_r101_fpn_3x_mstrain_fskd faster_rcnn_r101_fpn_1x_mstrain_fskd faster_rcnn_r101_fpn_2x_mstrain_fskd
+for MODEL_NAME in faster_rcnn_r101_fpn_3x_mstrain_fskd faster_rcnn_r101_fpn_1x_mstrain_fskd
 do
     CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                         --nproc_per_node=4 \
