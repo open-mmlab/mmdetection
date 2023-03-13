4ee69927b47d3d83219c14cddfeadd567bd06d69
fwenguang <95677191+fwenguang@users.noreply.github.com>
2023-03-13 21:22:29 +0800

    [Feature] support multi test for mlu (#9915)

--- mmdet/apis/test.py
+++ mmdet/apis/test.py
@@ -12,6 +12,7 @@ from mmcv.image import tensor2imgs
 from mmcv.runner import get_dist_info
 
 from mmdet.core import encode_mask_results
+from mmdet.utils import get_device
 
 
 def single_gpu_test(model,
@@ -139,6 +140,7 @@ def multi_gpu_test(model, data_loader, tmpdir=None, gpu_collect=False):
 
 def collect_results_cpu(result_part, size, tmpdir=None):
     rank, world_size = get_dist_info()
+    default_device = get_device()
     # create a tmp dir if it is not specified
     if tmpdir is None:
         MAX_LEN = 512
@@ -146,12 +148,14 @@ def collect_results_cpu(result_part, size, tmpdir=None):
         dir_tensor = torch.full((MAX_LEN, ),
                                 32,
                                 dtype=torch.uint8,
-                                device='cuda')
+                                device=default_device)
         if rank == 0:
             mmcv.mkdir_or_exist('.dist_test')
             tmpdir = tempfile.mkdtemp(dir='.dist_test')
             tmpdir = torch.tensor(
-                bytearray(tmpdir.encode()), dtype=torch.uint8, device='cuda')
+                bytearray(tmpdir.encode()),
+                dtype=torch.uint8,
+                device=default_device)
             dir_tensor[:len(tmpdir)] = tmpdir
         dist.broadcast(dir_tensor, 0)
         tmpdir = dir_tensor.cpu().numpy().tobytes().decode().rstrip()
@@ -182,16 +186,20 @@ def collect_results_cpu(result_part, size, tmpdir=None):
 
 def collect_results_gpu(result_part, size):
     rank, world_size = get_dist_info()
+    default_device = get_device()
     # dump result part to tensor with pickle
     part_tensor = torch.tensor(
-        bytearray(pickle.dumps(result_part)), dtype=torch.uint8, device='cuda')
+        bytearray(pickle.dumps(result_part)),
+        dtype=torch.uint8,
+        device=default_device)
     # gather all result part tensor shape
-    shape_tensor = torch.tensor(part_tensor.shape, device='cuda')
+    shape_tensor = torch.tensor(part_tensor.shape, device=default_device)
     shape_list = [shape_tensor.clone() for _ in range(world_size)]
     dist.all_gather(shape_list, shape_tensor)
     # padding result part tensor to max length
     shape_max = torch.tensor(shape_list).max()
-    part_send = torch.zeros(shape_max, dtype=torch.uint8, device='cuda')
+    part_send = torch.zeros(
+        shape_max, dtype=torch.uint8, device=default_device)
     part_send[:shape_tensor[0]] = part_tensor
     part_recv_list = [
         part_tensor.new_zeros(shape_max) for _ in range(world_size)
