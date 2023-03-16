cadb75f612c8271239c873ef88c9b6955cd770d8
Jingwei Zhang <zjw18@mails.tsinghua.edu.cn>
2023-03-16 11:44:03 +0800

    [Feature] Add common tracking transforms (#9906)

--- mmdet/datasets/transforms/__init__.py
+++ mmdet/datasets/transforms/__init__.py
@@ -3,14 +3,16 @@ from .augment_wrappers import AutoAugment, RandAugment
 from .colorspace import (AutoContrast, Brightness, Color, ColorTransform,
                          Contrast, Equalize, Invert, Posterize, Sharpness,
                          Solarize, SolarizeAdd)
-from .formatting import ImageToTensor, PackDetInputs, ToTensor, Transpose
+from .formatting import (ImageToTensor, PackDetInputs, PackTrackInputs,
+                         ToTensor, Transpose)
+from .frame_sampling import UniformSample
 from .geometric import (GeomTransform, Rotate, ShearX, ShearY, TranslateX,
                         TranslateY)
 from .instaboost import InstaBoost
 from .loading import (FilterAnnotations, InferencerLoader, LoadAnnotations,
                       LoadEmptyAnnotations, LoadImageFromNDArray,
                       LoadMultiChannelImageFromFiles, LoadPanopticAnnotations,
-                      LoadProposals)
+                      LoadProposals, LoadTrackAnnotations)
 from .transforms import (Albu, CachedMixUp, CachedMosaic, CopyPaste, CutOut,
                          Expand, FixShapeResize, MinIoURandomCrop, MixUp,
                          Mosaic, Pad, PhotoMetricDistortion, RandomAffine,
@@ -32,5 +34,6 @@ __all__ = [
     'RandAugment', 'Sharpness', 'Solarize', 'SolarizeAdd', 'Posterize',
     'AutoContrast', 'Invert', 'MultiBranch', 'RandomErasing',
     'LoadEmptyAnnotations', 'RandomOrder', 'CachedMosaic', 'CachedMixUp',
-    'FixShapeResize', 'ProposalBroadcaster', 'InferencerLoader'
+    'FixShapeResize', 'ProposalBroadcaster', 'InferencerLoader',
+    'LoadTrackAnnotations', 'UniformSample', 'PackTrackInputs'
 ]
