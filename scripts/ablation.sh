eb5e223bc4d58a30e1c691f85d97b13059e94c5f
tjdhg456 <hogili89@gist.ac.kr>
2023-03-22 00:37:04 +0000

    --

--- scripts/ablation.sh
+++ scripts/ablation.sh
@@ -16,37 +16,49 @@ done
 
 
 MODEL_NAME=faster_rcnn_r101_fpn_1x_mstrain_fskd
-for BACKBONE in 0.5 1.0 1.5 2.0
+for BACKBONE in 1.5 2.0
 do
     for RPN in 0.5 1.0
     do
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
-                                --nproc_per_node=4 \
-                                --master_port 799 \
-                                train.py \
-                                --config configs/faster_rcnn_kd/coco_$MODEL_NAME.py \
-                                --seed 0 \
-                                    --work-dir result/test/coco/$MODEL_NAME-backbone_$BACKBONE-rpn_$RPN \
-                                    --launcher pytorch \
-                                    --cfg-options model.distill_param=$RPN model.distill_param_backbone=$BACKBONE
+    
+    if [ $BACKBONE = 1.5 ] && [ $RPN = 0.5 ]
+        then
+        echo "a and b are both equal to 0.5. Continuing..."
+        else
+        CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+                                    --nproc_per_node=4 \
+                                    --master_port 799 \
+                                    train.py \
+                                    --config configs/faster_rcnn_kd/coco_$MODEL_NAME.py \
+                                    --seed 0 \
+                                        --work-dir result/test/coco/$MODEL_NAME-backbone_$BACKBONE-rpn_$RPN \
+                                        --launcher pytorch \
+                                        --cfg-options model.distill_param=$RPN model.distill_param_backbone=$BACKBONE
+        fi
     done
 done
 
 
 
 MODEL_NAME=faster_rcnn_r101_fpn_1x_mstrain_fskd
-for BACKBONE in 0.5 1.0 1.5 2.0
+for BACKBONE in 1.5 2.0
 do
     for RPN in 1.5 2.0
     do
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
-                                --nproc_per_node=4 \
-                                --master_port 852 \
-                                train.py \
-                                --config configs/faster_rcnn_kd/coco_$MODEL_NAME.py \
-                                --seed 0 \
-                                    --work-dir result/test/coco/$MODEL_NAME-backbone_$BACKBONE-rpn_$RPN \
-                                    --launcher pytorch \
-                                    --cfg-options model.distill_param=$RPN model.distill_param_backbone=$BACKBONE
+    
+    if [ $BACKBONE = 1.5 ] && [ $RPN = 1.5 ]
+        then
+        echo "a and b are both equal to 0.5. Continuing..."
+        else
+        CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+                                    --nproc_per_node=4 \
+                                    --master_port 799 \
+                                    train.py \
+                                    --config configs/faster_rcnn_kd/coco_$MODEL_NAME.py \
+                                    --seed 0 \
+                                        --work-dir result/test/coco/$MODEL_NAME-backbone_$BACKBONE-rpn_$RPN \
+                                        --launcher pytorch \
+                                        --cfg-options model.distill_param=$RPN model.distill_param_backbone=$BACKBONE
+        fi
     done
 done
