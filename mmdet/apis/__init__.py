d6a012918a47879181f6cedf2f287230bddc0295
zwhus <121282623+zwhus@users.noreply.github.com>
2023-03-23 17:05:46 +0800

    [Feature] Add tracking demo and visulization (#9908)

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .det_inferencer import DetInferencer
 from .inference import (async_inference_detector, inference_detector,
-                        init_detector)
+                        inference_mot, init_detector, init_track_model)
 
 __all__ = [
     'init_detector', 'async_inference_detector', 'inference_detector',
-    'DetInferencer'
+    'DetInferencer', 'inference_mot', 'init_track_model'
 ]
