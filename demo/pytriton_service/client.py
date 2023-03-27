296a014f763a73c9d1827e52fa5ae63277e9abdf
VanderBieu <519865017@qq.com>
2023-03-27 09:24:08 +0800

    specifying inf function of det and instance seg

--- demo/pytriton_service/client.py
+++ demo/pytriton_service/client.py
@@ -4,7 +4,8 @@ import numpy as np
 def inference_service(file):
     input1_data = cv2.imread(file)
     input1_data = np.expand_dims(input1_data, axis=0)
-    with ModelClient("localhost:8000", "Swin") as client:
+    with ModelClient("localhost:8000", "MMDetectionModel") as client:
         result_dict = client.infer_batch(input1_data)
+        print(result_dict)
 
 
