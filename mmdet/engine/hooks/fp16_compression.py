63e6caf8ade6a693177946f8637ba0bb482dec4b
zwhus <1062894314zwh@gmail.com>
2023-03-31 19:55:34 +0800

    vitdet

--- mmdet/engine/hooks/fp16_compression.py
+++ mmdet/engine/hooks/fp16_compression.py
@@ -1,16 +1,17 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmengine.hooks import Hook
-from torch.distributed.algorithms.ddp_comm_hooks import default as comm_hooks
 
 from mmdet.registry import HOOKS
 
 
 @HOOKS.register_module()
 class Fp16Compresssion(Hook):
-    """Set runner's epoch information to the model."""
+    """Support ddp fp16 compression."""
 
     def before_run(self, runner):
         if runner.cfg.get('model_wrapper_cfg') is None:
+            from torch.distributed.algorithms.ddp_comm_hooks import \
+                default as comm_hooks
             model = runner.model
             model.register_comm_hook(
                 state=None, hook=comm_hooks.fp16_compress_hook)
