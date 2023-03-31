5b03f6fd604802bfe461126ab7b007eac5d8aeec
zwhus <1062894314zwh@gmail.com>
2023-03-31 20:17:28 +0800

    support vitdet

--- mmdet/engine/hooks/fp16_compression.py
+++ mmdet/engine/hooks/fp16_compression.py
@@ -12,7 +12,6 @@ class Fp16Compresssion(Hook):
         if runner.cfg.get('model_wrapper_cfg') is None:
             from torch.distributed.algorithms.ddp_comm_hooks import \
                 default as comm_hooks
-            model = runner.model
-            model.register_comm_hook(
+            runner.model.register_comm_hook(
                 state=None, hook=comm_hooks.fp16_compress_hook)
             runner.logger.info('use ddp fp16 compression')
