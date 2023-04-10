3f1ca458a7d0d304d3332c6fb358abf57ffd20fe
HAOCHENYE <21724054@zju.edu.cn>
2023-04-10 15:30:32 +0800

    tmp save

--- tools/train.py
+++ tools/train.py
@@ -44,6 +44,11 @@ def parse_args():
         'Note that the quotation marks are necessary and that no white space '
         'is allowed.')
     parser.add_argument(
+        '--lazy-import',
+        action='store_true',
+        default=False
+    )
+    parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
@@ -67,7 +72,7 @@ def main():
     setup_cache_size_limit_of_dynamo()
 
     # load config
-    cfg = Config.fromfile(args.config)
+    cfg = Config.fromfile(args.config, lazy_import=args.lazy_import)
     cfg.launcher = args.launcher
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
