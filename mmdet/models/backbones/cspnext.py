a4c31fe7a4cd11d56e739b86a7bcc634000c86ec
Austin Welch <austinmw@users.noreply.github.com>
2023-04-13 15:13:38 -0400

    Seems to correct #8208

--- mmdet/models/backbones/cspnext.py
+++ mmdet/models/backbones/cspnext.py
@@ -169,6 +169,8 @@ class CSPNeXt(BaseModule):
             self.add_module(f'stage{i + 1}', nn.Sequential(*stage))
             self.layers.append(f'stage{i + 1}')
 
+        self._freeze_stages()
+
     def _freeze_stages(self) -> None:
         if self.frozen_stages >= 0:
             for i in range(self.frozen_stages + 1):
