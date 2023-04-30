0d8681608647e2acb2c7ae4e70f0f6477c39b652
zwhus <1062894314zwh@gmail.com>
2023-04-30 22:30:35 +0800

    support masktrack_rcnn

--- mmdet/models/trackers/__init__.py
+++ mmdet/models/trackers/__init__.py
@@ -1,6 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base_tracker import BaseTracker
 from .byte_tracker import ByteTracker
+from .masktrack_rcnn_tracker import MaskTrackRCNNTracker
 from .quasi_dense_tracker import QuasiDenseTracker
 
-__all__ = ['BaseTracker', 'ByteTracker', 'QuasiDenseTracker']
+__all__ = [
+    'BaseTracker', 'ByteTracker', 'QuasiDenseTracker', 'MaskTrackRCNNTracker'
+]
