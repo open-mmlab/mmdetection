decc82e216036859c163a1d0ebf4122afc4cf28b
HAOCHENYE <21724054@zju.edu.cn>
2023-05-22 17:36:44 +0800

    replacge merge with update

--- mmdet/configs/retinanet/retinanet_r50_fpn_1x_coco.py
+++ mmdet/configs/retinanet/retinanet_r50_fpn_1x_coco.py
@@ -4,8 +4,9 @@ if '_base_':
     from .._base_.schedules.schedule_1x import *
     from .._base_.default_runtime import *
     from .retinanet_tta import *
+
 from torch.optim.sgd import SGD
 
 # optimizer
-optim_wrapper.merge(
+optim_wrapper.update(
     dict(optimizer=dict(type=SGD, lr=0.01, momentum=0.9, weight_decay=0.0001)))
