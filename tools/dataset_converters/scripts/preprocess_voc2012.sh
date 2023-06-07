cb68dda913539e0b821d03a6faa7622d1518947f
huanghaian <huanghaian@sensetime.com>
2023-06-07 18:14:54 +0800

    update

---
+++ tools/dataset_converters/scripts/preprocess_voc2012.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+DOWNLOAD_DIR=$1
+DATA_ROOT=$2
+
+tar -xvf $DOWNLOAD_DIR/PASCAL_VOC2012/raw/VOCtrainval_11-May-2012.tar -C $DATA_ROOT
+tar -xvf $DOWNLOAD_DIR/PASCAL_VOC2012/raw/VOC2012test.tar -C $DATA_ROOT
+rm -rf $DATA_ROOT/PASCAL_VOC2012
