82f5946d4289905d9e1c58d4745302634acce3f6
huanghaian <huanghaian@sensetime.com>
2023-06-07 18:16:25 +0800

    update

--- tools/dataset_converters/scripts/preprocess_coco2017.sh
+++ tools/dataset_converters/scripts/preprocess_coco2017.sh
@@ -6,7 +6,7 @@ DATA_ROOT=$2
 unzip $DOWNLOAD_DIR/COCO_2017/raw/Images/val2017.zip -d $DATA_ROOT
 unzip $DOWNLOAD_DIR/COCO_2017/raw/Images/train2017.zip -d $DATA_ROOT
 unzip $DOWNLOAD_DIR/COCO_2017/raw/Images/test2017.zip -d $DATA_ROOT/
-unzip $DOWNLOAD_DIR/COCO_2017/raw/Images/unlabeled2017.zip .zip -d $DATA_ROOT
+unzip $DOWNLOAD_DIR/COCO_2017/raw/Images/unlabeled2017.zip -d $DATA_ROOT
 unzip $DOWNLOAD_DIR/COCO_2017/raw/Annotations/stuff_annotations_trainval2017.zip -d $DATA_ROOT/
 unzip $DOWNLOAD_DIR/COCO_2017/raw/Annotations/panoptic_annotations_trainval2017.zip -d $DATA_ROOT/
 unzip $DOWNLOAD_DIR/COCO_2017/raw/Annotations/image_info_unlabeled2017.zip -d $DATA_ROOT/
