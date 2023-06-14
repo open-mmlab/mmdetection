802265d3e60d6240611332dc044bee6455b19f10
hadiyouness10 <hadi.youness@lau.edu>
2023-06-14 11:27:29 +0200

    formatting

--- custom/inference.py
+++ custom/inference.py
@@ -4,6 +4,7 @@ from mmdet.apis import init_detector, inference_detector
 from convert.utils import extract_bounding_boxes
 import os
 import argparse
+
 # Specify the path to model config and checkpoint file
 # config_file = 'configs/custom/my_retinanet_pvt-t_fpn_1x_coco.py'
 # checkpoint_file = 'work_dirs/my_retinanet_pvt-t_fpn_1x_coco/epoch_7.pth'
@@ -18,11 +19,11 @@ import argparse
 # inf_out_dir = 'data/fst/random_select_/fstv2_first'
 
 
-class FPSLogger():
+class FPSLogger:
     def __init__(self):
-        self.tottime = 0.
+        self.tottime = 0.0
         self.count = 0
-        self.last_record = 0.
+        self.last_record = 0.0
         self.last_print = time.time()
         self.interval = 10
 
@@ -34,25 +35,23 @@ class FPSLogger():
         self.count += 1
         self.print_fps()
 
-
     def print_fps(self):
         if time.time() - self.last_print > self.interval:
             print(f"Inference running at {self.count / self.tottime:.3f} FPS")
             self.last_print = time.time()
 
 
-
-
-class Inference():
-    def __init__(self,
-                 config_file,
-                 checkpoint_file,
-                 inf_dir,
-                 inf_out_dir,
-                threshold=0.5,
-                output_file = None,
-                 ):
-        self.model = init_detector(config_file, checkpoint_file, device='cuda:0')
+class Inference:
+    def __init__(
+        self,
+        config_file,
+        checkpoint_file,
+        inf_dir,
+        inf_out_dir,
+        threshold=0.5,
+        output_file=None,
+    ):
+        self.model = init_detector(config_file, checkpoint_file, device="cuda:0")
         os.makedirs(inf_out_dir, exist_ok=True)
         self.inf_dir = inf_dir
         self.inf_out_dir = inf_out_dir
@@ -60,34 +59,46 @@ class Inference():
         self.fps_logger = FPSLogger()
         self.run(threshold)
 
-
-
-    
-    def run(self, threshold = 0.5):
-            results = []
-            for imn in os.listdir(self.inf_dir):
-                img = f'{self.inf_dir}/{imn}'
-                self.fps_logger.start_record()
-                result = inference_detector(self.model, img)
-                results.append({img : extract_bounding_boxes(result, threshold)})
-                self.fps_logger.end_record()
-                if self.inf_out_dir:
-                    self.model.show_result(img, result, out_file=f'{self.inf_out_dir}/{imn}', score_thr=threshold)
-            if self.output_file:
-                with open(self.output_file, 'w') as f:
-                    json.dump(results, f)
-            return results
-
+    def run(self, threshold=0.5):
+        results = []
+        for imn in os.listdir(self.inf_dir):
+            img = f"{self.inf_dir}/{imn}"
+            self.fps_logger.start_record()
+            result = inference_detector(self.model, img)
+            results.append({img: extract_bounding_boxes(result, threshold)})
+            self.fps_logger.end_record()
+            if self.inf_out_dir:
+                self.model.show_result(
+                    img,
+                    result,
+                    out_file=f"{self.inf_out_dir}/{imn}",
+                    score_thr=threshold,
+                )
+        if self.output_file:
+            with open(self.output_file, "w") as f:
+                json.dump(results, f)
+        return results
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run inference on images')
-    parser.add_argument('--config_file', type=str, help='Path to model config file')
-    parser.add_argument('--checkpoint_file', type=str, help='Path to checkpoint file')
-    parser.add_argument('--inf_dir', type=str, help='Directory containing images for inference')
-    parser.add_argument('--inf_out_dir', type=str, help='Directory to save inference results')
-    parser.add_argument('--threshold',default= 0.5, type=float, help='Directory to save inference results')
-    parser.add_argument('--output_file', type=str, help='Directory to save inference results')
+    parser = argparse.ArgumentParser(description="Run inference on images")
+    parser.add_argument("--config_file", type=str, help="Path to model config file")
+    parser.add_argument("--checkpoint_file", type=str, help="Path to checkpoint file")
+    parser.add_argument(
+        "--inf_dir", type=str, help="Directory containing images for inference"
+    )
+    parser.add_argument(
+        "--inf_out_dir", type=str, help="Directory to save inference results"
+    )
+    parser.add_argument(
+        "--threshold",
+        default=0.5,
+        type=float,
+        help="Directory to save inference results",
+    )
+    parser.add_argument(
+        "--output_file", type=str, help="Directory to save inference results"
+    )
 
     args = parser.parse_args()
     Inference(
@@ -95,6 +106,6 @@ if __name__ == "__main__":
         checkpoint_file=args.checkpoint_file,
         inf_dir=args.inf_dir,
         inf_out_dir=args.inf_out_dir,
-        threshold= args.threshold,
-        output_file= args.output_file
+        threshold=args.threshold,
+        output_file=args.output_file,
     )
