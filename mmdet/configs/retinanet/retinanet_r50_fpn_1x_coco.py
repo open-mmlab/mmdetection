eca07d910ce18399e83d11a9314ec01dd883033f
huanghaian <huanghaian@sensetime.com>
2023-06-29 15:24:37 +0800

    update

--- mmdet/configs/retinanet/retinanet_r50_fpn_1x_coco.py
+++ mmdet/configs/retinanet/retinanet_r50_fpn_1x_coco.py
@@ -1,4 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+
+# Please refer to https://mmengine.readthedocs.io/en/latest/advanced_tutorials/config.html#a-pure-python-style-configuration-file-beta for more details. # noqa
+# mmcv >= 2.0.1
+# mmengine >= 0.8.0
+
 from mmengine.config import read_base
 
 with read_base():
