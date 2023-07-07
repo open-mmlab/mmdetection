7eb0b17adcc4d2fbd4fca0578bfea3b510ced58f
Yuhang Cao <yhcao6@gmail.com>
2023-07-07 15:35:14 +0800

    add mp eval for v3det

---
+++ mmdet/datasets/v3det.py
@@ -0,0 +1,27 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import copy
+import os.path as osp
+from typing import List, Union
+
+from mmengine.fileio import get_local_path
+
+from mmdet.registry import DATASETS
+from .api_wrappers import COCO
+from .base_det_dataset import BaseDetDataset
+from .coco import CocoDataset
+
+import mmengine
+
+V3DET_CLASSES = tuple(
+    mmengine.list_from_file(
+        'data/V3Det/annotations/category_name_13216_v3det_2023_v1.txt'))
+
+
+@DATASETS.register_module()
+class V3DetDataset(CocoDataset):
+    """Dataset for COCO."""
+
+    METAINFO = {
+        'classes': V3DET_CLASSES,
+        'palette': None,  # TODO: add palette
+    }
