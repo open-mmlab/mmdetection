75c62043d24b87a95d883ebf3f7d6a0d36893ed6
ZhaoCake <zy9787101052763@126.com>
2023-07-28 19:26:57 +0800

    Put the new config into ./mmdet/configs instead of ./configs

--- configs/qdtrack/qdtrack_faster_rcnn_r50_fpn_8xb2-4e_mot17halftrain_test-mot17halfval.py
+++ configs/qdtrack/qdtrack_faster_rcnn_r50_fpn_8xb2-4e_mot17halftrain_test-mot17halfval.py
@@ -1,15 +1,12 @@
-from mmengine.config import read_base
-
-with read_base():
-    from .._base_.datasets.mot_challenge import *
-    from .qdtrack_faster_rcnn_r50_fpn_4e_base import *
-
-from mmdet.evaluation import CocoVideoMetric, MOTChallengeMetric
+_base_ = [
+    './qdtrack_faster-rcnn_r50_fpn_4e_base.py',
+    '../_base_/datasets/mot_challenge.py',
+]
 
 # evaluator
 val_evaluator = [
-    dict(type=CocoVideoMetric, metric=['bbox'], classwise=True),
-    dict(type=MOTChallengeMetric, metric=['HOTA', 'CLEAR', 'Identity'])
+    dict(type='CocoVideoMetric', metric=['bbox'], classwise=True),
+    dict(type='MOTChallengeMetric', metric=['HOTA', 'CLEAR', 'Identity'])
 ]
 
 test_evaluator = val_evaluator
