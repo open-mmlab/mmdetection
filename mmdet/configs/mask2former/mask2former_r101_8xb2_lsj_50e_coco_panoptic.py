8113d186d3316f2fc93ed6419d230f50b6294e8c
shenvinci <shenvinci@gmail.com>
2023-08-07 18:45:25 +0800

    新增mask2former配置文件

---
+++ mmdet/configs/mask2former/mask2former_r101_8xb2_lsj_50e_coco_panoptic.py
@@ -0,0 +1,10 @@
+from mmengine.config import read_base
+with read_base():
+    from .mask2former_r50_8xb2_lsj_50e_coco_panoptic import *
+
+model.merge(
+    dict(
+        backbone=dict(
+            depth=101,
+            init_cfg=dict(
+                type='Pretrained', checkpoint='torchvision://resnet101'))))
