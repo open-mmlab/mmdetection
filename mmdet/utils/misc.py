60b29b3e0f3e0e97745b58502a3e56029a8a0229
YanxingLiu <42299757+YanxingLiu@users.noreply.github.com>
2023-08-14 14:31:51 +0800

    [CodeCamp2023-500]add large_image_demo (#10719)

--- mmdet/utils/misc.py
+++ mmdet/utils/misc.py
@@ -2,11 +2,17 @@
 import glob
 import os
 import os.path as osp
+import urllib
 import warnings
 from typing import Union
 
+import torch
 from mmengine.config import Config, ConfigDict
 from mmengine.logging import print_log
+from mmengine.utils import scandir
+
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif',
+                  '.tiff', '.webp')
 
 
 def find_latest_checkpoint(path, suffix='pth'):
@@ -103,3 +109,41 @@ def get_test_pipeline_cfg(cfg: Union[str, ConfigDict]) -> ConfigDict:
         raise RuntimeError('Cannot find `pipeline` in `test_dataloader`')
 
     return _get_test_pipeline_cfg(cfg.test_dataloader.dataset)
+
+
+def get_file_list(source_root: str) -> [list, dict]:
+    """Get file list.
+
+    Args:
+        source_root (str): image or video source path
+
+    Return:
+        source_file_path_list (list): A list for all source file.
+        source_type (dict): Source type: file or url or dir.
+    """
+    is_dir = os.path.isdir(source_root)
+    is_url = source_root.startswith(('http:/', 'https:/'))
+    is_file = os.path.splitext(source_root)[-1].lower() in IMG_EXTENSIONS
+
+    source_file_path_list = []
+    if is_dir:
+        # when input source is dir
+        for file in scandir(source_root, IMG_EXTENSIONS, recursive=True):
+            source_file_path_list.append(os.path.join(source_root, file))
+    elif is_url:
+        # when input source is url
+        filename = os.path.basename(
+            urllib.parse.unquote(source_root).split('?')[0])
+        file_save_path = os.path.join(os.getcwd(), filename)
+        print(f'Downloading source file to {file_save_path}')
+        torch.hub.download_url_to_file(source_root, file_save_path)
+        source_file_path_list = [file_save_path]
+    elif is_file:
+        # when input source is single image
+        source_file_path_list = [source_root]
+    else:
+        print('Cannot find image file.')
+
+    source_type = dict(is_dir=is_dir, is_url=is_url, is_file=is_file)
+
+    return source_file_path_list, source_type
