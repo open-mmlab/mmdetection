9ae01897a466623685d7b6e54fc4d96580f4f6db
root <z2855064151@gmail.com>
2023-08-30 14:35:48 +0800

    H-dino

---
+++ mmdet/models/layers/transformer/H_dino_layers.py
@@ -0,0 +1,91 @@
+import warnings
+from typing import Tuple, Union
+
+import torch
+from mmengine.model import BaseModule
+from torch import Tensor, nn
+from mmdet.utils import OptConfigType
+from .dino_layers import CdnQueryGenerator
+
+class H_CdnQueryGenerator(CdnQueryGenerator):
+    def __init__(self,
+                 num_classes: int,
+                 embed_dims: int,
+                 num_matching_queries: int,
+                 label_noise_scale: float = 0.5,
+                 box_noise_scale: float = 1.0,
+                 group_cfg: OptConfigType = None) -> None:
+        super().__init__(num_classes,
+                 embed_dims,
+                 num_matching_queries,
+                 label_noise_scale,
+                 box_noise_scale,
+                 group_cfg)
+        if self.dynamic_dn_groups:
+            self.num_query_one2one = group_cfg.get('num_query_one2one', 300)
+
+    def generate_dn_mask(self, max_num_target: int, num_groups: int,
+                         device: Union[torch.device, str]) -> Tensor:
+        """Generate attention mask to prevent information leakage from
+        different denoising groups and matching parts.
+
+        .. code:: text
+
+                        0 0 0 0 1 1 1 1 0 0 0 0 0
+                        0 0 0 0 1 1 1 1 0 0 0 0 0
+                        0 0 0 0 1 1 1 1 0 0 0 0 0
+                        0 0 0 0 1 1 1 1 0 0 0 0 0
+                        1 1 1 1 0 0 0 0 0 0 0 0 0
+                        1 1 1 1 0 0 0 0 0 0 0 0 0
+                        1 1 1 1 0 0 0 0 0 0 0 0 0
+                        1 1 1 1 0 0 0 0 0 0 0 0 0
+                        1 1 1 1 1 1 1 1 0 0 0 0 0
+                        1 1 1 1 1 1 1 1 0 0 0 0 0
+                        1 1 1 1 1 1 1 1 0 0 0 0 0
+                        1 1 1 1 1 1 1 1 0 0 0 0 0
+                        1 1 1 1 1 1 1 1 0 0 0 0 0
+         max_num_target |_|           |_________| num_matching_queries
+                        |_____________| num_denoising_queries
+
+               1 -> True  (Masked), means 'can not see'.
+               0 -> False (UnMasked), means 'can see'.
+
+        Args:
+            max_num_target (int): The max target number of the input batch
+                samples.
+            num_groups (int): The number of denoising query groups.
+            device (obj:`device` or str): The device of generated mask.
+
+        Returns:
+            Tensor: The attention mask to prevent information leakage from
+            different denoising groups and matching parts, will be used as
+            `self_attn_mask` of the `decoder`, has shape (num_queries_total,
+            num_queries_total), where `num_queries_total` is the sum of
+            `num_denoising_queries` and `num_matching_queries`.
+        """
+        # import pdb;pdb.set_trace()
+        num_denoising_queries = int(max_num_target * 2 * num_groups)
+        num_queries_one2one = num_denoising_queries + self.num_query_one2one
+        num_queries_total = num_denoising_queries + self.num_matching_queries
+        attn_mask = torch.zeros(
+            num_queries_total,
+            num_queries_total,
+            device=device,
+            dtype=torch.bool)
+        # Make the matching part cannot see the denoising groups
+        attn_mask[num_denoising_queries:, :num_denoising_queries] = True
+        attn_mask[:num_queries_one2one, num_queries_one2one:] = True
+        attn_mask[num_queries_one2one:, :num_queries_one2one] = True
+        
+
+        # Make the denoising groups cannot see each other
+        for i in range(num_groups):
+            # Mask rows of one group per step.
+            row_scope = slice(max_num_target * 2 * i,
+                              max_num_target * 2 * (i + 1))
+            left_scope = slice(max_num_target * 2 * i)
+            right_scope = slice(max_num_target * 2 * (i + 1),
+                                num_queries_one2one)
+            attn_mask[row_scope, right_scope] = True
+            attn_mask[row_scope, left_scope] = True
+        return attn_mask
