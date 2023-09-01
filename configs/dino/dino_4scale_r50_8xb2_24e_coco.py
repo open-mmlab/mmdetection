09cb2d93f9eb95eb33075a5d290a6b44db5810a4
Markson-Young <a2246708680@gmail.com>
2023-09-01 09:40:03 +0800

    rename

---
+++ configs/dino/dino_4scale_r50_8xb2_24e_coco.py
@@ -0,0 +1,20 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .dino_4scale_r50_8xb2_12e_coco import *
+
+from mmengine.runner.loops import EpochBasedTrainLoop
+
+max_epochs = 24
+train_cfg.update(
+    dict(
+        type=EpochBasedTrainLoop, max_epochs=max_epochs, val_interval=1))
+param_scheduler = [
+    dict(
+        type=MultiStepLR,
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[20],
+        gamma=0.1)
+]
