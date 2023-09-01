2854c52d5f7c05715e5871aaf89603e4803a2426
Markson-Young <a2246708680@gmail.com>
2023-09-01 20:23:15 +0800

    new config for default runtime

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,12 +1,21 @@
 default_scope = 'mmdet'
 
+from mmengine.hooks.iter_timer_hook import IterTimerHook
+from mmengine.hooks.logger_hook import LoggerHook
+from mmengine.hooks.param_scheduler_hook import ParamSchedulerHook
+from mmengine.hooks.checkpoint_hook import CheckpointHook
+from mmengine.hooks.sampler_seed_hook import DistSamplerSeedHook
+from mmengine.visualization.vis_backend import LocalVisBackend
+from mmdet.engine.hooks.visualization_hook import DetVisualizationHook
+from mmdet.visualization.local_visualizer import DetLocalVisualizer
+
 default_hooks = dict(
-    timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', interval=1),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-    visualization=dict(type='DetVisualizationHook'))
+    timer=dict(type=IterTimerHook),
+    logger=dict(type=LoggerHook, interval=50),
+    param_scheduler=dict(type=ParamSchedulerHook),
+    checkpoint=dict(type=CheckpointHook, interval=1),
+    sampler_seed=dict(type=DistSamplerSeedHook),
+    visualization=dict(type=DetVisualizationHook))
 
 env_cfg = dict(
     cudnn_benchmark=False,
@@ -14,9 +23,9 @@ env_cfg = dict(
     dist_cfg=dict(backend='nccl'),
 )
 
-vis_backends = [dict(type='LocalVisBackend')]
+vis_backends = [dict(type=LocalVisBackend)]
 visualizer = dict(
-    type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
+    type=DetLocalVisualizer, vis_backends=vis_backends, name='visualizer')
 log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
 
 log_level = 'INFO'
