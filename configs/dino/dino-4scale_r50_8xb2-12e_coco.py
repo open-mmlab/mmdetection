f8bffbd8cd46585cfacb4425baa5a87fe90bd2ab
Markson-Young <a2246708680@gmail.com>
2023-09-01 09:34:31 +0800

    new config for dino

--- configs/dino/dino-4scale_r50_8xb2-12e_coco.py
+++ configs/dino/dino-4scale_r50_8xb2-12e_coco.py
@@ -1,34 +1,61 @@
-_base_ = [
-    '../_base_/datasets/coco_detection.py', '../_base_/default_runtime.py'
-]
+from mmengine.config import read_base
+
+with read_base():
+    from .._base_.default_runtime import *
+    from .._base_.datasets.coco_detection import *
+
+from mmcv.transforms.loading import LoadImageFromFile
+from mmcv.transforms.wrappers import RandomChoice
+from mmcv.transforms.processing import RandomChoiceResize
+from mmengine.optim.optimizer.optimizer_wrapper import OptimWrapper
+from mmengine.optim.scheduler.lr_scheduler import MultiStepLR
+from mmengine.runner.loops import EpochBasedTrainLoop, ValLoop, TestLoop
+from torch.nn import BatchNorm2d,GroupNorm
+from mmdet.models.detectors.dino import DINO
+from mmdet.models.data_preprocessors.data_preprocessor import \
+    DetDataPreprocessor
+from mmdet.models.backbones.resnet import ResNet
+from mmdet.models.necks.channel_mapper import ChannelMapper
+from mmdet.models.dense_heads.dino_head import DINOHead
+from mmdet.models.losses.focal_loss import FocalLoss
+from mmdet.models.losses.smooth_l1_loss import L1Loss
+from mmdet.models.losses.iou_loss import GIoULoss
+from mmdet.models.task_modules.assigners.hungarian_assigner import \
+    HungarianAssigner
+from mmdet.models.task_modules.assigners.match_cost import \
+    FocalLossCost, BBoxL1Cost, IoUCost
+from mmdet.datasets.transforms.loading import LoadAnnotations
+from mmdet.datasets.transforms.transforms import RandomFlip, RandomCrop
+from mmdet.datasets.transforms.formatting import PackDetInputs
+
 model = dict(
-    type='DINO',
+    type=DINO,
     num_queries=900,  # num_matching_queries
     with_box_refine=True,
     as_two_stage=True,
     data_preprocessor=dict(
-        type='DetDataPreprocessor',
+        type=DetDataPreprocessor,
         mean=[123.675, 116.28, 103.53],
         std=[58.395, 57.12, 57.375],
         bgr_to_rgb=True,
         pad_size_divisor=1),
     backbone=dict(
-        type='ResNet',
+        type=ResNet,
         depth=50,
         num_stages=4,
         out_indices=(1, 2, 3),
         frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=False),
+            norm_cfg=dict(type=BatchNorm2d, requires_grad=False),
         norm_eval=True,
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
     neck=dict(
-        type='ChannelMapper',
+        type=ChannelMapper,
         in_channels=[512, 1024, 2048],
         kernel_size=1,
         out_channels=256,
         act_cfg=None,
-        norm_cfg=dict(type='GN', num_groups=32),
+        norm_cfg=dict(type=GroupNorm, num_groups=32),
         num_outs=4),
     encoder=dict(
         num_layers=6,
@@ -58,17 +85,17 @@ model = dict(
         offset=0.0,  # -0.5 for DeformDETR
         temperature=20),  # 10000 for DeformDETR
     bbox_head=dict(
-        type='DINOHead',
+        type=DINOHead,
         num_classes=80,
         sync_cls_avg_factor=True,
         loss_cls=dict(
-            type='FocalLoss',
+            type=FocalLoss,
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
             loss_weight=1.0),  # 2.0 in DeformDETR
-        loss_bbox=dict(type='L1Loss', loss_weight=5.0),
-        loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
+        loss_bbox=dict(type=L1Loss, loss_weight=5.0),
+        loss_iou=dict(type=GIoULoss, loss_weight=2.0)),
     dn_cfg=dict(  # TODO: Move to model.train_cfg ?
         label_noise_scale=0.5,
         box_noise_scale=1.0,  # 0.4 for DN-DETR
@@ -77,26 +104,26 @@ model = dict(
     # training and testing settings
     train_cfg=dict(
         assigner=dict(
-            type='HungarianAssigner',
+            type=HungarianAssigner,
             match_costs=[
-                dict(type='FocalLossCost', weight=2.0),
-                dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
-                dict(type='IoUCost', iou_mode='giou', weight=2.0)
+                dict(type=FocalLossCost, weight=2.0),
+                dict(type=BBoxL1Cost, weight=5.0, box_format='xywh'),
+                dict(type=IoUCost, iou_mode='giou', weight=2.0)
             ])),
     test_cfg=dict(max_per_img=300))  # 100 for DeformDETR
 
 # train_pipeline, NOTE the img_scale and the Pad's size_divisor is different
 # from the default setting in mmdet.
 train_pipeline = [
-    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RandomFlip', prob=0.5),
+    dict(type=LoadImageFromFile, backend_args=backend_args),
+    dict(type=LoadAnnotations, with_bbox=True),
+    dict(type=RandomFlip, prob=0.5),
     dict(
-        type='RandomChoice',
+        type=RandomChoice,
         transforms=[
             [
                 dict(
-                    type='RandomChoiceResize',
+                    type=RandomChoiceResize,
                     scales=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
                             (608, 1333), (640, 1333), (672, 1333), (704, 1333),
                             (736, 1333), (768, 1333), (800, 1333)],
@@ -104,33 +131,33 @@ train_pipeline = [
             ],
             [
                 dict(
-                    type='RandomChoiceResize',
+                    type=RandomChoiceResize,
                     # The radio of all image in train dataset < 7
                     # follow the original implement
                     scales=[(400, 4200), (500, 4200), (600, 4200)],
                     keep_ratio=True),
                 dict(
-                    type='RandomCrop',
+                    type=RandomCrop,
                     crop_type='absolute_range',
                     crop_size=(384, 600),
                     allow_negative_crop=True),
                 dict(
-                    type='RandomChoiceResize',
+                    type=RandomChoiceResize,
                     scales=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
                             (608, 1333), (640, 1333), (672, 1333), (704, 1333),
                             (736, 1333), (768, 1333), (800, 1333)],
                     keep_ratio=True)
             ]
         ]),
-    dict(type='PackDetInputs')
+    dict(type=PackDetInputs)
 ]
-train_dataloader = dict(
+train_dataloader.update(
     dataset=dict(
         filter_cfg=dict(filter_empty_gt=False), pipeline=train_pipeline))
 
 # optimizer
 optim_wrapper = dict(
-    type='OptimWrapper',
+    type=OptimWrapper,
     optimizer=dict(
         type='AdamW',
         lr=0.0001,  # 0.0002 for DeformDETR
@@ -142,14 +169,14 @@ optim_wrapper = dict(
 # learning policy
 max_epochs = 12
 train_cfg = dict(
-    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
+    type=EpochBasedTrainLoop, max_epochs=max_epochs, val_interval=1)
 
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
+val_cfg = dict(type=ValLoop)
+test_cfg = dict(type=TestLoop)
 
 param_scheduler = [
     dict(
-        type='MultiStepLR',
+        type=MultiStepLR,
         begin=0,
         end=max_epochs,
         by_epoch=True,
@@ -160,4 +187,4 @@ param_scheduler = [
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
 # base_batch_size = (8 GPUs) x (2 samples per GPU)
-auto_scale_lr = dict(base_batch_size=16)
+auto_scale_lr = dict(base_batch_size=1)
