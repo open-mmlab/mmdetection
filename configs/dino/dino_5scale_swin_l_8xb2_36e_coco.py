3c5e59a4e3c44e8438f7411a9bb7aef18f9e7f6b
Markson-Young <a2246708680@gmail.com>
2023-09-02 21:38:17 +0800

    update config

--- configs/dino/dino_5scale_swin_l_8xb2_36e_coco.py
+++ configs/dino/dino_5scale_swin_l_8xb2_36e_coco.py
@@ -8,12 +8,5 @@ max_epochs = 36
 train_cfg.update(
     dict(
         type=EpochBasedTrainLoop, max_epochs=max_epochs, val_interval=1))
-param_scheduler = [
-    dict(
-        type=MultiStepLR,
-        begin=0,
-        end=max_epochs,
-        by_epoch=True,
-        milestones=[27, 33],
-        gamma=0.1)
-]
+param_scheduler[0].update(
+    dict(milestones=[27, 33]))
