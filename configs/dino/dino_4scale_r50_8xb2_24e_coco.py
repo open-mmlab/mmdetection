a8f8a2e7368e27378e7d3d8e2b368439cf5753fb
Markson-Young <a2246708680@gmail.com>
2023-09-03 19:00:35 +0800

    uniform format

--- configs/dino/dino_4scale_r50_8xb2_24e_coco.py
+++ configs/dino/dino_4scale_r50_8xb2_24e_coco.py
@@ -8,13 +8,9 @@ from mmengine.runner.loops import EpochBasedTrainLoop
 max_epochs = 24
 train_cfg.update(
     dict(
-        type=EpochBasedTrainLoop, max_epochs=max_epochs, val_interval=1))
-param_scheduler = [
-    dict(
-        type=MultiStepLR,
-        begin=0,
-        end=max_epochs,
-        by_epoch=True,
-        milestones=[20],
-        gamma=0.1)
-]
+        type=EpochBasedTrainLoop,
+        max_epochs=max_epochs,
+        val_interval=1))
+
+param_scheduler[0].update(
+    dict(milestones=[20]))
