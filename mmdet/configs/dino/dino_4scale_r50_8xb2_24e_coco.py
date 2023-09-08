ff7697f381e8b996e4b1ec0f2dd149b15eab977d
Markson-Young <a2246708680@gmail.com>
2023-09-08 20:03:11 +0800

    new configs for dino

---
+++ mmdet/configs/dino/dino_4scale_r50_8xb2_24e_coco.py
@@ -0,0 +1,16 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .dino_4scale_r50_8xb2_12e_coco import *
+
+from mmengine.runner.loops import EpochBasedTrainLoop
+
+max_epochs = 24
+train_cfg.update(
+    dict(
+        type=EpochBasedTrainLoop,
+        max_epochs=max_epochs,
+        val_interval=1))
+
+param_scheduler[0].update(
+    dict(milestones=[20]))
