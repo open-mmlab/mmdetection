26be588d4f1b675971c69525bd0ab9e3d803ebf4
huanghaian <huanghaian@sensetime.com>
2023-09-12 16:04:09 +0800

    add tood

--- projects/RF100-Benchmark/configs/tood_r50_fpn_ms_tweeter-profile.py
+++ projects/RF100-Benchmark/configs/tood_r50_fpn_ms_tweeter-profile.py
@@ -1,4 +1,4 @@
-_base_ = '../../../configs/faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py'
+_base_ = '../../../configs/tood/tood_r50_fpn_1x_coco.py'
 
 data_root = 'rf100/tweeter-profile/'
 class_name = ('profile_info', )
@@ -6,7 +6,7 @@ num_classes = len(class_name)
 metainfo = dict(classes=class_name)
 image_scale = (640, 640)
 
-model = dict(roi_head=dict(bbox_head=dict(num_classes=int(num_classes))))
+model = dict(bbox_head=dict(num_classes=int(num_classes)))
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -80,7 +80,7 @@ param_scheduler = [
         gamma=0.1)
 ]
 
-load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_mstrain_3x_coco/faster_rcnn_r50_fpn_mstrain_3x_coco_20210524_110822-e10bd31c.pth'  # noqa
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/tood/tood_r50_fpn_1x_coco/tood_r50_fpn_1x_coco_20211210_103425-20e20746.pth'  # noqa
 
 default_hooks = dict(checkpoint=dict(save_best='auto', max_keep_ckpts=2))
 
