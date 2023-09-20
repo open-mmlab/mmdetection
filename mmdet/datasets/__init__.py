70d2ea091a1bc8e465db4f9b8cbff4d854155fa0
ryylcc <1346837737@qq.com>
2023-09-20 23:23:19 +0800

    solve confict

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -10,7 +10,7 @@ from .coco_caption import CocoCaptionDataset
 from .coco_panoptic import CocoPanopticDataset
 from .coco_semantic import CocoSegDataset
 from .crowdhuman import CrowdHumanDataset
-from .dataset_wrappers import MultiImageMixDataset
+from .dataset_wrappers import ConcatDataset, MultiImageMixDataset
 from .deepfashion import DeepFashionDataset
 from .dsdl import DSDLDetDataset
 from .isaid import iSAIDDataset
@@ -42,5 +42,5 @@ __all__ = [
     'ReIDDataset', 'YouTubeVISDataset', 'TrackAspectRatioBatchSampler',
     'ADE20KPanopticDataset', 'CocoCaptionDataset', 'RefCocoDataset',
     'BaseSegDataset', 'ADE20KSegDataset', 'CocoSegDataset',
-    'ADE20KInstanceDataset', 'iSAIDDataset', 'V3DetDataset'
+    'ADE20KInstanceDataset', 'iSAIDDataset', 'V3DetDataset', 'ConcatDataset'
 ]
