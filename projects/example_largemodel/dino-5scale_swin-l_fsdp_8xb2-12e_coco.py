2a26a5932eca562fc7fe6e361d02ab3a6a54b56b
huanghaian <huanghaian@sensetime.com>
2023-09-27 19:30:45 +0800

    fix all

--- projects/example_largemodel/dino-5scale_swin-l_fsdp_8xb2-12e_coco.py
+++ projects/example_largemodel/dino-5scale_swin-l_fsdp_8xb2-12e_coco.py
@@ -1,24 +1,18 @@
 from mmengine.config import read_base
-from mmdet.models.backbones.swin import SwinBlock
-from mmdet.models.layers.transformer.deformable_detr_layers import DeformableDetrTransformerEncoderLayer
 
 with read_base():
-    from mmdet.configs.dino.dino_5scale_swin_l_8xb2_12e_coco import *
+    from mmdet.configs.dino.dino_5scale_swin_l_8xb2_12e_coco import *  # noqa
 
-custom_imports = dict(
-    imports=['projects.example_largemodel'], allow_failed_imports=False)
+from projects.example_largemodel import (checkpoint_check_fn,
+                                         layer_auto_wrap_policy)
 
-from projects.example_largemodel import layer_auto_wrap_policy, checkpoint_check_fn
+# The checkpoint needs to be controlled by the checkpoint_check_fn.
+model.update(dict(backbone=dict(with_cp=False)))  # noqa
 
-layer_cls = (SwinBlock, DeformableDetrTransformerEncoderLayer)
-
-model.update(dict(backbone=dict(with_cp=False)))
-
+# TODO: The new version of configs does not support passing a module list,
+#  so for now, it can only be hard-coded. We will fix this issue in the future.
 runner_type = 'FlexibleRunner'
 strategy = dict(
     type='FSDPStrategy',
-    gradient_checkpoint=dict(check_fn=dict(type=checkpoint_check_fn, layer_cls=layer_cls)),
-    model_wrapper=dict(
-        auto_wrap_policy=dict(
-            type=layer_auto_wrap_policy,
-            layer_cls=layer_cls)))
+    gradient_checkpoint=dict(check_fn=checkpoint_check_fn),
+    model_wrapper=dict(auto_wrap_policy=dict(type=layer_auto_wrap_policy)))
--- projects/example_largemodel/fsdp_utils.py
+++ projects/example_largemodel/fsdp_utils.py
@@ -1,31 +1,38 @@
-import torch.nn as nn
-from functools import partial
-from typing import Union, Sequence
-from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
-    checkpoint_wrapper,
-    CheckpointImpl
-)
+from typing import Sequence, Union
 
+import torch.nn as nn
+
+from mmdet.models.backbones.swin import SwinBlock
+from mmdet.models.layers.transformer.deformable_detr_layers import \
+    DeformableDetrTransformerEncoderLayer
+
+
+# TODO: The new version of configs does not support passing a module list,
+#  so for now, it can only be hard-coded. We will fix this issue in the future.
 def layer_auto_wrap_policy(
-        module,
-        recurse: bool,
-        nonwrapped_numel: int,
-        layer_cls: Union[nn.Module, Sequence[nn.Module]],
+    module,
+    recurse: bool,
+    nonwrapped_numel: int,
+    layer_cls: Union[nn.Module, Sequence[nn.Module]] = (
+        SwinBlock, DeformableDetrTransformerEncoderLayer),
 ) -> bool:
     if recurse:
         # always recurse
         return True
     else:
-        # if not recursing, decide whether we should wrap for the leaf node or reminder
+        # if not recursing, decide whether we should wrap for
+        # the leaf node or reminder
         return isinstance(module, tuple(layer_cls))
 
 
-def checkpoint_check_fn(layer_cls: Union[nn.Module, Sequence[nn.Module]]):
-    return lambda submodule: isinstance(submodule, tuple(layer_cls))
+def checkpoint_check_fn(submodule,
+                        layer_cls: Union[nn.Module, Sequence[nn.Module]] = (
+                            SwinBlock, DeformableDetrTransformerEncoderLayer)):
+    return isinstance(submodule, tuple(layer_cls))
+
 
 # non_reentrant_wrapper = partial(
 #     checkpoint_wrapper,
 #     offload_to_cpu=False,
 #     checkpoint_impl=CheckpointImpl.NO_REENTRANT,
 # )
-
