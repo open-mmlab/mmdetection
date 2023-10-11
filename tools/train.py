de118e94830d4806720f0424ac4801541bb297f5
huanghaian <huanghaian@sensetime.com>
2023-10-11 17:00:09 +0800

    fix ci

--- tools/train.py
+++ tools/train.py
@@ -1,11 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
-import logging
 import os
 import os.path as osp
 
 from mmengine.config import Config, DictAction
-from mmengine.logging import print_log
 from mmengine.registry import RUNNERS
 from mmengine.runner import Runner
 
