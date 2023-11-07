a8f79c8dea8beacede835948858243f23e9cf4b9
Yang Nie <flytocc@outlook.com>
2023-11-07 23:13:57 +0800

    update configs

---
+++ configs/rtdetr/rtdetr_r101vd_4xb4-72e_coco.py
@@ -0,0 +1,22 @@
+_base_ = './rtdetr_r50vd_4xb4-72e_coco.py'
+
+model = dict(
+    backbone=dict(depth=101),
+    neck=dict(out_channels=384),
+    encoder=dict(
+        in_channels=[384, 384, 384],
+        out_channels=384,
+        layer_cfg=dict(
+            self_attn_cfg=dict(embed_dims=384),
+            ffn_cfg=dict(embed_dims=384, feedforward_channels=2048))),
+    decoder=dict(
+        layer_cfg=dict(
+            self_attn_cfg=dict(embed_dims=384),
+            cross_attn_cfg=dict(embed_dims=384),
+            ffn_cfg=dict(embed_dims=384))),
+    bbox_head=dict(embed_dims=384))
+
+# set all layers in backbone to lr_mult=0.01
+for k, v in _base_.optim_wrapper.paramwise_cfg.custom_keys.items():
+    if 'backbone' in k:
+        v['lr_mult'] = 0.01
--- configs/rtdetr/rtdetr_r34vd_4xb4-72e_coco.py
+++ configs/rtdetr/rtdetr_r34vd_4xb4-72e_coco.py
@@ -2,10 +2,10 @@ _base_ = './rtdetr_r50vd_4xb4-72e_coco.py'
 
 model = dict(
     backbone=dict(
-        depth=18,
+        depth=34,
         frozen_stages=-1,
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         norm_eval=False),
     neck=dict(in_channels=[128, 256, 512]),
     encoder=dict(expansion=0.5),
-    decoder=dict(num_layers=3))
+    decoder=dict(num_layers=4))
