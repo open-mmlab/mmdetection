1a7cc5c222c82e5142719bcedce1ef2c4c8dbb92
huanghaian <huanghaian@sensetime.com>
2023-11-08 12:57:51 +0800

    add more configs

--- configs/glip/lvis/glip_atss_swin-t_a_fpn_dyhead_pretrain_zeroshot_lvis.py
+++ configs/glip/lvis/glip_atss_swin-t_a_fpn_dyhead_pretrain_zeroshot_lvis.py
@@ -1,4 +1,4 @@
-_base_ = 'glip_atss_swin-t_a_fpn_dyhead_pretrain_obj365.py'
+_base_ = '../glip_atss_swin-t_a_fpn_dyhead_pretrain_obj365.py'
 
 model = dict(test_cfg=dict(
     max_per_img=300,
@@ -12,13 +12,13 @@ val_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         type=dataset_type,
-        ann_file='annotations/lvis_v1_minival_inserted_image_name.json',
+        ann_file='annotations/lvis_od_val.json',
         data_prefix=dict(img='')))
 test_dataloader = val_dataloader
 
+# numpy < 1.24.0
 val_evaluator = dict(
     _delete_=True,
     type='LVISFixedAPMetric',
-    ann_file=data_root +
-    'annotations/lvis_v1_minival_inserted_image_name.json')
+    ann_file=data_root + 'annotations/lvis_od_val.json')
 test_evaluator = val_evaluator
