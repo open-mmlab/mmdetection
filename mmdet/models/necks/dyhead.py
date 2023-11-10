4d5b2a2501c5c278d7da60b38c6194bc3894d179
Bonbon-Tang <928081866@qq.com>
2023-11-10 11:02:32 +0800

    change dyhead

--- mmdet/models/necks/dyhead.py
+++ mmdet/models/necks/dyhead.py
@@ -113,13 +113,12 @@ class DyHeadBlock(nn.Module):
             if level < len(x) - 1:
                 # this upsample order is weird, but faster than natural order
                 # https://github.com/microsoft/DynamicHead/issues/25
-                output_h = x[level + 1].shape[-2]
-                output_w = x[level + 1].shape[-1]
-                offset = offset_and_mask[:, :self.offset_dim, \
-                    :output_h, :output_w]
-                mask = offset_and_mask[:, self.offset_dim:, \
-                    :output_h, :output_w].sigmoid()
-
+                out_h = x[level + 1].shape[-2]
+                out_w = x[level + 1].shape[-1]
+                offset = offset_and_mask[:, :self.offset_dim, :out_h, :out_w]
+                mask = offset_and_mask[:, self.offset_dim:, :out_h, :out_w]
+                mask = mask.sigmoid()
+                
                 high_feat = F.interpolate(
                     self.spatial_conv_high(x[level + 1], offset, mask),
                     size=x[level].shape[-2:],
