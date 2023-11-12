83bbbedebf492074679e2cf6d081eefc20cf2972
okotaku <to78314910@gmail.com>
2023-11-12 10:21:44 +0000

    add configs

---
+++ configs/oc_cost/faster-rcnn_r50_fpn_1x_coco_oc_cost.py
@@ -0,0 +1,12 @@
+_base_ = '../faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py'
+
+data_root = 'data/coco/'
+
+val_evaluator = dict(
+    _delete_=True,
+    type='CocoOCCostMetric',
+    alpha=0.5,
+    beta=0.6,
+    ann_file=data_root + 'annotations/instances_val2017.json',
+    metric='bbox')
+test_evaluator = val_evaluator
