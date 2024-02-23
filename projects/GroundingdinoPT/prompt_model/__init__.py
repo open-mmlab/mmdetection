c1e7bf74a1a7b4665257d388fdead2431930a105
jinweizhong <jinweizhong66@yahoo.com>
2024-02-23 16:22:22 +0800

    new feature: prompt tuning for grounding dino

---
+++ projects/GroundingdinoPT/prompt_model/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .gdptuning import GroundingDINOPTuning
+from .auto_coco import Auto_CocoDataset
+__all__ = [
+    'GroundingDINOPTuning','Auto_CocoDataset'
+]
